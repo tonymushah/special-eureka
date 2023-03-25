9d7ba03a2f46bb32557fd32ffb178cd7542bfc8e
tonymushah <95529016+tonymushah@users.noreply.github.com>
2023-03-24 20:56:57 -0400

    EsLint Added

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 //import react from '@vitejs/plugin-react'
-import react from "@vitejs/plugin-react-swc"
+import react from "@vitejs/plugin-react-swc";
 import remarkRehypePlugin from "vite-plugin-remark-rehype";
-import progress from "vite-plugin-progress";
-import { resolve } from 'path';
-import { ViteAliases } from 'vite-aliases';
-import VitePluginLegacy from "@vitejs/plugin-legacy";
+//import progress from "vite-plugin-progress";
+import { resolve } from "path";
+import { ViteAliases } from "vite-aliases";
+//import VitePluginLegacy from "@vitejs/plugin-legacy";
 //import ReactInspector from 'vite-plugin-react-inspector'
-
+import eslintPlugin from "@nabla/vite-plugin-eslint";
+//import eslintRollup from "@rollup/plugin-eslint";
 /*function getPathSrc(){
   const path = `${process.cwd()}/src`;
   console.log(path);
@@ -30,9 +31,10 @@ export default defineConfig({
     }),
     remarkRehypePlugin({
     }),
+    eslintPlugin({})
     //splitVendorChunkPlugin()
   ],
-  envPrefix: ['VITE_', 'TAURI_'],
+  envPrefix: ["VITE_", "TAURI_"],
   server: {
     port: 9305,
     strictPort: true,
@@ -40,9 +42,9 @@ export default defineConfig({
   },
   build: {
     // Tauri supports es2021
-    target: ['es2021', 'chrome100', 'safari13'],
+    target: ["es2021", "chrome100", "safari13"],
     // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
     outDir : "../dist",
@@ -55,4 +57,4 @@ export default defineConfig({
   },
   root: "./src",
   publicDir : "./public"
-})
+});
