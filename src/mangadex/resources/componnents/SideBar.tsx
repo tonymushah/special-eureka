e78a79ee3192ad47e24d92f99df74de2ba84ec9b
tonymushah <95529016+tonymushah@users.noreply.github.com>
2023-04-11 22:32:38 +0300

    rm : mangadex navbar

--- src/mangadex/resources/componnents/SideBar.tsx
+++ src/mangadex/resources/componnents/SideBar.tsx
@@ -1,17 +1,12 @@
 import * as Chakra from "@chakra-ui/react";
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { useProSidebar } from "react-pro-sidebar";
 import MyErrorBounderies from "./error/MyErrorBounderies";
 
 import mangadex_logo from "@mangadex/resources/ico/ddb5721c5458b5edc9d6782a5f107119.svg";
 
 const Side_bar = React.lazy(() => import("./sidebar/SideBar"));
 
-const Form1 = React.lazy(() => import("./sidebar/Form1"));
-
 export default function Content(props: React.PropsWithChildren) {
-    const { collapseSidebar, toggleSidebar, collapsed, toggled, broken, rtl } = useProSidebar();
     return (
 
         <div className='w-100 d-inline-flex'>
@@ -37,33 +32,6 @@ export default function Content(props: React.PropsWithChildren) {
                 overflowY={"scroll"}
             >
                 <Chakra.Box id="top-content">
-                    <Chakra.Box className="content-header">
-                        <Navbar expand={"sm"} >
-                            <Container>
-                                <Navbar.Brand>
-                                    <Chakra.Center onClick={() => toggleSidebar()} display={{
-                                        base: "flex",
-                                        md: "none"
-                                    }}>
-                                        <img src={mangadex_logo} /> &nbsp; <h4 className='d-inline'>MangaDex </h4>
-                                    </Chakra.Center>
-                                </Navbar.Brand>
-                                <Navbar.Toggle className=" float-end" aria-controls='searc_bar_' />
-                                <Navbar.Collapse id="searc_bar_" className=" justify-content-end">
-                                    <Nav>
-                                        <React.Suspense
-                                            fallback={<Chakra.Skeleton
-                                                height={"10px"}
-                                            />}
-                                        >
-                                            <Form1 />
-                                        </React.Suspense>
-                                    </Nav>
-                                </Navbar.Collapse>
-                            </Container>
-                        </Navbar>
-                        <hr className="header-hr" />
-                    </Chakra.Box>
                     <Chakra.Box id="content">
                         <MyErrorBounderies>
                             {props.children}
