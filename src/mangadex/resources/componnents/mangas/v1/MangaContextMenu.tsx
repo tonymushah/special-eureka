d843dbf5f862678eeb3203a8e9c7446337372302
tonymushah <tonyramanantsalama@gmail.com>
2023-06-01 20:31:02 +0300

    adding context menu

--- src/mangadex/resources/componnents/mangas/v1/MangaContextMenu.tsx
+++ src/mangadex/resources/componnents/mangas/v1/MangaContextMenu.tsx
@@ -2,19 +2,118 @@ import * as ChakraIcons from "@chakra-ui/icons";
 import * as Chakra from "@chakra-ui/react";
 import { useMangaDownload_Delete } from "../../../hooks/MangaStateHooks";
 import React from "react";
-import ReactContextMenu from "react-jsx-context-menu";
+import * as ContextMenu from "@radix-ui/react-context-menu";
 
 export default function MangaContextMenu(props: {
-    mangaId : string,
+    mangaId: string,
     refetch?: () => void,
     children: React.ReactElement
-}){
-    const {delete_, download_} = useMangaDownload_Delete({
-        mangaID : props.mangaId
+}) {
+    const { delete_, download_ } = useMangaDownload_Delete({
+        mangaID: props.mangaId
     });
+    function Refresh() {
+        const { refetch } = props;
+        if (refetch != undefined) {
+            return (
+                <Chakra.HStack spacing={"2"}
+                    onClick={() => {
+                        refetch();
+                    }}
+                >
+                    Refresh
+                </Chakra.HStack>
+            );
+        } else {
+            return (
+                <React.Fragment />
+            );
+        }
+    }
+    function Download() {
+        return (
+            <Chakra.HStack
+                spacing={"2"}
+                textColor={download_.isLoading && download_.fetchStatus == "fetching" ? "gray" : "green"}
+                onClick={() => {
+                    if (!(download_.isLoading) && download_.fetchStatus != "fetching") {
+                        download_.refetch();
+                    }
+                }}
+                as={ContextMenu.ContextMenuItem}
+            >
+                <ChakraIcons.DownloadIcon />
+                <Chakra.Text as="span">
+                    Download
+                </Chakra.Text>
+            </Chakra.HStack>
+        );
+    }
+    function Update(){
+        return (
+            <Chakra.HStack
+                spacing={"2"}
+                textColor={download_.isLoading && download_.fetchStatus == "fetching" ? "gray" : "blue"}
+                onClick={() => {
+                    if (!(download_.isLoading) && download_.fetchStatus != "fetching") {
+                        download_.refetch();
+                    }
+                }}
+                as={ContextMenu.ContextMenuItem}
+            >
+                <ChakraIcons.ReactIcon />
+                <Chakra.Text as="span">
+                    Update
+                </Chakra.Text>
+            </Chakra.HStack>
+        );
+    }
+    function Delete(){
+        return (
+            <Chakra.HStack
+                spacing={"2"}
+                textColor={delete_.isLoading && delete_.fetchStatus == "fetching" ? "gray" : "red"}
+                onClick={() => {
+                    if (!(download_.isLoading) && download_.fetchStatus != "fetching") {
+                        delete_.refetch();
+                    }
+                }}
+                as={ContextMenu.ContextMenuItem}
+            >
+                <ChakraIcons.DeleteIcon />
+                <Chakra.Text as="span">
+                    Delete
+                </Chakra.Text>
+            </Chakra.HStack>
+        );
+    }
     return (
-        <ReactContextMenu
-            menu={
+        <ContextMenu.Root>
+            <ContextMenu.Trigger>{
+                props.children
+            }</ContextMenu.Trigger>
+            <ContextMenu.Portal>
+                <Chakra.Box
+                    backgroundColor={"white"}
+                    borderColor={"#cccccc"}
+                    borderRadius={"15px"}
+                    boxShadow={"md"}
+                    as={ContextMenu.Content}
+                >
+                    <Chakra.VStack>
+                        <Refresh />
+                        <Download/>
+                        <Update/>
+                        <Delete/>
+                    </Chakra.VStack>
+                </Chakra.Box>
+            </ContextMenu.Portal>
+        </ContextMenu.Root>
+    );
+}
+
+/*
+menu={
                 <Chakra.Menu
                     isOpen
                 >
@@ -24,8 +123,8 @@ export default function MangaContextMenu(props: {
                         >Refresh</Chakra.MenuItem>
                         <Chakra.MenuItem
                             onClick={() => download_.refetch()}
-                            textColor={"green"}
-                            icon={<ChakraIcons.DownloadIcon />}
+                            
+                            icon={}
                         >
                             Download
                         </Chakra.MenuItem>
@@ -46,10 +145,4 @@ export default function MangaContextMenu(props: {
                     </Chakra.MenuList>
                 </Chakra.Menu>
             }
-        >
-            {
-                props.children
-            }
-        </ReactContextMenu>
-    );
-}
+*/
