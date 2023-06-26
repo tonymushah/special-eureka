d13cb802b1386ee03f7659725ad969536b9bc43c
tonymushah <tonyramanantsalama@gmail.com>
2023-06-26 17:53:27 +0300

    updating server starting mode

--- src/mangadex/plugin/tauri-plugin/src/commands/server.rs
+++ src/mangadex/plugin/tauri-plugin/src/commands/server.rs
@@ -2,6 +2,7 @@ use crate::Error;
 use crate::Result;
 use actix_web::dev::{Server, ServerHandle};
 use mangadex_desktop_api2;
+use tauri::async_runtime::JoinHandle;
 use tauri::{AppHandle, Runtime, State, Window};
 
 use tokio::sync::Mutex;
@@ -10,12 +11,14 @@ use std::sync::Arc;
 
 pub struct MangadexDesktopApiHandle {
     server: Arc<Mutex<Option<ServerHandle>>>,
+    server_handle: Arc<Mutex<Option<JoinHandle<std::result::Result<(), std::io::Error>>>>>,
 }
 
 impl Default for MangadexDesktopApiHandle {
     fn default() -> Self {
         MangadexDesktopApiHandle {
             server: Arc::new(Mutex::new(None)),
+            server_handle: Arc::new(Mutex::new(None)),
         }
     }
 }
@@ -34,7 +37,15 @@ pub async fn launch_server<R: Runtime>(
     mangadex_desktop_api2::verify_all_fs()?;
     let server: Server = mangadex_desktop_api2::launch_async_server_default()?;
     let handle: ServerHandle = server.handle();
-    tauri::async_runtime::spawn(server);
+
+    state
+        .server_handle
+        .clone()
+        .lock_owned()
+        .await
+        .replace(tauri::async_runtime::spawn(async move {
+            return server.await;
+        }));
     let mut inner_sirv = state.server.clone().lock_owned().await;
     if inner_sirv.is_none() {
         match inner_sirv.replace(handle) {
@@ -55,6 +66,12 @@ pub async fn stop_server(state: tauri::State<'_, MangadexDesktopApiHandle>) -> R
     let mut server_handle = state.server.clone().lock_owned().await;
     if let Some(handle) = server_handle.take() {
         handle.stop(true).await;
+        if let Some(s_handle) = state.server_handle.clone().lock_owned().await.take() {
+            match s_handle.await {
+                Ok(_) => (),
+                Err(e) => println!("{}", e.to_string()),
+            };
+        }
         return Ok("stopped server".to_string());
     } else {
         return Err(Error::Io(std::io::Error::new(
