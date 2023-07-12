860189f41847715912279bb1940f282851033448
tonymushah <tonyramanantsalama@gmail.com>
2023-07-12 23:06:06 +0300

    Removed @mangadex/Poppins

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
+import "@fontsource/poppins/100.css";
+import "@fontsource/poppins/200.css";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/800.css";
+import "@fontsource/poppins/900.css";
+import "@fontsource-variable/josefin-sans";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, Box, Spinner, AbsoluteCenter } from "@chakra-ui/react";
-
-import "@mangadex/resources/Poppins/Poppins.css";
-
-document.querySelectorAll("body")[0].style.fontFamily = "Poppins";
 
 // TODO Add custom window decoration 
 // import BoxDecoration from "@commons-res/components/BoxDecoration";
@@ -17,23 +24,23 @@ const app = ReactDOM.createRoot(document.getElementById("app")!);
 app.render(
     <React.StrictMode>
         <ChakraProvider>
-                <Box>
-                    <React.Suspense
-                        fallback={
-                            <Box width={"100%"} height={"100vh"}>
-                                <AbsoluteCenter>
-                                    <Spinner
-                                        size="xl"
-                                        color='orange.500'
-                                        thickness='4px'
-                                    />
-                                </AbsoluteCenter>
-                            </Box>
-                        }
-                    >
-                        <App />
-                    </React.Suspense>
-                </Box>
+            <Box>
+                <React.Suspense
+                    fallback={
+                        <Box width={"100%"} height={"100vh"}>
+                            <AbsoluteCenter>
+                                <Spinner
+                                    size="xl"
+                                    color='orange.500'
+                                    thickness='4px'
+                                />
+                            </AbsoluteCenter>
+                        </Box>
+                    }
+                >
+                    <App />
+                </React.Suspense>
+            </Box>
         </ChakraProvider>
     </React.StrictMode>
 );
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "'Poppins', 'Josefin Sans Variable', sans-serif",
+    body: "'Poppins', 'Josefin Sans Variable' , sans-serif",
+  },
+});
+
+export default theme;
