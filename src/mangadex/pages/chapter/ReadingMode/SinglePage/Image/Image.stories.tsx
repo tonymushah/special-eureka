67a72651fa62ba1ff18ba90e7e3eecc384994eb6
tonymushah <tonyramanantsalama@gmail.com>
2023-08-09 22:27:44 +0300

    Starting SinglePage reading lol haha

--- src/mangadex/pages/chapter/ReadingMode/SinglePage/Image/Image.stories.tsx
+++ src/mangadex/pages/chapter/ReadingMode/SinglePage/Image/Image.stories.tsx
@@ -1,6 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
 import ChapterImage from "./index";
-import { useHotkeys } from "react-hotkeys-hook";
 import MangadexNotFound from "@mangadex/resources/imgs/cover-not-found.jpg";
 import { ChakraProvider } from "@chakra-ui/react";
 
@@ -14,14 +13,16 @@ export default meta;
 
 export const Default: Story = {
     render: () => {
-        const ref = useHotkeys<HTMLDivElement>("*", () => {
-            console.log("key pressed");
-        });
         return (
             <ChakraProvider>
                 <ChapterImage
                     src={MangadexNotFound}
-                    previous_next_ref={ref}
+                    onNext={() => {
+                        console.log("next");
+                    }}
+                    onPrevious={() => {
+                        console.log("previous");
+                    }}
                 />
             </ChakraProvider>
         );
--- src/mangadex/pages/chapter/ReadingMode/SinglePage/Image/index.tsx
+++ src/mangadex/pages/chapter/ReadingMode/SinglePage/Image/index.tsx
@@ -1,58 +1,104 @@
-import { Box, Center, Image } from "@chakra-ui/react";
+import { Box, Center, Icon, Image } from "@chakra-ui/react";
 import MangadexSpinner from "@mangadex/resources/componnents/kuru_kuru/MangadexSpinner";
 import React from "react";
-import { RefType } from "react-hotkeys-hook/dist/types";
-import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
+import { HotkeyCallback, useHotkeys } from "react-hotkeys-hook";
+import { ReactZoomPanPinchRef, TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
+import NextPreviousHotKeys from "./NextPreviousHotKeys";
+import { useChakraToast } from "@commons-res/hooks/useChakraToast";
+import { TbZoomCancel, TbZoomCheck } from "react-icons/tb";
 
-export default function ChapterImage({ previous_next_ref, src }: {
+export default function ChapterImage({ src, onNext, onPrevious }: {
     src: string,
-    previous_next_ref: React.MutableRefObject<RefType<HTMLDivElement>>
+    onNext?: HotkeyCallback,
+    onPrevious?: HotkeyCallback
 }) {
+    const [isDisabled, setIsDisabled] = React.useState(true);
+    const toast = useChakraToast({
+        id : "zoom-toast",
+        position : "top",
+        duration : 5000,
+        isClosable : true
+    });
+    const transformWarperRef = React.createRef<ReactZoomPanPinchRef>();
+    useHotkeys("x", () => {
+        transformWarperRef.current?.resetTransform();
+    });
+    useHotkeys("ctrl", () => {
+        setIsDisabled(!isDisabled);
+    }, [isDisabled]);
+    React.useEffect(() => {
+        if(isDisabled){
+            toast({
+                icon : (<Icon as={TbZoomCancel}/>),
+                title : "The zoom is now disabled",
+                status : "warning",
+            });
+        }else{
+            toast({
+                icon : (<Icon as={TbZoomCheck}/>),
+                title : "The zoom is now enabled",
+                status : "success"
+            });
+        }
+    }, [isDisabled]);
     return (
-        <Box
-            key={src}
-            backgroundImage={src}
-            ref={previous_next_ref}
-        >
-            <Box backdropFilter={"auto"} backdropBlur={"25px"}>
-                <TransformWrapper
-                    initialScale={1}
-                    centerOnInit
-                    disablePadding={false}
-                >
-                    <TransformComponent
-                        contentStyle={{
-                            display: "block",
-                            width: "max-content",
-                            height: "max-content",
-                        }}
-                        wrapperStyle={{
-                            width: "max-content",
-                            height: "fit-content",
-                            maxHeight : "100vh"
+        <React.Fragment>
+            {
+                isDisabled ? (
+                    <NextPreviousHotKeys onNext={onNext} onPrevious={onPrevious} />
+                ) : (
+                    <React.Fragment/>
+                )
+            }
+            <Box
+                key={src}
+                backgroundImage={src}
+            >
+                <Box backdropFilter={"auto"} backdropBlur={"50px"}>
+                    <TransformWrapper
+                        initialScale={1}
+                        centerOnInit
+                        disablePadding={false}
+                        ref={transformWarperRef}
+                        disabled={isDisabled}
+                        panning={{
+                            disabled : isDisabled
                         }}
                     >
-                        <Center>
-                            <Image
-                                fallback={
-                                    <Box width={"full"}>
-                                        <Center>
-                                            <MangadexSpinner
-                                                size={"xl"}
-                                                color={"orange"}
-                                                thickness={"10px"}
-                                            />
-                                        </Center>
-                                    </Box>
-                                }
-                                src={src}
-                                alt={src}
-                                height={"100vh"}
-                            />
-                        </Center>
-                    </TransformComponent>
-                </TransformWrapper>
+                        <TransformComponent
+                            contentStyle={{
+                                display: "block",
+                                width: "100%",
+                                height: "max-content",
+                            }}
+                            wrapperStyle={{
+                                width: "100%",
+                                height: "fit-content",
+                                maxHeight: "100vh"
+                            }}
+                        >
+                            <Center>
+                                <Image
+                                    fallback={
+                                        <Box width={"full"}>
+                                            <Center>
+                                                <MangadexSpinner
+                                                    size={"xl"}
+                                                    color={"orange"}
+                                                    thickness={"10px"}
+                                                />
+                                            </Center>
+                                        </Box>
+                                    }
+                                    src={src}
+                                    alt={src}
+                                    height={"100vh"}
+                                />
+                            </Center>
+                        </TransformComponent>
+                    </TransformWrapper>
+                </Box>
             </Box>
-        </Box>
+        </React.Fragment>
     );
 }
