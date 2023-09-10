3c8cd51c3c0b706bbe3c81f97e81d3a16cff9e8b
tonymushah <tonyramanantsalama@gmail.com>
2023-09-10 05:07:45 +0300

    added todo

--- src/mangadex/plugin/server/index.ts
+++ src/mangadex/plugin/server/index.ts
@@ -5,3 +5,6 @@ export { default as stop_server } from "./stop_server";
 export { default as reset_queue } from "./reset_queue";
 
 export { default as is_server_started } from "./is_server_started";
+
+// [ ] Add get_running_tasks
+// [ ] Add get_tasks_limit
