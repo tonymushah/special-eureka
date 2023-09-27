626928585e31dffe461ba05ebce6d1a0a35044f7
tonymushah <tonyramanantsalama@gmail.com>
2023-09-27 15:36:16 +0300

    fixed type ubuntu build error

--- src/mangadex/pages/manga/Chapters_.tsx
+++ src/mangadex/pages/manga/Chapters_.tsx
@@ -1,4 +1,4 @@
-import { Top_Chaps } from "@mangadex/resources/componnents/mangas/Mainpage/Top_chap";
+import { Top_Chaps } from "@mangadex/resources/componnents/mangas/Mainpage/top_chap";
 
 export default function Chapters_() {
     return (
