a07565b815afed829a0bc104ec911d38a8869a49
tonymushah <tonyramanantsalama@gmail.com>
2023-10-08 09:41:29 +0300

    Fixed again

--- src/mangadex/resources/componnents/mangas/Mainpage/chapter_part/Aggregate_part.tsx
+++ src/mangadex/resources/componnents/mangas/Mainpage/chapter_part/Aggregate_part.tsx
@@ -31,16 +31,24 @@ function AggregateOptions() {
     );
 }
 
-export default function Aggregate_part(props: {
+function ReallAggregate({ src }: {
     src: Aggregate
 }) {
     const [order,] = useAtom(orderAtom);
     return (
         <Chakra.Box>
-            <AggregateOptions/>
-            <Chakra.Box>
-                <Aggregate_box selected={0} src={props.src} separator={3} isReverse={order} />
-            </Chakra.Box>
+            <Aggregate_box selected={0} src={src} separator={3} isReverse={order} />
+        </Chakra.Box>
+    );
+}
+
+export default function Aggregate_part({ src }: {
+    src: Aggregate
+}) {
+    return (
+        <Chakra.Box>
+            <AggregateOptions />
+            <ReallAggregate src={src} />
         </Chakra.Box>
     );
 }
