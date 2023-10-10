2a37c0fa3a10ba21b8dde192a64dc602a398bccc
tonymushah <tonyramanantsalama@gmail.com>
2023-10-10 14:50:52 +0300

    Moved AnimatePresence

--- src/mangadex/resources/componnents/router/layout/index.tsx
+++ src/mangadex/resources/componnents/router/layout/index.tsx
@@ -1,6 +1,7 @@
 import { Mangadex_suspense, trackEvent } from "@mangadex/index";
 import MyErrorBounderies from "@mangadex/resources/componnents/error/MyErrorBounderies";
 import { queryClient } from "@mangadex/resources/query.client";
+import { AnimatePresence } from "framer-motion";
 import React from "react";
 
 const BasicWebsitesRessources = React.lazy(() => import("@commons-res/components/BasicWebsitesRessources"));
@@ -14,16 +15,18 @@ const AnimationLayout = React.lazy(() => import("./Animation"));
 export default function MangadexLayout() {
     trackEvent("mangadex-page-entrance");
     return (
-        <MyErrorBounderies>
-            <Mangadex_suspense>
-                <BasicWebsitesRessources queryClient={queryClient}>
-                    <Loader />
-                    <Providers>
-                        <AnimationLayout/>
-                    </Providers>
-                </BasicWebsitesRessources>
-            </Mangadex_suspense>
-        </MyErrorBounderies >
+        <AnimatePresence>
+            <MyErrorBounderies>
+                <Mangadex_suspense>
+                    <BasicWebsitesRessources queryClient={queryClient}>
+                        <Loader />
+                        <Providers>
+                            <AnimationLayout />
+                        </Providers>
+                    </BasicWebsitesRessources>
+                </Mangadex_suspense>
+            </MyErrorBounderies >
+        </AnimatePresence>
     );
 }
 
