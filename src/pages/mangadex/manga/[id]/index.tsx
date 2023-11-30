8707e5b75ba1ac76f471af083a737cdc4623dde9
tonymushah <tonyramanantsalama@gmail.com>
2023-11-30 13:39:18 +0300

    moved `top_chap` to `top_chap_`

--- src/pages/mangadex/manga/[id]/index.tsx
+++ src/pages/mangadex/manga/[id]/index.tsx
@@ -1,3 +1,3 @@
-export { Top_Chaps as default } from "@mangadex/resources/componnents/mangas/Mainpage/top_chap";
+export { Top_Chaps as default } from "@mangadex/resources/componnents/mangas/Mainpage/Top_chap";
 
 export { default as Catch } from "@mangadex/resources/componnents/router/error/Boundary";
