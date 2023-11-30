9bb6805aafe52eda278346605a769d3bcff681b9
tonymushah <tonyramanantsalama@gmail.com>
2023-11-30 12:58:42 +0300

    Refactored `/mangadex/group`

---
+++ src/pages/mangadex/group/[id]/_layout.tsx
@@ -0,0 +1,3 @@
+export { default, loader as Loader } from "@mangadex/pages/groups/page/index";
+
+export { default as Catch } from "@mangadex/resources/componnents/router/error/Boundary";
---
+++ src/pages/mangadex/group/[id]/feeds.tsx
@@ -0,0 +1,3 @@
+export { default } from "@mangadex/pages/groups/page/Feeds";
+
+export { default as Catch } from "@mangadex/resources/componnents/router/error/Boundary";
