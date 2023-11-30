34bea3d1ead3d69769df8556aee87e4b567d0b32
tonymushah <tonyramanantsalama@gmail.com>
2023-11-30 13:25:51 +0300

    Updated Router.ts

--- src/router.ts
+++ src/router.ts
@@ -7,12 +7,15 @@ export type Path =
   | `/`
   | `/dashboard`
   | `/mangadex`
+  | `/mangadex/author/:id`
+  | `/mangadex/author/search`
   | `/mangadex/chapter/:id`
   | `/mangadex/download`
   | `/mangadex/group/:id`
   | `/mangadex/group/:id/feeds`
   | `/mangadex/group/:id/titles`
   | `/mangadex/group/search`
+  | `/mangadex/kuru`
   | `/mangadex/manga/:id`
   | `/mangadex/manga/:id/covers`
   | `/mangadex/manga/:id/related`
@@ -21,8 +24,11 @@ export type Path =
   | `/mangadex/titles/recently-added`
   | `/mangadex/titles/recently-popular`
   | `/mangadex/titles/search`
+  | `/mangadex/user/:id`
+  | `/mangadex/user/:id/feed`
 
 export type Params = {
+  '/mangadex/author/:id': { id: string }
   '/mangadex/chapter/:id': { id: string }
   '/mangadex/group/:id': { id: string }
   '/mangadex/group/:id/feeds': { id: string }
@@ -30,6 +36,8 @@ export type Params = {
   '/mangadex/manga/:id': { id: string }
   '/mangadex/manga/:id/covers': { id: string }
   '/mangadex/manga/:id/related': { id: string }
+  '/mangadex/user/:id': { id: string }
+  '/mangadex/user/:id/feed': { id: string }
 }
 
 export type ModalPath = never
