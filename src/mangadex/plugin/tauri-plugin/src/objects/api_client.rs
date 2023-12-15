3c7536c6eb73334d33070ee747a9d12db6ffde1a
tonymushah <tonyramanantsalama@gmail.com>
2023-12-15 09:29:02 +0300

    Added `api_client` object

--- src/mangadex/plugin/tauri-plugin/src/objects/api_client.rs
+++ src/mangadex/plugin/tauri-plugin/src/objects/api_client.rs
@@ -1,1 +1,44 @@
+use async_graphql::Object;
+use mangadex_api_schema_rust::{
+    v5::{ApiClientAttributes as Attributes, ApiClientObject as ApiClientData},
+    ApiObjectNoRelationships,
+};
+use uuid::Uuid;
+
+use self::attributes::ApiClientAttributes;
+
 pub mod attributes;
+
+#[derive(Clone)]
+pub enum ApiClient {
+    WithRelationship(ApiClientData),
+    WithoutRelationship(ApiObjectNoRelationships<Attributes>),
+}
+
+impl From<ApiClientData> for ApiClient {
+    fn from(value: ApiClientData) -> Self {
+        Self::WithRelationship(value)
+    }
+}
+
+impl From<ApiObjectNoRelationships<Attributes>> for ApiClient {
+    fn from(value: ApiObjectNoRelationships<Attributes>) -> Self {
+        Self::WithoutRelationship(value)
+    }
+}
+
+#[Object]
+impl ApiClient {
+    pub async fn id(&self) -> Uuid {
+        match self {
+            ApiClient::WithRelationship(i) => i.id,
+            ApiClient::WithoutRelationship(i) => i.id,
+        }
+    }
+    pub async fn attributes(&self) -> ApiClientAttributes {
+        match self {
+            ApiClient::WithRelationship(i) => i.attributes.clone().into(),
+            ApiClient::WithoutRelationship(i) => i.attributes.clone().into(),
+        }
+    }
+}
