0047726adee449631b60bcd79d6b6edb3376e0f5
tonymushah <tonyramanantsalama@gmail.com>
2023-12-15 09:32:31 +0300

    Added `Chapter`

--- src/mangadex/plugin/tauri-plugin/src/objects/chapter.rs
+++ src/mangadex/plugin/tauri-plugin/src/objects/chapter.rs
@@ -1,1 +1,44 @@
+use async_graphql::Object;
+use mangadex_api_schema_rust::{
+    v5::{ChapterAttributes as Attributes, ChapterObject},
+    ApiObjectNoRelationships,
+};
+use uuid::Uuid;
+
+use self::attributes::ChapterAttributes;
+
 pub mod attributes;
+
+#[derive(Clone)]
+pub enum Chapter {
+    WithRelationship(ChapterObject),
+    WithoutRelationship(ApiObjectNoRelationships<Attributes>),
+}
+
+impl From<ChapterObject> for Chapter {
+    fn from(value: ChapterObject) -> Self {
+        Self::WithRelationship(value)
+    }
+}
+
+impl From<ApiObjectNoRelationships<Attributes>> for Chapter {
+    fn from(value: ApiObjectNoRelationships<Attributes>) -> Self {
+        Self::WithoutRelationship(value)
+    }
+}
+
+#[Object]
+impl Chapter {
+    pub async fn id(&self) -> Uuid {
+        match self {
+            Chapter::WithRelationship(i) => i.id,
+            Chapter::WithoutRelationship(i) => i.id,
+        }
+    }
+    pub async fn attributes(&self) -> ChapterAttributes {
+        match self {
+            Chapter::WithRelationship(i) => i.attributes.clone().into(),
+            Chapter::WithoutRelationship(i) => i.attributes.clone().into(),
+        }
+    }
+}
