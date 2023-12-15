a51a76d8aa2172e9f94321cb288c920a7ec9c13d
tonymushah <tonyramanantsalama@gmail.com>
2023-12-15 10:14:20 +0300

    Added `User`

--- src/mangadex/plugin/tauri-plugin/src/objects/user.rs
+++ src/mangadex/plugin/tauri-plugin/src/objects/user.rs
@@ -1,1 +1,44 @@
+use async_graphql::Object;
+use mangadex_api_schema_rust::{
+    v5::{UserAttributes as Attributes, UserObject},
+    ApiObjectNoRelationships,
+};
+use uuid::Uuid;
+
+use self::attributes::UserAttributes;
+
 pub mod attributes;
+
+#[derive(Clone)]
+pub enum User {
+    WithRelationship(UserObject),
+    WithoutRelationship(ApiObjectNoRelationships<Attributes>),
+}
+
+impl From<UserObject> for User {
+    fn from(value: UserObject) -> Self {
+        Self::WithRelationship(value)
+    }
+}
+
+impl From<ApiObjectNoRelationships<Attributes>> for User {
+    fn from(value: ApiObjectNoRelationships<Attributes>) -> Self {
+        Self::WithoutRelationship(value)
+    }
+}
+
+#[Object]
+impl User {
+    pub async fn id(&self) -> Uuid {
+        match self {
+            User::WithRelationship(i) => i.id,
+            User::WithoutRelationship(i) => i.id,
+        }
+    }
+    pub async fn attributes(&self) -> UserAttributes {
+        match self {
+            User::WithRelationship(i) => i.attributes.clone().into(),
+            User::WithoutRelationship(i) => i.attributes.clone().into(),
+        }
+    }
+}
