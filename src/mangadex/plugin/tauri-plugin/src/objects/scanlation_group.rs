f98370cfad72b9aa18618d78f17f67e373b0bf13
tonymushah <tonyramanantsalama@gmail.com>
2023-12-16 12:40:23 +0300

    Added `ScanlationGroupRelationships`

--- src/mangadex/plugin/tauri-plugin/src/objects/scanlation_group.rs
+++ src/mangadex/plugin/tauri-plugin/src/objects/scanlation_group.rs
@@ -1,13 +1,17 @@
-use async_graphql::Object;
+use async_graphql::{Context, Object, Result as GraphQLResult};
 use mangadex_api_schema_rust::{
     v5::{GroupObject, ScanlationGroupAttributes as Attributes},
     ApiObjectNoRelationships,
 };
+use mangadex_api_types_rust::ReferenceExpansionResource;
 use uuid::Uuid;
 
-use self::attributes::ScanlationGroupAttributes;
+use crate::utils::get_mangadex_client_from_graphql_context;
+
+use self::{attributes::ScanlationGroupAttributes, relationships::ScanlationGroupRelationships};
 
 pub mod attributes;
+pub mod relationships;
 
 #[derive(Clone)]
 pub enum ScanlationGroup {
@@ -39,6 +43,35 @@ impl ScanlationGroup {
         match self {
             ScanlationGroup::WithRelationship(i) => i.attributes.clone().into(),
             ScanlationGroup::WithoutRelationship(i) => i.attributes.clone().into(),
+        }
+    }
+    pub async fn relationships(
+        &self,
+        ctx: &Context<'_>,
+    ) -> GraphQLResult<ScanlationGroupRelationships> {
+        match self {
+            ScanlationGroup::WithRelationship(o) => Ok(o.relationships.clone().into()),
+            ScanlationGroup::WithoutRelationship(o) => {
+                let client = get_mangadex_client_from_graphql_context::<tauri::Wry>(ctx)?;
+                let mut req = client.scanlation_group().id(o.id).get();
+                let mut includes: Vec<ReferenceExpansionResource> = Vec::new();
+                ctx.field().selection_set().for_each(|f| match f.name() {
+                    "leader" => {
+                        includes.push(ReferenceExpansionResource::Leader);
+                    }
+                    "members" => {
+                        includes.push(ReferenceExpansionResource::Member);
+                    }
+                    _ => {}
+                });
+                Ok(req
+                    .includes(includes)
+                    .send()
+                    .await?
+                    .data
+                    .relationships
+                    .into())
+            }
         }
     }
 }
