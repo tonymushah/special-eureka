76f0f47a1222ba3712c15d6a19ccbf48b848280b
tonymushah <tonyramanantsalama@gmail.com>
2023-12-16 10:40:40 +0300

    Added `CoverRelationships`

--- src/mangadex/plugin/tauri-plugin/src/objects/cover.rs
+++ src/mangadex/plugin/tauri-plugin/src/objects/cover.rs
@@ -1,13 +1,17 @@
-use async_graphql::Object;
+use async_graphql::{Context, Object, Result as GraphQLResult};
 use mangadex_api_schema_rust::{
     v5::{CoverAttributes as Attributes, CoverObject},
     ApiObjectNoRelationships,
 };
+use mangadex_api_types_rust::ReferenceExpansionResource;
 use uuid::Uuid;
 
-use self::attributes::CoverAttributes;
+use crate::utils::get_mangadex_client_from_graphql_context;
+
+use self::{attributes::CoverAttributes, relationships::CoverRelationships};
 
 pub mod attributes;
+pub mod relationships;
 
 #[derive(Clone)]
 pub enum Cover {
@@ -39,6 +43,33 @@ impl Cover {
         match self {
             Cover::WithRelationship(i) => i.attributes.clone().into(),
             Cover::WithoutRelationship(i) => i.attributes.clone().into(),
+        }
+    }
+    pub async fn relationships(&self, ctx: &Context<'_>) -> GraphQLResult<CoverRelationships> {
+        match self {
+            Cover::WithRelationship(o) => Ok(o.relationships.clone().into()),
+            Cover::WithoutRelationship(o) => {
+                let client = get_mangadex_client_from_graphql_context::<tauri::Wry>(ctx)?;
+                let mut req = client.cover().cover_id(o.id).get();
+                let mut includes: Vec<ReferenceExpansionResource> = Vec::new();
+                ctx.field().selection_set().for_each(|f| match f.name() {
+                    "manga" => {
+                        includes.push(ReferenceExpansionResource::Manga);
+                    }
+                    "user" => {
+                        includes.push(ReferenceExpansionResource::User);
+                    }
+                    _ => {}
+                });
+                includes.dedup();
+                Ok(req
+                    .includes(includes)
+                    .send()
+                    .await?
+                    .data
+                    .relationships
+                    .into())
+            }
         }
     }
 }
---
+++ src/mangadex/plugin/tauri-plugin/src/objects/cover/relationships.rs
@@ -0,0 +1,67 @@
+use std::ops::Deref;
+
+use async_graphql::{Error as GraphQLError, Object, Result as GraphQLResult};
+use mangadex_api_schema_rust::{
+    v5::{MangaAttributes, RelatedAttributes, Relationship, UserAttributes},
+    ApiObjectNoRelationships,
+};
+use mangadex_api_types_rust::{error::RelationshipConversionError, RelationshipType};
+
+use crate::objects::{manga::MangaObject, user::User};
+
+#[derive(Debug, Clone)]
+pub struct CoverRelationships(pub Vec<Relationship>);
+
+impl From<Vec<Relationship>> for CoverRelationships {
+    fn from(value: Vec<Relationship>) -> Self {
+        Self(value)
+    }
+}
+
+impl Deref for CoverRelationships {
+    type Target = Vec<Relationship>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[Object]
+impl CoverRelationships {
+    pub async fn manga(&self) -> GraphQLResult<MangaObject> {
+        self.iter()
+            .find(|rel| rel.type_ == RelationshipType::Manga)
+            .map(|rel| {
+                <ApiObjectNoRelationships<MangaAttributes> as TryFrom<Relationship>>::try_from(
+                    rel.clone(),
+                )
+            })
+            .and_then(|rel| rel.ok())
+            .map(<MangaObject as From<ApiObjectNoRelationships<MangaAttributes>>>::from)
+            .ok_or(GraphQLError::new("Related manga not found"))
+    }
+    pub async fn user(&self) -> GraphQLResult<User> {
+        self.iter()
+            .find(|e| e.type_ == RelationshipType::Creator || e.type_ == RelationshipType::User)
+            .map(
+                |value| -> Result<
+                    ApiObjectNoRelationships<UserAttributes>,
+                    RelationshipConversionError,
+                > {
+                    if let Some(RelatedAttributes::User(ref attributes)) = value.attributes {
+                        Ok(ApiObjectNoRelationships {
+                            id: value.id,
+                            type_: RelationshipType::User,
+                            attributes: attributes.clone(),
+                        })
+                    } else {
+                        Err(RelationshipConversionError::AttributesNotFound(
+                            RelationshipType::User,
+                        ))
+                    }
+                },
+            )
+            .and_then(|inner| inner.ok())
+            .map(<User as From<ApiObjectNoRelationships<UserAttributes>>>::from)
+            .ok_or(GraphQLError::new("Related Uploader or User not found"))
+    }
+}
