fe89c10efdddce46c822e3309233ee606ea3a880
tonymushah <tonyramanantsalama@gmail.com>
2023-12-18 10:01:19 +0300

    Added `CaptchaMutations`

--- src/mangadex/plugin/tauri-plugin/src/mutation.rs
+++ src/mangadex/plugin/tauri-plugin/src/mutation.rs
@@ -1,9 +1,10 @@
 pub mod api_client;
 pub mod author;
+pub mod captcha;
 
 use async_graphql::Object;
 
-use self::api_client::ApiClientMutation;
+use self::{api_client::ApiClientMutation, author::AuthorMutations, captcha::CaptchaMutations};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Mutation;
@@ -12,5 +13,11 @@ pub struct Mutation;
 impl Mutation {
     pub async fn api_client(&self) -> ApiClientMutation {
         ApiClientMutation
+    }
+    pub async fn author(&self) -> AuthorMutations {
+        AuthorMutations
+    }
+    pub async fn captcha(&self) -> CaptchaMutations {
+        CaptchaMutations
     }
 }
