c24d31164eb9b183d3a395a2c137bc70c9547dce
tonymushah <tonyramanantsalama@gmail.com>
2023-12-19 21:31:48 +0300

    Added `svelte-ui`

--- src-tauri/src/bin/mangadex-sdl.rs
+++ src-tauri/src/bin/mangadex-sdl.rs
@@ -2,6 +2,6 @@ use std::path::Path;
 
 fn main() {
     tauri_plugin_speu_mangadex::MangadexDesktopApi::default()
-        .export_sdl(Path::new("../src/mangadex/schema.graphqls").to_path_buf())
+        .export_sdl(Path::new("../src/lib/schemas/mangadex.graphqls").to_path_buf())
         .unwrap()
 }
