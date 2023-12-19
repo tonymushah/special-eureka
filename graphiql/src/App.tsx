4e74b3545161f857c92ce41f8f5785db0a6c30ab
tonymushah <tonyramanantsalama@gmail.com>
2023-12-19 21:05:54 +0300

    Graphiql Setup

--- graphiql/src/App.tsx
+++ graphiql/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fetcher } from '@graphiql/toolkit'
+import GraphiQL from 'graphiql'
+import { invoke as tauriInvoke, } from "@tauri-apps/api"
+import 'graphiql/graphiql.min.css'
+import { InvokeArgs } from '@tauri-apps/api/tauri'
+import "./App.css"
+
+async function invoke<T = unknown>(command: string, args: InvokeArgs): Promise<T> {
+    return await tauriInvoke(`plugin:mangadex-desktop-api|${command}`, args);
+}
+
+const fetcher: Fetcher = async function (params) {
+    const [data,] = await invoke<[string, boolean]>("graphql", params);
+    return JSON.parse(data);
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <GraphiQL fetcher={fetcher} />
+    )
 }
 
 export default App
