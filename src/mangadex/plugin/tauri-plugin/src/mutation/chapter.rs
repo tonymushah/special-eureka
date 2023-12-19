b563dade75f31effeaaa1c936dc0f7ec9f47d16d
tonymushah <tonyramanantsalama@gmail.com>
2023-12-19 10:23:57 +0300

    TODO: `ChapterQueries::download`

--- src/mangadex/plugin/tauri-plugin/src/mutation/chapter.rs
+++ src/mangadex/plugin/tauri-plugin/src/mutation/chapter.rs
@@ -1,4 +1,5 @@
-use async_graphql::{Context, EmptyMutation, Object, Result};
+use async_graphql::{Context, EmptyMutation, Enum, Object, Result};
+use mangadex_api::utils::download::chapter::DownloadMode as MDDownloadMode;
 use mangadex_api_input_types::chapter::edit::ChapterUpdateParams;
 use mangadex_api_schema_rust::{v5::ChapterAttributes, ApiObjectNoRelationships};
 use uuid::Uuid;
@@ -21,4 +22,44 @@ impl ChapterMutations {
         let _ = client.chapter().id(id).delete().send().await?;
         Ok(EmptyMutation)
     }
+    /// Remove the chapter from the current device or offline
+    pub async fn remove(&self, ctx: &Context<'_>, id: Uuid) -> Result<EmptyMutation> {
+        todo!()
+    }
+    pub async fn download(
+        &self,
+        ctx: &Context<'_>,
+        id: Uuid,
+        #[graphql(default_with = "default_download_quality()")] quality: DownloadMode,
+    ) -> Result<EmptyMutation> {
+        todo!()
+    }
+}
+
+#[derive(Clone, Enum, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum DownloadMode {
+    Normal,
+    DataSaver,
+}
+
+impl From<MDDownloadMode> for DownloadMode {
+    fn from(value: MDDownloadMode) -> Self {
+        match value {
+            MDDownloadMode::Normal => Self::Normal,
+            MDDownloadMode::DataSaver => Self::DataSaver,
+        }
+    }
+}
+
+impl From<DownloadMode> for MDDownloadMode {
+    fn from(value: DownloadMode) -> Self {
+        match value {
+            DownloadMode::Normal => Self::Normal,
+            DownloadMode::DataSaver => Self::DataSaver,
+        }
+    }
+}
+
+fn default_download_quality() -> DownloadMode {
+    DownloadMode::Normal
 }
