fc13b21a7aa6c3263644e8c807b997bbfbe93f0e
tonymushah <tonyramanantsalama@gmail.com>
2023-12-19 12:23:29 +0300

    Added `CustomListQueries` with `todo!()`

--- src/mangadex/plugin/tauri-plugin/src/query.rs
+++ src/mangadex/plugin/tauri-plugin/src/query.rs
@@ -3,13 +3,14 @@ pub mod auth;
 pub mod author;
 pub mod chapter;
 pub mod cover;
+pub mod custom_list;
 pub mod manga;
 
 use async_graphql::Object;
 
 use self::{
     api_client::ApiClientQueries, author::AuthorQueries, chapter::ChapterQueries,
-    cover::CoverQueries, manga::MangaQueries,
+    cover::CoverQueries, custom_list::CustomListQueries, manga::MangaQueries,
 };
 
 pub struct Query;
@@ -30,5 +31,8 @@ impl Query {
     }
     pub async fn cover(&self) -> CoverQueries {
         CoverQueries
+    }
+    pub async fn custom_list(&self) -> CustomListQueries {
+        CustomListQueries
     }
 }
---
+++ src/mangadex/plugin/tauri-plugin/src/query/custom_list.rs
@@ -0,0 +1,35 @@
+use async_graphql::{Context, InputObject, Object, Result};
+use mangadex_api_input_types::custom_list::get_user_lists::UserCustomListParams;
+use uuid::Uuid;
+
+use crate::objects::custom_list::{lists::CustomListResults, CustomList};
+
+#[derive(Debug, Clone, Copy)]
+pub struct CustomListQueries;
+
+#[Object]
+impl CustomListQueries {
+    pub async fn get(&self, ctx: &Context<'_>, id: Uuid) -> Result<CustomList> {
+        todo!()
+    }
+    pub async fn current_logged_lists(
+        &self,
+        ctx: &Context<'_>,
+        #[graphql(default)] params: CurrentLoggedLists,
+    ) -> Result<CustomListResults> {
+        todo!()
+    }
+    pub async fn get_user_lists(
+        &self,
+        ctx: &Context<'_>,
+        params: UserCustomListParams,
+    ) -> Result<CustomListResults> {
+        todo!()
+    }
+}
+
+#[derive(Debug, Clone, Default, InputObject)]
+pub struct CurrentLoggedLists {
+    offset: Option<u32>,
+    limit: Option<u32>,
+}
