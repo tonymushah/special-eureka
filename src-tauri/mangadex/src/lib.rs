6ee458b705d632e40befd5dc0e309102a3b8437d
tonymushah <tonyramanantsalama@gmail.com>
2023-12-30 11:07:24 +0300

    Added `register uri_scheme`

--- src-tauri/mangadex/src/lib.rs
+++ src-tauri/mangadex/src/lib.rs
@@ -97,6 +97,102 @@ impl MangadexDesktopApi {
     pub fn export_sdl(&self, path: PathBuf) -> std::io::Result<()> {
         <Self as MizukiPluginTrait<tauri::Wry, Q, M, S>>::export_sdl(self, path)
     }
+    pub fn register_uri_scheme_protocol<R: Runtime>(&self, app: &tauri::AppHandle<R>, config: serde_json::Value) -> tauri::plugin::Result<()> {
+    Builder::<R, ()>::new("mangadex-graphiql")
+                    .register_uri_scheme_protocol("mangadex", move |app, r| {
+                        let bad_request = tauri::http::ResponseBuilder::new()
+                            .header("access-control-allow-origin", "*")
+                            .status(StatusCode::BAD_REQUEST)
+                            .mimetype(MimeType::Txt.to_string().as_str())
+                            .body(Vec::new());
+                        let not_found = tauri::http::ResponseBuilder::new()
+                            .header("access-control-allow-origin", "*")
+                            .status(StatusCode::NOT_FOUND)
+                            .mimetype(MimeType::Txt.to_string().as_str())
+                            .body(Vec::new());
+                        let not_loaded = tauri::http::ResponseBuilder::new()
+                                .header("access-control-allow-origin", "*")
+                                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                                .mimetype(MimeType::Txt.to_string().as_str())
+                                .body(b"Offline App State is not loaded".to_vec());
+                        if let Some(offline_app_state) = app.try_state::<OfflineAppState>() {
+                            let app_state_read = offline_app_state.blocking_read();
+                            if let Some(app_state) = app_state_read.as_ref() {
+                                if let Ok(uri) = Url::parse(r.uri()) {
+                                if uri.domain() == Some("chapter") {
+                                    if let Ok(regex) = Regex::new(r"(?x)/(?P<chapter_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/(?P<mode>data|data-saver)/(?P<filename>\w*.\w*)") {
+                                        if let Some(res) = regex.captures(uri.path()) {
+                                            if let Some(chapter_id) = res.name("chapter_id").and_then(|id| Uuid::parse_str(id.as_str()).ok()) {
+                                                let chapter_util = app_state.chapter_utils().with_id(chapter_id);
+                                                if let Some(mode) = res.name("mode").and_then(|mode| {
+                                                    match mode.as_str() {
+                                                        "data" => Some(ChapterMode::Data),
+                                                        "data-saver" => Some(ChapterMode::DataSaver),
+                                                        _ => None
+                                                    }
+                                                }) {
+                                                    if let Some(filename) = res.name("filename").map(|f| f.as_str()) {
+                                                        let body: Bytes = {
+                                                            let mut to_res = BytesMut::new(); 
+                                                                    match mode {
+                                                                    ChapterMode::Data => {
+                                                                        let res = chapter_util.get_data_image(filename).and_then(|mut buf_reader| {
+                                                                            buf_reader.read_exact(&mut to_res)?;
+                                                                            Ok(())
+                                                                        });
+                                                                        if res.is_err() {
+                                                                            return not_found;
+                                                                        }
+                                                                    }
+                                                                    ChapterMode::DataSaver => {
+                                                                        let res = chapter_util.get_data_saver_image(filename).and_then(|mut buf_reader| {
+                                                                            buf_reader.read_exact(&mut to_res)?;
+                                                                            Ok(())
+                                                                        });
+                                                                        if res.is_err() {
+                                                                            return not_found;
+                                                                        }
+                                                                    }
+                                                                };
+                                                            to_res.into()
+                                                        };
+                                                        tauri::http::ResponseBuilder::new()
+                                                            .header("access-control-allow-origin", "*")
+                                                            .status(StatusCode::OK)
+                                                            // TODO Add jpeg mimetype
+                                                            .mimetype("")
+                                                            .body(body.to_vec())
+                                                    }else {
+                                                        not_found
+                                                    }
+                                                }else {
+                                                    not_found
+                                                }
+                                            }else {
+                                                not_found
+                                            }
+                                        }else {
+                                            not_found
+                                        }
+                                    }else {
+                                        bad_request
+                                    }
+                                } else {
+                                    not_found
+                                }
+                            } else {
+                                bad_request
+                            }
+                            }else {
+                                not_loaded
+                            }
+                        } else {
+                            not_loaded
+                        }
+                    })
+                    .build()
+                    .initialize(app, config)
+    }
 }
 
 impl<R> MizukiPluginTrait<R, Q, M, S> for MangadexDesktopApi
@@ -118,7 +214,7 @@ where
     fn initialize(
         &mut self,
         app: &tauri::AppHandle<R>,
-        _config: serde_json::Value,
+        config: serde_json::Value,
     ) -> tauri::plugin::Result<()> {
         let identifier = app.config().tauri.bundle.identifier.clone();
         match set_indentifier(identifier) {
@@ -127,102 +223,7 @@ where
                 panic!("{}", err.to_string());
             }
         };
-        {
-            Builder::<R, ()>::new("mangadex-graphiql")
-                .register_uri_scheme_protocol("mangadex", move |app, r| {
-                    let bad_request = tauri::http::ResponseBuilder::new()
-                        .header("access-control-allow-origin", "*")
-                        .status(StatusCode::BAD_REQUEST)
-                        .mimetype(MimeType::Txt.to_string().as_str())
-                        .body(Vec::new());
-                    let not_found = tauri::http::ResponseBuilder::new()
-                        .header("access-control-allow-origin", "*")
-                        .status(StatusCode::NOT_FOUND)
-                        .mimetype(MimeType::Txt.to_string().as_str())
-                        .body(Vec::new());
-                    let not_loaded = tauri::http::ResponseBuilder::new()
-                            .header("access-control-allow-origin", "*")
-                            .status(StatusCode::INTERNAL_SERVER_ERROR)
-                            .mimetype(MimeType::Txt.to_string().as_str())
-                            .body(b"Offline App State is not loaded".to_vec());
-                    if let Some(offline_app_state) = app.try_state::<OfflineAppState>() {
-                        let app_state_read = offline_app_state.blocking_read();
-                        if let Some(app_state) = app_state_read.as_ref() {
-                            if let Ok(uri) = Url::parse(r.uri()) {
-                            if uri.domain() == Some("chapter") {
-                                if let Ok(regex) = Regex::new(r"(?x)/(?P<chapter_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/(?P<mode>data|data-saver)/(?P<filename>\w*.\w*)") {
-                                    if let Some(res) = regex.captures(uri.path()) {
-                                        if let Some(chapter_id) = res.name("chapter_id").and_then(|id| Uuid::parse_str(id.as_str()).ok()) {
-                                            let chapter_util = app_state.chapter_utils().with_id(chapter_id);
-                                            if let Some(mode) = res.name("mode").and_then(|mode| {
-                                                match mode.as_str() {
-                                                    "data" => Some(ChapterMode::Data),
-                                                    "data-saver" => Some(ChapterMode::DataSaver),
-                                                    _ => None
-                                                }
-                                            }) {
-                                                if let Some(filename) = res.name("filename").map(|f| f.as_str()) {
-                                                    let body: Bytes = {
-                                                        let mut to_res = BytesMut::new(); 
-                                                                match mode {
-                                                                ChapterMode::Data => {
-                                                                    let res = chapter_util.get_data_image(filename).and_then(|mut buf_reader| {
-                                                                        buf_reader.read_exact(&mut to_res)?;
-                                                                        Ok(())
-                                                                    });
-                                                                    if res.is_err() {
-                                                                        return not_found;
-                                                                    }
-                                                                }
-                                                                ChapterMode::DataSaver => {
-                                                                    let res = chapter_util.get_data_saver_image(filename).and_then(|mut buf_reader| {
-                                                                        buf_reader.read_exact(&mut to_res)?;
-                                                                        Ok(())
-                                                                    });
-                                                                    if res.is_err() {
-                                                                        return not_found;
-                                                                    }
-                                                                }
-                                                            };
-                                                        to_res.into()
-                                                    };
-                                                    tauri::http::ResponseBuilder::new()
-                                                        .header("access-control-allow-origin", "*")
-                                                        .status(StatusCode::OK)
-                                                        // TODO Add jpeg mimetype
-                                                        .mimetype("")
-                                                        .body(body.to_vec())
-                                                }else {
-                                                    not_found
-                                                }
-                                            }else {
-                                                not_found
-                                            }
-                                        }else {
-                                            not_found
-                                        }
-                                    }else {
-                                        not_found
-                                    }
-                                }else {
-                                    bad_request
-                                }
-                            } else {
-                                not_found
-                            }
-                        } else {
-                            bad_request
-                        }
-                        }else {
-                            not_loaded
-                        }
-                    } else {
-                        not_loaded
-                    }
-                })
-                .build()
-                .initialize(app, _config)?;
-        }
+        self.register_uri_scheme_protocol(app, config)?;
 
         app.manage(self.offline_app_state.clone());
         app.manage(self.client.clone());
