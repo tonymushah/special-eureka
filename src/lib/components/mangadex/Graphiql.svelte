374bc33cfbddcbc890a7b176a3bc82521a654734
tonymushah <tonyramanantsalama@gmail.com>
2024-01-03 23:02:21 +0300

    Added codegen

---
+++ src/lib/components/mangadex/Graphiql.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import type { Fetcher } from "@graphiql/toolkit";
+	import { onMount, onDestroy } from 'svelte';
+	import ReactDOM from 'react-dom/client';
+    import Graphiql from "graphiql";
+	import React from 'react';
+    import { invoke } from "@tauri-apps/api"
+
+    const fetcher: Fetcher =async (params) => {
+        let [res, _] = await invoke<[string, boolean]>("plugin:mangadex-desktop-api|graphql", params);
+        return JSON.parse(res);
+    }
+    const e = React.createElement;
+
+	let container: HTMLDivElement | null = null;
+	let root: ReactDOM.Root | null = null;
+
+	onMount(() => {
+		try {
+			if (container != null) {
+				root = ReactDOM.createRoot(container);
+				root.render(e(Graphiql, {
+                    fetcher
+                }));
+			}
+		} catch (error) {
+			console.warn({ error });
+		}
+	});
+
+	onDestroy(() => {
+        try {
+            root?.unmount();
+        } catch (error) {
+            console.warn({ error });
+        }
+    });
+</script>
+
+<div bind:this={container} />
+
+
+<style>
+    @import "graphiql/graphiql.min.css";
+</style>
