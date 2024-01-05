c99e5d05f80ec83aff2722f47ac8f73953a47818
tonymushah <tonyramanantsalama@gmail.com>
2024-01-05 16:58:56 +0300

    Implemented `DefaultStore`

--- src-tauri/mangadex/src/store.rs
+++ src-tauri/mangadex/src/store.rs
@@ -4,6 +4,10 @@ use tauri_plugin_store::StoreBuilder;
 use self::{
     keys::PATH,
     types::{
+        enums::{
+            direction::{reading::ReadingDirectionStore, sidebar::SidebarDirectionStore},
+            reading_mode::ReadingModeStore,
+        },
         structs::{client_info::ClientInfoStore, refresh_token::RefreshTokenStore},
         DefaulStore,
     },
@@ -15,7 +19,11 @@ pub mod types;
 pub fn get_store_builder<R: Runtime>(app: AppHandle<R>) -> tauri::plugin::Result<StoreBuilder<R>> {
     let builder = {
         let b = StoreBuilder::new(app, PATH.parse()?);
-        RefreshTokenStore::default_store(ClientInfoStore::default_store(b)?)?
+        SidebarDirectionStore::default_store(ReadingModeStore::default_store(
+            ReadingDirectionStore::default_store(RefreshTokenStore::default_store(
+                ClientInfoStore::default_store(b)?,
+            )?)?,
+        )?)?
     };
     Ok(builder)
 }
