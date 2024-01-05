ee128316d5af611fae418ffc36885f3702cae786
tonymushah <tonyramanantsalama@gmail.com>
2024-01-05 10:09:19 +0300

    Added `UserWatch`

--- src-tauri/mangadex/src/objects/user/attributes.rs
+++ src-tauri/mangadex/src/objects/user/attributes.rs
@@ -4,6 +4,7 @@ use async_graphql::Object;
 use mangadex_api_schema_rust::v5::UserAttributes as Attributes;
 use mangadex_api_types_rust::UserRole;
 
+#[derive(Debug, Clone)]
 pub struct UserAttributes(Attributes);
 
 impl Deref for UserAttributes {
