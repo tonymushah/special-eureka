808df3fad9eefceb4bf1874de5e3cd423444eff0
tonymushah <tonyramanantsalama@gmail.com>
2024-01-05 12:30:55 +0300

    Added `ApiClientSubscriptions`

--- src-tauri/mangadex/src/subscription.rs
+++ src-tauri/mangadex/src/subscription.rs
@@ -1,3 +1,7 @@
+use async_graphql::{Context, Result, Subscription};
+use tokio_stream::Stream;
+use uuid::Uuid;
+
 pub mod api_client;
 pub mod author;
 pub mod chapter;
@@ -10,3 +14,23 @@ pub mod statistics;
 pub mod tag;
 pub mod upload;
 pub mod user;
+
+use self::api_client::ApiClientSubscriptions;
+use crate::objects::api_client::attributes::ApiClientAttributes;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Subscriptions;
+
+#[Subscription]
+impl Subscriptions {
+    pub async fn watch_api_client<'ctx>(
+        &'ctx self,
+        ctx: &'ctx Context<'ctx>,
+        api_client_id: Uuid,
+        sub_id: Uuid,
+    ) -> Result<impl Stream<Item = ApiClientAttributes> + 'ctx> {
+        ApiClientSubscriptions
+            .listen_by_id(ctx, api_client_id, sub_id)
+            .await
+    }
+}
---
+++ src-tauri/mangadex/src/subscription/api_client.rs
@@ -0,0 +1,67 @@
+use std::sync::Arc;
+
+use async_graphql::{Context, Result, Subscription};
+use tokio::{
+    sync::RwLock,
+    time::{sleep, Duration},
+};
+use tokio_stream::Stream;
+use uuid::Uuid;
+
+use crate::{
+    objects::api_client::attributes::ApiClientAttributes,
+    utils::{get_watches_from_graphql_context, get_window_from_async_graphql},
+};
+use async_stream::stream;
+
+#[derive(Debug, Clone, Copy)]
+pub struct ApiClientSubscriptions;
+
+#[Subscription]
+impl ApiClientSubscriptions {
+    pub async fn listen_by_id<'ctx>(
+        &'ctx self,
+        ctx: &'ctx Context<'ctx>,
+        api_client_id: Uuid,
+        sub_id: Uuid,
+    ) -> Result<impl Stream<Item = ApiClientAttributes> + 'ctx> {
+        let should_end = Arc::new(RwLock::new(false));
+        let should_end_un = should_end.clone();
+
+        let watches = get_watches_from_graphql_context::<tauri::Wry>(ctx)?;
+        let window = get_window_from_async_graphql::<tauri::Wry>(ctx)?;
+        let unlisten = window.listen("sub_end", move |e| {
+            if let Some(payload) = e.payload() {
+                let mut write = should_end_un.blocking_write();
+                if let Ok(id) = Uuid::parse_str(payload) {
+                    *write = id == sub_id;
+                }
+            }
+        });
+        let api_client_sub = watches.api_client.subscribe();
+        Ok(stream! {
+            loop {
+                if *should_end.read().await {
+                    if let Ok(has_changed) = api_client_sub.has_changed() {
+                        if has_changed {
+                            let borrow = {
+                                api_client_sub.borrow().as_ref().cloned()
+                            };
+                            if let Some(data) = borrow {
+                                if data.id == api_client_id {
+                                    yield data.attributes.clone()
+                                }
+                            }
+                        }
+                    }else {
+                        break;
+                    }
+                } else{
+                    break;
+                }
+                sleep(Duration::from_secs(1)).await;
+            }
+            window.unlisten(unlisten);
+        })
+    }
+}
