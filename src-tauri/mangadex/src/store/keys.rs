a3aa18dcf05364e22efb7a470a1bd225c55c9a1b
tonymushah <tonyramanantsalama@gmail.com>
2024-01-05 16:26:35 +0300

    Added `Direction`

--- src-tauri/mangadex/src/store/keys.rs
+++ src-tauri/mangadex/src/store/keys.rs
@@ -3,3 +3,9 @@ pub const CLIENT_INFO: &str = "client_info";
 pub const PATH: &str = "mangadex.store";
 
 pub const REFRESH_TOKEN: &str = "refresh_token";
+
+pub const PAGE_DIRECTION: &str = "page_direction";
+
+pub const SIDEBAR_DIRECTION: &str = "sidebar_direction";
+
+pub const READING_MODE: &str = "reading_mode";
---
+++ src-tauri/mangadex/src/store/types/enums/direction.rs
@@ -0,0 +1,14 @@
+use async_graphql::Enum;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, Enum, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+pub enum Direction {
+    RTL,
+    LTR,
+}
+
+impl Default for Direction {
+    fn default() -> Self {
+        Self::LTR
+    }
+}
