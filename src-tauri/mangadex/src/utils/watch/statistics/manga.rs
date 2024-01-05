cc3a1492335cfec8c61bdfae572fc13ba4c25956
tonymushah <tonyramanantsalama@gmail.com>
2024-01-05 09:28:32 +0300

    Added `StatisticsWatch` and `MangaStatisticsWatch`

--- src-tauri/mangadex/src/utils/watch/statistics/manga.rs
+++ src-tauri/mangadex/src/utils/watch/statistics/manga.rs
@@ -1,38 +1,39 @@
-pub mod manga;
-
 use std::{ops::Deref, sync::Arc};
 
 use tokio::sync::watch::Sender;
 
-use crate::objects::{statistics::StatisticsComments, GetAttributes, GetId};
+use crate::{
+    objects::{statistics::manga::MangaStatisticsAttributes, GetAttributes, GetId},
+    utils::watch::SendDataResult,
+};
 
 use super::{SendData, WatcherInnerData};
 
-type InnerData = WatcherInnerData<Option<StatisticsComments>>;
+type InnerData = WatcherInnerData<MangaStatisticsAttributes>;
 
 type Inner = Sender<Option<InnerData>>;
 
 #[derive(Clone, Debug)]
-pub struct StatisticsWatch(Arc<Inner>);
+pub struct MangaStatisticsWatch(Arc<Inner>);
 
-impl Deref for StatisticsWatch {
+impl Deref for MangaStatisticsWatch {
     type Target = Arc<Inner>;
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-impl Default for StatisticsWatch {
+impl Default for MangaStatisticsWatch {
     fn default() -> Self {
         Self(Arc::new(Sender::new(None)))
     }
 }
 
-impl<T> SendData<T> for StatisticsWatch
+impl<T> SendData<T> for MangaStatisticsWatch
 where
-    T: GetId + GetAttributes<Attributes = Option<StatisticsComments>>,
+    T: GetId + GetAttributes<Attributes = MangaStatisticsAttributes>,
 {
-    fn send_data(&self, data: T) -> super::SendDataResult {
+    fn send_data(&self, data: T) -> SendDataResult {
         if let Err(err) = self.send(Some(data.into())) {
             Err(err.to_string())
         } else {
