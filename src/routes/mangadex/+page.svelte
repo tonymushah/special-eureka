8a82328870e95c770c8552ad03bdcb992dcf773e
tonymushah <tonyramanantsalama@gmail.com>
2024-01-07 18:57:45 +0300

    added storybook

--- src/routes/mangadex/+page.svelte
+++ src/routes/mangadex/+page.svelte
@@ -1,8 +1,4 @@
 <script>
-    import { browser, dev, building, version } from '$app/environment';
-	import Graphiql from "@mangadex/components/Graphiql.svelte";
 
 </script>
-{#if dev}
-    <Graphiql/>
-{/if}
+
