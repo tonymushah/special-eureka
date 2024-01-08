07467e1a63f7eeac628b89802207088c714cb512
tonymushah <tonyramanantsalama@gmail.com>
2024-01-08 08:53:08 +0300

    inited `mizuki-project`

--- src/lib/codegen/mangadex.ts
+++ src/lib/codegen/mangadex.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
     schema: './src/lib/schemas/mangadex.graphqls',
-    documents: ['src/lib/components/mangadex/**/*.svelte', 'src/routes/mangadex/**/*.svelte'],
+    documents: ['src/lib/components/mangadex/**/*.svelte', 'src/lib/store/mangadex/**/*.ts', 'src/lib/components/mangadex/**/*.ts', 'src/routes/mangadex/**/*.svelte'],
     ignoreNoDocuments: true, // for better experience with the watcher
     generates: {
         './src/lib/gql/mangadex/': {
