98e186e4e1e13917bf76963ebb6b2a7b9ee23d5d
tonymushah <tonyramanantsalama@gmail.com>
2024-01-22 21:20:17 +0300

    Added subs in `UserQueries`

--- src-tauri/mangadex/src/query/user.rs
+++ src-tauri/mangadex/src/query/user.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use async_graphql::{Context, Object, Result};
 use mangadex_api_input_types::user::list::UserListParam;
 use uuid::Uuid;
@@ -7,6 +9,7 @@ use crate::{
     utils::{
         get_mangadex_client_from_graphql_context,
         get_mangadex_client_from_graphql_context_with_auth_refresh,
+        get_watches_from_graphql_context, watch::SendData,
     },
 };
 
@@ -16,21 +19,40 @@ pub struct UserQueries;
 #[Object]
 impl UserQueries {
     pub async fn get(&self, ctx: &Context<'_>, id: Uuid) -> Result<User> {
+        let watches = get_watches_from_graphql_context::<tauri::Wry>(ctx)?;
         let client = get_mangadex_client_from_graphql_context::<tauri::Wry>(ctx)?;
-        Ok(client.user().id(id).get().send().await?.data.into())
+        let data: User = client.user().id(id).get().send().await?.data.into();
+        let _ = watches.user.send_data(data.clone());
+        Ok(data)
     }
     pub async fn list(
         &self,
         ctx: &Context<'_>,
         #[graphql(default)] params: UserListParam,
     ) -> Result<UserResults> {
+        let watches = get_watches_from_graphql_context::<tauri::Wry>(ctx)?
+            .deref()
+            .clone();
         let client =
             get_mangadex_client_from_graphql_context_with_auth_refresh::<tauri::Wry>(ctx).await?;
-        Ok(params.send(&client).await?.into())
+
+        let res: UserResults = params.send(&client).await?.into();
+
+        let _res = res.clone();
+        tauri::async_runtime::spawn(async move {
+            for data in _res {
+                let _ = watches.user.send_data(data);
+            }
+        });
+
+        Ok(res)
     }
     pub async fn me(&self, ctx: &Context<'_>) -> Result<User> {
+        let watches = get_watches_from_graphql_context::<tauri::Wry>(ctx)?;
         let client =
             get_mangadex_client_from_graphql_context_with_auth_refresh::<tauri::Wry>(ctx).await?;
-        Ok(client.user().me().get().send().await?.data.into())
+        let data: User = client.user().me().get().send().await?.data.into();
+        let _ = watches.user_me.send_data(data.clone());
+        Ok(data)
     }
 }
