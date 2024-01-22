575da64dab88e8248e2688ffcb52f29603f6ae5c
tonymushah <tonyramanantsalama@gmail.com>
2024-01-22 18:47:01 +0300

    Added sub in `OfflineAppStateQueries`

--- src-tauri/mangadex/src/query/offline_app_state.rs
+++ src-tauri/mangadex/src/query/offline_app_state.rs
@@ -1,6 +1,6 @@
 use async_graphql::{Context, Object, Result};
 
-use crate::utils::get_offline_app_state;
+use crate::utils::{get_offline_app_state, get_watches_from_graphql_context, watch::SendData};
 
 #[derive(Debug, Clone, Copy)]
 pub struct OfflineAppStateQueries;
@@ -8,8 +8,10 @@ pub struct OfflineAppStateQueries;
 #[Object]
 impl OfflineAppStateQueries {
     pub async fn is_mounted(&self, ctx: &Context<'_>) -> Result<bool> {
+        let watches = get_watches_from_graphql_context::<tauri::Wry>(ctx)?;
         let app_state = get_offline_app_state::<tauri::Wry>(ctx)?;
         let read = app_state.read().await;
+        let _ = watches.is_appstate_mounted.send_data(read.is_some());
         Ok(read.is_some())
     }
 }
