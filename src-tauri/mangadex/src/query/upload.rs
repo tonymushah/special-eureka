11e10da0e964445ce86cea403aa1bd3b4fe08b19
tonymushah <tonyramanantsalama@gmail.com>
2024-01-22 20:45:37 +0300

    Added subs for `UploadQueries`

--- src-tauri/mangadex/src/query/upload.rs
+++ src-tauri/mangadex/src/query/upload.rs
@@ -2,7 +2,10 @@ use async_graphql::{Context, Error, Object, Result};
 
 use crate::{
     objects::upload::session::UploadSession,
-    utils::get_mangadex_client_from_graphql_context_with_auth_refresh,
+    utils::{
+        get_mangadex_client_from_graphql_context_with_auth_refresh,
+        get_watches_from_graphql_context, watch::SendData,
+    },
 };
 
 use mangadex_api_types_rust::error::Error as MangaDexErrors;
@@ -13,10 +16,15 @@ pub struct UploadQueries;
 #[Object]
 impl UploadQueries {
     pub async fn get_current(&self, ctx: &Context<'_>) -> Result<Option<UploadSession>> {
+        let watches = get_watches_from_graphql_context::<tauri::Wry>(ctx)?;
         let client =
             get_mangadex_client_from_graphql_context_with_auth_refresh::<tauri::Wry>(ctx).await?;
         match client.upload().get().send().await {
-            Ok(u) => Ok(Some(u.body.data.into())),
+            Ok(u) => {
+                let data: UploadSession = u.body.data.into();
+                let _ = watches.upload_session.send_data(data.clone());
+                Ok(Some(data))
+            }
             Err(e) => {
                 if let MangaDexErrors::Api(ref error) = e {
                     if error.errors.iter().any(|error_| error_.status == 404) {
