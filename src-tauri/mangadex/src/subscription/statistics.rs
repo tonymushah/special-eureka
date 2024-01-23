1e26c2216e7db6995413a07b52825b7c66b0b7e4
tonymushah <tonyramanantsalama@gmail.com>
2024-01-23 16:40:53 +0300

    Added `initial_loading` in `StatisticsSubscriptions`

--- src-tauri/mangadex/src/subscription/statistics.rs
+++ src-tauri/mangadex/src/subscription/statistics.rs
@@ -20,12 +20,25 @@ impl StatisticsSubscriptions {
         id: Uuid,
         sub_id: Uuid,
     ) -> Result<impl Stream<Item = StatisticsComments> + 'ctx> {
-        let (watches, should_end, unlisten, window) =
+        let (watches, should_end, unlisten, window, is_initial_loading) =
             init_watch_subscription::<tauri::Wry>(ctx, sub_id)?;
         let cover_sub = watches.statistics.subscribe();
         Ok(stream! {
             loop {
-                if !*should_end.read().await {
+                if *is_initial_loading.read().await {
+                    let mut write = is_initial_loading.write().await;
+                    *write = false;
+                    let borrow = {
+                        cover_sub.borrow().as_ref().copied()
+                    };
+                    if let Some(data) = borrow {
+                        if data.id == id {
+                            if let Some(attributes) = data.attributes {
+                                yield attributes;
+                            }
+                        }
+                    }
+                } else if !*should_end.read().await {
                     if let Ok(has_changed) = cover_sub.has_changed() {
                         if has_changed {
                             let borrow = {
