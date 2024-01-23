77633e1f3ae436290e550879a3008f2d0fcc111a
tonymushah <tonyramanantsalama@gmail.com>
2024-01-23 16:09:31 +0300

    Added initial_loading in `AuthorSubscriptions`

--- src-tauri/mangadex/src/subscription/author.rs
+++ src-tauri/mangadex/src/subscription/author.rs
@@ -18,12 +18,23 @@ impl AuthorSubscriptions {
         author_id: Uuid,
         sub_id: Uuid,
     ) -> Result<impl Stream<Item = AuthorAttributes> + 'ctx> {
-        let (watches, should_end, unlisten, window) =
+        let (watches, should_end, unlisten, window, is_initial_loading) =
             init_watch_subscription::<tauri::Wry>(ctx, sub_id)?;
         let author_sub = watches.author.subscribe();
         Ok(stream! {
             loop {
-                if !*should_end.read().await {
+                if *is_initial_loading.read().await {
+                    let borrow = {
+                        author_sub.borrow().as_ref().cloned()
+                    };
+                    if let Some(data) = borrow {
+                        if data.id == author_id {
+                            yield data.attributes.clone()
+                        }
+                    }
+                    let mut write = is_initial_loading.write().await;
+                    *write = false;
+                }else if !*should_end.read().await {
                     if let Ok(has_changed) = author_sub.has_changed() {
                         if has_changed {
                             let borrow = {
