2c9ef9e8572690cc90b9dcc0e7f14ebd2dffd1df
tonymushah <tonyramanantsalama@gmail.com>
2024-01-23 16:26:23 +0300

    Added `initial_loading`in `DownloadStateSubscriptions`

--- src-tauri/mangadex/src/subscription/download_state.rs
+++ src-tauri/mangadex/src/subscription/download_state.rs
@@ -18,12 +18,23 @@ impl DownloadStateSubscriptions {
         object_id: Uuid,
         sub_id: Uuid,
     ) -> Result<impl Stream<Item = DownloadState> + 'ctx> {
-        let (watches, should_end, unlisten, window) =
+        let (watches, should_end, unlisten, window, is_initial_loading) =
             init_watch_subscription::<tauri::Wry>(ctx, sub_id)?;
         let download_state_sub = watches.download_state.subscribe();
         Ok(stream! {
             loop {
-                if !*should_end.read().await {
+                if *is_initial_loading.read().await {
+                    let mut write = is_initial_loading.write().await;
+                    *write = false;
+                    let borrow = {
+                        download_state_sub.borrow().as_ref().copied()
+                    };
+                    if let Some(data) = borrow {
+                        if data.id == object_id {
+                            yield data.attributes
+                        }
+                    }
+                } else if !*should_end.read().await {
                     if let Ok(has_changed) = download_state_sub.has_changed() {
                         if has_changed {
                             let borrow = {
