aae8a4967aa3133456e8c8bc5e64c16c3d548834
tonymushah <tonyramanantsalama@gmail.com>
2024-01-26 09:37:59 +0300

    Added subs in `ReadMarkerMutations`

--- src-tauri/mangadex/src/mutation/read_marker.rs
+++ src-tauri/mangadex/src/mutation/read_marker.rs
@@ -1,7 +1,10 @@
 use async_graphql::{Context, Object, Result};
 use mangadex_api_input_types::read_marker::batch_read_marker::MarkChapterBatchParam;
 
-use crate::utils::get_mangadex_client_from_graphql_context_with_auth_refresh;
+use crate::utils::{
+    get_mangadex_client_from_graphql_context_with_auth_refresh, get_watches_from_graphql_context,
+    watch::SendData,
+};
 
 #[derive(Debug, Clone, Copy)]
 pub struct ReadMarkerMutations;
@@ -13,9 +16,16 @@ impl ReadMarkerMutations {
         ctx: &Context<'_>,
         params: MarkChapterBatchParam,
     ) -> Result<bool> {
+        let watches = get_watches_from_graphql_context::<tauri::Wry>(ctx)?;
         let client =
             get_mangadex_client_from_graphql_context_with_auth_refresh::<tauri::Wry>(ctx).await?;
-        params.send(&client).await?;
+        params.clone().send(&client).await?;
+        params.chapter_ids_read.iter().for_each(|id| {
+            let _ = watches.read_marker.send_data((*id, true));
+        });
+        params.chapter_ids_unread.iter().for_each(|id| {
+            let _ = watches.read_marker.send_data((*id, false));
+        });
         Ok(true)
     }
 }
