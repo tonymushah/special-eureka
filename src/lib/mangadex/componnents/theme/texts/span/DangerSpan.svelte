18a9e1591313906044557cd65c903acd1b93c04d
tonymushah <tonyramanantsalama@gmail.com>
2024-02-09 15:24:30 +0300

    Added Spans

---
+++ src/lib/mangadex/componnents/theme/texts/span/DangerSpan.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let variant: "default" | "l1" | "l2" = "default";
+	$: l1 = variant == "l1";
+	$: l2 = variant == "l2";
+	$: _default = !l1 && !l2;
+</script>
+
+<span class:l1 class:l2 class:default={_default}>
+	<slot />
+</span>
+
+<style lang="scss">
+	span.default {
+		color: var(--danger);
+	}
+	span.l1 {
+		color: var(--danger-l1);
+	}
+	span.l2 {
+		color: var(--danger-l2);
+	}
+</style>
---
+++ src/lib/mangadex/componnents/theme/texts/span/DefaultSpan.svelte
@@ -0,0 +1,9 @@
+<span>
+    <slot/>
+</span>
+
+<style lang="scss">
+    span {
+        color: var(--text-color);
+    }
+</style>
