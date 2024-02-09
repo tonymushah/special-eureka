79829454e24d882097c920c94448f83bc72d7445
tonymushah <tonyramanantsalama@gmail.com>
2024-02-09 12:06:23 +0300

    Fixed Button Accent story

--- src/lib/mangadex/componnents/theme/buttons/ButtonAccentOnlyLabel.stories.ts
+++ src/lib/mangadex/componnents/theme/buttons/ButtonAccentOnlyLabel.stories.ts
@@ -1,26 +1,26 @@
-import type { Meta, StoryObj } from '@storybook/svelte';
+import type { Meta, StoryObj } from "@storybook/svelte";
 
-import ButtonAccent from './ButtonAccentOnlyLabel.svelte';
-import MangadexThemeProviderForStory from '../MangadexThemeProviderForStory.svelte';
+import ButtonAccent from "./ButtonAccentOnlyLabel.svelte";
+import MangadexThemeProviderForStory from "../MangadexThemeProviderForStory.svelte";
 
 const meta = {
-    decorators: [() => MangadexThemeProviderForStory],
-    title: "AccentButton",
-    component: ButtonAccent,
-    tags: ["autodocs"],
-    argTypes: {
-        variant: {
-            type: {
-                name: "enum",
-                value: ["default", "1", "2", "3", "4", "5"]
-            },
-        },
-        style: {
-            "type": "string",
-            "name": "Style",
-            "description": "The button custom style"
-        }
-    }
+	decorators: [() => MangadexThemeProviderForStory],
+	title: "AccentButton",
+	component: ButtonAccent,
+	tags: ["autodocs"],
+	argTypes: {
+		variant: {
+			type: {
+				name: "enum",
+				value: ["default", "1", "2", "3", "4", "5"]
+			}
+		},
+		style: {
+			type: "string",
+			name: "Style",
+			description: "The button custom style"
+		}
+	}
 } satisfies Meta<ButtonAccent>;
 
 export default meta;
@@ -29,58 +29,58 @@ type Story = StoryObj<typeof meta>;
 
 const label = "Button";
 
-export const Default: Story = {
-    args: {
-        variant: "default",
-        label
-    }
-}
+export const AccentDefault: Story = {
+	args: {
+		variant: "default",
+		label
+	}
+};
 
-export const L1: Story = {
-    args: {
-        variant: "1",
-        label
-    }
-}
+export const AccentL1: Story = {
+	args: {
+		variant: "1",
+		label
+	}
+};
 
-export const L2: Story = {
-    args: {
-        variant: "2",
-        label
-    }
-}
+export const AccentL2: Story = {
+	args: {
+		variant: "2",
+		label
+	}
+};
 
-export const L3: Story = {
-    args: {
-        variant: "3",
-        label
-    }
-}
+export const AccentL3: Story = {
+	args: {
+		variant: "3",
+		label
+	}
+};
 
-export const L4: Story = {
-    args: {
-        variant: "4",
-        label
-    }
-}
+export const AccentL4: Story = {
+	args: {
+		variant: "4",
+		label
+	}
+};
 
-export const L5: Story = {
-    args: {
-        variant: "5",
-        label
-    }
-}
+export const AccentL5: Story = {
+	args: {
+		variant: "5",
+		label
+	}
+};
 
 export const Accent: Story = {
-    args: {
-        variant: "accent",
-        label
-    }
-}
+	args: {
+		variant: "accent",
+		label
+	}
+};
 
 export const AccentAlt: Story = {
-    args: {
-        variant: "accent-alt",
-        label
-    }
-}
+	args: {
+		variant: "accent-alt",
+		label
+	}
+};
