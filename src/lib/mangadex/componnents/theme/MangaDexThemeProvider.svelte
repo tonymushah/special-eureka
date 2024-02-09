b6bad45d53db8885935ca68e986e260f9ab5e885
tonymushah <tonyramanantsalama@gmail.com>
2024-02-09 11:59:33 +0300

    Added mangadexTheme to svelte-ui theme conversion

--- src/lib/mangadex/componnents/theme/MangaDexThemeProvider.svelte
+++ src/lib/mangadex/componnents/theme/MangaDexThemeProvider.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
-	import type { MangadexTheme } from "@mangadex/theme";
+	import { buildSvelteUITheme, type MangadexTheme } from "@mangadex/theme";
 	import MangadexBackground from "./MangadexBackground.svelte";
 	import MangadexThemeProviderBase from "./MangadexThemeProviderBase.svelte";
+	import { SvelteUIProvider, createTheme } from "@svelteuidev/core";
 
 	export let theme: MangadexTheme;
 	export let fonts = "Poppins";
+	$: svelte_ui_theme = buildSvelteUITheme(theme);
 </script>
 
 <MangadexThemeProviderBase {theme} {fonts}>
-	<MangadexBackground>
-		<slot />
-	</MangadexBackground>
+	<SvelteUIProvider bind:class={svelte_ui_theme} themeObserver={undefined}>
+		<MangadexBackground>
+			<slot />
+		</MangadexBackground>
+	</SvelteUIProvider>
 </MangadexThemeProviderBase>
