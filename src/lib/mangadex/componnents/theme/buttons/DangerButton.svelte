ce6b909ebd79d14b8526255a23fc11dbd96eb869
tonymushah <tonyramanantsalama@gmail.com>
2024-02-09 14:31:16 +0300

    Added DangerButton with it's story

---
+++ src/lib/mangadex/componnents/theme/buttons/DangerButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import L1 from "./danger/L1.svelte";
+	import L2 from "./danger/L2.svelte";
+	import Default from "./danger/Default.svelte";
+
+    export let variant: "default" | "1" | "2" = "default";
+    createEventDispatcher<{
+        click: MouseEvent & {
+            currentTarget: EventTarget & HTMLButtonElement
+        }
+    }>();
+    export let type: "reset" | "submit" | "button" = "button";
+    export let style: string | undefined = undefined;
+</script>
+
+{#if variant == "1"}
+    <L1 on:click {type} {style}>
+        <slot/>
+    </L1>
+{:else if variant == "2"}
+    <L2 on:click {type} {style}>
+        <slot/>
+    </L2>
+{:else}
+    <Default on:click {type} {style}>
+        <slot/>
+    </Default>
+{/if}
---
+++ src/lib/mangadex/componnents/theme/buttons/danger/L1.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+    export let type: "reset" | "submit" | "button" = "button"
+    createEventDispatcher<{
+        click: MouseEvent & {
+            currentTarget: EventTarget & HTMLButtonElement
+        }
+    }>();
+    export let style: string | undefined = undefined;
+</script>
+
+<button {style} on:click {type}>
+    <slot/>
+</button>
+
+<style lang="scss">
+    button {
+        min-height: 3em;
+        min-width: 3em;
+        border-radius: .25rem;
+        border: 0;
+        padding-left: .75rem;
+        padding-right: .75rem;
+        transition: filter 100ms ease-in-out;
+        display: flex;
+        align-items: center;
+        color: var(--text-color);
+        background-color: var(--danger-l1);
+        font-family: var(--fonts);
+    }
+    button:hover {
+        filter: brightness(90%);
+    }
+    button:active {
+        filter: brightness(80%);
+    }
+</style>
