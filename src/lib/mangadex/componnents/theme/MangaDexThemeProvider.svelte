c892fab4a09cc3a84454a3bcf8100e65bd883fc8
tonymushah <tonyramanantsalama@gmail.com>
2024-02-10 06:54:38 +0300

    Fixed imports on MangadexThemeProvider

--- src/lib/mangadex/componnents/theme/MangaDexThemeProvider.svelte
+++ src/lib/mangadex/componnents/theme/MangaDexThemeProvider.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import { buildSvelteUITheme, type MangadexTheme } from "@mangadex/theme";
+	import { type MangadexTheme } from "@mangadex/theme";
 	import MangadexBackground from "./MangadexBackground.svelte";
 	import MangadexThemeProviderBase from "./MangadexThemeProviderBase.svelte";
-	import { SvelteUIProvider, createTheme } from "@svelteuidev/core";
 
 	export let theme: MangadexTheme;
 	export let fonts = "Poppins";
