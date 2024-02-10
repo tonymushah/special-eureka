431169e9b863b1a8f50fb49709570f0b92db7aaa
tonymushah <tonyramanantsalama@gmail.com>
2024-02-10 06:48:31 +0300

    Added spans stories

---
+++ src/lib/mangadex/componnents/theme/texts/span/stories/DangerSpanOnlyLabel.stories.ts
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import "@fontsource/poppins/index.css";
+
+import DangerSpan from "./DangerSpanOnlyLabel.svelte";
+import MangadexThemeProviderForStory from "@mangadex/componnents/theme/MangadexThemeProviderForStory.svelte";
+
+const meta = {
+	decorators: [() => MangadexThemeProviderForStory],
+	title: "DangerSpan",
+	component: DangerSpan,
+	tags: ["autodocs"],
+	argTypes: {
+		variant: {
+			type: {
+				name: "enum",
+				value: ["default", "l1", "l2"]
+			}
+		}
+	}
+} satisfies Meta<DangerSpan>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const innerText =
+	"Reiciendis occaecati cum assumenda aut et ducimus repellendus. Fugit quibusdam id aut autem ut dolorem voluptas. Totam molestiae consequatur enim inventore quas.";
+
+export const DangerDefault: Story = {
+	args: {
+		variant: "default",
+		innerText
+	}
+};
+
+export const DangerL1: Story = {
+	args: {
+		variant: "l1",
+		innerText
+	}
+};
+
+export const DangerL2: Story = {
+	args: {
+		variant: "l2",
+		innerText
+	}
+};
---
+++ src/lib/mangadex/componnents/theme/texts/span/stories/DefaultSpanOnlyLabel.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import "@fontsource/poppins/index.css";
+
+import DefaultSpan from "./DefaultSpanOnlyLabel.svelte";
+import MangadexThemeProviderForStory from "@mangadex/componnents/theme/MangadexThemeProviderForStory.svelte";
+
+const meta = {
+	decorators: [() => MangadexThemeProviderForStory],
+	title: "DefaultSpan",
+	component: DefaultSpan,
+	tags: ["autodocs"]
+} satisfies Meta<DefaultSpan>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const innerText =
+	"Reiciendis occaecati cum assumenda aut et ducimus repellendus. Fugit quibusdam id aut autem ut dolorem voluptas. Totam molestiae consequatur enim inventore quas.";
+
+export const Default: Story = {
+	args: {
+		innerText
+	}
+};
