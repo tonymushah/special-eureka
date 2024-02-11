2217d9ba12edcbe9ad40258a23c44c4d675dbbed
tonymushah <tonyramanantsalama@gmail.com>
2024-02-11 04:30:24 +0300

    Added Status Span and it's story

---
+++ src/lib/mangadex/componnents/theme/texts/span/StatusSpan.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let type: "red" | "green" | "yellow" | "blue" | "gray" | "purple";
+	$: red = type == "red";
+	$: green = type == "green";
+	$: yellow = type == "yellow";
+	$: blue = type == "blue";
+	$: gray = type == "gray";
+	$: purple = type == "purple";
+</script>
+
+<span class:blue class:gray class:green class:purple class:red class:yellow>
+	<slot />
+</span>
+
+<style lang="scss">
+	span {
+		font-size: var(--font-size);
+		font-family: var(--fonts);
+		color: var(--text-color);
+	}
+	.red {
+		color: var(--status-red);
+	}
+	.green {
+		color: var(--status-green);
+	}
+	.yellow {
+		color: var(--status-yellow);
+	}
+	.blue {
+		color: var(--status-blue);
+	}
+	.gray {
+		color: var(--status-gray);
+	}
+	.purple {
+		color: var(--status-purple);
+	}
+</style>
