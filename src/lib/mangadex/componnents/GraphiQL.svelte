343a937c266e0c198b95d802f48c289d0abf7ab4
tonymushah <tonyramanantsalama@gmail.com>
2024-02-12 09:45:36 +0300

    Fixed GraphiQL chunk loading

--- src/lib/mangadex/componnents/GraphiQL.svelte
+++ src/lib/mangadex/componnents/GraphiQL.svelte
@@ -1,21 +1,25 @@
 <script lang="ts">
-	import 'graphiql/graphiql.min.css';
-	import { createRoot, type Root } from 'react-dom/client';
-	import { onDestroy, onMount } from 'svelte';
-	import { GraphiQL } from 'graphiql';
-	import { invoke } from '@tauri-apps/api';
-	import type { Fetcher } from '@graphiql/toolkit';
-	import { pluginName } from '@mangadex/const';
+	import "graphiql/graphiql.min.css";
+	import { type Root } from "react-dom/client";
+	import { onDestroy, onMount } from "svelte";
+	import { invoke } from "@tauri-apps/api";
+	import type { Fetcher } from "@graphiql/toolkit";
+	import { pluginName } from "@mangadex/const";
 
 	const fetcher: Fetcher = async (params) => {
-        let [stringData, _] = await invoke<[string, boolean]>(`plugin:${pluginName}|graphql`, params);
-        return JSON.parse(stringData);
-    };
+		let [stringData, _] = await invoke<[string, boolean]>(
+			`plugin:${pluginName}|graphql`,
+			params
+		);
+		return JSON.parse(stringData);
+	};
 
-    let divRoot: HTMLElement | undefined;
+	let divRoot: HTMLElement | undefined;
 	let root: Root | undefined;
-	
-	onMount(function () {
+
+	onMount(async function () {
+		const { createRoot } = await import("react-dom/client");
+		const { GraphiQL } = await import("graphiql");
 		if (divRoot != undefined) {
 			if (root != undefined) {
 				root.unmount();
@@ -29,17 +33,16 @@
 			);
 		}
 	});
-    
-    onDestroy(function () {
-        if(root != undefined) root.unmount();
-    });
 
+	onDestroy(function () {
+		if (root != undefined) root.unmount();
+	});
 </script>
 
 <div bind:this={divRoot} />
 
 <style>
-    div {
-        height: 100vh;
-    }
+	div {
+		height: 100vh;
+	}
 </style>
