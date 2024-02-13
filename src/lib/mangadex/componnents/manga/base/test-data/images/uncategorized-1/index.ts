0c0cd67771534de1c45a5ed540b0089f41f5690e
tonymushah <tonyramanantsalama@gmail.com>
2024-02-13 21:16:35 +0300

    Added manga test cover image

---
+++ src/lib/mangadex/componnents/manga/base/test-data/images/uncategorized-1/index.ts
@@ -0,0 +1,7 @@
+import i1 from "./A3E6DE33EE6791A0C08DE9C653E35F761ADA336D.jpeg";
+import i2 from "./EC31B18675E076BC16CAEE7B8FF7237871F14EF4.jpeg";
+import i3 from "./EB80E0315B13200A8587C4490A6985B7E5B8B435.jpeg";
+
+export const volume1 = i1;
+export const volume3 = i2;
+export const volume5 = i3;
---
+++ src/lib/mangadex/componnents/manga/base/test-data/images/yuusha-party/index.ts
@@ -0,0 +1,3 @@
+import v7 from "./1e23d9b0-b200-4cd9-9eda-815319fb42cc.jpg";
+
+export const volume7 = v7;
