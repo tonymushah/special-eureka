def22eeb32f8f8b5f279581187d2435dd08c11a6
tonymushah <tonyramanantsalama@gmail.com>
2024-02-13 17:21:24 +0300

    Added `TagComponnent`

---
+++ src/lib/mangadex/componnents/tag/TagComponnent.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import is_tag_danger from "@mangadex/utils/is_tag_danger";
+	import { onMount } from "svelte";
+	import StatusBadge from "../theme/tag/StatusBadge.svelte";
+
+	export let id: string;
+	export let name: string;
+	let color: "red" | "gray" = "gray";
+	onMount(() => {
+		if (is_tag_danger(id)) {
+			color = "red";
+		}
+	});
+</script>
+
+<StatusBadge {color}>
+	{name}
+</StatusBadge>
