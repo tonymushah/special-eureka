5410714a29f89cc78c1e3861d3e5715d948939bf
tonymushah <tonyramanantsalama@gmail.com>
2024-02-13 14:15:01 +0300

    Migrated to `ButtonBase` in `StatusButton`

--- src/lib/mangadex/componnents/theme/buttons/status/Purple.svelte
+++ src/lib/mangadex/componnents/theme/buttons/status/Purple.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { createEventDispatcher } from "svelte";
+	import ButtonBase from "../base/ButtonBase.svelte";
 	export let type: "reset" | "submit" | "button" = "button";
 	createEventDispatcher<{
 		click: MouseEvent & {
@@ -9,30 +10,6 @@
 	export let style: string | undefined = undefined;
 </script>
 
-<button {style} on:click {type}>
+<ButtonBase --button-color={"var(--status-purple)"} {style} on:click {type}>
 	<slot />
-</button>
-
-<style lang="scss">
-	button {
-		font-size: var(--font-size);
-		min-height: 3em;
-		min-width: 3em;
-		border-radius: 0.25rem;
-		border: 0;
-		padding-left: 0.75rem;
-		padding-right: 0.75rem;
-		transition: filter 100ms ease-in-out;
-		display: flex;
-		align-items: center;
-		color: var(--text-color);
-		background-color: var(--status-purple);
-		font-family: var(--fonts);
-	}
-	button:hover {
-		filter: brightness(85%);
-	}
-	button:active {
-		filter: brightness(70%);
-	}
-</style>
+</ButtonBase>
