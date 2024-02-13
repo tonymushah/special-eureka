a27a832eb26c7742487b8854edd3a683d82556d7
tonymushah <tonyramanantsalama@gmail.com>
2024-02-13 14:30:24 +0300

    Added `IndicationButton` and it's stories

---
+++ src/lib/mangadex/componnents/theme/buttons/IndicationButtonOnlyLabel.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import "@fontsource/poppins/index.css";
+
+import IndicationButton from "./IndicationButtonOnlyLabel.svelte";
+import MangadexThemeProviderForStory from "../MangadexThemeProviderForStory.svelte";
+
+const meta = {
+	decorators: [() => MangadexThemeProviderForStory],
+	title: "MangaDex/theme/buttons/IndicationButton",
+	component: IndicationButton,
+	tags: ["autodocs"],
+	argTypes: {
+		variant: {
+			type: {
+				name: "enum",
+				value: ["blue"]
+			}
+		},
+		style: {
+			type: "string",
+			name: "Style",
+			description: "The button custom style"
+		}
+	}
+} satisfies Meta<IndicationButton>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const label = "Button";
+
+export const Blue: Story = {
+	args: {
+		variant: "blue",
+		label
+	}
+};
