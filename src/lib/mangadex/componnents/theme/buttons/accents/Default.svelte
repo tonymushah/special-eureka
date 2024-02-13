f17974fa1ea3b99a2aebdaebddb83887a36a2c92
tonymushah <tonyramanantsalama@gmail.com>
2024-02-13 14:06:09 +0300

    Migrated `ButtonBase` in `AccentsButtons`

--- src/lib/mangadex/componnents/theme/buttons/accents/Default.svelte
+++ src/lib/mangadex/componnents/theme/buttons/accents/Default.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { createEventDispatcher } from "svelte";
+	import ButtonBase from "../base/ButtonBase.svelte";
 	export let type: "reset" | "submit" | "button" = "button";
 	createEventDispatcher<{
 		click: MouseEvent & {
@@ -9,31 +10,15 @@
 	export let style: string | undefined = undefined;
 </script>
 
-<button {style} on:click {type}>
+<ButtonBase
+	--button-color="var(--accent)"
+	--button-hover="var(--accent-hover)"
+	--button-active="var(--accent-active)"
+	with_active
+	with_hover
+	{style}
+	on:click
+	{type}
+>
 	<slot />
-</button>
-
-<style lang="scss">
-	button {
-		font-size: var(--font-size);
-		min-height: 3em;
-		min-width: 3em;
-		border-radius: 0.25rem;
-		border: 0;
-		padding-left: 0.75rem;
-		padding-right: 0.75rem;
-		transition: background-color 100ms ease-in-out;
-		display: flex;
-		align-items: center;
-		border-color: var(--accent-active);
-		color: var(--text-color);
-		background-color: var(--accent);
-		font-family: var(--fonts);
-	}
-	button:hover {
-		background-color: var(--accent-hover);
-	}
-	button:active {
-		background-color: var(--accent-active);
-	}
-</style>
+</ButtonBase>
