21b9de401a2c99b57988fc9e707e6edf11e85c29
tonymushah <tonyramanantsalama@gmail.com>
2024-02-13 12:57:31 +0300

    Added AccentBadge stories

---
+++ src/lib/mangadex/componnents/theme/tag/AccentBadgeOnlyLabel.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import AccentBadge from "./AccentBadge.svelte";
+
+	export let label: string;
+	export let variant: "default" | "1" | "2" | "3" | "4" | "5" | "accent" | "accent-alt" =
+		"default";
+</script>
+
+<AccentBadge {variant}>
+	{label}
+</AccentBadge>
