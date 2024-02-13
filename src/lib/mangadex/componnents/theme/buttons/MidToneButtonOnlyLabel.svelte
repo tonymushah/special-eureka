a60126e0181ecc43b2b6d151e593321b4f5348cd
tonymushah <tonyramanantsalama@gmail.com>
2024-02-13 15:51:04 +0300

    Added `MidToneButton` and it's story

---
+++ src/lib/mangadex/componnents/theme/buttons/MidToneButtonOnlyLabel.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import MidToneButton from "./MidToneButton.svelte";
+
+	export let type: "reset" | "submit" | "button" = "button";
+	createEventDispatcher<{
+		click: MouseEvent & {
+			currentTarget: EventTarget & HTMLButtonElement;
+		};
+	}>();
+	export let style: string | undefined = undefined;
+	export let label: string;
+</script>
+
+<MidToneButton {type} {style} on:click>
+	{label}
+</MidToneButton>
