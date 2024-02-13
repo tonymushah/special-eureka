95fc191acfa3cffde0887c2e2542cd65bfd8875d
tonymushah <tonyramanantsalama@gmail.com>
2024-02-13 17:24:54 +0300

    Added `TagComponnents`

---
+++ src/lib/mangadex/componnents/tag/TagComponnents.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import is_tag_danger from "@mangadex/utils/is_tag_danger";
+	import { onMount } from "svelte";
+	import TagComponnent from "./TagComponnent.svelte";
+
+	type Tag = {
+		id: string;
+		name: string;
+	};
+	export let tags: Tag[];
+	onMount(() => {
+		tags.sort((a) => {
+			if (is_tag_danger(a.id)) {
+				return -1;
+			} else {
+				return 0;
+			}
+		});
+	});
+</script>
+
+{#each tags as { id, name }}
+	<TagComponnent {id} {name} />
+{/each}
