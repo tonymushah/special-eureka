556684ac46ff04d97a29b32d7ba5aea009288381
tonymushah <tonyramanantsalama@gmail.com>
2024-02-14 00:45:51 +0300

    Added `MangaElementBase1`

---
+++ src/lib/mangadex/componnents/manga/base/MangaElementBase1.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+	import DefaultSpan from "@mangadex/componnents/theme/texts/span/DefaultSpan.svelte";
+	import type { MangaStatus } from "@mangadex/gql/graphql";
+	import { createEventDispatcher, onMount } from "svelte";
+	import PublicationStatusTag from "../publicationStatusTag/PublicationStatusTag.svelte";
+	import Layout from "./base1/Layout.svelte";
+	createEventDispatcher<{
+		click: MouseEvent & {
+			currentTarget: EventTarget & HTMLButtonElement;
+		};
+	}>();
+	export let coverImage: string;
+	export let coverImageAlt: string;
+	export let title: string;
+	export let status: MangaStatus;
+	export let description: string;
+	let isLoadingImage = true;
+	let isError = false;
+	let isSuccess = false;
+	onMount(() => {
+		let timer: number | undefined = undefined;
+		const img = new Image();
+		img.addEventListener(
+			"load",
+			() => {
+				if (timer) {
+					window.clearTimeout(timer);
+				}
+				isLoadingImage = false;
+				isSuccess = true;
+			},
+			{
+				once: true
+			}
+		);
+		img.addEventListener(
+			"error",
+			() => {
+				if (timer) {
+					window.clearTimeout(timer);
+				}
+				if (!isSuccess) {
+					isLoadingImage = false;
+					isError = true;
+				}
+			},
+			{
+				once: true
+			}
+		);
+		img.src = coverImage;
+		timer = window.setTimeout(() => {
+			isLoadingImage = false;
+			isError = true;
+		});
+	});
+</script>
+
+<Layout on:click>
+	<div class="cover-image">
+		<img src={coverImage} alt={coverImageAlt} />
+	</div>
+	<div class="content">
+		<div class="title">
+			<p>{title}</p>
+		</div>
+		<div class="publication">
+			<DefaultSpan --font-size="12px"
+				>Publication : <PublicationStatusTag {status} /></DefaultSpan
+			>
+		</div>
+		<div class="description">
+			<p>
+				{description}
+			</p>
+		</div>
+	</div>
+</Layout>
+
+<style lang="scss">
+	div.cover-image {
+		grid-area: cover;
+	}
+	div.cover-image > img {
+		height: 160px;
+		width: 100px;
+		object-fit: cover;
+	}
+	div.title {
+		font-weight: 800;
+		font-size: 16px;
+	}
+	div.title > p {
+		margin: 2px;
+		height: 3em;
+		overflow: clip;
+		text-overflow: ellipsis;
+	}
+	div.publication {
+		display: flex;
+	}
+	div.content {
+		padding-left: 12px;
+		grid-area: content;
+		text-overflow: ellipsis;
+		text-align: start;
+		display: flex;
+		flex-direction: column;
+		align-items: start;
+		justify-items: center;
+		gap: 8px;
+	}
+	div.description {
+		font-size: 12px;
+	}
+	div.description > p {
+		height: 4.5em;
+		margin: 2px;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+</style>
---
+++ src/lib/mangadex/componnents/manga/base/base1/Layout.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import ButtonBase from "@mangadex/componnents/theme/buttons/base/ButtonBase.svelte";
+	import { createEventDispatcher } from "svelte";
+
+	createEventDispatcher<{
+		click: MouseEvent & {
+			currentTarget: EventTarget & HTMLButtonElement;
+		};
+	}>();
+</script>
+
+<ButtonBase
+	type="button"
+	on:click
+	isBase={false}
+	with_active
+	with_hover
+	noPadding
+	--button-color={"var(--accent-l3)"}
+	--button-hover={"var(--accent-l3-hover)"}
+	--button-active={"var(--accent-l3-active)"}
+>
+	<div class="layout">
+		<slot />
+	</div>
+</ButtonBase>
+
+<style lang="scss">
+	div.layout {
+		width: 19em;
+		display: grid;
+		grid-template-areas: "cover content content";
+		grid-column-gap: 3;
+		grid-row-gap: 1;
+		padding-right: 10px;
+		height: 160px;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		border-radius: 0.25em;
+	}
+</style>
