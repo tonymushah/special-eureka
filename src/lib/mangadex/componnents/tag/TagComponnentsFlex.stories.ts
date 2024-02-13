93f33186e80dbf280e92901cdbc71e3f08fc3dda
tonymushah <tonyramanantsalama@gmail.com>
2024-02-13 18:16:12 +0300

    Added `TagComponnentsFlex` and it's stories

---
+++ src/lib/mangadex/componnents/tag/TagComponnentsFlex.stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import "@fontsource/poppins/index.css";
+
+import TagComponnentsFlex from "./TagComponnentsFlex.svelte";
+import MangadexThemeProviderForStory from "@mangadex/componnents/theme/MangadexThemeProviderForStory.svelte";
+import { data3 } from "./testDatas";
+
+const meta = {
+	decorators: [() => MangadexThemeProviderForStory],
+	title: "MangaDex/tag/TagComponnentsFlex",
+	component: TagComponnentsFlex,
+	tags: ["autodocs"]
+} satisfies Meta<TagComponnentsFlex>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const tags = data3.map((d) => ({
+	id: d.id,
+	name: d.attributes.name.en
+}));
+
+export const Default: Story = {
+	args: {
+		tags
+	}
+};
---
+++ src/lib/mangadex/componnents/tag/TagComponnentsFlex.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import TagComponnents from "./TagComponnents.svelte";
+
+	type Tag = {
+		id: string;
+		name: string;
+	};
+	export let tags: Tag[];
+</script>
+
+<div>
+	<TagComponnents {tags} />
+</div>
+
+<style lang="scss">
+	div {
+		display: flex;
+		gap: 6px;
+		flex-wrap: wrap;
+	}
+</style>
