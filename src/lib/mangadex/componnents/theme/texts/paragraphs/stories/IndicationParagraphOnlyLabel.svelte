c89bd1f09a30497cd1b9d5ee3b5058a42375827e
tonymushah <tonyramanantsalama@gmail.com>
2024-02-13 13:28:54 +0300

    Added `IndicationParagraph` and it's stories

---
+++ src/lib/mangadex/componnents/theme/texts/paragraphs/stories/IndicationParagraphOnlyLabel.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import IndicationParagraph from "../IndicationParagraph.svelte";
+	export let type: "blue" = "blue";
+	export let innerText: string;
+</script>
+
+<IndicationParagraph {type}>
+	{innerText}
+</IndicationParagraph>
