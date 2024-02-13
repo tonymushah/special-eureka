3ae22822c3f3209b8b41c23fa8d282d359cd52ac
tonymushah <tonyramanantsalama@gmail.com>
2024-02-13 13:12:36 +0300

    Added `IndicationBlue`

---
+++ src/lib/mangadex/componnents/theme/tag/indication/Blue.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Badge } from "@svelteuidev/core";
+</script>
+
+<Badge
+	style={"background-color: var(--indication-blue); color: var(--text-color); font-family: var(--fonts)"}
+>
+	<slot />
+</Badge>
