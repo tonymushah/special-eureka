e87c636d25155acf3baa6dba70c8b23a2c35d0cd
tonymushah <tonyramanantsalama@gmail.com>
2024-02-13 15:37:38 +0300

    Fixed `/mangadex/+page.svelte`

--- src/routes/mangadex/+page.svelte
+++ src/routes/mangadex/+page.svelte
@@ -1,38 +1,134 @@
 <script>
-	import { Text, Title } from "@svelteuidev/core";
-
-
+	import Title from "@mangadex/componnents/theme/texts/title/Title.svelte";
+	import Text from "@mangadex/componnents/theme/texts/paragraphs/DefaultParagraph.svelte";
 </script>
 
-<Title>
-    Mangadex Home
-</Title>
+<Title>Mangadex Home</Title>
 
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
 
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
 
-
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
-<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus dicta quisquam assumenda consequatur.</Text>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
+<Text
+	>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis qui ipsa voluptas
+	officia sequi animi explicabo voluptatem! Illo culpa sequi porro! Iure, voluptatem omnis minus
+	dicta quisquam assumenda consequatur.</Text
+>
