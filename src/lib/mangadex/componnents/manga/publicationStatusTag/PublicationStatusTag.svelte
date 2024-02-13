51c1f2ba5b5af1e1392eab66fb827aa3edeba63d
tonymushah <tonyramanantsalama@gmail.com>
2024-02-13 16:49:37 +0300

    Added `PublicationStatusTag` and it's story

---
+++ src/lib/mangadex/componnents/manga/publicationStatusTag/PublicationStatusTag.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import StatusBadge from "@mangadex/componnents/theme/tag/StatusBadge.svelte";
+	import { MangaStatus } from "@mangadex/gql/graphql";
+	import make_first_upper_case from "@mangadex/utils/make_first_upper_case";
+	import { onMount } from "svelte";
+	export let status: MangaStatus;
+	let color: "blue" | "gray" | "green" | "purple" | "red" | "yellow" = "gray";
+	onMount(() => {
+		switch (status) {
+			case MangaStatus.Cancelled:
+				color = "red";
+				break;
+			case MangaStatus.Completed:
+				color = "blue";
+				break;
+			case MangaStatus.Hiatus:
+				color = "purple";
+				break;
+			case MangaStatus.Ongoing:
+				color = "green";
+			default:
+				break;
+		}
+	});
+	$: _status = make_first_upper_case(status.toLowerCase());
+</script>
+
+<StatusBadge {color}>
+	{_status}
+</StatusBadge>
