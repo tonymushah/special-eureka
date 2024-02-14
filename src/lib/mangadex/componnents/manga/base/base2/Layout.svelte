935dda0643b49684933327775ad962e8a63246a6
tonymushah <tonyramanantsalama@gmail.com>
2024-02-14 12:32:13 +0300

    Added `MangaElementBase2`

---
+++ src/lib/mangadex/componnents/manga/base/base2/Layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import ButtonBase from "@mangadex/componnents/theme/buttons/base/ButtonBase.svelte";
+	import { createEventDispatcher } from "svelte";
+
+	createEventDispatcher<{
+		click: MouseEvent & {
+			currentTarget: EventTarget & HTMLButtonElement;
+		};
+	}>();
+</script>
+
+<ButtonBase
+	on:click
+	isBase={false}
+	with_active
+	with_hover
+	noPadding
+	--button-color={"var(--accent-l3)"}
+	--button-hover={"var(--accent-l3-hover)"}
+	--button-active={"var(--accent-l3-active)"}
+>
+	<div class="layout">
+		<slot />
+	</div>
+</ButtonBase>
+
+<style lang="scss">
+	div.layout {
+		min-width: 10em;
+		max-height: 11em;
+		display: flex;
+		flex-direction: row;
+		overflow-y: hidden;
+		border-radius: 0.25rem;
+	}
+</style>
