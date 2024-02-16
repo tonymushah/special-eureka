f923db8ede74f7a9df32b0726702502612fa5afb
tonymushah <tonyramanantsalama@gmail.com>
2024-02-16 14:12:51 +0300

    Reswitched to `Badge` for `TagBase`

--- src/lib/mangadex/componnents/theme/tag/base/TagBase.svelte
+++ src/lib/mangadex/componnents/theme/tag/base/TagBase.svelte
@@ -1,25 +1,18 @@
 <script lang="ts">
-	import ButtonBase from "../../buttons/base/ButtonBase.svelte";
 	import { createEventDispatcher } from "svelte";
-
-	createEventDispatcher<{
+	import { Badge } from "@svelteuidev/core";
+	const dispatch = createEventDispatcher<{
 		click: MouseEvent & {
 			currentTarget: EventTarget & HTMLButtonElement;
 		};
 	}>();
 </script>
 
-<ButtonBase on:click noPadding --button-color="var(--tag-color)" --text-color="var(--text-color)">
-	<span>
-		<slot />
-	</span>
-</ButtonBase>
-
-<style lang="scss">
-	span {
-		font-size: var(--tag-font-size);
-		font-weight: var(--tag-font-weight);
-		font-family: var(--fonts);
-		padding: var(--tag-padding);
-	}
-</style>
+<Badge
+	style={"font-size: var(--tag-font-size); font-weight: var(--tag-font-weight); background-color: var(--tag-color); color: var(--text-color); font-family: var(--fonts); padding: var(--tag-padding);"}
+	on:click={({ detail }) => {
+		dispatch("click", detail);
+	}}
+>
+	<slot />
+</Badge>
