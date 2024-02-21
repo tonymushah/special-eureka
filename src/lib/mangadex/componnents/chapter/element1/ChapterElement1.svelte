562144cecaded81401712890ccb0f95e954533e8
tonymushah <tonyramanantsalama@gmail.com>
2024-02-21 11:27:24 +0300

    Added TimeAgo on ChapterElement1

--- src/lib/mangadex/componnents/chapter/element1/ChapterElement1.svelte
+++ src/lib/mangadex/componnents/chapter/element1/ChapterElement1.svelte
@@ -12,6 +12,8 @@
 		XIcon
 	} from "svelte-feather-icons";
 	import MangaDexFlagIcon from "@mangadex/componnents/FlagIcon.svelte";
+	import { onDestroy, onMount } from "svelte";
+	import { render as timeRender, cancel as timeCancel } from "timeago.js";
 	type Group = {
 		id: string;
 		name: string;
@@ -28,6 +30,13 @@
 	export let haveBeenRead: boolean = true;
 	export let download_state: ChapterDownloadState;
 	export let comments: number;
+	let timeago: HTMLTimeElement;
+	onMount(() => {
+		timeRender(timeago);
+	});
+	onDestroy(() => {
+		timeCancel(timeago);
+	});
 </script>
 
 <div class="border">
@@ -47,7 +56,7 @@
 			<div>
 				<MangaDexFlagIcon bind:lang />
 			</div>
-			<div>
+			<div role="button" on:click={() => {}} tabindex={1} on:keypress={() => {}}>
 				{#if !haveBeenRead}
 					<EyeIcon />
 				{:else}
@@ -70,7 +79,7 @@
 		</div>
 		<div class="date-uploader">
 			<p>
-				<time datetime={upload_date.toDateString()} />
+				<time datetime={upload_date.toDateString()} bind:this={timeago} />
 			</p>
 			<a href="/" class="uploader">
 				{uploader.name}
@@ -97,16 +106,16 @@
 		display: flex;
 		flex-direction: row;
 		align-self: start;
-		gap: 10px;
+		column-gap: 20px;
 		color: var(--text-color);
 		padding: 5px;
 		transition: background-color 300ms ease-in-out;
 		border-radius: 0.5rem;
 	}
 	.layout:not(.haveBeenRead) {
-		border-color: var(--indication-blue);
 		border-style: solid;
 		border-width: 0px 0px 0px 5px;
+		border-color: var(--indication-blue);
 	}
 	.layout > div {
 		display: flex;
@@ -117,7 +126,7 @@
 		margin: 0px;
 	}
 	.title-groups {
-		flex-grow: 2;
+		flex-grow: 3;
 	}
 	.groups {
 		display: flex;
@@ -125,6 +134,7 @@
 	}
 	.reading-number-comments {
 		align-items: center;
+		flex-grow: 0.15;
 	}
 	.flag-reading-state {
 		align-items: center;
@@ -156,5 +166,11 @@
 	.uploader {
 		color: var(--text-color);
 		text-decoration: none;
+	}
+	.date-uploader > p {
+		margin: 0px;
+	}
+	.date-uploader {
+		flex-grow: 0.15;
 	}
 </style>
