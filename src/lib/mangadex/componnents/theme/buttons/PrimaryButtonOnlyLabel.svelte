471705de2d459eadb6a8f24dbab84a2d1c953a86
tonymushah <tonyramanantsalama@gmail.com>
2024-02-23 20:16:09 +0300

    Added `export let isBase = false;` in `PrimaryButton`

--- src/lib/mangadex/componnents/theme/buttons/PrimaryButtonOnlyLabel.svelte
+++ src/lib/mangadex/componnents/theme/buttons/PrimaryButtonOnlyLabel.svelte
@@ -2,17 +2,18 @@
 	import { createEventDispatcher } from "svelte";
 	import PrimaryButton from "./PrimaryButton.svelte";
 
-    export let variant: "default" | "1" | "2" = "default";
-    createEventDispatcher<{
-        click: MouseEvent & {
-            currentTarget: EventTarget & HTMLButtonElement
-        }
-    }>();
-    export let type: "reset" | "submit" | "button" = "button";
-    export let label: string;
-    export let style: string | undefined = undefined;
+	export let variant: "default" | "1" | "2" = "default";
+	createEventDispatcher<{
+		click: MouseEvent & {
+			currentTarget: EventTarget & HTMLButtonElement;
+		};
+	}>();
+	export let type: "reset" | "submit" | "button" = "button";
+	export let label: string;
+	export let style: string | undefined = undefined;
+	export let isBase = false;
 </script>
 
-<PrimaryButton on:click {variant} {style} {type}>
-    {label}
+<PrimaryButton on:click {variant} {style} {type} {isBase}>
+	{label}
 </PrimaryButton>
