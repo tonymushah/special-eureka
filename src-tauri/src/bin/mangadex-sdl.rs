09e596b23d68e2b93b446ac4d215e952492ef5ea
tonymushah <tonyramanantsalama@gmail.com>
2024-02-25 21:55:52 +0300

    Removed mangadex-sdl bin

--- src-tauri/src/bin/mangadex-sdl.rs
+++
@@ -1,7 +0,0 @@
-use std::path::Path;
-
-fn main() {
-    tauri_plugin_speu_mangadex::MangadexDesktopApi::default()
-        .export_sdl(Path::new("../src/lib/mangadex/schemas.graphqls").to_path_buf())
-        .unwrap()
-}
