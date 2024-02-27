a978af8ee39aa2d94c6add3b4424311678a755b8
tonymushah <tonyramanantsalama@gmail.com>
2024-02-27 12:44:29 +0300

    Added ExtLink.svelte

---
+++ src/lib/mangadex/componnents/theme/links/ExtLink.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { open } from "@tauri-apps/api/shell";
+
+	export let href: string;
+</script>
+
+<span
+	role="link"
+	tabindex="0"
+	on:keydown={(e) => {
+		if (document.activeElement === e.currentTarget) {
+			if (e.key == "Enter") {
+				open(href);
+			}
+		}
+	}}
+	on:click={(e) => {
+		open(href);
+	}}
+>
+	<slot />
+</span>
+
+<style lang="scss">
+	span {
+		transition: color 300ms ease-in-out;
+		color: var(--primary);
+	}
+	span:hover {
+		color: var(--primary-l1);
+		text-decoration: underline;
+	}
+</style>
