f3394e30eae4df1ba2eaabb3d6e386cf2fc94e26
tonymushah <tonyramanantsalama@gmail.com>
2024-02-27 12:47:01 +0300

    Added `MarkDown.svelte`

---
+++ src/lib/mangadex/componnents/markdown/Markdown.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import SvelteMarkdown from "svelte-markdown";
+	import ExtLink from "../theme/links/ExtLink.svelte";
+	export let source: string;
+</script>
+
+<SvelteMarkdown
+	{source}
+	renderers={{
+		link: ExtLink
+	}}
+/>
