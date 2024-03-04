589e2a79f6c4a8aa436ebaf617e69cef07b2c954
tonymushah <tonyramanantsalama@gmail.com>
2024-03-04 15:20:31 +0300

    Added `MangaElementBase5WithReadableCoverImage.svelte`

---
+++ src/lib/mangadex/componnents/manga/base/base5/Layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let isHover: boolean;
+</script>
+
+<div
+	role="document"
+	class="layout"
+	on:mouseenter={() => {
+		isHover = true;
+	}}
+	on:mouseleave={() => {
+		isHover = false;
+	}}
+>
+	<slot />
+</div>
+
+<style lang="scss">
+	.layout {
+		width: var(--width);
+		height: var(--height);
+		overflow: hidden;
+		border-radius: 0.25rem;
+		color: var(--text-color);
+	}
+</style>
---
+++ src/lib/mangadex/componnents/manga/base/base5/MangaElementBase5WithReadableCoverImage.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import Content from "./Content.svelte";
+	import type { Readable } from "svelte/store";
+	import Skeleton from "@mangadex/componnents/theme/loader/Skeleton.svelte";
+	import Layout from "./Layout.svelte";
+
+	const dispatch = createEventDispatcher<{
+		readClick: MouseEvent & {
+			currentTarget: EventTarget & HTMLButtonElement;
+		};
+		moreInfoClick: MouseEvent & {
+			currentTarget: EventTarget & HTMLButtonElement;
+		};
+	}>();
+	export let coverImage: Readable<string | undefined>;
+	export let coverImageAlt: string;
+	export let title: string;
+	export let description: string;
+	let isHover = false;
+	$: image = $coverImage;
+</script>
+
+<Layout bind:isHover>
+	{#if image}
+		<img src={image} alt={coverImageAlt} />
+	{:else}
+		<Skeleton height={"var(--s-h)"} width={"var(--s-w)"} />
+	{/if}
+	<Content {title} {description} {isHover} on:moreInfoClick on:readClick />
+</Layout>
+
+<style lang="scss">
+	:root {
+		--transition-duration: 500ms;
+		--transition-timing-function: ease-in-out;
+		--width: 16em;
+		--height: 23em;
+		--height-c: 24em;
+		--s-w: var(--width);
+		--s-h: var(--height);
+	}
+	img {
+		width: var(--width);
+		height: var(--height);
+		object-fit: cover;
+		border-radius: 0.25rem;
+	}
+</style>
