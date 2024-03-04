56d6581c5b2f8dec252030b78f59e066b2d0321f
tonymushah <tonyramanantsalama@gmail.com>
2024-03-04 13:01:22 +0300

    Added `manga-title-to-lang-map.ts`

---
+++ src/lib/mangadex/utils/lang/manga-title-to-lang-map.ts
@@ -0,0 +1,12 @@
+import type { LangDataMap } from ".";
+
+export default function manga_title_to_lang_map(data: Record<string, string>): LangDataMap {
+    const res: Map<string, string> = new Map();
+    for (const key in data) {
+        if (Object.prototype.hasOwnProperty.call(data, key)) {
+            const element = data[key];
+            res.set(key, element);
+        }
+    }
+    return res;
+}
