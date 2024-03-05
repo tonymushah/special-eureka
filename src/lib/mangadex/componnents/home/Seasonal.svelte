e39e39d96b15c80987e6e85bda3b7b1b7f34928f
tonymushah <tonyramanantsalama@gmail.com>
2024-03-05 04:08:45 +0300

    Added `Seasonal.svelte` and its query

---
+++ src/lib/mangadex/componnents/home/Seasonal.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import TopTitle from "./utils/TopTitle.svelte";
+</script>
+
+<TopTitle label="Seasonal" fetching={false} />
---
+++ src/lib/mangadex/componnents/home/seasonal/query.ts
@@ -0,0 +1,31 @@
+import { graphql } from "@mangadex/gql";
+
+const query = graphql(/** GraphQL */`
+	query seasonal {
+		home {
+			seasonal {
+				id
+				relationships {
+					titles {
+						id
+						attributes {
+							title
+							altTitles
+							description
+						}
+						relationships {
+							coverArt {
+								id
+								attributes {
+									fileName
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+`);
+
+export default query;
