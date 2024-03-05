94dfd490482d547ecdaa78ec7f0cda2ddce7cb12
tonymushah <tonyramanantsalama@gmail.com>
2024-03-05 17:00:23 +0300

    Added MangaElementBase3 layout

--- src/lib/mangadex/componnents/manga/base/MangaElementBase3.svelte
+++ src/lib/mangadex/componnents/manga/base/MangaElementBase3.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import ButtonBase from "@mangadex/componnents/theme/buttons/base/ButtonBase.svelte";
 	import { createEventDispatcher } from "svelte";
 	import Content from "./base3/Content.svelte";
+	import Layout from "./base3/Layout.svelte";
 
 	createEventDispatcher<{
 		click: MouseEvent & {
@@ -13,33 +13,16 @@
 	export let title: string;
 </script>
 
-<ButtonBase
-	on:click
-	isBase={false}
-	with_hover
-	noPadding
-	--button-color={"var(--accent-l3)"}
-	--button-hover={"var(--accent-l3-hover)"}
-	--button-active={"var(--accent-l-active)"}
->
-	<div class="layout">
-		<img src={coverImage} alt={coverImageAlt} />
-		<Content {title} />
-	</div>
-</ButtonBase>
+<Layout>
+	<img src={coverImage} alt={coverImageAlt} />
+	<Content {title} />
+</Layout>
 
 <style lang="scss">
 	img {
 		width: 10em;
 		height: 15em;
 		object-fit: cover;
-		border-radius: 0.25rem;
-	}
-
-	.layout {
-		width: 10em;
-		height: 15em;
-		overflow: hidden;
 		border-radius: 0.25rem;
 	}
 </style>
---
+++ src/lib/mangadex/componnents/manga/base/base3/Layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import ButtonBase from "@mangadex/componnents/theme/buttons/base/ButtonBase.svelte";
+	import { createEventDispatcher } from "svelte";
+	createEventDispatcher<{
+		click: MouseEvent & {
+			currentTarget: EventTarget & HTMLButtonElement;
+		};
+	}>();
+</script>
+
+<ButtonBase
+	on:click
+	isBase={false}
+	with_hover
+	noPadding
+	--button-color={"var(--accent-l3)"}
+	--button-hover={"var(--accent-l3-hover)"}
+	--button-active={"var(--accent-l-active)"}
+>
+	<div class="layout">
+		<slot />
+	</div>
+</ButtonBase>
+
+<style lang="scss">
+	.layout {
+		width: 10em;
+		height: 15em;
+		overflow: hidden;
+		border-radius: 0.25rem;
+	}
+</style>
