0206347780767ba1e608c42ee351b0c45c9b7a86
tonymushah <tonyramanantsalama@gmail.com>
2024-03-05 08:35:19 +0300

    added seasonal content

--- src/lib/mangadex/componnents/home/Seasonal.svelte
+++ src/lib/mangadex/componnents/home/Seasonal.svelte
@@ -1,5 +1,42 @@
 <script lang="ts">
 	import TopTitle from "./utils/TopTitle.svelte";
+	import query from "./seasonal/query";
+	import specialQueryStore from "@mangadex/utils/gql-stores/specialQueryStore";
+	import { getContextClient } from "@urql/svelte";
+	import { onMount } from "svelte";
+	import HomeErrorComponnent from "./utils/HomeErrorComponnent.svelte";
+	import PopularTitleSpinner from "./utils/PopularTitleSpinner.svelte";
+	import Content from "./seasonal/Content.svelte";
+
+	const client = getContextClient();
+	const query_store = specialQueryStore({
+		client,
+		query,
+		variable: {}
+	});
+	$: error = $query_store?.error;
+	$: data = $query_store?.data;
+	const isFetching = query_store.isFetching;
+	$: fetching = $isFetching;
+	onMount(async () => {
+		await query_store.execute();
+	});
 </script>
 
-<TopTitle label="Seasonal" fetching={false} />
+<TopTitle
+	label="Seasonal"
+	bind:fetching
+	on:refresh={async () => {
+		if (!fetching) {
+			await query_store.execute();
+		}
+	}}
+/>
+
+{#if data}
+	<Content {data} />
+{:else if error}
+	<HomeErrorComponnent {error} label="Oops! Something happens when loading the seasonal manga" />
+{:else}
+	<PopularTitleSpinner --height="20em" />
+{/if}
---
+++ src/lib/mangadex/componnents/home/seasonal/Content.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import type { SeasonalQuery } from "@mangadex/gql/graphql";
+
+	export let data: SeasonalQuery;
+</script>
