8afcf84c51af5e17850c324a37533d96061a6fa0
tonymushah <tonyramanantsalama@gmail.com>
2024-03-06 19:24:53 +0300

    Added `init_states`

---
+++ src-tauri/mangadex/src/plugin_setup/init_states.rs
@@ -0,0 +1,17 @@
+use tauri::Runtime;
+
+use crate::{
+    app_state::OfflineAppState, store::get_store_builder, utils::store::MangaDexStoreState,
+};
+
+use super::{init_client_state::init_client_state, init_watches_states::init_watches_states};
+
+pub fn init_states<R: Runtime>(app: &tauri::AppHandle<R>) -> tauri::plugin::Result<()> {
+    let mut store = get_store_builder(app.app_handle())?.build();
+    let _ = store.load();
+    init_watches_states(app, &store)?;
+    init_client_state(app, &store)?;
+    app.manage(OfflineAppState::default());
+    app.manage(MangaDexStoreState::new_from_store(store));
+    Ok(())
+}
