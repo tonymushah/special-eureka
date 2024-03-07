da741c0f30d90044d924fde7b9ecae1fd72e943c
tonymushah <tonyramanantsalama@gmail.com>
2024-03-07 15:40:09 +0300

    Added `RecentlyAdded.svelte` base

--- src/routes/mangadex/+page.svelte
+++ src/routes/mangadex/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import LatestUpdates from "@mangadex/componnents/home/LatestUpdates.svelte";
 	import PopularTitles from "@mangadex/componnents/home/PopularTitles.svelte";
+	import RecentlyAdded from "@mangadex/componnents/home/RecentlyAdded.svelte";
 	import Seasonal from "@mangadex/componnents/home/Seasonal.svelte";
 	import StaffPicks from "@mangadex/componnents/home/StaffPicks.svelte";
 	import MidToneLine from "@mangadex/componnents/theme/lines/MidToneLine.svelte";
@@ -26,6 +27,12 @@
 
 <div class="seasonal">
 	<Seasonal />
+</div>
+
+<MidToneLine />
+
+<div class="recently-added">
+	<RecentlyAdded />
 </div>
 
 <style lang="scss">
