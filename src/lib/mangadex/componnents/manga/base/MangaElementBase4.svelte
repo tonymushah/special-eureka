c186057ce63b9442214871ddca2e9464966444ea
tonymushah <tonyramanantsalama@gmail.com>
2024-03-07 16:12:27 +0300

    Refactored MangaElementBase4 `img` to `./base4/CoverImage.svelte`

--- src/lib/mangadex/componnents/manga/base/MangaElementBase4.svelte
+++ src/lib/mangadex/componnents/manga/base/MangaElementBase4.svelte
@@ -2,6 +2,7 @@
 	import Layout from "./base4/Layout.svelte";
 	import Content from "./base4/Content.svelte";
 	import { createEventDispatcher } from "svelte";
+	import CoverImage from "./base4/CoverImage.svelte";
 
 	createEventDispatcher<{
 		click: MouseEvent & {
@@ -14,15 +15,6 @@
 </script>
 
 <Layout on:click>
-	<img src={coverImage} alt={coverImageAlt} />
+	<CoverImage {coverImage} {coverImageAlt} />
 	<Content {title} />
 </Layout>
-
-<style lang="scss">
-	img {
-		width: 9.5em;
-		height: 12.5em;
-		object-fit: cover;
-		border-radius: 0.25rem;
-	}
-</style>
---
+++ src/lib/mangadex/componnents/manga/base/base4/CoverImage.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let coverImage: string;
+	export let coverImageAlt: string;
+</script>
+
+<img src={coverImage} alt={coverImageAlt} />
+
+<style lang="scss">
+	img {
+		width: 9.5em;
+		height: 12.5em;
+		object-fit: cover;
+		border-radius: 0.25rem;
+	}
+</style>
