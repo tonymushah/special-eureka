dbd83eba9a9376356fb39d4d8136817720795f47
tonymushah <tonyramanantsalama@gmail.com>
2024-03-13 19:49:35 +0300

    Added props and storybook test data

---
+++ src/lib/mangadex/componnents/manga/page/top-info/MangaPageTopInfo.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { writable, type Readable } from "svelte/store";
+	import { setTopCoverAltContextStore, setTopCoverContextStore } from "./context";
+	import type { Tag } from "@mangadex/utils/types/Tag";
+	import type { MangaStatus } from "@mangadex/gql/graphql";
+
+	type Author = {
+		id: string;
+		name: string;
+	};
+	export let id: string;
+	export let title: string;
+	export let altTitle: string | undefined = undefined;
+	export let coverImage: Readable<string | undefined>;
+	export let coverImageAlt: string;
+	export let authors: Author[];
+	export let tags: Tag[];
+	export let status: MangaStatus;
+	export let year: number | undefined = undefined;
+	export let description: string | undefined = undefined;
+
+	setTopCoverContextStore(coverImage);
+	setTopCoverAltContextStore(coverImageAlt);
+</script>
---
+++ src/lib/mangadex/componnents/manga/page/top-info/stories/MangaPageTopInfo.stories.ts
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import "@fontsource/poppins/latin.css";
+
+import TopInfo from "../MangaPageTopInfo.svelte";
+import MangadexThemeProviderForStory from "@mangadex/componnents/theme/MangadexThemeProviderForStory.svelte";
+import { volume7 } from "@mangadex/test-data/images/yuusha-party";
+import { id, attributes, author } from "./data/b4c93297-b32f-4f90-b619-55456a38b0aa";
+import manga_altTitle_to_lang_map from "@mangadex/utils/lang/record-to-map/manga-altTitle-to-lang-map";
+import get_value_and_random_if_undefined from "@mangadex/utils/lang/get_value_and_random_if_undefined";
+import { writable } from "svelte/store";
+import { MangaStatus } from "@mangadex/gql/graphql";
+
+const altTitle = manga_altTitle_to_lang_map(attributes.altTitles);
+
+const meta = {
+	decorators: [() => MangadexThemeProviderForStory],
+	title: "MangaDex/manga/page/TopInfo",
+	component: TopInfo,
+	tags: ["autodocs"]
+} satisfies Meta<TopInfo>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	args: {
+		id,
+		title: attributes.title.en,
+		altTitle: get_value_and_random_if_undefined(altTitle, "en"),
+		coverImage: writable(volume7),
+		coverImageAlt: volume7,
+		authors: [
+			{
+				id: author.id,
+				name: author.attributes.name
+			}
+		],
+		tags: attributes.tags.map((t) => ({
+			id: t.id,
+			name: t.attributes.name.en
+		})),
+		status: MangaStatus.Ongoing,
+		description: attributes.description.en
+	}
+};
