344d672547f26d92180a8dbeb8a8b20bf659ba30
tonymushah <tonyramanantsalama@gmail.com>
2024-03-14 13:11:06 +0300

    mangadex-theme to Readable

--- src/lib/mangadex/componnents/theme/MangadexThemeProviderBase.svelte
+++ src/lib/mangadex/componnents/theme/MangadexThemeProviderBase.svelte
@@ -2,56 +2,58 @@
 	import { buildSvelteUITheme, type MangadexTheme } from "@mangadex/theme";
 	import SomeDiv from "./SomeDiv.svelte";
 	import { SvelteUIProvider } from "@svelteuidev/core";
-	import { setMangaDexThemeContext } from "@mangadex/utils/contexts/theme";
+	import { setMangaDexThemeContextWritable } from "@mangadex/utils/contexts";
+	import { writable } from "svelte/store";
 
 	export let theme: MangadexTheme;
 	export let fonts = "Poppins";
-	setMangaDexThemeContext(theme);
-	$: svelte_ui_theme = buildSvelteUITheme(theme);
+	let theme_store = writable(theme);
+	setMangaDexThemeContextWritable(theme_store);
+	$: svelte_ui_theme = buildSvelteUITheme($theme_store);
 </script>
 
 <SomeDiv
 	--font-size={"16px"}
 	--fonts={fonts}
-	--text-color={theme.textColor}
-	--main-background={theme.mainBackground}
-	--accent={theme.accents.default.default}
-	--accent-hover={theme.accents.default.hover}
-	--accent-active={theme.accents.default.active}
-	--accent-l1={theme.accents.l1.default}
-	--accent-l1-hover={theme.accents.l1.hover}
-	--accent-l1-active={theme.accents.l1.default}
-	--accent-l2={theme.accents.l2.default}
-	--accent-l2-hover={theme.accents.l2.hover}
-	--accent-l2-active={theme.accents.l2.default}
-	--accent-l3={theme.accents.l3.default}
-	--accent-l3-hover={theme.accents.l3.hover}
-	--accent-l3-active={theme.accents.l3.default}
-	--accent-l4={theme.accents.l4.default}
-	--accent-l4-hover={theme.accents.l4.hover}
-	--accent-l4-active={theme.accents.l4.default}
-	--accent-l5={theme.accents.l5.default}
-	--accent-l5-hover={theme.accents.l5.hover}
-	--accent-l5-active={theme.accents.l5.default}
-	--mid-tone={theme.mid_tone}
-	--contrast-l1={theme.contrast.l1}
-	--scrollbar-color={theme.scrollbar.default}
-	--scrollbar-color-hover={theme.scrollbar.default}
-	--button-accent={theme.button.default}
-	--button-accent-alt={theme.button.alternate}
-	--primary={theme.primary.primary}
-	--primary-l1={theme.primary.primary1}
-	--primary-l2={theme.primary.primary2}
-	--status-red={theme.status.red}
-	--status-green={theme.status.green}
-	--status-yellow={theme.status.yellow}
-	--status-blue={theme.status.blue}
-	--status-gray={theme.status.grey}
-	--status-purple={theme.status.purple}
-	--indication-blue={theme.indication.blue}
-	--danger={theme.danger.default}
-	--danger-l1={theme.danger.l1}
-	--danger-l2={theme.danger.l2}
+	--text-color={$theme_store.textColor}
+	--main-background={$theme_store.mainBackground}
+	--accent={$theme_store.accents.default.default}
+	--accent-hover={$theme_store.accents.default.hover}
+	--accent-active={$theme_store.accents.default.active}
+	--accent-l1={$theme_store.accents.l1.default}
+	--accent-l1-hover={$theme_store.accents.l1.hover}
+	--accent-l1-active={$theme_store.accents.l1.default}
+	--accent-l2={$theme_store.accents.l2.default}
+	--accent-l2-hover={$theme_store.accents.l2.hover}
+	--accent-l2-active={$theme_store.accents.l2.default}
+	--accent-l3={$theme_store.accents.l3.default}
+	--accent-l3-hover={$theme_store.accents.l3.hover}
+	--accent-l3-active={$theme_store.accents.l3.default}
+	--accent-l4={$theme_store.accents.l4.default}
+	--accent-l4-hover={$theme_store.accents.l4.hover}
+	--accent-l4-active={$theme_store.accents.l4.default}
+	--accent-l5={$theme_store.accents.l5.default}
+	--accent-l5-hover={$theme_store.accents.l5.hover}
+	--accent-l5-active={$theme_store.accents.l5.default}
+	--mid-tone={$theme_store.mid_tone}
+	--contrast-l1={$theme_store.contrast.l1}
+	--scrollbar-color={$theme_store.scrollbar.default}
+	--scrollbar-color-hover={$theme_store.scrollbar.default}
+	--button-accent={$theme_store.button.default}
+	--button-accent-alt={$theme_store.button.alternate}
+	--primary={$theme_store.primary.primary}
+	--primary-l1={$theme_store.primary.primary1}
+	--primary-l2={$theme_store.primary.primary2}
+	--status-red={$theme_store.status.red}
+	--status-green={$theme_store.status.green}
+	--status-yellow={$theme_store.status.yellow}
+	--status-blue={$theme_store.status.blue}
+	--status-gray={$theme_store.status.grey}
+	--status-purple={$theme_store.status.purple}
+	--indication-blue={$theme_store.indication.blue}
+	--danger={$theme_store.danger.default}
+	--danger-l1={$theme_store.danger.l1}
+	--danger-l2={$theme_store.danger.l2}
 	--tag-padding={"6px 10px"}
 	--tag-font-size={"10px"}
 	--tag-font-weight={"600"}
--- src/lib/mangadex/utils/contexts/theme.ts
+++ src/lib/mangadex/utils/contexts/theme.ts
@@ -1,12 +1,23 @@
 import type { MangadexTheme } from "@mangadex/theme";
 import { getContext, setContext } from "svelte";
+import type { Readable, Writable } from "svelte/store";
 
 const key = "mangadex-theme";
 
-export function setMangaDexThemeContext(theme: MangadexTheme): MangadexTheme {
+export function setMangaDexThemeContext(theme: Readable<MangadexTheme>): Readable<MangadexTheme> {
 	return setContext(key, theme);
 }
 
-export function getMangaDexThemeContext(): MangadexTheme {
+export function getMangaDexThemeContext(): Readable<MangadexTheme> {
+	return getContext(key);
+}
+
+export function setMangaDexThemeContextWritable(
+	theme: Writable<MangadexTheme>
+): Writable<MangadexTheme> {
+	return setContext(key, theme);
+}
+
+export function getMangaDexThemeContextWritable(): Writable<MangadexTheme> {
 	return getContext(key);
 }
