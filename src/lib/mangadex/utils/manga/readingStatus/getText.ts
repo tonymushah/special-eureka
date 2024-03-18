c2a762abc4c8e4d746633440ff1a2f6ca9244c0f
tonymushah <tonyramanantsalama@gmail.com>
2024-03-18 11:13:52 +0300

    Added `getText.ts` for ReadingStatus

---
+++ src/lib/mangadex/utils/manga/readingStatus/getText.ts
@@ -0,0 +1,20 @@
+import { ReadingStatus } from "@mangadex/gql/graphql";
+
+export default function getText(status: ReadingStatus | undefined): string {
+	switch (status) {
+		case ReadingStatus.Reading:
+			return "Reading";
+		case ReadingStatus.Completed:
+			return "Completed";
+		case ReadingStatus.Dropped:
+			return "Dropped";
+		case ReadingStatus.OnHold:
+			return "On Hold";
+		case ReadingStatus.PlanToRead:
+			return "Plan to Read";
+		case ReadingStatus.ReReading:
+			return "Re-Reading";
+		default:
+			return "Add to Library";
+	}
+}
