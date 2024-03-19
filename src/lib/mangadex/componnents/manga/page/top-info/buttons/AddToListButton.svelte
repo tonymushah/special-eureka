d28ca3510a46f9d49c899ab69e12dd4a46ce0737
tonymushah <tonyramanantsalama@gmail.com>
2024-03-19 16:43:16 +0300

    Added `AddToListButton`

---
+++ src/lib/mangadex/componnents/manga/page/top-info/buttons/AddToListButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import ButtonAccent from "@mangadex/componnents/theme/buttons/ButtonAccent.svelte";
+	import { createEventDispatcher } from "svelte";
+	import AddToListIcon from "./add-to-list/AddToListIcon.svelte";
+
+	const dispatch = createEventDispatcher<{
+		onClick: MouseEvent & {
+			currentTarget: EventTarget & HTMLButtonElement;
+		};
+	}>();
+</script>
+
+<ButtonAccent
+	isBase
+	on:click={({ detail }) => {
+		dispatch("onClick", detail);
+	}}
+>
+	<AddToListIcon />
+</ButtonAccent>
