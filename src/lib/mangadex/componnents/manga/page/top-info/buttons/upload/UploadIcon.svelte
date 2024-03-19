1ad50cdd180e6461a227533c4fc3bd7ce129960e
tonymushah <tonyramanantsalama@gmail.com>
2024-03-19 17:09:13 +0300

    Added UploadButton and it's icon

---
+++ src/lib/mangadex/componnents/manga/page/top-info/buttons/upload/UploadIcon.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { UploadIcon } from "svelte-feather-icons";
+</script>
+
+<div class="upload-icon">
+	<UploadIcon size="24px" />
+</div>
+
+<style lang="scss">
+	.upload-icon {
+		height: 24px;
+		width: 24px;
+	}
+</style>
