742132e087e2efbc78f3254c979ff1a183c4b867
tonymushah <tonyramanantsalama@gmail.com>
2024-03-19 17:02:57 +0300

    Added `ReportButton`

---
+++ src/lib/mangadex/componnents/manga/page/top-info/buttons/report/ReportIcon.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { FlagIcon } from "svelte-feather-icons";
+</script>
+
+<div class="report-icon">
+	<FlagIcon size="24px" />
+</div>
+
+<style lang="scss">
+	.report-icon {
+		width: 24px;
+		height: 24px;
+	}
+</style>
