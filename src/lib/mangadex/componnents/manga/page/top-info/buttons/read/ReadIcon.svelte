282b6db5fc521f005a23c2a62c46a61e82263b7c
tonymushah <tonyramanantsalama@gmail.com>
2024-03-19 16:53:09 +0300

    Added `ReadButton` and its icon

---
+++ src/lib/mangadex/componnents/manga/page/top-info/buttons/read/ReadIcon.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { BookOpenIcon } from "svelte-feather-icons";
+</script>
+
+<div class="read-icon">
+	<BookOpenIcon size="24px" />
+</div>
+
+<style lang="scss">
+	.read-icon {
+		height: 24px;
+		width: 24px;
+	}
+</style>
