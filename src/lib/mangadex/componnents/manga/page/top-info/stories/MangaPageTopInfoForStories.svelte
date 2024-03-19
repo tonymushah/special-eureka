f8802e4acc3d78a4d6f8a7121f25c1c270b79744
tonymushah <tonyramanantsalama@gmail.com>
2024-03-19 11:55:46 +0300

    Fixed interactivity once again

--- src/lib/mangadex/componnents/manga/page/top-info/stories/MangaPageTopInfoForStories.svelte
+++ src/lib/mangadex/componnents/manga/page/top-info/stories/MangaPageTopInfoForStories.svelte
@@ -6,6 +6,8 @@
 	import type { Tag } from "@mangadex/utils/types/Tag";
 	import type { MangaStatus, ReadingStatus } from "@mangadex/gql/graphql";
 	import MangaPageTopInfo from "../MangaPageTopInfo.svelte";
+	import { ChapterDownloadState } from "@mangadex/utils/types/DownloadState";
+	import { download } from "@svelteuidev/composables";
 
 	export let id: string;
 	export let title: string;
@@ -22,6 +24,7 @@
 	);
 	let isFollowing: Writable<boolean | undefined> = writable<boolean | undefined>(undefined);
 	let rating: Writable<number | undefined> = writable(undefined);
+	let downloadState = writable(ChapterDownloadState.NotDownloaded);
 </script>
 
 <MangaPageTopInfo
@@ -38,11 +41,28 @@
 	{reading_status}
 	{isFollowing}
 	{rating}
+	{downloadState}
 	on:readingStatus={({ detail }) => {
 		reading_status.set(detail.readingStatus);
 		isFollowing.set(detail.isFollowing);
 	}}
 	on:rating={({ detail }) => {
 		rating.set(detail);
+	}}
+	on:download={() => {
+		downloadState.set(ChapterDownloadState.Downloading);
+		setTimeout(
+			() => {
+				if (Math.floor(Math.random() * 10) % 2) {
+					downloadState.set(ChapterDownloadState.Downloaded);
+				} else {
+					downloadState.set(ChapterDownloadState.Failed);
+				}
+			},
+			Math.floor(Math.random() * 10) * 1000
+		);
+	}}
+	on:delete={() => {
+		downloadState.set(ChapterDownloadState.NotDownloaded);
 	}}
 />
