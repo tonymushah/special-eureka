6f40a70e114c86b1a19b8de1a1b3ef826b550e25
tonymushah <tonyramanantsalama@gmail.com>
2024-03-20 09:55:00 +0300

    Added `DistributionItem`

---
+++ src/lib/mangadex/componnents/manga/page/top-info/stats/note/DistributionItem.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import "@fontsource/poppins/latin.css";
+
+import DistributionItem from "./DistributionItem.svelte";
+import MangadexThemeProviderForStory from "@mangadex/componnents/theme/MangadexThemeProviderForStory.svelte";
+
+const meta = {
+	decorators: [() => MangadexThemeProviderForStory],
+	title: "MangaDex/manga/page/top-info/stats/DistributionItem",
+	component: DistributionItem,
+	tags: ["autodocs"]
+} satisfies Meta<DistributionItem>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	args: {
+		distribution: 1,
+		total: 1000,
+		value: 450
+	}
+};
---
+++ src/lib/mangadex/componnents/manga/page/top-info/stats/note/DistributionItem.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import ProgressBar from "@mangadex/componnents/theme/progress/ProgressBar.svelte";
+
+	export let distribution: number;
+	export let total: number;
+	export let value: number;
+
+	$: progress = (value / total) * 100;
+</script>
+
+<div class="distribution-item">
+	<div class="distribution">
+		{distribution}
+	</div>
+	<div class="progress">
+		<ProgressBar variant="rounded" lineColor="var(--primary)" bind:progress />
+	</div>
+	<div class="value">
+		({value})
+	</div>
+</div>
+
+<style lang="scss">
+	.distribution-item {
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		gap: 10px;
+		justify-content: center;
+		align-items: center;
+		div {
+			display: flex;
+			width: 100%;
+			align-items: center;
+			justify-content: center;
+		}
+		.distribution {
+			grid-column: 1/1;
+		}
+		.progress {
+			grid-column: 2/11;
+		}
+		.value {
+			grid-column: 11/-1;
+		}
+	}
+</style>
