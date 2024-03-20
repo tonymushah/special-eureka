383d93d909c3aef50e10babe3c4a58f339fef9f5
tonymushah <tonyramanantsalama@gmail.com>
2024-03-20 13:21:45 +0300

    Added Top Manga Stats Note

---
+++ src/lib/mangadex/componnents/manga/page/top-info/stats/NoteForStories.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { TopMangaStatsInner } from ".";
+	import Note from "./Note.svelte";
+
+	export let average: number;
+	export let inner: TopMangaStatsInner;
+</script>
+
+<div class="center">
+	<Note bind:average bind:inner />
+</div>
+
+<style lang="scss">
+	.center {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
---
+++ src/lib/mangadex/componnents/manga/page/top-info/stats/note/StarIcon.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { StarIcon } from "svelte-feather-icons";
+</script>
+
+<div class="star-icon">
+	<StarIcon size="20" />
+</div>
+
+<style lang="scss">
+	.star-icon {
+		width: 20px;
+		height: 20px;
+	}
+</style>
