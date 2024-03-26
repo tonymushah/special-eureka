b658c7cd3d7b9bd5149c18d992a7b8c207919884
tonymushah <tonyramanantsalama@gmail.com>
2024-03-26 20:41:30 +0300

    Added manga layout on the title page

---
+++ src/routes/mangadex/title/[id]/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import MangaPageTopInfo from "@mangadex/componnents/manga/page/top-info/MangaPageTopInfo.svelte";
+	import type { LayoutData } from "./$types";
+
+	export let data: LayoutData;
+
+	$: layoutData = data.layoutData!;
+</script>
+
+<MangaPageTopInfo
+	bind:id={layoutData.id}
+	title={layoutData.title ?? ""}
+	altTitle={layoutData.altTitle}
+	coverImage={layoutData.coverImage}
+	coverImageAlt={layoutData.coverImageAlt}
+	authors={layoutData.authors}
+	tags={layoutData.tags}
+	status={layoutData.status}
+	year={layoutData.year ?? undefined}
+/>
+
+<slot />
