4b516baff0fb9c85212dfd39c201cea4a09dc9a6
tonymushah <tonyramanantsalama@gmail.com>
2024-03-26 17:16:59 +0300

    Query formating

--- src/routes/mangadex/title/[id]/(layout)/query.ts
+++ src/routes/mangadex/title/[id]/(layout)/query.ts
@@ -4,72 +4,72 @@ const query = graphql(/* GraphQL */`
     query getMangaTests($id: UUID!) {
         manga {
             get(id: $id) {
-            id
-            attributes {
-                title
-                altTitles
-                state
-                status
-                description
-                availableTranslatedLanguages
-                year
-                contentRating
-                links {
-                hasNoLinks
-                amazon
-                anilist
-                animePlanet
-                bookWalker
-                cdJapan
-                ebookJapan
-                englishTranslation
-                kitsu
-                mangaUpdates
-                myAnimeList
-                novelUpdates
-                raw
-                }
-                tags {
                 id
                 attributes {
-                    name
+                    title
+                    altTitles
+                    state
+                    status
+                    description
+                    availableTranslatedLanguages
+                    year
+                    contentRating
+                    links {
+                        hasNoLinks
+                        amazon
+                        anilist
+                        animePlanet
+                        bookWalker
+                        cdJapan
+                        ebookJapan
+                        englishTranslation
+                        kitsu
+                        mangaUpdates
+                        myAnimeList
+                        novelUpdates
+                        raw
+                    }
+                    tags {
+                        id
+                        attributes {
+                            name
+                        }
+                    }
                 }
+                relationships {
+                    authorArtists {
+                        id
+                        attributes {
+                            name
+                        }
+                    }
+                    authors {
+                        id
+                        attributes {
+                            name
+                        }
+                    }
+                    artists {
+                        id
+                        attributes {
+                            name
+                        }
+                    }
+                    coverArt {
+                        id
+                        attributes {
+                            fileName
+                            locale
+                        }
+                    }
+                    manga {
+                        id
+                        related
+                    }
                 }
-            }
-            relationships {
-                authorArtists {
-                id
-                attributes {
-                    name
-                }
-                }
-                authors {
-                id
-                attributes {
-                    name
-                }
-                }
-                artists {
-                id
-                attributes {
-                    name
-                }
-                }
-                coverArt {
-                id
-                attributes {
-                    fileName
-                    locale
-                }
-                }
-                manga {
-                id
-                related
-                }
-            }
             }
         }
-        }
+    }
 `);
 
 export default query;
