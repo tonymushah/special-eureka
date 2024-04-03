af0be9f4c8aaf6c94e20d2ace45f10296d15943a
tonymushah <tonyramanantsalama@gmail.com>
2024-04-03 20:56:49 +0300

    Added `MangaAltTitles`

---
+++ src/lib/mangadex/componnents/manga/page/chapters/alt-titles/MangaAltTitles.svelte
@@ -0,0 +1,28 @@
+<script lang="ts" context="module">
+	import type { Language } from "@mangadex/gql/graphql";
+
+	export type AltTitleItem = {
+		locale: Language;
+		title: string;
+	};
+</script>
+
+<script lang="ts">
+	import AltTitle from "./AltTitle.svelte";
+
+	export let titles: AltTitleItem[];
+</script>
+
+<div class="alt-titles">
+	{#each titles as { title, locale } (title)}
+		<AltTitle bind:title bind:locale />
+	{/each}
+</div>
+
+<style lang="scss">
+	.alt-titles {
+		display: flex;
+		flex-direction: column;
+		gap: 2px;
+	}
+</style>
