0e59f9bf26a265f14d5b9cf923754c92c3b4259b
tonymushah <tonyramanantsalama@gmail.com>
2024-04-03 20:19:24 +0300

    Added `AltTitle` simpleComponnent

---
+++ src/lib/mangadex/componnents/manga/page/chapters/alt-titles/AltTitle.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import FlagIcon from "@mangadex/componnents/FlagIcon.svelte";
+	import type { Language } from "@mangadex/gql/graphql";
+
+	export let locale: Language;
+	export let title: string;
+</script>
+
+<div class="alt-title">
+	<FlagIcon bind:lang={locale} />
+	<h5>{title}</h5>
+</div>
+
+<style lang="scss">
+	.alt-title {
+		display: flex;
+		flex-direction: row;
+		gap: 5px;
+		align-items: center;
+		h5 {
+			margin: 0px;
+		}
+	}
+</style>
