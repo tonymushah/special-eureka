b23215b7cacb6d30728f9dccfb45c0a52ef5a130
tonymushah <tonyramanantsalama@gmail.com>
2024-04-07 09:51:50 +0300

    Added `MangaElementBase1WithReadableCoverImage.svelte`

---
+++ src/lib/mangadex/componnents/manga/base/base1/MangaElementBase1WithReadableCoverImage.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { MangaStatus } from "@mangadex/gql/graphql";
+	import { createEventDispatcher } from "svelte";
+	import Content from "./Content.svelte";
+	import Image from "./Image.svelte";
+	import Layout from "./Layout.svelte";
+	import type { Readable } from "svelte/store";
+	import Skeleton from "@mangadex/componnents/theme/loader/Skeleton.svelte";
+	createEventDispatcher<{
+		click: MouseEvent & {
+			currentTarget: EventTarget & HTMLButtonElement;
+		};
+	}>();
+	export let coverImage: Readable<string | undefined>;
+	export let coverImageAlt: string;
+	export let title: string;
+	export let status: MangaStatus;
+	export let description: string;
+	$: src = $coverImage;
+</script>
+
+<Layout on:click --layout-width="19em">
+	{#if src}
+		<Image bind:coverImage={src} {coverImageAlt} />
+	{:else}
+		<Skeleton width="100px" height="160px" />
+	{/if}
+	<Content {title} {status} {description} />
+</Layout>
