112da0497cd2868bab04e272b4b5cd070792b44c
tonymushah <tonyramanantsalama@gmail.com>
2024-04-12 16:39:18 +0300

    Dev Home Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,38 @@
 	import { route } from "$lib/ROUTES";
 </script>
 
-<div>
-	<h1>Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-	<p><a href={route("/mangadex")}>Mangadex Home</a></p>
-    <p><a href={route("/mangadex-graphiql")}>Mangadex GraphiQL</a></p>
+<div class="top">
+	<h1>Welcome to Special Eureka</h1>
+	<div class="alert">
+		<h2>If you see this page, means that you're in a prerealease version</h2>
+		<p>A lot of thing can be improved, so please be patient and wait for the stable realease</p>
+	</div>
+	<div class="links">
+		<p><a href={route("/mangadex")}>Mangadex Home</a></p>
+		<p><a href={route("/mangadex-graphiql")}>Mangadex GraphiQL</a></p>
+	</div>
 </div>
 
-<style lang="less">
-    div {
-        margin: 8px;
-    }
+<style lang="scss">
+	div.top {
+		margin: 8px;
+		display: flex;
+		align-content: center;
+		justify-content: center;
+		flex-direction: column;
+		flex-wrap: wrap;
+		width: 100vw;
+		height: 100vh;
+		text-align: center;
+		.alert {
+			display: flex;
+			background-color: #ffb9b9;
+			flex-direction: column;
+			border-radius: 0.25em;
+			padding: 5px;
+			h2 {
+				margin: 5px 0px;
+			}
+		}
+	}
 </style>
