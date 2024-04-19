0227a68fb5c0e33fb704c9531af0b4fc1c046b68
tonymushah <tonyramanantsalama@gmail.com>
2024-04-19 07:54:05 +0300

    Added svg support for favicons

--- src/lib/mangadex/componnents/manga/page/chapters/info/links/button/Favicon.svelte
+++ src/lib/mangadex/componnents/manga/page/chapters/info/links/button/Favicon.svelte
@@ -1,15 +1,37 @@
 <script lang="ts">
+	import { XMLParser } from "fast-xml-parser";
 	export let icon: string | undefined = undefined;
 	export let alt: string;
+	function isSvg(icon: string): boolean {
+		const xmlp = new XMLParser();
+		try {
+			const jObj = xmlp.parse(icon);
+			return jObj.svg != undefined;
+		} catch (error) {
+			return false;
+		}
+	}
+	$: isSvg_ = isSvg(icon ?? "");
 </script>
 
 {#if icon != undefined}
-	<img src={icon} {alt} />
+	{#if isSvg_}
+		<div class="ico">
+			{@html icon}
+		</div>
+	{:else}
+		<img class="ico" src={icon} {alt} />
+	{/if}
 {/if}
 
 <style lang="scss">
-	img {
+	.ico {
 		width: 20px;
 		border-radius: 0.25em;
+	}
+	div.ico {
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 </style>
