55768026e95317847af1afef27beb5c57020a32d
tonymushah <tonyramanantsalama@gmail.com>
2024-04-21 21:41:32 +0300

    Added `ActionButtons`

---
+++ src/lib/window-decoration/ActionButtons.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+	import { MaximizeIcon, MinimizeIcon, MinusIcon, XIcon } from "svelte-feather-icons";
+	import { appWindow } from "@tauri-apps/api/window";
+	export let isMaximize: boolean;
+	export let isMaximized: () => Promise<void>;
+</script>
+
+<div class="actions-icons">
+	<button
+		class="min"
+		on:click={() => {
+			appWindow.minimize();
+		}}
+		on:contextmenu|preventDefault={() => {
+			appWindow.hide();
+		}}
+	>
+		<MinusIcon />
+	</button>
+	<button
+		class="max"
+		on:click={async () => {
+			await appWindow.toggleMaximize();
+			isMaximized();
+		}}
+	>
+		{#if isMaximize}
+			<MaximizeIcon />
+		{:else}
+			<MinimizeIcon />
+		{/if}
+	</button>
+	<button
+		class="close"
+		on:click={async () => {
+			await appWindow.close();
+		}}
+	>
+		<XIcon />
+	</button>
+</div>
+
+<style lang="scss">
+	.actions-icons {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 5px;
+		padding-right: 5px;
+		button {
+			color: var(--textColor);
+			border-radius: 0.25em;
+			border: none;
+			display: flex;
+			justify-content: center;
+			align-content: center;
+		}
+		.min {
+			background-color: var(--minBack);
+		}
+		.max {
+			background-color: var(--maxBack);
+		}
+		.close {
+			background-color: var(--closeBack);
+		}
+		.min:hover {
+			background-color: var(--minBackHover);
+		}
+		.max:hover {
+			background-color: var(--maxBackHover);
+		}
+		.close:hover {
+			background-color: var(--closeBackHover);
+		}
+		.min:active {
+			background-color: var(--minBackActive);
+		}
+		.max:active {
+			background-color: var(--maxBackActive);
+		}
+		.close:active {
+			background-color: var(--closeBackActive);
+		}
+	}
+</style>
--- src/lib/window-decoration/WindowDecoration.svelte
+++ src/lib/window-decoration/WindowDecoration.svelte
@@ -30,10 +30,10 @@
 </script>
 
 <script lang="ts">
-	import { MaximizeIcon, MinimizeIcon, MinusIcon, XIcon } from "svelte-feather-icons";
 	import DragRegion from "./DragRegion.svelte";
 	import { onDestroy, onMount } from "svelte";
 	import type { UnlistenFn } from "@tauri-apps/api/event";
+	import ActionButtons from "./ActionButtons.svelte";
 
 	let isMaximize = false;
 	let unlistens: UnlistenFn[] = [];
@@ -62,12 +62,18 @@
 	const minBack = derived(style, ($style) => $style.minBackground?.default ?? "#4F6");
 	const maxBack = derived(style, ($style) => $style.maxBackground?.default ?? "#46F");
 	const closeBack = derived(style, ($style) => $style.closeBackground?.default ?? "#F46");
+
 	const minBackHover = derived(style, ($style) => $style.minBackground?.hover ?? "#3d4");
 	const maxBackHover = derived(style, ($style) => $style.maxBackground?.hover ?? "#24d");
 	const closeBackHover = derived(style, ($style) => $style.closeBackground?.hover ?? "#d25");
+
 	const minBackActive = derived(style, ($style) => $style.minBackground?.active ?? "#2c0");
 	const maxBackActive = derived(style, ($style) => $style.maxBackground?.active ?? "#02c");
 	const closeBackActive = derived(style, ($style) => $style.closeBackground?.active ?? "#b03");
+
+	const menuBack = derived(style, ($style) => $style.menuBackground?.default ?? "#eee");
+	const menuBackHover = derived(style, ($style) => $style.menuBackground?.hover ?? "#ccc");
+	const menuBackActive = derived(style, ($style) => $style.menuBackground?.active ?? "#aaa");
 </script>
 
 <DragRegion
@@ -84,46 +90,16 @@
 	--maxBackActive={$maxBackActive}
 	--closeBackActive={$closeBackActive}
 	--fonts={$fonts}
+	--menuBack={$menuBack}
+	--menuBackHover={$menuBack}
+	--menuBackActive={$menuBackActive}
 >
 	<div class="title-bar" data-tauri-drag-region>
 		<div class="title" data-tauri-drag-region>
 			<img data-tauri-drag-region src={$logo} alt="icon" />
 			<h4 data-tauri-drag-region>{$title}</h4>
 		</div>
-		<div class="actions-icons">
-			<button
-				class="min"
-				on:click={() => {
-					appWindow.minimize();
-				}}
-				on:contextmenu|preventDefault={() => {
-					appWindow.hide();
-				}}
-			>
-				<MinusIcon />
-			</button>
-			<button
-				class="max"
-				on:click={async () => {
-					await appWindow.toggleMaximize();
-					isMaximized();
-				}}
-			>
-				{#if isMaximize}
-					<MaximizeIcon />
-				{:else}
-					<MinimizeIcon />
-				{/if}
-			</button>
-			<button
-				class="close"
-				on:click={async () => {
-					await appWindow.close();
-				}}
-			>
-				<XIcon />
-			</button>
-		</div>
+		<ActionButtons bind:isMaximize {isMaximized} />
 	</div>
 </DragRegion>
 
@@ -149,48 +125,6 @@
 			img {
 				width: 25px;
 				padding-left: 5px;
-			}
-		}
-		.actions-icons {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			gap: 5px;
-			padding-right: 5px;
-			button {
-				color: var(--textColor);
-				border-radius: 0.25em;
-				border: none;
-				display: flex;
-				justify-content: center;
-				align-content: center;
-			}
-			.min {
-				background-color: var(--minBack);
-			}
-			.max {
-				background-color: var(--maxBack);
-			}
-			.close {
-				background-color: var(--closeBack);
-			}
-			.min:hover {
-				background-color: var(--minBackHover);
-			}
-			.max:hover {
-				background-color: var(--maxBackHover);
-			}
-			.close:hover {
-				background-color: var(--closeBackHover);
-			}
-			.min:active {
-				background-color: var(--minBackActive);
-			}
-			.max:active {
-				background-color: var(--maxBackActive);
-			}
-			.close:active {
-				background-color: var(--closeBackActive);
 			}
 		}
 	}
