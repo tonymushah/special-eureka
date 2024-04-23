1be2cf27dd467b6fc684740f6b5202e83ebff69b
tonymushah <tonyramanantsalama@gmail.com>
2024-04-23 20:55:10 +0300

    Added `Volumes`

---
+++ src/lib/mangadex/componnents/manga/page/chapters/aggreate/Volumes.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { createEventDispatcher, type ComponentProps } from "svelte";
+	import VolumeAccordion from "./VolumeAccordion.svelte";
+
+	export let volumes: ComponentProps<VolumeAccordion>[];
+	type MouseEnvDiv = MouseEvent & {
+		currentTarget: HTMLDivElement & EventTarget;
+	};
+	type KeyboardEnvDiv = KeyboardEvent & {
+		currentTarget: HTMLDivElement & EventTarget;
+	};
+	const dispatch = createEventDispatcher<{
+		download: MouseEnvDiv & {
+			id: string;
+		};
+		downloadKeyPress: KeyboardEnvDiv & {
+			id: string;
+		};
+		read: MouseEnvDiv & {
+			id: string;
+		};
+		readKeyPress: KeyboardEnvDiv & {
+			id: string;
+		};
+	}>();
+</script>
+
+<div class="volumes">
+	{#each volumes as volume}
+		<VolumeAccordion
+			{...volume}
+			on:download={({ detail }) => {
+				dispatch("download", detail);
+			}}
+			on:downloadKeyPress={({ detail }) => {
+				dispatch("downloadKeyPress", detail);
+			}}
+			on:read={({ detail }) => {
+				dispatch("read", detail);
+			}}
+			on:readKeyPress={({ detail }) => {
+				dispatch("readKeyPress", detail);
+			}}
+		/>
+	{/each}
+</div>
+
+<style lang="scss">
+</style>
