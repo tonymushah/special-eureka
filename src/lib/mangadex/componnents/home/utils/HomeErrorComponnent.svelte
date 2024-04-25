622538b31a9b276422670be650ba067e675ec3b2
tonymushah <tonyramanantsalama@gmail.com>
2024-04-25 21:19:41 +0300

    some `HomeErrorComponnent` fixes

--- src/lib/mangadex/componnents/home/utils/HomeErrorComponnent.svelte
+++ src/lib/mangadex/componnents/home/utils/HomeErrorComponnent.svelte
@@ -8,9 +8,9 @@
 <div class="error with-margin">
 	<h3>{label}</h3>
 	{#each error.graphQLErrors as { name, message }}
-		<div>
+		<div class="details">
 			<h4>{name}</h4>
-			<p>{message}</p>
+			<div>{message}</div>
 		</div>
 	{/each}
 </div>
@@ -24,11 +24,13 @@
 		h3 {
 			margin: 0em;
 		}
-		div {
+		.details {
 			display: flex;
 			align-items: center;
 			justify-self: center;
 			gap: 10px;
+			flex-direction: row;
+			overflow: hidden;
 		}
 		background-color: color-mix(in srgb, var(--danger-l1) 50%, var(--main-background) 0%);
 		border-left: 10px;
