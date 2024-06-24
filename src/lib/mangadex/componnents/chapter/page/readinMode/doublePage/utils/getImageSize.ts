3450a95cebac73424a94f3d6624b9a49234e2684
tonymushah <tonyramanantsalama@gmail.com>
2024-06-24 10:36:20 +0300

    Added `getImageSize`

---
+++ src/lib/mangadex/componnents/chapter/page/readinMode/doublePage/utils/getImageSize.ts
@@ -0,0 +1,22 @@
+type ImageSize = {
+	width: number;
+	height: number;
+};
+
+export default function getImageSize(src: string): Promise<ImageSize> {
+	return new Promise((resolve, reject) => {
+		const image = new Image();
+		console.log("load");
+		image.addEventListener("error", (e) => {
+			reject(e.error);
+		});
+		image.addEventListener("load", () => {
+			const { height, width } = image;
+			resolve({
+				height,
+				width
+			});
+		});
+		image.src = src;
+	});
+}
