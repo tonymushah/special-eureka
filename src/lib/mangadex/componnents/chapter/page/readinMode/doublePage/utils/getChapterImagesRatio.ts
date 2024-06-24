dc1a3c7fa608257191981c702dacd0d66a3a06a3
tonymushah <tonyramanantsalama@gmail.com>
2024-06-24 15:38:35 +0300

    Some types fixes on `getChapterImagesRatio`

--- src/lib/mangadex/componnents/chapter/page/readinMode/doublePage/utils/getChapterImagesRatio.ts
+++ src/lib/mangadex/componnents/chapter/page/readinMode/doublePage/utils/getChapterImagesRatio.ts
@@ -2,21 +2,20 @@ import { derived, type Readable } from "svelte/store";
 import { getChapterImageContext } from "../../../contexts/images";
 import getImageRatio from "./getImageRatio";
 
-export default function getChapterImagesRatio(): Readable<[string[], Map<string, number>]> {
+export default function getChapterImagesRatio(): Readable<Map<string, number>> {
 	const images = getChapterImageContext();
 	return derived(
 		images,
 		($imgs, set, update) => {
-			set([$imgs, new Map()]);
 			$imgs.forEach((img) => {
 				getImageRatio(img).then((ratio) => {
 					update((inner) => {
-						inner[1].set(img, ratio);
+						inner.set(img, ratio);
 						return inner;
 					});
 				});
 			});
 		},
-		[new Array<string>(), new Map()]
+		new Map()
 	);
 }
