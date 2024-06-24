ab3f527d43950e110ddc72a0eae9f7cd06ac1fb5
tonymushah <tonyramanantsalama@gmail.com>
2024-06-24 16:46:04 +0300

    Added `getChapterDoublePageCurrentPage` with rtl support

--- src/lib/mangadex/componnents/chapter/page/readinMode/doublePage/utils/getChapterDoublePageCurrentPage.ts
+++ src/lib/mangadex/componnents/chapter/page/readinMode/doublePage/utils/getChapterDoublePageCurrentPage.ts
@@ -3,13 +3,26 @@ import { currentChapterPage } from "../../../stores/currentPage";
 import type { ChapterDoublePageImage } from "./getChapterImagesAsDoublePage";
 import getChapterDoublePageCurrentPageIndex from "./getChapterDoublePageCurrentPageIndex";
 import getChapterImagesAsDoublePage from "./getChapterImagesAsDoublePage";
+import { ReadingDirection, readingDirection } from "../../../stores/readingDirection";
+import { isArray } from "lodash";
 
 export default function getChapterDoublePageCurrentPage(
 	currentChapter = currentChapterPage
 ): Readable<ChapterDoublePageImage | undefined> {
 	const images = getChapterImagesAsDoublePage();
 	const current = getChapterDoublePageCurrentPageIndex(currentChapter);
-	return derived([images, current], ([$images, $currentPage]) => {
-		return $images.at($currentPage);
+	return derived([images, current, readingDirection], ([$images, $currentPage, $rd]) => {
+		const image = $images.at($currentPage);
+		if (image) {
+			if (isArray(image)) {
+				if ($rd == ReadingDirection.Ltr) {
+					return image;
+				} else {
+					return [image[1], image[0]] satisfies ChapterDoublePageImage;
+				}
+			} else {
+				return image;
+			}
+		}
 	});
 }
