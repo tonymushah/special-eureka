d857d41039198e02a411b5e33fca715c27b4b692
tonymushah <tonyramanantsalama@gmail.com>
2024-06-25 18:45:30 +0300

    Added `getLongStripImagesWidthContext` support for `LongStrip`

--- src/lib/mangadex/componnents/chapter/page/readinMode/longStrip/LongStrip.svelte
+++ src/lib/mangadex/componnents/chapter/page/readinMode/longStrip/LongStrip.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
+	import { derived } from "svelte/store";
 	import { getChapterImageContext } from "../../contexts/images";
+	import { getLongStripImagesWidthContext } from "./utils/context/longstrip_images_width";
 
 	const images = getChapterImageContext();
+	const imageWidth = derived(getLongStripImagesWidthContext(), ($width) => {
+		if ($width == 0) {
+			return 100;
+		} else {
+			return Math.abs($width);
+		}
+	});
 </script>
 
 <div class="longstrip">
 	<slot name="top" />
 	{#each $images as image}
 		<div>
-			<img src={image} alt={image} />
+			<img src={image} alt={image} width="{$imageWidth}%" />
 		</div>
 	{/each}
 	<slot name="bottom" />
