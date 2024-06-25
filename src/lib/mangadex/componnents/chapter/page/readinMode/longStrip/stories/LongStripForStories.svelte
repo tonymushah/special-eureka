f010095ecd5138667f55db3ecb59f4ca440e180f
tonymushah <tonyramanantsalama@gmail.com>
2024-06-25 17:16:59 +0300

    Added `LongStrip` stories

---
+++ src/lib/mangadex/componnents/chapter/page/readinMode/longStrip/stories/LongStripForStories.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { initChapterImageContext } from "../../../contexts/images";
+	import LongStrip from "../LongStrip.svelte";
+	export let images: string[];
+	initChapterImageContext(images);
+</script>
+
+<LongStrip />
