7d30964a2016c3cdddedcf0b46f6375bc678865c
tonymushah <tonyramanantsalama@gmail.com>
2024-06-25 17:56:49 +0300

    Added `generateContextMethods` and `generateContextMethodsIgnoreUndefined`

--- src/lib/mangadex/utils/contexts/index.ts
+++ src/lib/mangadex/utils/contexts/index.ts
@@ -1,1 +1,31 @@
+import { getContext, setContext } from "svelte";
+
 export * from "./theme";
+
+export function generateContextMethods<T>(key: string, customErrorMessage?: string) {
+	return {
+		init(value: T) {
+			return setContext<T>(key, value);
+		},
+		get() {
+			const data = getContext<T | undefined>(key);
+			if (data != undefined) {
+				return data;
+			} else {
+				throw new Error(customErrorMessage ?? `The context ${key} should be defined`);
+			}
+		}
+	};
+}
+
+export function generateContextMethodsIgnoreUndefined<T>(key: string, customErrorMessage?: string) {
+	return {
+		init(value: T) {
+			return setContext<T>(key, value);
+		},
+		get() {
+			const data = getContext<T>(key);
+			return data;
+		}
+	};
+}
