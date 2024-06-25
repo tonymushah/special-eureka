c1ce73f7323bede000c5fd4a48dfa06bbfbb6d50
tonymushah <tonyramanantsalama@gmail.com>
2024-06-25 17:08:04 +0300

    Added `LongStrip` base

---
+++ src/lib/mangadex/componnents/chapter/page/readinMode/longStrip/LongStrip.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { getChapterImageContext } from "../../contexts/images";
+
+	const images = getChapterImageContext();
+</script>
+
+<div class="longstrip">
+	<slot name="top" />
+	{#each $images as image}
+		<div>
+			<img src={image} alt={image} />
+		</div>
+	{/each}
+	<slot name="bottom" />
+</div>
