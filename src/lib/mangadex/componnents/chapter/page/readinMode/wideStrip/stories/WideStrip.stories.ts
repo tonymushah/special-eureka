dcf9277f52ef0ff64ac75494595f9c78b4227b66
tonymushah <tonyramanantsalama@gmail.com>
2024-06-26 21:11:22 +0300

    Added `WideStrip` stories

---
+++ src/lib/mangadex/componnents/chapter/page/readinMode/wideStrip/stories/WideStrip.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import "@fontsource/poppins/latin.css";
+import MangadexThemeProviderForStory from "@mangadex/componnents/theme/MangadexThemeProviderForStory.svelte";
+import Page from "./WideStripForStories.svelte";
+import fuufuIjouChapter56 from "../../tests/bee8582d-dbed-4075-be3d-4361052d31c1/images";
+
+const meta = {
+	decorators: [() => MangadexThemeProviderForStory],
+	title: "MangaDex/chapter/readingMode/WideStrip",
+	component: Page,
+	tags: ["autodocs"]
+} satisfies Meta<Page>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	args: {
+		images: fuufuIjouChapter56
+	}
+};
