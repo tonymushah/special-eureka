ee3ee9eff508d09f79db9b056a24cedddde56e80
tonymushah <tonyramanantsalama@gmail.com>
2024-06-26 21:21:48 +0300

    Added `TODO` with `intersection observer API`

--- src/lib/mangadex/componnents/chapter/page/readinMode/wideStrip/WideStrip.svelte
+++ src/lib/mangadex/componnents/chapter/page/readinMode/wideStrip/WideStrip.svelte
@@ -6,15 +6,21 @@
 	const images = getChapterImageContext();
 	export let innerOverflow = true;
 	$: rtl = $readingDirection == ReadingDirection.Rtl;
+	// TODO Add support with the intersection observer API
+	/*new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            entry.target.getAttribute("data-page")
+        })
+    })*/
 </script>
 
 <slot name="top" />
 
 <div class="wide-strip" class:rtl class:innerOverflow>
 	<slot name="before" />
-	{#each $images as image}
+	{#each $images as image, page}
 		<div>
-			<img src={image} alt={image} />
+			<img src={image} alt={image} data-page={page} />
 		</div>
 	{/each}
 	<slot name="after" />
