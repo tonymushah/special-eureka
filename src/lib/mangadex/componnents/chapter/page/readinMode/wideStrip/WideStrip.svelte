f574b58e04d160165aec10a3865232a0f732f183
tonymushah <tonyramanantsalama@gmail.com>
2024-06-26 21:04:07 +0300

    Added `WideStrip` base

---
+++ src/lib/mangadex/componnents/chapter/page/readinMode/wideStrip/WideStrip.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { getChapterImageContext } from "../../contexts/images";
+	import { ReadingDirection, readingDirection } from "../../stores/readingDirection";
+
+	const images = getChapterImageContext();
+	$: rtl = $readingDirection == ReadingDirection.Rtl;
+</script>
+
+<slot name="top" />
+
+<div class="wide-strip" class:rtl>
+	<slot name="before" />
+	{#each $images as image}
+		<div>
+			<img src={image} alt={image} />
+		</div>
+	{/each}
+	<slot name="after" />
+</div>
+
+<slot name="bottom" />
+
+<style lang="scss">
+	.wide-strip {
+		display: flex;
+		flex-direction: column;
+	}
+	.wide-strip.rtl {
+		flex-direction: column-reverse;
+	}
+	img {
+		max-height: 100%;
+	}
+</style>
