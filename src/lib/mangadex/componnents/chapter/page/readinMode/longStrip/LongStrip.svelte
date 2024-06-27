825638923f0031f7a95b1c08db128dcd6054d663
tonymushah <tonyramanantsalama@gmail.com>
2024-06-27 10:10:27 +0300

    Added intersection observer in `LongStrip`

--- src/lib/mangadex/componnents/chapter/page/readinMode/longStrip/LongStrip.svelte
+++ src/lib/mangadex/componnents/chapter/page/readinMode/longStrip/LongStrip.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-	import { derived } from "svelte/store";
+	import { derived, writable, type Readable } from "svelte/store";
 	import { getChapterImageContext } from "../../contexts/images";
 	import { getLongStripImagesWidthContext } from "./utils/context/longstrip_images_width";
+	import { currentChapterPage } from "../../stores/currentPage";
+	import { onMount } from "svelte";
 
+	let longstrip_root: HTMLDivElement | undefined;
 	const images = getChapterImageContext();
 	const imageWidth = derived(getLongStripImagesWidthContext(), ($width) => {
 		if ($width == 0) {
@@ -11,13 +14,76 @@
 			return Math.abs($width);
 		}
 	});
+	const isFromIntersector = writable(false);
+	let shouldIgnore = false;
+	function fromIntersector<T = unknown>(fn: () => T): T {
+		shouldIgnore = true;
+		isFromIntersector.set(true);
+		try {
+			return fn();
+		} catch (error) {
+			throw error;
+		} finally {
+			isFromIntersector.set(false);
+			shouldIgnore = false;
+		}
+	}
+	const currentChapter: Readable<[number, boolean]> = derived(
+		[currentChapterPage, isFromIntersector],
+		([$page, $fromInter]) => {
+			return [$page, $fromInter] satisfies [number, boolean];
+		}
+	);
+	onMount(() => {
+		return currentChapter.subscribe(([page, fromInter]) => {
+			if (!shouldIgnore && !fromInter) {
+				if (longstrip_root != undefined) {
+					const current = longstrip_root.querySelector(`div[data-page=\"${page}\"]`);
+					if (current != null) {
+						current.scrollIntoView();
+					}
+				}
+			}
+		});
+	});
+	const interObserver = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				const isInitialLoading = entry.target.getAttribute("data-initial-loading");
+				if (isInitialLoading == "true") {
+					entry.target.setAttribute("data-initial-loading", "false");
+				} else {
+					const page = entry.target.getAttribute("data-page");
+					if (page != null) {
+						if (entry.isIntersecting && entry.intersectionRatio > 0) {
+							fromIntersector(() => {
+								currentChapterPage.set(Number(page));
+							});
+						}
+					}
+				}
+			});
+		},
+		{
+			root: longstrip_root
+		}
+	);
 </script>
 
-<div class="longstrip">
+<div class="longstrip" bind:this={longstrip_root}>
 	<slot name="top" />
-	{#each $images as image}
-		<div>
-			<img src={image} alt={image} width="{$imageWidth}%" />
+	{#each $images as image, page}
+		<div data-page={page}>
+			<img
+				data-initial-loading="true"
+				data-page={page}
+				on:load={(e) => {
+					interObserver.observe(e.currentTarget);
+				}}
+				src={image}
+				alt={image}
+				width="{$imageWidth}%"
+			/>
 		</div>
 	{/each}
 	<slot name="bottom" />
