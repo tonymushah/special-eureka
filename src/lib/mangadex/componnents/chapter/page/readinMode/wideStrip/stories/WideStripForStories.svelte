ae204fd1412d9e66acd59f193850013fb6725dc3
tonymushah <tonyramanantsalama@gmail.com>
2024-06-27 08:52:21 +0300

    Added simple navigate button

--- src/lib/mangadex/componnents/chapter/page/readinMode/wideStrip/stories/WideStripForStories.svelte
+++ src/lib/mangadex/componnents/chapter/page/readinMode/wideStrip/stories/WideStripForStories.svelte
@@ -1,10 +1,36 @@
 <script lang="ts">
+	import { onMount } from "svelte";
 	import { initChapterImageContext } from "../../../contexts/images";
 	import { ReadingDirection, readingDirection } from "../../../stores/readingDirection";
 	import Page from "../WideStrip.svelte";
+	import { derived } from "svelte/store";
+	import { currentChapterPage } from "../../../stores/currentPage";
 	export let images: string[];
+	const currentPage = derived(currentChapterPage, (page) => page + 1);
 	initChapterImageContext(images);
 </script>
+
+<p>
+	Current Page: {$currentPage}
+	<button
+		on:click={() => {
+			if ($readingDirection == ReadingDirection.Ltr) {
+				$currentChapterPage--;
+			} else {
+				$currentChapterPage++;
+			}
+		}}>{"<-"}</button
+	>
+	<button
+		on:click={() => {
+			if ($readingDirection == ReadingDirection.Ltr) {
+				$currentChapterPage++;
+			} else {
+				$currentChapterPage--;
+			}
+		}}>{"->"}</button
+	>
+</p>
 
 <p>
 	Current mode:
