b9ec79ec0f0ec7d15fe400b1ac2b1c29a74b165f
tonymushah <tonyramanantsalama@gmail.com>
2024-06-27 08:57:00 +0300

    Added intersection observer api support in `WideStrip`

--- src/lib/mangadex/componnents/chapter/page/readinMode/wideStrip/WideStrip.svelte
+++ src/lib/mangadex/componnents/chapter/page/readinMode/wideStrip/WideStrip.svelte
@@ -1,26 +1,80 @@
 <script lang="ts">
+	import { derived, writable, type Readable } from "svelte/store";
 	import { getChapterImageContext } from "../../contexts/images";
 	import { currentChapterPage } from "../../stores/currentPage";
 	import { ReadingDirection, readingDirection } from "../../stores/readingDirection";
-
+	import { onMount } from "svelte";
+	let widestrip_root: HTMLDivElement | undefined;
 	const images = getChapterImageContext();
+	const isFromIntersector = writable(false);
+	let shouldIgnore = false;
+	function fromIntersector<T = unknown>(fn: () => T): T {
+		shouldIgnore = true;
+		isFromIntersector.set(true);
+		try {
+			shouldIgnore = false;
+			return fn();
+		} catch (error) {
+			throw error;
+		} finally {
+			isFromIntersector.set(false);
+			shouldIgnore = false;
+		}
+	}
+	const currentChapter: Readable<[number, boolean]> = derived(
+		[currentChapterPage, isFromIntersector],
+		([$page, $fromInter]) => {
+			return [$page, $fromInter] satisfies [number, boolean];
+		}
+	);
+	onMount(() =>
+		currentChapter.subscribe(([page, fromInter]) => {
+			if (!shouldIgnore && !fromInter) {
+				if (widestrip_root != undefined) {
+					const current = widestrip_root.querySelector(`div[data-page=\"${page}\"]`);
+					if (current != null) {
+						current.scrollIntoView();
+					}
+				}
+			}
+		})
+	);
 	export let innerOverflow = true;
 	$: rtl = $readingDirection == ReadingDirection.Rtl;
 	// TODO Add support with the intersection observer API
-	/*new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            entry.target.getAttribute("data-page")
-        })
-    })*/
+	const interObserver = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				const page = entry.target.getAttribute("data-page");
+				if (page != null) {
+					if (entry.isIntersecting && entry.intersectionRatio > 0) {
+						fromIntersector(() => {
+							currentChapterPage.set(Number(page));
+						});
+					}
+				}
+			});
+		},
+		{
+			root: widestrip_root
+		}
+	);
 </script>
 
 <slot name="top" />
 
-<div class="wide-strip" class:rtl class:innerOverflow>
+<div class="wide-strip" class:rtl class:innerOverflow bind:this={widestrip_root}>
 	<slot name="before" />
 	{#each $images as image, page}
-		<div>
-			<img src={image} alt={image} data-page={page} />
+		<div data-page={page}>
+			<img
+				on:load={(e) => {
+					interObserver.observe(e.currentTarget);
+				}}
+				src={image}
+				alt={image}
+				data-page={page}
+			/>
 		</div>
 	{/each}
 	<slot name="after" />
