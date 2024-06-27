21e90901c2395bcf917f7ba20c1259ed721de810
tonymushah <tonyramanantsalama@gmail.com>
2024-06-27 12:03:41 +0300

    Added `initDefaultChapterCurrentPageContext` in `currentPage`

--- src/lib/mangadex/componnents/chapter/page/contexts/currentPage.ts
+++ src/lib/mangadex/componnents/chapter/page/contexts/currentPage.ts
@@ -1,7 +1,14 @@
 import { generateContextStoresMethods } from "@mangadex/utils/contexts";
+import { writable, type StartStopNotifier, type Writable } from "svelte/store";
 
 export const {
 	get: getChapterCurrentPageContext,
 	getReadonly: getChapterCurrentPageContextReadOnly,
 	init: initChapterCurrentPageContext
 } = generateContextStoresMethods<number>("CHAPTER-CURRENT-PAGE");
+
+export function initDefaultChapterCurrentPageContext(
+	start?: StartStopNotifier<number>
+): Writable<number> {
+	return initChapterCurrentPageContext(writable(0, start));
+}
