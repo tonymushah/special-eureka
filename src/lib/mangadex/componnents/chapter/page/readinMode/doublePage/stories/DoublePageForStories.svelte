fd8cc61ab11f0287f7dc6e5433a169f9fca871ab
tonymushah <tonyramanantsalama@gmail.com>
2024-06-27 12:32:53 +0300

    Added `initChapterCurrentPageContext` and `currentPage` props in `DoublePageForStories`

--- src/lib/mangadex/componnents/chapter/page/readinMode/doublePage/stories/DoublePageForStories.svelte
+++ src/lib/mangadex/componnents/chapter/page/readinMode/doublePage/stories/DoublePageForStories.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import { writable } from "svelte/store";
+	import { initChapterCurrentPageContext } from "../../../contexts/currentPage";
 	import { initChapterImageContext } from "../../../contexts/images";
 	import { ReadingDirection, readingDirection } from "../../../stores/readingDirection";
 	import Page from "../DoublePage.svelte";
 	export let images: string[];
+	export let currentPage: number = 0;
 	initChapterImageContext(images);
+	initChapterCurrentPageContext(writable(currentPage));
 </script>
 
 <p>
