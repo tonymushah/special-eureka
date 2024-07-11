285418ad5683fe0cfe9144f3824cf5d8a32845d7
tonymushah <tonyramanantsalama@gmail.com>
2024-07-11 23:17:26 +0300

    Added `isOnZoomableImage` in `currentChapterReadingMode`

--- src/lib/mangadex/componnents/chapter/page/contexts/currentChapterReadingMode.ts
+++ src/lib/mangadex/componnents/chapter/page/contexts/currentChapterReadingMode.ts
@@ -1,8 +1,16 @@
-import type { ReadingMode } from "@mangadex/gql/graphql";
+import { ReadingMode } from "@mangadex/gql/graphql";
 import { generateContextStoresMethods } from "@mangadex/utils/contexts";
+import { derived } from "svelte/store";
 
 export const {
 	getReadonly: getCurrentChapterReadingMode,
 	get: getCurrentChapterReadingModeWritable,
 	init: initCurrentChapterReadingMode
 } = generateContextStoresMethods<ReadingMode>("CURRENT_CHAPTER_READING_MODE");
+
+export function isOnZoomableImage() {
+	return derived(
+		getCurrentChapterReadingMode(),
+		($mode) => $mode == ReadingMode.DoublePage || $mode == ReadingMode.SinglePage
+	);
+}
