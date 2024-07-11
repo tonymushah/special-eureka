4ab48c55b2a08abe4ad6f247ae2533881970bee1
tonymushah <tonyramanantsalama@gmail.com>
2024-07-11 22:34:59 +0300

    Added `Line`

--- src/lib/mangadex/componnents/chapter/page/drawer/ChapterDrawerContent.svelte
+++ src/lib/mangadex/componnents/chapter/page/drawer/ChapterDrawerContent.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
 	import Header from "./content/Header.svelte";
+	import Line from "./content/Line.svelte";
 
 	export let left = false;
 </script>
 
 <div class="content">
 	<Header bind:left />
+	<Line />
 </div>
 
 <style lang="scss">
 	.content {
 		display: flex;
 		flex-direction: column;
+		gap: 3px;
 	}
 </style>
