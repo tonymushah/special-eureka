c73623fe3567355f3cc6a1f47e5d1671306b7f14
tonymushah <tonyramanantsalama@gmail.com>
2024-07-12 09:45:17 +0300

    Added `ReadingDirection` settings in Drawer

--- src/lib/mangadex/componnents/chapter/page/drawer/content/Settings.svelte
+++ src/lib/mangadex/componnents/chapter/page/drawer/content/Settings.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
+	import ReadingDirection from "./settings/ReadingDirection.svelte";
 	import ResetZoom from "./settings/ResetZoom.svelte";
 </script>
 
 <section>
 	<ResetZoom />
+	<ReadingDirection />
 </section>
 
 <style lang="scss">
 	section {
 		display: flex;
 		flex-direction: column;
+		gap: 5px;
 	}
 </style>
---
+++ src/lib/mangadex/componnents/chapter/page/drawer/content/settings/ReadingDirection.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import ButtonAccentOnlyLabel from "@mangadex/componnents/theme/buttons/ButtonAccentOnlyLabel.svelte";
+	import { derived } from "svelte/store";
+	import { isReadingDirectionModifiable as isModifiable } from "../../../contexts/currentChapterReadingMode";
+	import { ReadingDirection, readingDirection } from "../../../stores/readingDirection";
+	import Icon from "./reading-direction/Icon.svelte";
+	const isReadingDirectionModifiable = isModifiable();
+	const label = derived(readingDirection, ($direction) => {
+		if ($direction == ReadingDirection.Ltr) {
+			return "Left to Right";
+		} else {
+			return "Right to Left";
+		}
+	});
+</script>
+
+{#if $isReadingDirectionModifiable}
+	<ButtonAccentOnlyLabel
+		label={$label}
+		icon={Icon}
+		on:click={() => {
+			if ($readingDirection == ReadingDirection.Ltr) {
+				readingDirection.set(ReadingDirection.Rtl);
+			} else {
+				readingDirection.set(ReadingDirection.Ltr);
+			}
+		}}
+	/>
+{/if}
