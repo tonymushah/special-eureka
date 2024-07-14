1489c4eac2b8eddf60feee4590003d3a155bebf5
tonymushah <tonyramanantsalama@gmail.com>
2024-07-14 18:36:55 +0300

    Added `DoublePagePreviousNext` Content

--- src/lib/mangadex/componnents/chapter/page/drawer/content/navigation/previous-next/DoublePagePreviousNext.svelte
+++ src/lib/mangadex/componnents/chapter/page/drawer/content/navigation/previous-next/DoublePagePreviousNext.svelte
@@ -1,1 +1,87 @@
+<script lang="ts">
+	import { getChapterCurrentPageContext } from "@mangadex/componnents/chapter/page/contexts/currentPage";
+	import getChapterDoublePageCurrentPageIndex from "@mangadex/componnents/chapter/page/readinMode/doublePage/utils/getChapterDoublePageCurrentPageIndex";
+	import getChapterDoublePageIndexes from "@mangadex/componnents/chapter/page/readinMode/doublePage/utils/getChapterDoublePageIndexes";
+	import getChapterImagesAsDoublePage from "@mangadex/componnents/chapter/page/readinMode/doublePage/utils/getChapterImagesAsDoublePage";
+	import {
+		readingDirection as direction,
+		ReadingDirection
+	} from "@mangadex/componnents/chapter/page/stores/readingDirection";
+	import ButtonAccent from "@mangadex/componnents/theme/buttons/ButtonAccent.svelte";
+	import { ceil, isArray, random } from "lodash";
+	import { createEventDispatcher } from "svelte";
+	import { ArrowLeftIcon, ArrowRightIcon } from "svelte-feather-icons";
+	import { derived } from "svelte/store";
+
+	const currentChapterPage = getChapterCurrentPageContext();
+	const currentPageIndex = getChapterDoublePageCurrentPageIndex();
+	const images_indexes = getChapterDoublePageIndexes();
+	const images = getChapterImagesAsDoublePage();
+	const images_length = derived(images, ($imgs) => $imgs.length);
+	const dispatch = createEventDispatcher<{
+		next: {};
+		previous: {};
+	}>();
+	$: next = function () {
+		if ($currentPageIndex < $images_length - 1) {
+			currentChapterPage.update(() => {
+				const index = $images_indexes[$currentPageIndex + 1];
+				if (isArray(index)) {
+					return index[ceil(random(0, 1))];
+				} else {
+					return index;
+				}
+			});
+		} else {
+			dispatch("next", {});
+		}
+	};
+	$: previous = function () {
+		if ($currentPageIndex > 0) {
+			currentChapterPage.update(() => {
+				const index = $images_indexes[$currentPageIndex - 1];
+				if (isArray(index)) {
+					return index[ceil(random(0, 1))];
+				} else {
+					return index;
+				}
+			});
+		} else {
+			dispatch("previous", {});
+		}
+	};
+	$: onNext = function () {
+		switch ($direction) {
+			case ReadingDirection.Ltr:
+				next();
+				break;
+			case ReadingDirection.Rtl:
+				previous();
+				break;
+			default:
+				break;
+		}
+	};
+	$: onPrevious = function () {
+		switch ($direction) {
+			case ReadingDirection.Ltr:
+				previous();
+				break;
+			case ReadingDirection.Rtl:
+				next();
+				break;
+			default:
+				break;
+		}
+	};
+</script>
+
+<ButtonAccent on:click={onPrevious}>
+	<ArrowLeftIcon />
+</ButtonAccent>
+
 <slot />
+
+<ButtonAccent on:click={onNext}>
+	<ArrowRightIcon />
+</ButtonAccent>
