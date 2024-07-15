c3217c6cd7e6ecfa69837b2ae9f963e301357229
tonymushah <tonyramanantsalama@gmail.com>
2024-07-15 15:23:58 +0300

    Added `PreviousNext` for `chapter-navigation`

---
+++ src/lib/mangadex/componnents/chapter/page/drawer/content/chapters-navigation/PreviousNext.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import ButtonAccent from "@mangadex/componnents/theme/buttons/ButtonAccent.svelte";
+	import { isLongStrip } from "../../../contexts/currentChapterReadingMode";
+	import {
+		fireChapterNextEvent,
+		fireChapterPreviousEvent
+	} from "../../../contexts/previousNextEventTarget";
+	import {
+		readingDirection as direction,
+		ReadingDirection
+	} from "../../../stores/readingDirection";
+	import { ArrowLeftIcon, ArrowRightIcon } from "svelte-feather-icons";
+	const isLong = isLongStrip();
+	$: next = function () {
+		fireChapterNextEvent();
+	};
+	$: previous = function () {
+		fireChapterPreviousEvent();
+	};
+	$: onNext = function () {
+		if (!$isLong) {
+			switch ($direction) {
+				case ReadingDirection.Ltr:
+					next();
+					break;
+				case ReadingDirection.Rtl:
+					previous();
+					break;
+				default:
+					break;
+			}
+		} else {
+			next();
+		}
+	};
+	$: onPrevious = function () {
+		if (!$isLong) {
+			switch ($direction) {
+				case ReadingDirection.Ltr:
+					previous();
+					break;
+				case ReadingDirection.Rtl:
+					next();
+					break;
+				default:
+					break;
+			}
+		} else {
+			previous();
+		}
+	};
+</script>
+
+<ButtonAccent on:click={onPrevious}>
+	<ArrowLeftIcon />
+</ButtonAccent>
+
+<slot />
+
+<ButtonAccent on:click={onNext}>
+	<ArrowRightIcon />
+</ButtonAccent>
