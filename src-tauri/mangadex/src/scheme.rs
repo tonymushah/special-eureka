8890419ecaf9b45b41974827e715b4c70c862665
tonymushah <tonyramanantsalama@gmail.com>
2024-07-25 10:23:18 +0300

    Some scheme handling refactor

--- src-tauri/mangadex/src/scheme.rs
+++ src-tauri/mangadex/src/scheme.rs
@@ -1,49 +1,113 @@
 pub mod chapters;
 pub mod covers;
 
+use std::error::Error;
+
 use tauri::{
-    http::{status::StatusCode, MimeType},
+    http::{status::StatusCode, MimeType, Request},
     plugin::{Builder, Plugin},
-    AppHandle, Runtime,
+    AppHandle, Manager, Runtime, State,
 };
 use url::Url;
 
+use crate::app_state::OfflineAppState;
+
 use self::{chapters::handle_chapters, covers::handle_covers};
 
-pub type SchemeResponse = Result<tauri::http::Response, Box<dyn std::error::Error>>;
+type SchemeResponse = Result<tauri::http::Response, Box<dyn std::error::Error>>;
 
-type SchemeBadResponse = SchemeResponse;
-
-pub fn bad_request(body: Vec<u8>) -> SchemeBadResponse {
-    tauri::http::ResponseBuilder::new()
-        .header("access-control-allow-origin", "*")
-        .status(StatusCode::BAD_REQUEST)
-        .mimetype(MimeType::Txt.to_string().as_str())
-        .body(body)
+#[derive(Debug, Default)]
+enum SchemeResponseError {
+    BadRequest(Vec<u8>),
+    NotFound(Vec<u8>),
+    NotLoaded,
+    #[default]
+    InvalidURLInput,
+    InternalError(Box<dyn Error>),
 }
 
-pub fn not_found(body: Vec<u8>) -> SchemeBadResponse {
-    tauri::http::ResponseBuilder::new()
-        .header("access-control-allow-origin", "*")
-        .status(StatusCode::NOT_FOUND)
-        .mimetype(MimeType::Txt.to_string().as_str())
-        .body(body)
+impl From<SchemeResponseError> for SchemeResponse {
+    fn from(value: SchemeResponseError) -> Self {
+        match value {
+            SchemeResponseError::BadRequest(body) => tauri::http::ResponseBuilder::new()
+                .header("access-control-allow-origin", "*")
+                .status(StatusCode::BAD_REQUEST)
+                .mimetype(MimeType::Txt.to_string().as_str())
+                .body(body),
+            SchemeResponseError::NotFound(body) => tauri::http::ResponseBuilder::new()
+                .header("access-control-allow-origin", "*")
+                .status(StatusCode::NOT_FOUND)
+                .mimetype(MimeType::Txt.to_string().as_str())
+                .body(body),
+            SchemeResponseError::NotLoaded => tauri::http::ResponseBuilder::new()
+                .header("access-control-allow-origin", "*")
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .mimetype(MimeType::Txt.to_string().as_str())
+                .body(b"Offline App State is not loaded".to_vec()),
+            SchemeResponseError::InvalidURLInput => tauri::http::ResponseBuilder::new()
+                .status(StatusCode::NOT_ACCEPTABLE)
+                .header("access-control-allow-origin", "*")
+                .mimetype(MimeType::Txt.to_string().as_str())
+                .body(String::from("invalid url input").into_bytes()),
+            SchemeResponseError::InternalError(error) => tauri::http::ResponseBuilder::new()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .header("access-control-allow-origin", "*")
+                .mimetype(MimeType::Txt.to_string().as_str())
+                .body(error.to_string().into()),
+        }
+    }
 }
 
-pub fn not_loaded() -> SchemeBadResponse {
-    tauri::http::ResponseBuilder::new()
-        .header("access-control-allow-origin", "*")
-        .status(StatusCode::INTERNAL_SERVER_ERROR)
-        .mimetype(MimeType::Txt.to_string().as_str())
-        .body(b"Offline App State is not loaded".to_vec())
+pub type SchemeResponseResult<T, E = SchemeResponseError> = Result<T, E>;
+
+impl<E: Error + 'static> From<E> for SchemeResponseError {
+    fn from(value: E) -> Self {
+        Self::InternalError(Box::new(value))
+    }
 }
 
-pub fn invalid_url_input() -> SchemeResponse {
-    tauri::http::ResponseBuilder::new()
-        .status(StatusCode::NOT_ACCEPTABLE)
-        .header("access-control-allow-origin", "*")
-        .mimetype(MimeType::Txt.to_string().as_str())
-        .body(String::from("invalid url input").into_bytes())
+trait IntoResponse {
+    fn into_response(self) -> SchemeResponse;
+}
+
+impl IntoResponse for SchemeResponseError {
+    fn into_response(self) -> SchemeResponse {
+        self.into()
+    }
+}
+
+impl IntoResponse for SchemeResponse {
+    fn into_response(self) -> SchemeResponse {
+        self
+    }
+}
+
+impl<T: IntoResponse, E: IntoResponse> IntoResponse for Result<T, E> {
+    fn into_response(self) -> SchemeResponse {
+        match self {
+            Ok(d) => d.into_response(),
+            Err(er) => er.into_response(),
+        }
+    }
+}
+
+impl<E: IntoResponse> IntoResponse for Result<tauri::http::Response, E> {
+    fn into_response(self) -> SchemeResponse {
+        match self {
+            Ok(resp) => Ok(resp),
+            Err(err) => err.into_response(),
+        }
+    }
+}
+
+pub fn parse_uri(req: &Request) -> SchemeResponseResult<Url> {
+    Ok(Url::parse(req.uri())?)
+}
+pub fn get_offline_app_state<'a, R: Runtime>(
+    app: &'a AppHandle<R>,
+) -> SchemeResponseResult<State<'a, OfflineAppState>> {
+    app.try_state::<OfflineAppState>()
+        .ok_or(SchemeResponseError::NotLoaded)
 }
 
 pub fn register_scheme<R: Runtime>(
@@ -51,18 +115,20 @@ pub fn register_scheme<R: Runtime>(
     config: serde_json::Value,
 ) -> tauri::plugin::Result<()> {
     Builder::<R, ()>::new("mangadex-graphiql")
-        .register_uri_scheme_protocol("mangadex", move |app, req| {
-            if let Ok(uri) = Url::parse(req.uri()) {
+        .register_uri_scheme_protocol("mangadex", move |app, req| match parse_uri(req) {
+            Ok(uri) => {
                 if uri.domain() == Some("chapter") {
-                    handle_chapters(app, req)
+                    handle_chapters(app, req).into_response()
                 } else if uri.domain() == Some("covers") {
                     handle_covers(app, req)
                 } else {
-                    not_found(String::from("The given domain is not defined").into_bytes())
+                    SchemeResponseError::NotFound(
+                        String::from("The given domain is not defined").into_bytes(),
+                    )
+                    .into()
                 }
-            } else {
-                invalid_url_input()
             }
+            Err(error) => error.into(),
         })
         .build()
         .initialize(app, config)
--- src-tauri/mangadex/src/scheme/chapters.rs
+++ src-tauri/mangadex/src/scheme/chapters.rs
@@ -3,116 +3,120 @@ use std::{io::Read, path::Path};
 use bytes::Bytes;
 use regex::Regex;
 use tauri::{api::http::StatusCode, http::Request, AppHandle, Manager, Runtime};
-use url::Url;
 use uuid::Uuid;
 
-use crate::app_state::OfflineAppState;
+use super::{
+    get_offline_app_state, parse_uri, SchemeResponse, SchemeResponseError, SchemeResponseResult,
+};
 
-use super::{bad_request, invalid_url_input, not_found, not_loaded, SchemeResponse};
-
+#[derive(Debug, Clone, Copy)]
 enum ChapterMode {
     Data,
     DataSaver,
 }
 
-fn not_found_chapter_image(chapter_id: Uuid, filename: &str) -> SchemeResponse {
-    not_found(format!("the given chapter `{chapter_id}`/{filename} is not found").into_bytes())
+fn not_found_chapter_image(chapter_id: Uuid, filename: &str) -> SchemeResponseError {
+    SchemeResponseError::NotFound(
+        format!("the given chapter `{chapter_id}`/{filename} is not found").into_bytes(),
+    )
 }
 
-pub fn handle_chapters<'a, R: Runtime>(app: &'a AppHandle<R>, req: &'a Request) -> SchemeResponse {
-    let uri = {
-        let p_uri = Url::parse(req.uri());
-        if let Ok(i) = p_uri {
-            i
-        } else {
-            return invalid_url_input();
-        }
-    };
-    if let Some(offline_app_state) = app.try_state::<OfflineAppState>() {
+struct ChaptersHandlerOffline<'a, R: Runtime> {
+    app_handle: &'a AppHandle<R>,
+    chapter_id: Uuid,
+    mode: ChapterMode,
+    filename: String,
+}
+
+impl<'a, R: Runtime> ChaptersHandlerOffline<'a, R> {
+    fn handle(&'a self) -> SchemeResponseResult<tauri::http::Response> {
+        let offline_app_state = get_offline_app_state(self.app_handle)?;
         let app_state_read = offline_app_state.blocking_read();
-        if let Some(app_state) = app_state_read.as_ref() {
-            if let Ok(regex) = Regex::new(
-                r"(?x)/(?P<chapter_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/(?P<mode>data|data-saver)/(?P<filename>\w*.*)",
-            ) {
-                if let Some(res) = regex.captures(uri.path()) {
-                    if let Some(chapter_id) = res
-                        .name("chapter_id")
-                        .and_then(|id| Uuid::parse_str(id.as_str()).ok())
-                    {
-                        let chapter_util = app_state.chapter_utils().with_id(chapter_id);
-                        if let Some(mode) = res.name("mode").and_then(|mode| match mode.as_str() {
-                            "data" => Some(ChapterMode::Data),
-                            "data-saver" => Some(ChapterMode::DataSaver),
-                            _ => None,
-                        }) {
-                            if let Some(filename) = res.name("filename").map(|f| f.as_str()) {
-                                let body: Bytes = {
-                                    let mut to_res = Vec::<u8>::new();
-                                    match mode {
-                                        ChapterMode::Data => {
-                                            let res = chapter_util
-                                                .get_data_image(filename)
-                                                .and_then(|mut buf_reader| {
-                                                    buf_reader.read_to_end(&mut to_res)?;
-                                                    Ok(())
-                                                });
-                                            if res.is_err() {
-                                                return not_found_chapter_image(
-                                                    chapter_id, filename,
-                                                );
-                                            }
-                                        }
-                                        ChapterMode::DataSaver => {
-                                            let res = chapter_util
-                                                .get_data_saver_image(filename)
-                                                .and_then(|mut buf_reader| {
-                                                    buf_reader.read_to_end(&mut to_res)?;
-                                                    Ok(())
-                                                });
-                                            if res.is_err() {
-                                                return not_found_chapter_image(
-                                                    chapter_id, filename,
-                                                );
-                                            }
-                                        }
-                                    };
-                                    to_res.into()
-                                };
-                                tauri::http::ResponseBuilder::new()
-                                    .header("access-control-allow-origin", "*")
-                                    .status(StatusCode::OK)
-                                    // TODO Add jpeg mimetype
-                                    .mimetype(
-                                        format!(
-                                            "image/{}",
-                                            Path::new(filename)
-                                                .to_path_buf()
-                                                .extension()
-                                                .and_then(|f| f.to_str())
-                                                .unwrap_or("*")
-                                        )
-                                        .as_str(),
-                                    )
-                                    .body(body.to_vec())
-                            } else {
-                                invalid_url_input()
-                            }
-                        } else {
-                            invalid_url_input()
-                        }
-                    } else {
-                        invalid_url_input()
+        let chapter_util = app_state_read.chapter_utils().with_id(self.chapter_id);
+        let body: Bytes = {
+            let mut to_res = Vec::<u8>::new();
+            match self.mode {
+                ChapterMode::Data => {
+                    let res = chapter_util.get_data_image(self.filename.clone()).and_then(
+                        |mut buf_reader| {
+                            buf_reader.read_to_end(&mut to_res)?;
+                            Ok(())
+                        },
+                    );
+                    if res.is_err() {
+                        return Err(not_found_chapter_image(self.chapter_id, &self.filename));
                     }
-                } else {
-                    invalid_url_input()
                 }
-            } else {
-                bad_request(String::from("Invalid chapters regex pattern").into_bytes())
-            }
-        } else {
-            not_loaded()
-        }
-    } else {
-        not_loaded()
+                ChapterMode::DataSaver => {
+                    let res = chapter_util
+                        .get_data_saver_image(self.filename.clone())
+                        .and_then(|mut buf_reader| {
+                            buf_reader.read_to_end(&mut to_res)?;
+                            Ok(())
+                        });
+                    if res.is_err() {
+                        return not_found_chapter_image(self.chapter_id, &self.filename);
+                    }
+                }
+            };
+            to_res.into()
+        };
+        Ok(tauri::http::ResponseBuilder::new()
+            .header("access-control-allow-origin", "*")
+            .status(StatusCode::OK)
+            // TODO Add jpeg mimetype
+            .mimetype(
+                format!(
+                    "image/{}",
+                    Path::new(&self.filename)
+                        .to_path_buf()
+                        .extension()
+                        .and_then(|f| f.to_str())
+                        .unwrap_or("*")
+                )
+                .as_str(),
+            )
+            .body(body.to_vec())?)
     }
+}
+
+fn get_chapters_params(req: &Request) -> SchemeResponseResult<(Uuid, ChapterMode, String)> {
+    let regex = Regex::new(
+        r"(?x)/(?P<chapter_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/(?P<mode>data|data-saver)/(?P<filename>\w*.*)",
+    )?;
+    let uri = parse_uri(req)?;
+    let captures = regex
+        .captures(uri.path())
+        .ok_or(SchemeResponseError::InvalidURLInput)?;
+    let chapter_id = captures
+        .name("chapter_id")
+        .and_then(|id| Uuid::parse_str(id.as_str()).ok())
+        .ok_or(SchemeResponseError::InvalidURLInput)?;
+    let mode = captures
+        .name("mode")
+        .and_then(|mode| match mode.as_str() {
+            "data" => Some(ChapterMode::Data),
+            "data-saver" => Some(ChapterMode::DataSaver),
+            _ => None,
+        })
+        .ok_or(SchemeResponseError::InvalidURLInput)?;
+    let filename = captures
+        .name("filename")
+        .map(|match_| match_.as_str().into())
+        .ok_or(SchemeResponseError::InvalidURLInput)?;
+    Ok((chapter_id, mode, filename))
+}
+
+pub fn handle_chapters<'a, R: Runtime>(
+    app: &'a AppHandle<R>,
+    req: &'a Request,
+) -> SchemeResponseResult<SchemeResponse> {
+    let (chapter_id, mode, filename) = get_chapters_params(req)?;
+    ChaptersHandlerOffline {
+        chapter_id,
+        app_handle: app,
+        mode,
+        filename: filename.clone(),
+    }
+    .handle()
 }
