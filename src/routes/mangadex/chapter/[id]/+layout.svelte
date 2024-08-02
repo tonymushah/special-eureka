8d0fa0f59e62f095cdaab4b95a9243cf4fbace8e
tonymushah <tonyramanantsalama@gmail.com>
2024-08-03 00:42:00 +0300

    some gql update

---
+++ src/routes/mangadex/chapter/[id]/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import type { LayoutData } from "./$types";
+
+	export let data: LayoutData;
+</script>
+
+<slot />
---
+++ src/routes/mangadex/chapter/[id]/+layout.ts
@@ -0,0 +1,67 @@
+import { graphql } from "@mangadex/gql";
+import type { LayoutLoad } from "./$types";
+import getClient from "@mangadex/gql/urql/getClient";
+import { error } from "@sveltejs/kit";
+
+const query = graphql(`
+	query getChapterPageData($id: UUID!) {
+		chapter {
+			get(id: $id) {
+				id
+				attributes {
+					title
+					volume
+					chapter
+					pages
+					translatedLanguage
+					externalUrl
+					readableAt
+				}
+				relationships {
+					manga {
+						id
+						attributes {
+							title
+						}
+					}
+					scanlationGroups {
+						id
+						attributes {
+							name
+						}
+					}
+					user {
+						id
+						attributes {
+							username
+						}
+					}
+				}
+			}
+		}
+	}
+`);
+
+export const load: LayoutLoad = async ({ params }) => {
+	console.log("Insert");
+	const { id } = params;
+	const client = await getClient();
+	const result = await client.query(query, {
+		id
+	});
+	if (result.data != undefined) {
+		const data = result.data.chapter.get;
+		console.log(data);
+		return {
+			data
+		};
+	} else if (result.error != undefined) {
+		error(500, {
+			message: result.error.message
+		});
+	} else {
+		error(400, {
+			message: "Chapter Not found"
+		});
+	}
+};
