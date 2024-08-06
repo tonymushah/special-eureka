7cdf4ce492dc7b1a0e577738417d808e21a88cd6
tonymushah <tonyramanantsalama@gmail.com>
2024-08-06 21:30:36 +0300

    Some updates

--- src/lib/mangadex/componnents/chapter/page/stories/ChapterPageForStories.svelte
+++ src/lib/mangadex/componnents/chapter/page/stories/ChapterPageForStories.svelte
@@ -3,7 +3,7 @@
 	import { initCurrentChapterData, type CurrentChapterData } from "../contexts/currentChapter";
 	import { initChapterCurrentPageContext } from "../contexts/currentPage";
 	import { initChapterImageContext } from "../contexts/images";
-	import { Direction, ReadingMode } from "@mangadex/gql/graphql";
+	import { Direction, ImageFit, ReadingMode } from "@mangadex/gql/graphql";
 	import { initIsDrawerFixedWritable } from "../contexts/isDrawerFixed";
 	import { initCurrentChapterReadingMode } from "../contexts/currentChapterReadingMode";
 	import ChapterPage from "../ChapterPage.svelte";
@@ -11,6 +11,7 @@
 	import { initLongStripImagesWidthContext } from "../readinMode/longStrip/utils/context/longstrip_images_width";
 	import { initRelatedChapters, type RelatedChapters } from "../contexts/relatedChapters";
 	import { initCurrentChapterDirection } from "../contexts/readingDirection";
+	import { initCurrentChapterImageFit } from "../contexts/imageFit";
 
 	export let chapter: CurrentChapterData;
 	export let images: string[];
@@ -21,6 +22,10 @@
 	export let longStripImageWidth = 0;
 	export let relatedChapters: RelatedChapters = [];
 	export let direction = Direction.Ltr;
+	export let imageFit = ImageFit.Default;
+
+	const imageFitStore = initCurrentChapterImageFit(writable(imageFit));
+	$: imageFitStore.set(imageFit);
 	const lsImgWidth = initLongStripImagesWidthContext(writable(longStripImageWidth));
 	$: lsImgWidth.set(longStripImageWidth);
 	const is = initChapterImageContext(images);
