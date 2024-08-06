e269aeeef5edf6f99a527d3b947d6522e6798cf0
tonymushah <tonyramanantsalama@gmail.com>
2024-08-06 18:59:02 +0300

    Removed unnecessary code stack

--- src/lib/mangadex/componnents/manga/page/chapters/info/links/button/Favicon.svelte
+++ src/lib/mangadex/componnents/manga/page/chapters/info/links/button/Favicon.svelte
@@ -1,37 +1,15 @@
 <script lang="ts">
-	import { XMLParser } from "fast-xml-parser";
 	export let icon: string | undefined = undefined;
 	export let alt: string;
-	function isSvg(icon: string): boolean {
-		const xmlp = new XMLParser();
-		try {
-			const jObj = xmlp.parse(icon);
-			return jObj.svg != undefined;
-		} catch (error) {
-			return false;
-		}
-	}
-	$: isSvg_ = isSvg(icon ?? "");
 </script>
 
 {#if icon != undefined}
-	{#if isSvg_}
-		<div class="ico">
-			{@html icon}
-		</div>
-	{:else}
-		<img class="ico" src={icon} {alt} />
-	{/if}
+	<img class="ico" src={icon} {alt} />
 {/if}
 
 <style lang="scss">
 	.ico {
 		width: 20px;
 		border-radius: 0.25em;
-	}
-	div.ico {
-		display: flex;
-		align-items: center;
-		justify-content: center;
 	}
 </style>
