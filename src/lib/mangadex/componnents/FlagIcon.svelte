ea9cdd89dbedba85546125173b38d56551af865d
tonymushah <tonyramanantsalama@gmail.com>
2024-08-06 09:17:15 +0300

    flag icon updates

--- src/lib/mangadex/componnents/FlagIcon.svelte
+++ src/lib/mangadex/componnents/FlagIcon.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Language } from "@mangadex/gql/graphql";
 	import language_to_flag_icon from "@mangadex/utils/lang/language_to_flag_icon";
-	import "flag-icons/css/flag-icons.min.css";
+	import "flag-icons/sass/flag-icons.scss";
 	import { onMount } from "svelte";
 	export let square = false;
 	export let lang: Language;
