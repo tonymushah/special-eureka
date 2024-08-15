880d8618a92e8ce4df2606334b7c8f49e2c17d7d
tonymushah <tonyramanantsalama@gmail.com>
2024-08-15 11:06:36 +0300

    Added `CoverMangaList`

---
+++ src/lib/mangadex/componnents/manga/list/cover/CoverMangaList.svelte
@@ -0,0 +1,40 @@
+<script lang="ts" context="module">
+	import type { ComponentProps } from "svelte";
+
+	export type CoverMangaListItemProps = ComponentProps<MangaElementBase3> & {
+		id: string;
+	};
+</script>
+
+<script lang="ts">
+	import MangaElementBase3 from "../../base/base3/MangaElementBase3WithReadableCoverImage.svelte";
+	import { goto } from "$app/navigation";
+	import { route } from "$lib/ROUTES";
+
+	export let data: CoverMangaListItemProps[] = [];
+</script>
+
+<section class="cover-list">
+	{#each data as item}
+		<MangaElementBase3
+			{...item}
+			on:click={() => {
+				goto(
+					route("/mangadex/title/[id]", {
+						id: item.id
+					})
+				);
+			}}
+		/>
+	{/each}
+</section>
+
+<style lang="scss">
+	.cover-list {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: stretch;
+		gap: 10px;
+	}
+</style>
