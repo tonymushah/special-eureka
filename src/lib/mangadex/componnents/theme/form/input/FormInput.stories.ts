7b152de5446af5f0c7eff980310caf882ed072a9
tonymushah <tonyramanantsalama@gmail.com>
2024-08-24 14:57:01 +0300

    Added `FormInput` base

---
+++ src/lib/mangadex/componnents/theme/form/input/FormInput.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import "@fontsource/poppins/latin.css";
+
+import MidtoneLine from "./FormInput.svelte";
+import MangadexThemeProviderForStory from "../../MangadexThemeProviderForStory.svelte";
+
+const meta = {
+    decorators: [() => MangadexThemeProviderForStory],
+    title: "MangaDex/theme/form/text-input",
+    component: MidtoneLine,
+    tags: ["autodocs"]
+} satisfies Meta<MidtoneLine>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
