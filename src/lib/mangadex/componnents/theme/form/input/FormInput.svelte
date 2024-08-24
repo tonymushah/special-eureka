4efc71a44d549513402b4e436addf7da6dd5e4bc
tonymushah <tonyramanantsalama@gmail.com>
2024-08-24 15:33:55 +0300

    FormInputComponnents

---
+++ src/lib/mangadex/componnents/theme/form/input/FormInput.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type { HTMLInputTypeAttribute } from "svelte/elements";
+
+    export let type: HTMLInputTypeAttribute = "text";
+    export let value: any = ""
+    export let name: string | null | undefined;
+</script>
+
+<input {type} {value} {name}/>
+
+<style lang="scss">
+    input {
+        transition: background-color 0.25s ease-in-out;
+        color: var(--text-color);
+        background-color: var(--accent);
+        box-shadow: 0px 0px 2px var(--accent);
+        font-family: var(--fonts);
+        font-size: var(--font-size);
+        border-style: solid;
+        border-radius: 0.25em;
+        padding: 5px;
+        border-color: var(--mid-tone);
+    }
+    input:hover {
+        background-color: var(--accent-hover);
+    }
+    input:focus {
+        background-color: var(--accent-active);
+        border-style: dashed;
+    }
+    input:focus-visible {
+        //border-style: dashed;
+        outline: none;
+    }
+</style>
