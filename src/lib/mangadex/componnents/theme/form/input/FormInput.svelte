f151a5d931846c7927f6ac72560ca4a189f1af4e
tonymushah <tonyramanantsalama@gmail.com>
2024-08-24 21:07:38 +0300

    added `widthFull` in `FormInput`

--- src/lib/mangadex/componnents/theme/form/input/FormInput.svelte
+++ src/lib/mangadex/componnents/theme/form/input/FormInput.svelte
@@ -1,35 +1,39 @@
 <script lang="ts">
 	import type { HTMLInputTypeAttribute } from "svelte/elements";
 
-    export let type: HTMLInputTypeAttribute = "text";
-    export let value: any = ""
-    export let name: string | null | undefined = undefined;
+	export let type: HTMLInputTypeAttribute = "text";
+	export let value: any = "";
+	export let name: string | null | undefined = undefined;
+	export let widthFull = false;
 </script>
 
-<input {type} {value} {name}/>
+<input {type} {value} {name} class:widthFull />
 
 <style lang="scss">
-    input {
-        transition: background-color 0.25s ease-in-out;
-        color: var(--text-color);
-        background-color: var(--accent);
-        box-shadow: 0px 0px 2px var(--accent);
-        font-family: var(--fonts);
-        font-size: var(--font-size);
-        border-style: solid;
-        border-radius: 0.25em;
-        padding: 5px;
-        border-color: var(--mid-tone);
-    }
-    input:hover {
-        background-color: var(--accent-hover);
-    }
-    input:focus {
-        background-color: var(--accent-active);
-        border-style: dashed;
-    }
-    input:focus-visible {
-        //border-style: dashed;
-        outline: none;
-    }
+	input {
+		transition: background-color 0.25s ease-in-out;
+		color: var(--text-color);
+		background-color: var(--accent);
+		box-shadow: 0px 0px 2px var(--accent);
+		font-family: var(--fonts);
+		font-size: var(--font-size);
+		border-style: solid;
+		border-radius: 0.25em;
+		padding: 5px;
+		border-color: var(--mid-tone);
+	}
+	input.widthFull {
+		width: 100%;
+	}
+	input:hover {
+		background-color: var(--accent-hover);
+	}
+	input:focus {
+		background-color: var(--accent-active);
+		border-style: dashed;
+	}
+	input:focus-visible {
+		//border-style: dashed;
+		outline: none;
+	}
 </style>
