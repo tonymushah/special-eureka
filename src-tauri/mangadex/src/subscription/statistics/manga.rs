d53e6126cc0fe5bbdd011c1323bdf2667941d5ed
tonymushah <tonyramanantsalama@gmail.com>
2024-08-26 01:20:13 +0300

    refactored `MangaStatisticsSubscriptions`

--- src-tauri/mangadex/src/subscription/statistics/manga.rs
+++ src-tauri/mangadex/src/subscription/statistics/manga.rs
@@ -1,12 +1,11 @@
 use crate::Result;
 use async_graphql::{Context, Subscription};
-use tokio_stream::Stream;
+use tokio_stream::{Stream, StreamExt};
 use uuid::Uuid;
 
 use crate::objects::statistics::manga::MangaStatisticsAttributes;
-use async_stream::stream;
 
-use crate::subscription::{init_watch_subscription, sub_sleep};
+use crate::subscription::utils::WatchSubscriptionStream;
 
 #[derive(Debug, Clone, Copy)]
 pub struct MangaStatisticsSubscriptions;
@@ -19,44 +18,16 @@ impl MangaStatisticsSubscriptions {
         manga_id: Uuid,
         sub_id: Uuid,
     ) -> Result<impl Stream<Item = MangaStatisticsAttributes> + 'ctx> {
-        let (watches, should_end, unlisten, window, is_initial_loading) =
-            init_watch_subscription::<tauri::Wry>(ctx, sub_id)?;
-        let cover_sub = watches.manga_statistics.subscribe();
-        Ok(stream! {
-            loop {
-                if is_initial_loading.read().map(|read| *read).unwrap_or(false) {
-                    if let Ok(mut write) = is_initial_loading.write() {
-                        *write = false;
-                    }
-                    let borrow = {
-                        cover_sub.borrow().as_ref().copied()
-                    };
-                    if let Some(data) = borrow {
-                        if data.id == manga_id {
-                            yield data.attributes
-                        }
-                    }
-                } else if !should_end.read().map(|read| *read).unwrap_or(true) {
-                    if let Ok(has_changed) = cover_sub.has_changed() {
-                        if has_changed {
-                            let borrow = {
-                                cover_sub.borrow().as_ref().copied()
-                            };
-                            if let Some(data) = borrow {
-                                if data.id == manga_id {
-                                    yield data.attributes
-                                }
-                            }
-                        }
-                    }else {
-                        break;
-                    }
-                } else{
-                    break;
-                }
-                sub_sleep().await;
-            }
-            window.unlisten(unlisten);
-        })
+        Ok(
+            WatchSubscriptionStream::<tauri::Wry, _>::from_async_graphql_context(
+                ctx,
+                sub_id,
+                |watches| watches.manga_statistics.subscribe(),
+            )?
+            .filter_map(move |v| {
+                v.filter(|data| data.id == manga_id)
+                    .map(|data| data.attributes)
+            }),
+        )
     }
 }
