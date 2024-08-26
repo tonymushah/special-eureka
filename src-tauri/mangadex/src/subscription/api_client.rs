2a7186578c67d2881be583f2206b01e20b553fd4
tonymushah <tonyramanantsalama@gmail.com>
2024-08-26 13:38:05 +0300

    Added `ApiClientSubscriptions`

--- src-tauri/mangadex/src/subscription/api_client.rs
+++ src-tauri/mangadex/src/subscription/api_client.rs
@@ -1,12 +1,11 @@
 use crate::Result;
 use async_graphql::{Context, Subscription};
-use tokio_stream::Stream;
+use tokio_stream::{Stream, StreamExt};
 use uuid::Uuid;
 
 use crate::objects::api_client::attributes::ApiClientAttributes;
-use async_stream::stream;
 
-use super::{init_watch_subscription, sub_sleep};
+use super::utils::WatchSubscriptionStream;
 
 #[derive(Debug, Clone, Copy)]
 pub struct ApiClientSubscriptions;
@@ -19,44 +18,13 @@ impl ApiClientSubscriptions {
         api_client_id: Uuid,
         sub_id: Uuid,
     ) -> Result<impl Stream<Item = ApiClientAttributes> + 'ctx> {
-        let (watches, should_end, unlisten, window, is_initial_loading) =
-            init_watch_subscription::<tauri::Wry>(ctx, sub_id)?;
-        let api_client_sub = watches.api_client.subscribe();
-        Ok(stream! {
-            loop {
-                if is_initial_loading.read().map(|read| *read).unwrap_or(false) {
-                    if let Ok(mut write) = is_initial_loading.write() {
-                        *write = false;
-                    }
-                    let borrow = {
-                        api_client_sub.borrow().as_ref().cloned()
-                    };
-                    if let Some(data) = borrow {
-                        if data.id == api_client_id {
-                            yield data.attributes.clone()
-                        }
-                    }
-                } else if !should_end.read().map(|read| *read).unwrap_or(true) {
-                    if let Ok(has_changed) = api_client_sub.has_changed() {
-                        if has_changed {
-                            let borrow = {
-                                api_client_sub.borrow().as_ref().cloned()
-                            };
-                            if let Some(data) = borrow {
-                                if data.id == api_client_id {
-                                    yield data.attributes.clone()
-                                }
-                            }
-                        }
-                    }else {
-                        break;
-                    }
-                } else{
-                    break;
-                }
-                sub_sleep().await;
-            }
-            window.unlisten(unlisten);
-        })
+        Ok(
+            WatchSubscriptionStream::<tauri::Wry, _>::from_async_graphql_context(
+                ctx,
+                sub_id,
+                |w| w.api_client.subscribe(),
+            )?
+            .filter_map(move |data| data.filter(|i| i.id == api_client_id).map(|i| i.attributes)),
+        )
     }
 }
