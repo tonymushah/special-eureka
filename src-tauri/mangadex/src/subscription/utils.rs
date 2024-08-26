708b12715aec87d69cd0e00f36255a3d53a212e3
tonymushah <tonyramanantsalama@gmail.com>
2024-08-26 14:01:17 +0300

    added `FilterWatchDataById` and `FilterWatchOptionDataById`

--- src-tauri/mangadex/src/subscription/utils.rs
+++ src-tauri/mangadex/src/subscription/utils.rs
@@ -1,3 +1,37 @@
 pub mod watch;
 
+use tokio_stream::{Stream, StreamExt};
+use uuid::Uuid;
 pub use watch::WatchSubscriptionStream;
+
+use crate::utils::watch::WatcherInnerData;
+
+pub trait FilterWatchDataById: Stream {
+    type OutputItem;
+    fn filter_by_id(self, id: Uuid) -> impl Stream<Item = Self::OutputItem>;
+}
+
+pub trait FilterWatchOptionDataById: Stream {
+    type OutputItem;
+    fn option_filter_by_id(self, id: Uuid) -> impl Stream<Item = Self::OutputItem>;
+}
+
+impl<S, T> FilterWatchDataById for S
+where
+    S: Stream<Item = WatcherInnerData<T>>,
+{
+    type OutputItem = T;
+    fn filter_by_id(self, id: Uuid) -> impl Stream<Item = Self::OutputItem> {
+        self.filter(move |data| data.id == id).map(|d| d.attributes)
+    }
+}
+
+impl<S, T> FilterWatchOptionDataById for S
+where
+    S: Stream<Item = Option<WatcherInnerData<T>>>,
+{
+    type OutputItem = T;
+    fn option_filter_by_id(self, id: Uuid) -> impl Stream<Item = Self::OutputItem> {
+        self.filter_map(move |v| v.filter(|data| data.id == id).map(|data| data.attributes))
+    }
+}
