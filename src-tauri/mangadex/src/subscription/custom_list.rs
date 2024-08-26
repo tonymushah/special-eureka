64ae612f0c511f33711f9e6f676a53524a068659
tonymushah <tonyramanantsalama@gmail.com>
2024-08-26 14:35:10 +0300

    Refactored `CustomListSubscriptions`

--- src-tauri/mangadex/src/subscription/custom_list.rs
+++ src-tauri/mangadex/src/subscription/custom_list.rs
@@ -4,9 +4,8 @@ use tokio_stream::Stream;
 use uuid::Uuid;
 
 use crate::objects::custom_list::attributes::CustomListAttributes;
-use async_stream::stream;
 
-use super::{init_watch_subscription, sub_sleep};
+use super::utils::{FilterWatchOptionDataById, WatchSubscriptionStream};
 
 #[derive(Debug, Clone, Copy)]
 pub struct CustomListSubscriptions;
@@ -19,44 +18,13 @@ impl CustomListSubscriptions {
         custom_list_id: Uuid,
         sub_id: Uuid,
     ) -> Result<impl Stream<Item = CustomListAttributes> + 'ctx> {
-        let (watches, should_end, unlisten, window, is_initial_loading) =
-            init_watch_subscription::<tauri::Wry>(ctx, sub_id)?;
-        let custom_list_sub = watches.custom_list.subscribe();
-        Ok(stream! {
-            loop {
-                if is_initial_loading.read().map(|read| *read).unwrap_or(false) {
-                    if let Ok(mut write) = is_initial_loading.write() {
-                        *write = false;
-                    }
-                    let borrow = {
-                        custom_list_sub.borrow().as_ref().cloned()
-                    };
-                    if let Some(data) = borrow {
-                        if data.id == custom_list_id {
-                            yield data.attributes.clone();
-                        }
-                    }
-                } else if !should_end.read().map(|read| *read).unwrap_or(true) {
-                    if let Ok(has_changed) = custom_list_sub.has_changed() {
-                        if has_changed {
-                            let borrow = {
-                                custom_list_sub.borrow().as_ref().cloned()
-                            };
-                            if let Some(data) = borrow {
-                                if data.id == custom_list_id {
-                                    yield data.attributes.clone()
-                                }
-                            }
-                        }
-                    }else {
-                        break;
-                    }
-                } else{
-                    break;
-                }
-                sub_sleep().await;
-            }
-            window.unlisten(unlisten);
-        })
+        Ok(
+            WatchSubscriptionStream::<tauri::Wry, _>::from_async_graphql_context(
+                ctx,
+                sub_id,
+                |w| w.custom_list.subscribe(),
+            )?
+            .option_filter_by_id(custom_list_id),
+        )
     }
 }
