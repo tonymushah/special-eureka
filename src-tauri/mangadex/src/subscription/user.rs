d99181a080a36e5e9d0dcdcdbdb97c3de8fdea42
tonymushah <tonyramanantsalama@gmail.com>
2024-08-26 15:44:17 +0300

    Refactored `UserSubscriptions`

--- src-tauri/mangadex/src/subscription/user.rs
+++ src-tauri/mangadex/src/subscription/user.rs
@@ -6,9 +6,8 @@ use tokio_stream::Stream;
 use uuid::Uuid;
 
 use crate::objects::user::attributes::UserAttributes;
-use async_stream::stream;
 
-use super::{init_watch_subscription, sub_sleep};
+use super::utils::{FilterWatchOptionDataById, WatchSubscriptionStream};
 
 #[derive(Debug, Clone, Copy)]
 pub struct UserSubscriptions;
@@ -21,44 +20,13 @@ impl UserSubscriptions {
         user_id: Uuid,
         sub_id: Uuid,
     ) -> Result<impl Stream<Item = UserAttributes> + 'ctx> {
-        let (watches, should_end, unlisten, window, is_initial_loading) =
-            init_watch_subscription::<tauri::Wry>(ctx, sub_id)?;
-        let user_sub = watches.user.subscribe();
-        Ok(stream! {
-            loop {
-                if is_initial_loading.read().map(|read| *read).unwrap_or(false) {
-                    if let Ok(mut write) = is_initial_loading.write() {
-                        *write = false;
-                    }
-                    let borrow = {
-                        user_sub.borrow().as_ref().cloned()
-                    };
-                    if let Some(data) = borrow {
-                        if data.id == user_id {
-                            yield data.attributes.clone()
-                        }
-                    }
-                } else if !should_end.read().map(|read| *read).unwrap_or(true) {
-                    if let Ok(has_changed) = user_sub.has_changed() {
-                        if has_changed {
-                            let borrow = {
-                                user_sub.borrow().as_ref().cloned()
-                            };
-                            if let Some(data) = borrow {
-                                if data.id == user_id {
-                                    yield data.attributes.clone()
-                                }
-                            }
-                        }
-                    }else {
-                        break;
-                    }
-                } else{
-                    break;
-                }
-                sub_sleep().await;
-            }
-            window.unlisten(unlisten);
-        })
+        Ok(
+            WatchSubscriptionStream::<tauri::Wry, _>::from_async_graphql_context(
+                ctx,
+                sub_id,
+                |w| w.user.subscribe(),
+            )?
+            .option_filter_by_id(user_id),
+        )
     }
 }
