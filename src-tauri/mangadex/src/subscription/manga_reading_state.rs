64d9c92285df16312fb8fe87e631e87a32c155aa
tonymushah <tonyramanantsalama@gmail.com>
2024-08-26 15:05:14 +0300

    Refactored `MangaReadingStatusSubscriptions`

--- src-tauri/mangadex/src/subscription/manga_reading_state.rs
+++ src-tauri/mangadex/src/subscription/manga_reading_state.rs
@@ -5,9 +5,7 @@ use uuid::Uuid;
 
 use mangadex_api_types_rust::ReadingStatus;
 
-use async_stream::stream;
-
-use super::{init_watch_subscription, sub_sleep};
+use super::utils::{FilterWatchOptionDataById, WatchSubscriptionStream};
 
 #[derive(Debug, Clone, Copy)]
 pub struct MangaReadingStatusSubscriptions;
@@ -20,44 +18,13 @@ impl MangaReadingStatusSubscriptions {
         manga_id: Uuid,
         sub_id: Uuid,
     ) -> Result<impl Stream<Item = Option<ReadingStatus>> + 'ctx> {
-        let (watches, should_end, unlisten, window, is_initial_loading) =
-            init_watch_subscription::<tauri::Wry>(ctx, sub_id)?;
-        let manga_reading_state_sub = watches.manga_reading_state.subscribe();
-        Ok(stream! {
-            loop {
-                if is_initial_loading.read().map(|read| *read).unwrap_or(false) {
-                    if let Ok(mut write) = is_initial_loading.write() {
-                        *write = false;
-                    }
-                    let borrow = {
-                        manga_reading_state_sub.borrow().as_ref().copied()
-                    };
-                    if let Some(data) = borrow {
-                        if data.id == manga_id {
-                            yield data.attributes
-                        }
-                    }
-                } else if !should_end.read().map(|read| *read).unwrap_or(true) {
-                    if let Ok(has_changed) = manga_reading_state_sub.has_changed() {
-                        if has_changed {
-                            let borrow = {
-                                manga_reading_state_sub.borrow().as_ref().copied()
-                            };
-                            if let Some(data) = borrow {
-                                if data.id == manga_id {
-                                    yield data.attributes
-                                }
-                            }
-                        }
-                    }else {
-                        break;
-                    }
-                } else{
-                    break;
-                }
-                sub_sleep().await;
-            }
-            window.unlisten(unlisten);
-        })
+        Ok(
+            WatchSubscriptionStream::<tauri::Wry, _>::from_async_graphql_context(
+                ctx,
+                sub_id,
+                |w| w.manga_reading_state.subscribe(),
+            )?
+            .option_filter_by_id(manga_id),
+        )
     }
 }
