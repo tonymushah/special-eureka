cb6c2c0d54e593d3357222094c46f33a4d0c3d3b
tonymushah <tonyramanantsalama@gmail.com>
2024-08-26 13:29:15 +0300

    Refactored `UploadSessionSubscriptions`

--- src-tauri/mangadex/src/subscription/upload/session.rs
+++ src-tauri/mangadex/src/subscription/upload/session.rs
@@ -1,12 +1,11 @@
 use crate::Result;
 use async_graphql::{Context, Subscription};
-use tokio_stream::Stream;
+use tokio_stream::{Stream, StreamExt};
 use uuid::Uuid;
 
 use crate::objects::upload::session::attributes::UploadSessionAttributes;
-use async_stream::stream;
 
-use crate::subscription::{init_watch_subscription, sub_sleep};
+use crate::subscription::utils::WatchSubscriptionStream;
 
 #[derive(Debug, Clone, Copy)]
 pub struct UploadSessionSubscriptions;
@@ -19,44 +18,16 @@ impl UploadSessionSubscriptions {
         upload_session_id: Uuid,
         sub_id: Uuid,
     ) -> Result<impl Stream<Item = UploadSessionAttributes> + 'ctx> {
-        let (watches, should_end, unlisten, window, is_initial_loading) =
-            init_watch_subscription::<tauri::Wry>(ctx, sub_id)?;
-        let upload_session_sub = watches.upload_session.subscribe();
-        Ok(stream! {
-            loop {
-                if is_initial_loading.read().map(|read| *read).unwrap_or(false) {
-                    if let Ok(mut write) = is_initial_loading.write() {
-                        *write = false;
-                    }
-                    let borrow = {
-                        upload_session_sub.borrow().as_ref().cloned()
-                    };
-                    if let Some(data) = borrow {
-                        if data.id == upload_session_id {
-                            yield data.attributes
-                        }
-                    }
-                } else if !should_end.read().map(|read| *read).unwrap_or(true) {
-                    if let Ok(has_changed) = upload_session_sub.has_changed() {
-                        if has_changed {
-                            let borrow = {
-                                upload_session_sub.borrow().as_ref().cloned()
-                            };
-                            if let Some(data) = borrow {
-                                if data.id == upload_session_id {
-                                    yield data.attributes
-                                }
-                            }
-                        }
-                    }else {
-                        break;
-                    }
-                } else{
-                    break;
-                }
-                sub_sleep().await;
-            }
-            window.unlisten(unlisten);
-        })
+        Ok(
+            WatchSubscriptionStream::<tauri::Wry, _>::from_async_graphql_context(
+                ctx,
+                sub_id,
+                |w| w.upload_session.subscribe(),
+            )?
+            .filter_map(move |v| {
+                v.filter(|data| data.id == upload_session_id)
+                    .map(|data| data.attributes)
+            }),
+        )
     }
 }
