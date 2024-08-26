949c3e4acbb17ba0fcc72923ddcde7db822bd314
tonymushah <tonyramanantsalama@gmail.com>
2024-08-26 14:27:32 +0300

    Refactored `ChapterSubscriptions`

--- src-tauri/mangadex/src/subscription/chapter.rs
+++ src-tauri/mangadex/src/subscription/chapter.rs
@@ -1,12 +1,11 @@
 use crate::Result;
 use async_graphql::{Context, Subscription};
-use tokio_stream::Stream;
+use tokio_stream::{Stream, StreamExt};
 use uuid::Uuid;
 
 use crate::objects::chapter::attributes::ChapterAttributes;
-use async_stream::stream;
 
-use super::{init_watch_subscription, sub_sleep};
+use super::utils::{FilterWatchOptionDataById, WatchSubscriptionStream};
 
 #[derive(Debug, Clone, Copy)]
 pub struct ChapterSubscriptions;
@@ -19,44 +18,14 @@ impl ChapterSubscriptions {
         chapter_id: Uuid,
         sub_id: Uuid,
     ) -> Result<impl Stream<Item = ChapterAttributes> + 'ctx> {
-        let (watches, should_end, unlisten, window, is_initial_loading) =
-            init_watch_subscription::<tauri::Wry>(ctx, sub_id)?;
-        let chapter_sub = watches.chapter.subscribe();
-        Ok(stream! {
-            loop {
-                if is_initial_loading.read().map(|read| *read).unwrap_or(false) {
-                    if let Ok(mut write) = is_initial_loading.write() {
-                        *write = false;
-                    }
-                    let borrow = {
-                        chapter_sub.borrow().as_ref().cloned()
-                    };
-                    if let Some(data) = borrow {
-                        if data.id == chapter_id {
-                            yield data.attributes.inner_data()
-                        }
-                    }
-                } else if !should_end.read().map(|read| *read).unwrap_or(true) {
-                    if let Ok(has_changed) = chapter_sub.has_changed() {
-                        if has_changed {
-                            let borrow = {
-                                chapter_sub.borrow().as_ref().cloned()
-                            };
-                            if let Some(data) = borrow {
-                                if data.id == chapter_id {
-                                    yield data.attributes.inner_data()
-                                }
-                            }
-                        }
-                    }else {
-                        break;
-                    }
-                } else{
-                    break;
-                }
-                sub_sleep().await;
-            }
-            window.unlisten(unlisten);
-        })
+        Ok(
+            WatchSubscriptionStream::<tauri::Wry, _>::from_async_graphql_context(
+                ctx,
+                sub_id,
+                |w| w.chapter.subscribe(),
+            )?
+            .option_filter_by_id(chapter_id)
+            .map(|data| data.inner_data()),
+        )
     }
 }
