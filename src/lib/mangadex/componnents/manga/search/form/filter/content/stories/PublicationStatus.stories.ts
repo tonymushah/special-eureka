7a37cd8fddb37591fa7aa5cd903222c7f4d228f3
tonymushah <tonyramanantsalama@gmail.com>
2024-08-28 00:53:59 +0300

    Added `PublicationStatusForm`

---
+++ src/lib/mangadex/componnents/manga/search/form/filter/content/stories/PublicationStatus.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import "@fontsource/poppins/latin.css";
+
+import PublicationStatusForm from "./PublicationStatusForStories.svelte";
+import MangadexThemeProviderForStory from "@mangadex/componnents/theme/MangadexThemeProviderForStory.svelte";
+
+const meta = {
+    decorators: [() => MangadexThemeProviderForStory],
+    title: "MangaDex/manga/search/Form/Dialog/Content/Status",
+    component: PublicationStatusForm,
+    tags: ["autodocs"]
+} satisfies Meta<PublicationStatusForm>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
