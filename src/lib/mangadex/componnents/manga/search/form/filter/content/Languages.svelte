bea769f49c3384c329309b7e3f88e1a7405d07f5
tonymushah <tonyramanantsalama@gmail.com>
2024-08-29 14:04:36 +0300

    Added `LanguagesForm`

---
+++ src/lib/mangadex/componnents/manga/search/form/filter/content/Languages.svelte
@@ -0,0 +1,18 @@
+<script>
+	import {
+		getMangaSearchAvailableTranslatedLanguageContextWritableStore,
+		getMangaSearchExcludedOriginalLanguageContextWritableStore,
+		getMangaSearchOriginalLanguageContextWritableStore
+	} from "../contexts/languages";
+	import LanguagesBase from "./languages/LanguagesBase.svelte";
+
+	const original = getMangaSearchOriginalLanguageContextWritableStore();
+	const excludedOriginal = getMangaSearchExcludedOriginalLanguageContextWritableStore();
+	const availableTranslation = getMangaSearchAvailableTranslatedLanguageContextWritableStore();
+</script>
+
+<LanguagesBase selecteds={original} title="Original Language" />
+
+<LanguagesBase selecteds={excludedOriginal} title="Excluded Original Language" />
+
+<LanguagesBase selecteds={availableTranslation} title="Available Translated Language" />
