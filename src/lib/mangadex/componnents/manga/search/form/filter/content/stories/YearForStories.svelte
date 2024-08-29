c09525e0674c3305728e7f5b3c51f7fc133131af
tonymushah <tonyramanantsalama@gmail.com>
2024-08-29 14:33:04 +0300

    Added `YearForm`

---
+++ src/lib/mangadex/componnents/manga/search/form/filter/content/stories/YearForStories.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { writable } from "svelte/store";
+	import { initMangaSearchYearContextStore } from "../../contexts/year";
+	import Year from "../Year.svelte";
+
+	const year = initMangaSearchYearContextStore(writable(null));
+</script>
+
+<Year />
