6fbf862fb97fafab5bf752088df3ea60b8bceca1
tonymushah <tonyramanantsalama@gmail.com>
2024-09-06 13:31:21 +0300

    Added `TagModeToggle`

---
+++ src/lib/mangadex/componnents/manga/search/form/filter/content/tagMode/TagModeToggle.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import "@fontsource/poppins/latin.css";
+
+import TagModeToggle from "./TagModeToggle.svelte";
+import MangadexThemeProviderForStory from "@mangadex/componnents/theme/MangadexThemeProviderForStory.svelte";
+import { writable } from "svelte/store";
+import { TagSearchMode } from "@mangadex/gql/graphql";
+
+const meta = {
+    decorators: [() => MangadexThemeProviderForStory],
+    title: "MangaDex/manga/search/Form/Dialog/Content/TagMode/Toggle",
+    component: TagModeToggle,
+    tags: ["autodocs"]
+} satisfies Meta<TagModeToggle>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        writableTag: writable(TagSearchMode.And)
+    }
+};
