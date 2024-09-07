179faf288630539ed83343193f2627ba94f45759
tonymushah <tonyramanantsalama@gmail.com>
2024-09-07 03:12:12 +0300

    Added `AuthorsSearchSelect`

---
+++ src/lib/mangadex/componnents/manga/search/form/filter/content/authors/stories/AuthorSearchSelect.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import "@fontsource/poppins/latin.css";
+
+import AuthorSearchBaseForm from "./AuthorSearchSelectForStories.svelte";
+import MangadexThemeProviderForStory from "@mangadex/componnents/theme/MangadexThemeProviderForStory.svelte";
+
+const meta = {
+    decorators: [() => MangadexThemeProviderForStory],
+    title: "MangaDex/manga/search/Form/Dialog/Content/AuthorSearch/Base",
+    component: AuthorSearchBaseForm,
+    tags: ["autodocs"]
+} satisfies Meta<AuthorSearchBaseForm>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
