efedde9dd21855ab0d7c4ab563cc92e073b2a207
tonymushah <tonyramanantsalama@gmail.com>
2024-09-12 05:08:32 +0300

    Added `MangaDexContextDataProvider`

---
+++ src/lib/mangadex/componnents/MangaDexContextDataProvider.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import mangaListStyleStore from "./manga/list/contexts/gql";
+	import { initMangaListStyleContext } from "./manga/list/contexts/style";
+
+	initMangaListStyleContext(mangaListStyleStore);
+</script>
+
+<slot />
