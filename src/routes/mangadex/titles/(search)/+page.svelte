758e86c2e1e145e4d8fcdfe06b0ebbc8d0a8258d
tonymushah <tonyramanantsalama@gmail.com>
2024-09-12 13:38:21 +0300

    Finally added `advanced search`

--- src/routes/mangadex/titles/(search)/+page.svelte
+++ src/routes/mangadex/titles/(search)/+page.svelte
@@ -3,14 +3,59 @@
 	import Title from "@mangadex/componnents/theme/texts/title/Title.svelte";
 	import SearchContent from "./SearchContent.svelte";
 	import MidToneLine from "@mangadex/componnents/theme/lines/MidToneLine.svelte";
+	import type { PageData } from "./$types";
+	import defaultMangaSearchParams, {
+		toMangaListParams,
+		type MangaSearchParams
+	} from "@mangadex/componnents/manga/search/form/state";
+	import { derived, writable } from "svelte/store";
+	import { TagOptionState } from "@mangadex/componnents/manga/search/form/filter/contexts/tags";
+	import type { MangaListParams } from "@mangadex/gql/graphql";
+	export let data: PageData;
+	const defaultParams = writable(defaultMangaSearchParams());
+	$: {
+		defaultParams.update((p) => {
+			data.tags?.forEach((tag) => {
+				p.filter.tags.set(tag.id, {
+					state: TagOptionState.NONE,
+					name: tag.name,
+					group: tag.group
+				});
+			});
+			return p;
+		});
+	}
+	const currentSearchParams = writable<MangaSearchParams | undefined>(undefined);
+	const listParams = derived(currentSearchParams, ($p) => {
+		if ($p) {
+			return toMangaListParams($p);
+		} else {
+			return {} satisfies MangaListParams;
+		}
+	});
+	let realTime = false;
+	const offlineStore = derived(currentSearchParams, ($p) => $p?.offlineOnly ?? false);
 </script>
 
 <section class="title">
 	<Title>Advanced Titles Search</Title>
 </section>
 
-<MangaSearchForm />
+<MangaSearchForm
+	bind:realTime
+	defaultParams={$defaultParams}
+	on:change={({ detail }) => {
+		if (realTime) {
+			currentSearchParams.set(detail);
+		}
+	}}
+	on:submit={({ detail }) => {
+		if (!realTime) {
+			currentSearchParams.set(detail);
+		}
+	}}
+/>
 
 <MidToneLine />
 
-<SearchContent />
+<SearchContent params={listParams} {offlineStore} />
---
+++ src/routes/mangadex/titles/(search)/SearchContent.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+	import MangaList from "@mangadex/componnents/manga/list/MangaList.svelte";
+	import type { MangaListContentItemProps } from "@mangadex/componnents/manga/list/MangaListContent.svelte";
+	import type { MangaListParams } from "@mangadex/gql/graphql";
+	import { getContextClient } from "@urql/svelte";
+	import { debounce, type DebouncedFunc } from "lodash";
+	import { onDestroy, onMount } from "svelte";
+	import { derived, get, writable, type Readable } from "svelte/store";
+	import type { IMangaSearchResult } from "./search";
+	import executeSearchQuery from "./search";
+	let isFetching = false;
+	const client = getContextClient();
+	const titles = writable<MangaListContentItemProps[]>([]);
+	const debounce_wait = 450;
+	export let params: Readable<MangaListParams>;
+	export let offlineStore: Readable<boolean>;
+	const p_p_offline = derived([params, offlineStore], (merged) => merged);
+	let debounce_func: DebouncedFunc<() => Promise<void>> | undefined = undefined;
+	const currentResult = writable<IMangaSearchResult | undefined>(undefined);
+	onMount(() =>
+		p_p_offline.subscribe(([p, offline]) => {
+			debounce_func?.flush();
+			currentResult.set(undefined);
+
+			debounce_func = debounce(async () => {
+				isFetching = true;
+				try {
+					const res = await executeSearchQuery(client, p, offline);
+					currentResult.set(res);
+				} finally {
+					isFetching = false;
+				}
+			}, debounce_wait);
+			debounce_func();
+		})
+	);
+	onMount(() =>
+		currentResult.subscribe((inner) => {
+			console.log("changed current result", inner);
+			if (inner) {
+				titles.update((ts) => {
+					ts.push(...inner.data);
+					return ts;
+				});
+			} else {
+				titles.set([]);
+			}
+		})
+	);
+	const observer = new IntersectionObserver(
+		(entries) => {
+			if (!isFetching && $currentResult?.hasNext()) {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						debounce_func?.flush();
+						console.log("should fetch next");
+						debounce_func = debounce(async () => {
+							isFetching = true;
+							try {
+								const res = await $currentResult.next();
+								currentResult.set(res);
+							} finally {
+								isFetching = false;
+							}
+						}, debounce_wait);
+						debounce_func();
+					}
+				});
+			}
+		},
+		{
+			threshold: 1.0
+		}
+	);
+	onDestroy(() => {
+		debounce_func?.cancel();
+		observer.disconnect();
+	});
+	let to_obserce_bind: HTMLElement | undefined = undefined;
+	$: {
+		if (to_obserce_bind) {
+			observer.unobserve(to_obserce_bind);
+			observer.observe(to_obserce_bind);
+		}
+	}
+	$: console.log(`isFetching: ${isFetching}`);
+</script>
+
+<MangaList list={$titles}></MangaList>
+
+<div bind:this={to_obserce_bind}>Infinite scroll WoOoOoOoO!</div>
