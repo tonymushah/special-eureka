ef9f61da298a573654885c4e75915436ab0c97b8
tonymushah <tonyramanantsalama@gmail.com>
2024-09-12 06:13:42 +0300

    some outer layout update

--- src/routes/mangadex/titles/(search)/+layout.svelte
+++ src/routes/mangadex/titles/(search)/+layout.svelte
@@ -5,4 +5,17 @@
 	initMangaSearchAuthorSearchFetcher(gqlAuthorFetcher);
 </script>
 
-<slot />
+<div>
+	<slot />
+</div>
+
+<style lang="scss">
+	div {
+		--layout-margin-x: 1.25em;
+		--layout-margin-y: 0.5em;
+		margin-left: var(--layout-margin-x);
+		margin-right: var(--layout-margin-x);
+		margin-top: var(--layout-margin-y);
+		margin-bottom: var(--layout-margin-y);
+	}
+</style>
