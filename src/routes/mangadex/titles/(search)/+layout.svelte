872373debcd6b3e4f85a7ba1ac00807b5e64c13f
tonymushah <tonyramanantsalama@gmail.com>
2024-09-12 05:47:31 +0300

    Added base route `/mangadex/titles`

---
+++ src/routes/mangadex/titles/(search)/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { initMangaSearchAuthorSearchFetcher } from "@mangadex/componnents/manga/search/form/filter/contexts/authorArtist";
+	import gqlAuthorFetcher from "@mangadex/componnents/manga/search/form/filter/contexts/authorArtist/gql";
+
+	initMangaSearchAuthorSearchFetcher(gqlAuthorFetcher);
+</script>
+
+<slot />
