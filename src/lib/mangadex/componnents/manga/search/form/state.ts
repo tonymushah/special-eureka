7f4ac9d22c8a8b0e16d386689eb65f2d5d7feb83
tonymushah <tonyramanantsalama@gmail.com>
2024-09-12 11:34:34 +0300

    `MangaSearchParams` to `MangaListParams`

--- src/lib/mangadex/componnents/manga/search/form/state.ts
+++ src/lib/mangadex/componnents/manga/search/form/state.ts
@@ -1,4 +1,6 @@
+import type { MangaListParams } from "@mangadex/gql/graphql"
 import { defaultMangaFilterParams, type MangaSearchFilterParams } from "./filter/contexts"
+import { TagOptionState } from "./filter/contexts/tags"
 
 export type MangaSearchParams = {
     title: string | undefined,
@@ -11,5 +13,29 @@ export default function defaultMangaSearchParams(): MangaSearchParams {
         title: undefined,
         filter: defaultMangaFilterParams(),
         offlineOnly: false
+    }
+}
+
+export function toMangaListParams(params: MangaSearchParams): MangaListParams {
+    const tags = Array.from(params.filter.tags.entries())
+    return {
+        artists: params.filter.authorArtists.artists.map((a) => a.id),
+        authors: params.filter.authorArtists.authors.map((a) => a.id),
+        availableTranslatedLanguage: params.filter.languages.availableTranslatedLanguage,
+        contentRating: params.filter.contentRating,
+        excludedOriginalLanguage: params.filter.languages.excludedOriginalLanguage,
+        excludedTagsMode: params.filter.tagModes.exclude,
+        includedTagsMode: params.filter.tagModes.include,
+        publicationDemographic: params.filter.demographic,
+        status: params.filter.status,
+        year: params.filter.year,
+        includedTags: tags.filter(([, data]) => {
+            return data.state != TagOptionState.INCLUDE
+        }).map(([id]) => id),
+        title: params.title,
+        excludedTags: tags.filter(([, data]) => {
+            return data.state != TagOptionState.EXCLUDE
+        }).map(([id]) => id),
+        originalLanguage: params.filter.languages.originalLanguage,
     }
 }
