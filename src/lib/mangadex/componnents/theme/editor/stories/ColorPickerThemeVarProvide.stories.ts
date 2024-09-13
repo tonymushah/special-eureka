f79862d96574f84df6b35b76ffd257b033a1cde0
tonymushah <tonyramanantsalama@gmail.com>
2024-09-13 16:16:27 +0300

    added `ColorPickerThemeVarProvider`

---
+++ src/lib/mangadex/componnents/theme/editor/stories/ColorPickerThemeVarProvide.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import "@fontsource/poppins/latin.css";
+
+import ColorPickerVarProvider from "./ColorPickerThemeVarProviderForStories.svelte";
+import MangadexThemeProviderForStory from "../../MangadexThemeProviderForStory.svelte";
+
+const meta = {
+    decorators: [() => MangadexThemeProviderForStory],
+    title: "MangaDex/theme/editor/ColorPicker/VarThemeProvider",
+    component: ColorPickerVarProvider,
+    tags: ["autodocs"]
+} satisfies Meta<ColorPickerVarProvider>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
