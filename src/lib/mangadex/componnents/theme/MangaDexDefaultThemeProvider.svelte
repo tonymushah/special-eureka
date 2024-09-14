168c9f7bf9880416bb4104636e793b6c17e08932
tonymushah <tonyramanantsalama@gmail.com>
2024-09-14 21:13:41 +0300

    DefaultThemeProvider

---
+++ src/lib/mangadex/componnents/theme/MangaDexDefaultThemeProvider.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import defaultTheme from "@mangadex/theme/graphql/defaultTheme";
+	import { setMangaDexThemeContextWritable } from "@mangadex/utils/contexts";
+	import { setMangaDexFontsContext } from "@mangadex/utils/contexts/fonts";
+	import { writable } from "svelte/store";
+	import MangaDexVarThemeProvider from "./MangaDexVarThemeProvider.svelte";
+	import MangadexBackground from "./MangadexBackground.svelte";
+	import Toaster from "./toast/Toaster.svelte";
+
+	export let fonts = "Poppins";
+	$: setMangaDexThemeContextWritable(defaultTheme);
+	$: setMangaDexFontsContext(writable(fonts));
+</script>
+
+<MangaDexVarThemeProvider>
+	<Toaster />
+	<MangadexBackground>
+		<slot />
+	</MangadexBackground>
+</MangaDexVarThemeProvider>
