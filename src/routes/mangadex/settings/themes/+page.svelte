46a24a5943f8f26c7cef96f21a57b0eb52f15d69
tonymushah <tonyramanantsalama@gmail.com>
2024-09-14 23:23:34 +0300

    Added Theme profiles update

--- src/routes/mangadex/settings/themes/+page.svelte
+++ src/routes/mangadex/settings/themes/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import MangaDexThemeEditorInner from "@mangadex/componnents/theme/editor/MangaDexThemeEditorInner.svelte";
+	import ThemeProfilesSelector from "@mangadex/componnents/theme/profiles/ThemeProfilesSelector.svelte";
 </script>
 
-<section class="profiles"></section>
+<section class="profiles">
+	<ThemeProfilesSelector />
+</section>
 <section class="editor">
 	<MangaDexThemeEditorInner />
 </section>
