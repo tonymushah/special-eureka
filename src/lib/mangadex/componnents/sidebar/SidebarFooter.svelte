1305ec7adba2af9138d0b81ed5dc2dd81dc0c8a7
tonymushah <tonyramanantsalama@gmail.com>
2024-10-09 14:28:56 +0300

    Added `userMe` and `isLogged` subscription

--- src/lib/mangadex/componnents/sidebar/SidebarFooter.svelte
+++ src/lib/mangadex/componnents/sidebar/SidebarFooter.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 	import { graphql } from "@mangadex/gql";
-	import { isLoggedSubDoc, userMeSubDoc } from "@mangadex/gql-docs/userMe";
-	import { sub_end } from "@mangadex/utils";
-	import { getContextClient, subscriptionStore } from "@urql/svelte";
-	import { onDestroy, onMount } from "svelte";
-	import { UserCheckIcon, UserXIcon, UserIcon } from "svelte-feather-icons";
-	import { v4 } from "uuid";
+	import { isLogged, userMe } from "@mangadex/utils/auth";
+	import { getContextClient } from "@urql/svelte";
+	import { onMount } from "svelte";
+	import { UserCheckIcon, UserIcon, UserXIcon } from "svelte-feather-icons";
 	import Menu from "./base/Menu.svelte";
 	const client = getContextClient();
 	let initial_user_name: string | undefined = undefined;
@@ -36,26 +34,8 @@
 	onMount(async () => {
 		await loadUserMe();
 	});
-	const sub_ids = [v4(), v4()];
-	const userMe = subscriptionStore({
-		client,
-		query: userMeSubDoc,
-		variables: {
-			sub_id: sub_ids[0]
-		}
-	});
-	const isLogged = subscriptionStore({
-		client,
-		query: isLoggedSubDoc,
-		variables: {
-			sub_id: sub_ids[1]
-		}
-	});
-	onDestroy(() => {
-		sub_ids.forEach((id) => sub_end(id));
-	});
 
-	$: label = $userMe.data?.watchUserMe.username ?? "Login";
+	$: label = $userMe?.name ?? "Login";
 </script>
 
 <Menu bind:label>
@@ -71,7 +51,7 @@
 	>
 		{#if isRefreshing}
 			<UserIcon size="24" />
-		{:else if $isLogged.data?.watchIsLogged}
+		{:else if $isLogged}
 			<UserCheckIcon size="24" />
 		{:else}
 			<UserXIcon size="24" />
