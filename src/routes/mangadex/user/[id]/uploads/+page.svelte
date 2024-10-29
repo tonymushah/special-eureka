9a3ce6123ac76f770444d5ca80faea8320a5cefb
tonymushah <tonyramanantsalama@gmail.com>
2024-10-29 21:37:10 +0300

    Added content for user uploads

---
+++ src/routes/mangadex/user/[id]/uploads/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { readonly, writable } from "svelte/store";
+	import type { PageData } from "./$types";
+	import SearchContent from "./SearchContent.svelte";
+	export let data: PageData;
+	const idWritable = writable(data.id);
+	$: idWritable.set(data.id);
+	const id = readonly(idWritable);
+</script>
+
+<SearchContent userId={id} />
