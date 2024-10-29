c06ff5bcd6b967f395c8901e89cacdafc8f71b8a
tonymushah <tonyramanantsalama@gmail.com>
2024-10-29 20:59:03 +0300

    added `search.ts` for `uploads`

---
+++ src/routes/mangadex/user/[id]/uploads/search.ts
@@ -0,0 +1,153 @@
+import type { Client } from "@urql/svelte";
+import { query } from "./query";
+import AbstractSearchResult from "@mangadex/utils/searchResult/AbstractSearchResult";
+import { CoverImageQuality, type ChapterSortOrder, type Language, type MangaListParams } from "@mangadex/gql/graphql";
+import type { ChapterFeedListItem } from "@mangadex/componnents/chapter/feed/list";
+import get_cover_art from "@mangadex/utils/cover-art/get_cover_art";
+import get_value_from_title_and_random_if_undefined from "@mangadex/utils/lang/get_value_from_title_and_random_if_undefined";
+import type { Chapter } from "@mangadex/componnents/chapter/feed";
+import get_chapters_stats from "@mangadex/utils/statistics/chapter/query";
+import { readable } from "svelte/store";
+import { ChapterDownloadState } from "@mangadex/utils/types/DownloadState";
+import getChapterDownloadState from "@mangadex/componnents/home/latest-updates/getChapterDownloadState";
+
+export type UserUploadsFeedChapterParams = {
+    user: string,
+    languages?: Language[],
+    offset?: number,
+    limit?: number,
+    order?: ChapterSortOrder
+}
+
+type UserUploadsFeedChapterConstructorParams = {
+    data: ChapterFeedListItem[]
+    client: Client
+    params: UserUploadsFeedChapterParams;
+    mangaListParams?: MangaListParams;
+    offset: number
+    limit: number
+    total: number
+}
+
+export type ChapterFeedListItemExt = ChapterFeedListItem & {
+    threadUrl?: string
+};
+
+export class UserUploadsFeedResult extends AbstractSearchResult<ChapterFeedListItem> {
+    client: Client
+    params: UserUploadsFeedChapterParams
+    mangaListParams?: MangaListParams;
+    offset: number
+    limit: number
+    total: number
+    constructor(param: UserUploadsFeedChapterConstructorParams) {
+        super(param.data);
+        this.client = param.client;
+        this.params = param.params;
+        this.limit = param.limit;
+        this.offset = param.offset;
+        this.total = param.total
+        this.mangaListParams = param.mangaListParams;
+    }
+    hasNext(): boolean {
+        return this.offset < this.total && this.offset >= 0;
+    }
+    next(): Promise<AbstractSearchResult<ChapterFeedListItem>> {
+        return executeSearchQuery(this.client, {
+            ...this.params,
+            offset: this.offset + this.limit,
+            limit: this.limit
+        }, this.mangaListParams);
+    }
+}
+
+export default async function executeSearchQuery(client: Client, params: UserUploadsFeedChapterParams, mangaListParams?: MangaListParams): Promise<AbstractSearchResult<ChapterFeedListItem>> {
+    const results = await client.query(query, {
+        user: params.user,
+        translatedLanguages: params.languages,
+        offset: params.offset,
+        limit: params.limit,
+        order: params.order,
+        mangaListParams
+    }).toPromise();
+    if (results.error) {
+        throw results.error;
+    }
+    if (results.data) {
+        const data = results.data.chapter.listWithGroupByManga;
+        const comments = await get_chapters_stats(client, data.data.flatMap((d) => d.chapters.map<string>((c) => c.id)), true);
+        return new UserUploadsFeedResult({
+            client,
+            params,
+            offset: data.offset,
+            total: data.total,
+            limit: data.limit,
+            mangaListParams,
+            data: data.data.map<ChapterFeedListItem>((e) => {
+                const cover_art = get_cover_art({
+                    client,
+                    cover_id: e.manga.relationships.coverArt.id,
+                    manga_id: e.manga.id,
+                    filename: e.manga.relationships.coverArt.attributes.fileName,
+                    mode: CoverImageQuality.V256
+                });
+                return {
+                    mangaId: e.manga.id,
+                    title: get_value_from_title_and_random_if_undefined(e.manga.attributes.title, "en") ?? e.manga.id,
+                    coverImage: cover_art,
+                    coverImageAlt: e.manga.relationships.coverArt.id,
+                    mangaLang: e.manga.attributes.originalLanguage,
+                    chapters: e.chapters.map<Chapter>((chap) => {
+                        const title = (() => {
+                            const _title = chap.attributes.title;
+                            const volume = chap.attributes.volume;
+                            const chapter = chap.attributes.chapter;
+
+                            const _volume = volume ? `Vol.${volume} ` : ""
+                            const _chapter = chapter ? `Chap.${chapter}` : "";
+                            const __title = _title ? ` - ${_title}` : "";
+                            return `${_volume}${_chapter}${__title}`;
+                        })();
+                        const user = chap.relationships.user;
+                        const scanGroups = chap.relationships.scanlationGroups;
+                        const threadInfo = (() => {
+                            const _data = comments.get(chap.id);
+                            if (_data) {
+                                return {
+                                    comments: _data.repliesCount,
+                                    threadUrl: _data.threadUrl
+                                };
+                            } else {
+                                return {
+                                    comments: 0
+                                }
+                            }
+                        })()
+                        return {
+                            chapterId: chap.id,
+                            title,
+                            lang: chap.attributes.translatedLanguage,
+                            upload_date: new Date(chap.attributes.readableAt ?? chap.attributes.createdAt),
+                            uploader: {
+                                id: user.id,
+                                name: user.attributes.username,
+                                roles: user.attributes.roles
+                            },
+                            haveBeenRead: false,
+                            groups: scanGroups.map((group) => ({
+                                id: group.id,
+                                name: group.attributes.name
+                            })),
+                            ...threadInfo,
+                            download_state: getChapterDownloadState({
+                                id: chap.id,
+                                client
+                            })
+                        }
+                    })
+                }
+            })
+        })
+    }
+    throw new Error("No results??");
+}
