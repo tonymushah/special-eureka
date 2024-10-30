fc449ead9e23e8aa3eb21f4e20527e490cd04254
tonymushah <tonyramanantsalama@gmail.com>
2024-10-30 13:35:53 +0300

    Added `+page.svelte`

---
+++ src/routes/mangadex/author/[id]/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import SearchContent from "@mangadex/routes/titles/(search)/SearchContent.svelte";
+	import type { PageData } from "./$types";
+	import { derived, readable, writable } from "svelte/store";
+	import type { MangaListParams } from "@mangadex/gql/graphql";
+	export let data: PageData;
+	const authorId = writable<string>(data.id);
+	$: authorId.set(data.id);
+	const offlineStore = readable(false);
+
+	const listParams = derived([authorId], ([$id]) => {
+		return {
+			authorOrArtist: $id
+		} as MangaListParams;
+	});
+</script>
+
+<section class="content">
+	<SearchContent params={listParams} {offlineStore} />
+</section>
+
+<style lang="scss">
+	.content {
+		margin-top: 12px;
+	}
+</style>
