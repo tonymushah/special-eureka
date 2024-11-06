322cd9d07fdc239ad6e18aa01da4f6cfaac2d722
tonymushah <tonyramanantsalama@gmail.com>
2024-11-07 02:20:08 +0300

    Added `TimeAgo.svelte`

---
+++ src/lib/mangadex/componnents/TimeAgo.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { render as timeRender, cancel as timeCancel } from "timeago.js";
+	import { onDestroy, onMount } from "svelte";
+	let timeago: HTMLTimeElement;
+	onMount(() => {
+		if (timeago) timeRender(timeago);
+	});
+	onDestroy(() => {
+		if (timeago) timeCancel(timeago);
+	});
+	export let date: Date;
+</script>
+
+<timeago datetime={date.toDateString()} bind:this={timeago} />
