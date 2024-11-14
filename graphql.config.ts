993f223343e893bdb61c6232a0e91fb3ed387646
tonymushah <tonyramanantsalama@gmail.com>
2024-11-14 20:00:40 +0300

    graphql schema changes

--- graphql.config.ts
+++ graphql.config.ts
@@ -1,13 +1,14 @@
 import { IGraphQLConfig } from "graphql-config";
 
 const config: IGraphQLConfig = {
-    "projects": {
-        "mangadex": {
-            "documents": ['./src/lib/mangadex/**/*.svelte', './src/lib/mangadex/**/*.ts', './src/routes/mangadex/**/*.svelte'],
-            "schema": './src/lib/mangadex/schemas.graphqls',
-            "includes": ["./src/lib/mangadex/**/*", "./src/routes/mangadex/**/*"],
+    projects: {
+        mangadex: {
+            documents: ['./src/lib/mangadex/**/*.svelte', './src/lib/mangadex/**/*.ts', './src/routes/mangadex/**/*.svelte'],
+            schemaPath: './src/lib/mangadex/schemas.graphqls',
+            includes: ["./src/lib/mangadex/**/*", "./src/routes/mangadex/**/*"],
         }
-    }
+    },
+    excludes: ["./src-tauri/*", "./.svelte-kit/*"]
 };
 
 export default config;
