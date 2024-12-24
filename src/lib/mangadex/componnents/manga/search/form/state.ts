5a1d01cce5bf19bcb0523e3487c9c978a707e081
tonymushah <tonyramanantsalama@gmail.com>
2024-12-24 20:31:03 +0300

    fixed search lol

--- src/lib/mangadex/componnents/manga/search/form/state.ts
+++ src/lib/mangadex/componnents/manga/search/form/state.ts
@@ -1,41 +1,62 @@
-import type { MangaListParams } from "@mangadex/gql/graphql"
-import { defaultMangaFilterParams, type MangaSearchFilterParams } from "./filter/contexts"
-import { TagOptionState } from "./filter/contexts/tags"
+import type { MangaListParams } from "@mangadex/gql/graphql";
+import { defaultMangaFilterParams, type MangaSearchFilterParams } from "./filter/contexts";
+import { TagOptionState } from "./filter/contexts/tags";
 
 export type MangaSearchParams = {
-    title: string | undefined,
-    filter: MangaSearchFilterParams,
-    offlineOnly: boolean
-}
+	title: string | undefined;
+	filter: MangaSearchFilterParams;
+	offlineOnly: boolean;
+};
 
 export default function defaultMangaSearchParams(): MangaSearchParams {
-    return {
-        title: undefined,
-        filter: defaultMangaFilterParams(),
-        offlineOnly: false
-    }
+	return {
+		title: undefined,
+		filter: defaultMangaFilterParams(),
+		offlineOnly: false
+	};
 }
 
 export function toMangaListParams(params: MangaSearchParams): MangaListParams {
-    const tags = Array.from(params.filter.tags.entries())
-    return {
-        artists: params.filter.authorArtists.artists.map((a) => a.id),
-        authors: params.filter.authorArtists.authors.map((a) => a.id),
-        availableTranslatedLanguage: params.filter.languages.availableTranslatedLanguage,
-        contentRating: params.filter.contentRating,
-        excludedOriginalLanguage: params.filter.languages.excludedOriginalLanguage,
-        excludedTagsMode: params.filter.tagModes.exclude,
-        includedTagsMode: params.filter.tagModes.include,
-        publicationDemographic: params.filter.demographic,
-        status: params.filter.status,
-        year: params.filter.year,
-        includedTags: tags.filter(([, data]) => {
-            return data.state == TagOptionState.INCLUDE
-        }).map(([id]) => id),
-        title: params.title,
-        excludedTags: tags.filter(([, data]) => {
-            return data.state == TagOptionState.EXCLUDE
-        }).map(([id]) => id),
-        originalLanguage: params.filter.languages.originalLanguage,
-    }
+	const tags = Array.from(params.filter.tags.entries());
+	const year = (() => {
+		if (params.filter.year) {
+			let strYear = `${params.filter.year}`;
+			if (strYear.length == 0) {
+				return null;
+			} else {
+				let year = Number(strYear);
+				if (isNaN(year)) {
+					return null;
+				} else {
+					return year;
+				}
+			}
+		} else {
+			return null;
+		}
+	})();
+	return {
+		artists: params.filter.authorArtists.artists.map((a) => a.id),
+		authors: params.filter.authorArtists.authors.map((a) => a.id),
+		availableTranslatedLanguage: params.filter.languages.availableTranslatedLanguage,
+		contentRating: params.filter.contentRating,
+		excludedOriginalLanguage: params.filter.languages.excludedOriginalLanguage,
+		excludedTagsMode: params.filter.tagModes.exclude,
+		includedTagsMode: params.filter.tagModes.include,
+		publicationDemographic: params.filter.demographic,
+		status: params.filter.status,
+		year,
+		includedTags: tags
+			.filter(([, data]) => {
+				return data.state == TagOptionState.INCLUDE;
+			})
+			.map(([id]) => id),
+		title: params.title?.length != 0 ? params.title : undefined,
+		excludedTags: tags
+			.filter(([, data]) => {
+				return data.state == TagOptionState.EXCLUDE;
+			})
+			.map(([id]) => id),
+		originalLanguage: params.filter.languages.originalLanguage
+	};
 }
