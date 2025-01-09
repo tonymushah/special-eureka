23d4ff914b68c75aaec68712d36019485644f946
tonymushah <tonyramanantsalama@gmail.com>
2025-01-09 18:28:01 +0300

    refactored `+layout.svelte`

--- src/routes/mangadex/group/[id]/+layout.svelte
+++ src/routes/mangadex/group/[id]/+layout.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-	import { run } from "svelte/legacy";
-
 	import ButtonAccent from "@mangadex/componnents/theme/buttons/ButtonAccent.svelte";
 	import PrimaryButton from "@mangadex/componnents/theme/buttons/PrimaryButton.svelte";
 	import TimeAgo from "@mangadex/componnents/TimeAgo.svelte";
 	import UsersPageBase from "@mangadex/componnents/users/page/UsersPageBase.svelte";
 	import { writeText } from "@tauri-apps/plugin-clipboard-manager";
-	import { open as shellOpen } from "@tauri-apps/plugin-shell";
+	import { openUrl as shellOpen } from "@tauri-apps/plugin-opener";
 	import { BookmarkIcon, ExternalLinkIcon, FlagIcon } from "svelte-feather-icons";
 	import type { LayoutData } from "./$types";
 	import NavTab from "./NavTab.svelte";
@@ -19,10 +17,10 @@
 
 	let { data, children }: Props = $props();
 	let description = $derived(data.description ?? undefined);
-	run(() => {
+	$effect(() => {
 		console.log(`duration: ${data.publishDelay}`);
 	});
-	run(() => {
+	$effect(() => {
 		console.log(`since: ${data.createdAt}`);
 	});
 	let createdSince = $derived(data.createdAt);
@@ -30,7 +28,7 @@
 </script>
 
 <UsersPageBase title={data.name} {description}>
-	{#snippet left()}
+	{#snippet _left()}
 		<div class="buttons">
 			<PrimaryButton isBase>
 				<p><BookmarkIcon />Follow</p>
@@ -57,51 +55,54 @@
 			/>
 		</div>
 	{/snippet}
-	<!-- TODO @migration-task: migrate this slot by hand, `top-right` is an invalid identifier -->
-	<div slot="top-right" class="info">
-		<p>
-			Group ID: <span
-				onkeydown={() => {}}
-				role="button"
-				tabindex={0}
-				onclick={() => {
-					writeText(data.id);
-				}}
-				class="copiable">{data.id}</span
-			>
-		</p>
-		<section class="uploads">
+
+	{#snippet topRight()}
+		<div class="info">
 			<p>
-				{data.titles}
-				<span>
-					title{#if data.titles > 1}s{/if} scanlated
-				</span>
+				Group ID: <span
+					onkeydown={() => {}}
+					role="button"
+					tabindex={0}
+					onclick={() => {
+						writeText(data.id);
+					}}
+					class="copiable">{data.id}</span
+				>
 			</p>
-			<p>
-				{data.uploads}
-				<span>
-					upload{#if data.uploads > 1}s{/if}
-				</span>
-			</p>
-		</section>
-		<section class="state">
-			<p>
-				State: {#if data.locked}Locked{:else}Unlocked{/if}{#if data.official}
-					, Official
-				{/if}
-				{#if data.verified}
-					, Verified
-				{/if}
-				{#if data.exLicensed}
-					exLicensed
-				{/if}
-			</p>
-			<p>
-				Created <TimeAgo date={createdSince} />
-			</p>
-		</section>
-	</div>
-	{#snippet right()}
+			<section class="uploads">
+				<p>
+					{data.titles}
+					<span>
+						title{#if data.titles > 1}s{/if} scanlated
+					</span>
+				</p>
+				<p>
+					{data.uploads}
+					<span>
+						upload{#if data.uploads > 1}s{/if}
+					</span>
+				</p>
+			</section>
+			<section class="state">
+				<p>
+					State: {#if data.locked}Locked{:else}Unlocked{/if}{#if data.official}
+						, Official
+					{/if}
+					{#if data.verified}
+						, Verified
+					{/if}
+					{#if data.exLicensed}
+						exLicensed
+					{/if}
+				</p>
+				<p>
+					Created <TimeAgo date={createdSince} />
+				</p>
+			</section>
+		</div>
+	{/snippet}
+
+	{#snippet _right()}
 		<div>
 			<section class="nav-tab">
 				<NavTab id={data.id} />
