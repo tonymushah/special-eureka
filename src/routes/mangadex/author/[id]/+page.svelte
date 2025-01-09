65891f6767ffcf67caad78b86380bbac21c39301
tonymushah <tonyramanantsalama@gmail.com>
2025-01-09 18:27:37 +0300

    some run update

--- src/routes/mangadex/author/[id]/+page.svelte
+++ src/routes/mangadex/author/[id]/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { run } from 'svelte/legacy';
-
 	import SearchContent from "@mangadex/routes/titles/(search)/SearchContent.svelte";
 	import type { PageData } from "./$types";
 	import { derived, readable, writable } from "svelte/store";
@@ -11,7 +9,7 @@
 
 	let { data }: Props = $props();
 	const authorId = writable<string>(data.id);
-	run(() => {
+	$effect(() => {
 		authorId.set(data.id);
 	});
 	const offlineStore = readable(false);
