7f834ac543139f84481a632bc5fa2e22b7bf2e5f
tonymushah <tonyramanantsalama@gmail.com>
2025-01-13 22:45:32 +0300

    Added rate limit implementation

--- src-tauri/mangadex/src/plugin_setup/plugin_config.rs
+++ src-tauri/mangadex/src/plugin_setup/plugin_config.rs
@@ -1,4 +1,29 @@
+use std::time::Duration;
+
+use duration_string::DurationString;
 use serde::Deserialize;
+use tower::limit::RateLimitLayer;
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct RateLimitConfig {
+    pub(crate) number: u64,
+    pub(crate) period: DurationString,
+}
+
+impl Default for RateLimitConfig {
+    fn default() -> Self {
+        Self {
+            number: 5,
+            period: DurationString::new(Duration::from_secs(1)),
+        }
+    }
+}
+
+impl From<RateLimitConfig> for RateLimitLayer {
+    fn from(value: RateLimitConfig) -> Self {
+        Self::new(value.number, *value.period)
+    }
+}
 
 #[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -6,6 +31,8 @@ pub struct PluginConfig {
     #[serde(default)]
     pub(crate) user_agent: Option<String>,
     #[serde(default)]
-    pub(crate) timeout: Option<f64>,
+    pub(crate) timeout: Option<DurationString>,
     pub(crate) sdl_export_path: String,
+    #[serde(default)]
+    pub(crate) ratelimit: RateLimitConfig,
 }
--- src-tauri/mangadex/src/plugin_setup/register_mangadex_client.rs
+++ src-tauri/mangadex/src/plugin_setup/register_mangadex_client.rs
@@ -1,11 +1,10 @@
-use std::time::Duration;
-
 use mangadex_api::MangaDexClient;
 use reqwest::{
     header::{HeaderMap, HeaderValue, USER_AGENT},
     Client,
 };
 use tauri::{Manager, Runtime};
+use tower::{buffer::BufferLayer, limit::RateLimitLayer, ServiceBuilder};
 
 use super::plugin_config::PluginConfig;
 
@@ -14,7 +13,7 @@ pub fn register_mangadex_client<R: Runtime>(
     config: &PluginConfig,
 ) -> crate::PluginSetupResult<()> {
     let mut default_headers = HeaderMap::new();
-    let default_user_agent = String::from("special-eureka 0.2.0");
+    let default_user_agent = String::from("special-eureka/0.2.0");
 
     let mut ua = config.user_agent.clone().unwrap_or(default_user_agent);
     if let Some(version) = app.config().version.clone() {
@@ -28,8 +27,12 @@ pub fn register_mangadex_client<R: Runtime>(
     default_headers.append(USER_AGENT, HeaderValue::from_str(&ua)?);
     let mut cli_builder = Client::builder().default_headers(default_headers);
     if let Some(timeout) = config.timeout {
-        cli_builder = cli_builder.timeout(Duration::from_secs_f64(timeout));
+        cli_builder = cli_builder.timeout(*timeout);
     }
+    let rate_limit = ServiceBuilder::new()
+        .layer(BufferLayer::new(1024))
+        .layer(Into::<RateLimitLayer>::into(config.ratelimit.clone()));
+    cli_builder = cli_builder.connector_layer(rate_limit);
     app.manage(MangaDexClient::new(cli_builder.build()?));
     Ok(())
 }
