feb1a3815ed778859f9129dbccd10419ac438101
tonymushah <tonyramanantsalama@gmail.com>
2025-01-14 21:54:29 +0300

    Added `ContentProfileSelector` and `ContentProfilesEditor`

---
+++ src/routes/mangadex/settings/content-profiles/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import ContentProfilesEditor from "@mangadex/componnents/content-profiles/ContentProfilesEditor.svelte";
+	import ContentProfileSelector from "@mangadex/componnents/content-profiles/ContentProfileSelector.svelte";
+</script>
+
+<section class="selector">
+	<ContentProfileSelector />
+</section>
+<section class="editor">
+	<ContentProfilesEditor />
+</section>
