729fb39e905c88104fa979c0e859513317bea750
tonymushah <tonyramanantsalama@gmail.com>
2025-01-24 02:33:39 +0300

    FeedContentImplentation

--- src-tauri/mangadex/src/store/types/structs/content.rs
+++ src-tauri/mangadex/src/store/types/structs/content.rs
@@ -1,9 +1,14 @@
+mod feed_impl;
 pub mod profiles;
 
 use async_graphql::{InputObject, SimpleObject};
+use impl_trait_for_tuples::impl_for_tuples;
 use mangadex_api_types_rust::{ContentRating, Demographic, Language, MangaStatus, TagSearchMode};
 use serde::{Deserialize, Serialize};
+use tauri::{Manager, Runtime};
 use uuid::Uuid;
+
+use crate::utils::traits_utils::{MangadexAsyncGraphQLContextExt, MangadexTauriManagerExt};
 
 #[derive(
     Debug, Clone, PartialEq, Eq, SimpleObject, InputObject, Default, Deserialize, Serialize,
@@ -45,5 +50,41 @@ pub struct ContentProfile {
 }
 
 pub trait FeedContent {
-    fn feed(self, content_profile: ContentProfile) -> Self;
+    fn feed(self, content_profile: &ContentProfile) -> Self;
+}
+
+#[impl_for_tuples(10)]
+impl FeedContent for Tuple {
+    fn feed(mut self, content_profile: &ContentProfile) -> Self {
+        for_tuples!(#( self.Tuple = Tuple.feed(content_profile); )*);
+        self
+    }
+}
+
+pub fn feed_from_manager<R, M, TF>(manager: &M, to_feed: TF) -> crate::Result<TF>
+where
+    R: Runtime,
+    M: Manager<R>,
+    TF: FeedContent,
+{
+    let content_profile: ContentProfile = {
+        let watches = manager.get_watches()?;
+        let content_profiles_ref = watches.content_profiles.borrow();
+        let content_profile_key_ref = watches.content_profiles_default_key.borrow();
+        if let Some(key) = content_profile_key_ref.as_ref() {
+            content_profiles_ref.get(key).cloned().unwrap_or_default()
+        } else {
+            Default::default()
+        }
+    };
+    Ok(to_feed.feed(&content_profile))
+}
+
+pub fn feed_from_gql_ctx<R, TF>(ctx: &async_graphql::Context<'_>, to_feed: TF) -> crate::Result<TF>
+where
+    R: Runtime,
+    TF: FeedContent,
+{
+    let app_handle = ctx.get_app_handle::<R>()?;
+    feed_from_manager(app_handle, to_feed)
 }
---
+++ src-tauri/mangadex/src/store/types/structs/content/feed_impl.rs
@@ -0,0 +1,180 @@
+use mangadex_api_input_types::{
+    chapter::list::ChapterListParams,
+    feed::{
+        custom_list_feed::CustomListMangaFeedParams, followed_manga_feed::FollowedMangaFeedParams,
+    },
+    manga::{
+        aggregate::MangaAggregateParam, feed::MangaFeedParams, list::MangaListParams,
+        random::MangaRandomParams,
+    },
+};
+
+use super::FeedContent;
+
+impl FeedContent for MangaListParams {
+    fn feed(mut self, content_profile: &super::ContentProfile) -> Self {
+        if self.original_language.is_empty() {
+            self.original_language = content_profile.original_languages.clone();
+        }
+        if self.excluded_original_language.is_empty() {
+            self.excluded_original_language = content_profile.excluded_original_language.clone();
+        }
+        if self.publication_demographic.is_empty() {
+            self.publication_demographic = content_profile.publication_demographic.clone();
+        }
+        if self.included_tags.is_empty() {
+            self.included_tags = content_profile.included_tags.clone();
+        }
+        if self.included_tags_mode.is_none() {
+            self.included_tags_mode = content_profile.included_tags_mode;
+        }
+        if self.excluded_tags.is_empty() {
+            self.excluded_tags = content_profile.excluded_tags.clone();
+        }
+        if self.excluded_tags_mode.is_none() {
+            self.excluded_tags_mode = content_profile.excluded_tags_mode;
+        }
+        if self.status.is_empty() {
+            self.status = content_profile.status.clone();
+        }
+        if self.available_translated_language.is_empty() {
+            self.available_translated_language = content_profile.translated_languages.clone();
+        }
+        if self.content_rating.is_empty() {
+            self.content_rating = content_profile.content_rating.clone();
+        }
+        self
+    }
+}
+
+impl FeedContent for ChapterListParams {
+    fn feed(mut self, content_profile: &super::ContentProfile) -> Self {
+        if self.content_rating.is_empty() {
+            self.content_rating = content_profile.content_rating.clone();
+        }
+        if self.excluded_original_languages.is_empty() {
+            self.excluded_original_languages = content_profile.excluded_original_language.clone();
+        }
+        if self.excluded_groups.is_empty() {
+            self.excluded_groups = content_profile.excluded_groups.clone();
+        }
+        if self.excluded_uploaders.is_empty() {
+            self.excluded_uploaders = content_profile.excluded_uploaders.clone();
+        }
+        if self.original_languages.is_empty() {
+            self.original_languages = content_profile.original_languages.clone();
+        }
+        if self.translated_languages.is_empty() {
+            self.translated_languages = content_profile.translated_languages.clone();
+        }
+        self
+    }
+}
+
+impl FeedContent for MangaAggregateParam {
+    fn feed(mut self, content_profile: &super::ContentProfile) -> Self {
+        if self.translated_language.is_empty() {
+            self.translated_language = content_profile.translated_languages.clone();
+        }
+        self
+    }
+}
+
+impl FeedContent for MangaRandomParams {
+    fn feed(mut self, content_profile: &super::ContentProfile) -> Self {
+        if self.included_tags.is_empty() {
+            self.included_tags = content_profile.included_tags.clone();
+        }
+        if self.included_tags_mode.is_none() {
+            self.included_tags_mode = content_profile.included_tags_mode;
+        }
+        if self.excluded_tags.is_empty() {
+            self.excluded_tags = content_profile.excluded_tags.clone();
+        }
+        if self.excluded_tags_mode.is_none() {
+            self.excluded_tags_mode = content_profile.excluded_tags_mode;
+        }
+        if self.content_rating.is_empty() {
+            self.content_rating = content_profile.content_rating.clone();
+        }
+        self
+    }
+}
+
+impl FeedContent for MangaFeedParams {
+    fn feed(mut self, content_profile: &super::ContentProfile) -> Self {
+        if self.content_rating.is_empty() {
+            self.content_rating = content_profile.content_rating.clone();
+        }
+        if self.excluded_groups.is_empty() {
+            self.excluded_groups = content_profile.excluded_groups.clone();
+        }
+        if self.excluded_uploaders.is_empty() {
+            self.excluded_uploaders = content_profile.excluded_uploaders.clone();
+        }
+        if self.excluded_original_language.is_empty() {
+            self.excluded_original_language = content_profile.excluded_original_language.clone();
+        }
+        if self.excluded_uploaders.is_empty() {
+            self.excluded_uploaders = content_profile.excluded_uploaders.clone();
+        }
+        if self.original_language.is_empty() {
+            self.original_language = content_profile.original_languages.clone();
+        }
+        if self.translated_language.is_empty() {
+            self.translated_language = content_profile.translated_languages.clone();
+        }
+        self
+    }
+}
+
+impl FeedContent for CustomListMangaFeedParams {
+    fn feed(mut self, content_profile: &super::ContentProfile) -> Self {
+        if self.content_rating.is_empty() {
+            self.content_rating = content_profile.content_rating.clone();
+        }
+        if self.excluded_groups.is_empty() {
+            self.excluded_groups = content_profile.excluded_groups.clone();
+        }
+        if self.excluded_original_language.is_empty() {
+            self.excluded_original_language = content_profile.excluded_original_language.clone();
+        }
+        if self.excluded_original_language.is_empty() {
+            self.excluded_original_language = content_profile.excluded_original_language.clone();
+        }
+        if self.excluded_uploaders.is_empty() {
+            self.excluded_uploaders = content_profile.excluded_uploaders.clone();
+        }
+        if self.original_language.is_empty() {
+            self.original_language = content_profile.original_languages.clone();
+        }
+        if self.translated_language.is_empty() {
+            self.translated_language = content_profile.translated_languages.clone();
+        }
+        self
+    }
+}
+
+impl FeedContent for FollowedMangaFeedParams {
+    fn feed(mut self, content_profile: &super::ContentProfile) -> Self {
+        if self.excluded_groups.is_empty() {
+            self.excluded_groups = content_profile.excluded_groups.clone();
+        }
+        if self.excluded_original_language.is_empty() {
+            self.excluded_original_language = content_profile.excluded_original_language.clone();
+        }
+        if self.excluded_original_language.is_empty() {
+            self.excluded_original_language = content_profile.excluded_original_language.clone();
+        }
+        if self.excluded_uploaders.is_empty() {
+            self.excluded_uploaders = content_profile.excluded_uploaders.clone();
+        }
+        if self.original_language.is_empty() {
+            self.original_language = content_profile.original_languages.clone();
+        }
+        if self.translated_language.is_empty() {
+            self.translated_language = content_profile.translated_languages.clone();
+        }
+        self
+    }
+}
