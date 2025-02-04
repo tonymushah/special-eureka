026bf2c9259c06ea4581833338cccf2dc31f2aa9
tonymushah <tonyramanantsalama@gmail.com>
2025-02-05 02:19:11 +0300

    removed windows-vibrancy

--- src-tauri/core/src/builder/setup.rs
+++ src-tauri/core/src/builder/setup.rs
@@ -1,11 +1,5 @@
 use actix::System;
 use tauri::{App, Manager, Runtime};
-#[cfg(any(windows, target_os = "macos"))]
-use window_shadows::set_shadow;
-#[cfg(target_os = "windows")]
-use window_vibrancy::apply_blur;
-#[cfg(target_os = "macos")]
-use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
 
 use crate::states::last_focused_window::LastFocusedWindow;
 
@@ -27,17 +21,5 @@ pub fn setup<R: Runtime>(app: &mut App<R>) -> SetupResult {
         let _ = set_menu_window(&main.as_ref().window());
     }
     */
-
-    #[cfg(any(windows, target_os = "macos"))]
-    if let Some(splashscreen) = app.get_window("splashscreen") {
-        set_shadow(&splashscreen, true).unwrap();
-        #[cfg(target_os = "macos")]
-        apply_vibrancy(&splashscreen, NSVisualEffectMaterial::HudWindow, None, None)
-            .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
-
-        #[cfg(target_os = "windows")]
-        apply_blur(&splashscreen, Some((18, 18, 18, 125)))
-            .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
-    }
     Ok(())
 }
