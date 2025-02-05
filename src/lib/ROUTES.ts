8c8d07427406de8d20d0cf737f6aa5dafb39f707
tonymushah <tonyramanantsalama@gmail.com>
2025-02-05 03:05:57 +0300

    routes updates

--- src/lib/ROUTES.ts
+++ src/lib/ROUTES.ts
@@ -14,26 +14,26 @@ const PAGES = {
   "/mangadex": `/mangadex`,
   "/mangadex/author": `/mangadex/author`,
   "/mangadex/author/[id]": (params: { id: (string | number) }) => {
-    return `/mangadex/author/${params.id}`
+    return `/mangadex/author/${params['id']}`
   },
   "/mangadex/chapter/[id]": (params: { id: (string | number) }) => {
-    return `/mangadex/chapter/${params.id}`
+    return `/mangadex/chapter/${params['id']}`
   },
   "/mangadex/graphiql": `/mangadex/graphiql`,
   "/mangadex/group": `/mangadex/group`,
   "/mangadex/group/[id]": (params: { id: (string | number) }) => {
-    return `/mangadex/group/${params.id}`
+    return `/mangadex/group/${params['id']}`
   },
   "/mangadex/group/[id]/titles": (params: { id: (string | number) }) => {
-    return `/mangadex/group/${params.id}/titles`
+    return `/mangadex/group/${params['id']}/titles`
   },
   "/mangadex/group/[id]/uploads": (params: { id: (string | number) }) => {
-    return `/mangadex/group/${params.id}/uploads`
+    return `/mangadex/group/${params['id']}/uploads`
   },
   "/mangadex/group/new": `/mangadex/group/new`,
   "/mangadex/list": `/mangadex/list`,
   "/mangadex/list/[id]": (params: { id: (string | number) }) => {
-    return `/mangadex/list/${params.id}`
+    return `/mangadex/list/${params['id']}`
   },
   "/mangadex/list/new": `/mangadex/list/new`,
   "/mangadex/login": `/mangadex/login`,
@@ -42,29 +42,29 @@ const PAGES = {
   "/mangadex/settings/content-profiles": `/mangadex/settings/content-profiles`,
   "/mangadex/settings/themes": `/mangadex/settings/themes`,
   "/mangadex/tag/[id]": (params: { id: (string | number) }) => {
-    return `/mangadex/tag/${params.id}`
+    return `/mangadex/tag/${params['id']}`
   },
   "/mangadex/title/[id]": (params: { id: (string | number) }) => {
-    return `/mangadex/title/${params.id}`
+    return `/mangadex/title/${params['id']}`
   },
   "/mangadex/title/[id]/covers": (params: { id: (string | number) }) => {
-    return `/mangadex/title/${params.id}/covers`
+    return `/mangadex/title/${params['id']}/covers`
   },
   "/mangadex/title/[id]/related": (params: { id: (string | number) }) => {
-    return `/mangadex/title/${params.id}/related`
+    return `/mangadex/title/${params['id']}/related`
   },
   "/mangadex/titles": `/mangadex/titles`,
   "/mangadex/titles/feed": `/mangadex/titles/feed`,
   "/mangadex/upload": `/mangadex/upload`,
   "/mangadex/user": `/mangadex/user`,
   "/mangadex/user/[id]": (params: { id: (string | number) }) => {
-    return `/mangadex/user/${params.id}`
+    return `/mangadex/user/${params['id']}`
   },
   "/mangadex/user/[id]/lists": (params: { id: (string | number) }) => {
-    return `/mangadex/user/${params.id}/lists`
+    return `/mangadex/user/${params['id']}/lists`
   },
   "/mangadex/user/[id]/uploads": (params: { id: (string | number) }) => {
-    return `/mangadex/user/${params.id}/uploads`
+    return `/mangadex/user/${params['id']}/uploads`
   },
   "/mangadex/user/me": `/mangadex/user/me`,
   "/mangadex/user/me/lists": `/mangadex/user/me/lists`,
@@ -97,7 +97,10 @@ type ParamValue = string | number | undefined
 /**
  * Append search params to a string
  */
-export const appendSp = (sp?: Record<string, ParamValue | ParamValue[]>, prefix: '?' | '&' = '?') => {
+export const appendSp = (
+  sp?: Record<string, ParamValue | ParamValue[]>,
+  prefix: '?' | '&' = '?',
+) => {
   if (sp === undefined) return ''
 
   const params = new URLSearchParams()
@@ -107,7 +110,12 @@ export const appendSp = (sp?: Record<string, ParamValue | ParamValue[]>, prefix:
     }
   }
 
+  let anchor = ''
   for (const [name, val] of Object.entries(sp)) {
+    if (name === '__KIT_ROUTES_ANCHOR__' && val !== undefined) {
+      anchor = `#${val}`
+      continue
+    }
     if (Array.isArray(val)) {
       for (const v of val) {
         append(name, v)
@@ -118,8 +126,8 @@ export const appendSp = (sp?: Record<string, ParamValue | ParamValue[]>, prefix:
   }
 
   const formatted = params.toString()
-  if (formatted) {
-    return `${prefix}${formatted}`
+  if (formatted || anchor) {
+    return `${prefix}${formatted}${anchor}`.replace('?#', '#')
   }
   return ''
 }
@@ -178,7 +186,7 @@ export function route<T extends keyof AllTypes>(key: T, ...params: any[]): strin
 *
 * Full example:
 * ```ts
-* import type { KIT_ROUTES } from '$_lib/ROUTES'
+* import type { KIT_ROUTES } from '$lib/ROUTES'
 * import { kitRoutes } from 'vite-plugin-kit-routes'
 *
 * kitRoutes<KIT_ROUTES>({
@@ -193,5 +201,5 @@ export type KIT_ROUTES = {
   SERVERS: Record<string, never>
   ACTIONS: Record<string, never>
   LINKS: Record<string, never>
-  Params: { id: never }
+  Params: { 'id': never }
 }
