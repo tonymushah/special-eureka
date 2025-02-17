be27c3de4fd0bce4d7c1d15ce922fa40549aeb69
tonymushah <tonyramanantsalama@gmail.com>
2025-02-17 23:31:28 +0300

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import { kitRoutes } from "vite-plugin-kit-routes";
 
 export default defineConfig({
