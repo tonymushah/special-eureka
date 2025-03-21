686045ec1fc7ed2d9bf7e88b634fd61b37e5b6b1
tonymushah <tonyramanantsalama@gmail.com>
2025-03-21 13:35:48 +0300

    some format

--- apps/mangadex/lib/titlebar.ts
+++ apps/mangadex/lib/titlebar.ts
@@ -3,20 +3,20 @@ import { debounce } from "lodash";
 
 const TOGGLE_SIDEBAR_KEY = "TOGGLE_SIDEBAR";
 const debunc = debounce(async () => {
-	const { isSidebarRtl } = await import("./componnents/sidebar/states/isRtl");
-	isSidebarRtl.update((d) => !d);
+    const { isSidebarRtl } = await import("./componnents/sidebar/states/isRtl");
+    isSidebarRtl.update((d) => !d);
 });
 const TOGGLE_SIDEBAR_HANDLER: MouseEventHandler<HTMLButtonElement> = () => {
-	debunc();
+    debunc();
 };
 
 export async function mangadexTitleBar() {
-	const { defaultBehavior, titleBarButtons } = await import(
-		"$lib/window-decoration/stores/decorations.svelte"
-	);
-	defaultBehavior();
-	titleBarButtons.set(TOGGLE_SIDEBAR_KEY, {
-		title: "Sidebar Direction",
-		onclick: TOGGLE_SIDEBAR_HANDLER
-	});
+    const { defaultBehavior, titleBarButtons } = await import(
+        "$lib/window-decoration/stores/decorations.svelte"
+    );
+    defaultBehavior();
+    titleBarButtons.set(TOGGLE_SIDEBAR_KEY, {
+        title: "Sidebar Direction",
+        onclick: TOGGLE_SIDEBAR_HANDLER
+    });
 }
