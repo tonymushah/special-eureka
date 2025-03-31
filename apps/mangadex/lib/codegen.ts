67ffb751a2547df1b4292725b4338041920ec64e
tonymushah <tonyramanantsalama@gmail.com>
2025-03-31 13:39:52 +0300

    codegen update

--- apps/mangadex/lib/codegen.ts
+++ apps/mangadex/lib/codegen.ts
@@ -1,23 +1,22 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-	schema: "./src/lib/mangadex/schemas.graphqls",
-	documents: [
-		// "./src/lib/mangadex/**/*.svelte",
-		"src/lib/mangadex/**/*.ts",
-		// "src/routes/mangadex/**/*.svelte",
-		"src/routes/mangadex/**/*.ts"
-	],
-	ignoreNoDocuments: false, // for better experience with the watcher
-	generates: {
-		"./src/lib/mangadex/gql/": {
-			preset: "client",
-			config: {
-				useTypeImports: true
-			},
-			plugins: []
-		}
-	}
+    schema: "./apps/mangadex/lib/schemas.graphqls",
+    documents: [
+        // "./src/lib/mangadex/**/*.svelte",
+        "./apps/mangadex/**/*.ts",
+        // "src/routes/mangadex/**/*.svelte",
+    ],
+    ignoreNoDocuments: false, // for better experience with the watcher
+    generates: {
+        "./apps/mangadex/lib/gql/": {
+            preset: "client",
+            config: {
+                useTypeImports: true
+            },
+            plugins: []
+        }
+    }
 };
 
 export default config;
--- graphql.config.ts
+++ graphql.config.ts
@@ -1,18 +1,17 @@
 import type { IGraphQLConfig } from "graphql-config";
 
 const config: IGraphQLConfig = {
-	projects: {
-		mangadex: {
-			documents: [
-				"./src/lib/mangadex/**/*.svelte",
-				"./src/lib/mangadex/**/*.ts",
-				"./src/routes/mangadex/**/*.svelte"
-			],
-			schemaPath: "./src/lib/mangadex/schemas.graphqls",
-			includes: ["./src/lib/mangadex/**/*", "./src/routes/mangadex/**/*"]
-		}
-	},
-	excludes: ["./src-tauri/*", "./.svelte-kit/*"]
+    projects: {
+        mangadex: {
+            documents: [
+                "./apps/mangadex/**/*.svelte",
+                "./apps/mangadex/**/*.ts"
+            ],
+            schemaPath: "./apps/mangadex/lib/schemas.graphqls",
+            includes: ["./apps/mangadex/**/*"]
+        }
+    },
+    excludes: ["./src-tauri/*", "./.svelte-kit/*"]
 };
 
 export default config;
