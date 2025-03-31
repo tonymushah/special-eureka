a87c8b468f71bbd893d935d14f10703ca1e4979a
tonymushah <tonyramanantsalama@gmail.com>
2025-03-31 13:44:11 +0300

    changing editor config

--- apps/mangadex/lib/download/chapter.ts
+++ apps/mangadex/lib/download/chapter.ts
@@ -1,34 +1,43 @@
 import { graphql } from "@mangadex/gql";
 
 const mutation = graphql(`
-    mutation downloadChapterMutation($id: UUID!, $quality: DownloadMode) {
-        chapter {
-            download(id: $id, quality: $quality) {
-                hasFailed
-                isDownloaded
-            }
-        }
-    }
+	mutation downloadChapterMutation($id: UUID!, $quality: DownloadMode) {
+		chapter {
+			download(id: $id, quality: $quality) {
+				hasFailed
+				isDownloaded
+			}
+		}
+	}
 `);
 
 const subscription = graphql(`
-    subscription chapterDownloadState($id: UUID!) {
-        watchChapterDownloadState(chapterId: $id) {
-            isPending
-            isDone
-            isCanceled
-            isOfflineAppStateNotLoaded
-            error
-            downloading {
-                isPreloading
-                isFetchingData
-                fetchingImage {
-                    filename
-                    index
-                    len
-                }
-                isFetchingAtHomeData
-            }
-        }
-    }
+	subscription chapterDownloadState($id: UUID!) {
+		watchChapterDownloadState(chapterId: $id) {
+			isPending
+			isDone
+			isCanceled
+			isOfflineAppStateNotLoaded
+			error
+			downloading {
+				isPreloading
+				isFetchingData
+				fetchingImage {
+					filename
+					index
+					len
+				}
+				isFetchingAtHomeData
+			}
+		}
+	}
 `)
+
+export class ChapterDownloadState {
+	public static mutation() {
+		return mutation
+	}
+	public static subscription() {
+		return subscription
+	}
+}
