9c618d6f298a64b414defd02d606459d42bd31a0
tonymushah <tonyramanantsalama@gmail.com>
2025-03-31 21:52:29 +0300

    Added selecto manga list

--- apps/mangadex/lib/componnents/manga/list/MangaListContent.svelte
+++ apps/mangadex/lib/componnents/manga/list/MangaListContent.svelte
@@ -14,6 +14,9 @@
 	import CoverMangaList from "./cover/CoverMangaList.svelte";
 	import LongMangaList from "./long/LongMangaList.svelte";
 	import MediumMangaList from "./medium/MediumMangaList.svelte";
+	import Selecto from "selecto";
+	import { validate } from "uuid";
+	import ChapterFeedSelectoDialog from "@mangadex/componnents/selecto/ChapterFeedSelectoDialog.svelte";
 
 	interface Props {
 		list?: MangaListContentItemProps[];
@@ -21,9 +24,75 @@
 
 	let { list }: Props = $props();
 	const style = getMangaListStyleContext();
+	let container: HTMLElement | undefined = $state();
+	let selectedMangas: Set<string> = $state(new Set());
+	let selectedChapters: Set<string> = $state(new Set());
+	let selected_mangas = $derived(Array.from(selectedMangas.values()));
+	let selected_chapters = $derived(Array.from(selectedChapters.values()));
+	let dialog: HTMLDialogElement | undefined = $state(undefined);
+	function openDialog() {
+		if (dialog) {
+			dialog.showModal();
+		}
+	}
+	$effect(() => {
+		if (container) {
+			const selecto = new Selecto({
+				container,
+				selectableTargets: ["manga-element", "chapter-element"],
+				toggleContinueSelect: "shift"
+			});
+			selecto
+				.on("dragStart", () => {
+					selectedChapters.clear();
+					selectedMangas.clear();
+				})
+				.on("selectStart", (ev) => {
+					ev.added.forEach((element) => {
+						element.setAttribute("data-selecto-selected", "");
+					});
+					ev.removed.forEach((element) => {
+						element.removeAttribute("data-selecto-selected");
+					});
+				})
+				.on("selectEnd", (ev) => {
+					ev.afterAdded.forEach((element) => {
+						element.setAttribute("data-selecto-selected", "");
+					});
+					ev.afterRemoved.forEach((element) => {
+						element.removeAttribute("data-selecto-selected");
+					});
+					ev.selected.forEach((element) => {
+						const maybeChapterId = element.getAttribute("data-chapter-id");
+						if (maybeChapterId != null) {
+							if (validate(maybeChapterId)) {
+								selectedChapters.add(maybeChapterId);
+							}
+						}
+						const maybeMangaId = element.getAttribute("data-manga-id");
+						if (maybeMangaId != null) {
+							if (validate(maybeMangaId)) {
+								selectedChapters.add(maybeMangaId);
+							}
+						}
+					});
+					openDialog();
+				});
+
+			return () => {
+				selecto.destroy();
+			};
+		}
+	});
 </script>
 
-<div class="manga-list-content">
+<ChapterFeedSelectoDialog
+	bind:dialog
+	selectedChapters={selected_chapters}
+	selectedMangas={selected_mangas}
+/>
+
+<div class="manga-list-content" bind:this={container}>
 	{#if $style == MangaListStyle.Grid}
 		<MediumMangaList {list} />
 	{:else if $style == MangaListStyle.Rows}
