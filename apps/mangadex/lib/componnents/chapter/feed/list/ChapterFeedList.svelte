ac3c72dd1190b147d8bcc3973971934e63e19d32
tonymushah <tonyramanantsalama@gmail.com>
2025-03-31 21:06:57 +0300

    added selecto for chapter feed list

--- apps/mangadex/lib/componnents/chapter/feed/list/ChapterFeedList.svelte
+++ apps/mangadex/lib/componnents/chapter/feed/list/ChapterFeedList.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	/*
+	 TODO implement selecto
+	 */
 	import MidToneLine from "@mangadex/componnents/theme/lines/MidToneLine.svelte";
 	import { ChapterFeedStyle } from "@mangadex/gql/graphql";
 	import { type Writable } from "svelte/store";
@@ -6,18 +9,87 @@
 	import ChapterFeedElement2 from "../element2/ChapterFeedElement2.svelte";
 	import ChapterFeedElement3 from "../element3/ChapterFeedElement3.svelte";
 	import ChapterFeedListSelector from "./select/ChapterFeedListSelector.svelte";
+	import Selecto from "selecto";
+	import { validate } from "uuid";
+	import ChapterFeedSelectoDialog from "@mangadex/componnents/selecto/ChapterFeedSelectoDialog.svelte";
 
 	interface Props {
 		list?: ChapterFeedListItem[];
 		style: Writable<ChapterFeedStyle>;
-		children?: import('svelte').Snippet;
+		children?: import("svelte").Snippet;
 	}
 
 	let { list = [], style, children }: Props = $props();
 	let coverfull = $derived($style == ChapterFeedStyle.CoverFull);
 	let coverless = $derived($style == ChapterFeedStyle.CoverLess);
 	let isEmpty = $derived(list.length == 0);
+	let container: HTMLElement | undefined = $state();
+	let selectedMangas: Set<string> = $state(new Set());
+	let selectedChapters: Set<string> = $state(new Set());
+	let selected_mangas = $derived(Array.from(selectedMangas.values()));
+	let selected_chapters = $derived(Array.from(selectedChapters.values()));
+	let dialog: HTMLDialogElement | undefined = $state(undefined);
+	function openDialog() {
+		if (dialog) {
+			dialog.showModal();
+		}
+	}
+	$effect(() => {
+		if (container) {
+			const selecto = new Selecto({
+				container,
+				selectableTargets: ["manga-element", "chapter-element"],
+				toggleContinueSelect: "shift"
+			});
+			selecto
+				.on("dragStart", () => {
+					selectedChapters.clear();
+					selectedMangas.clear();
+				})
+				.on("selectStart", (ev) => {
+					ev.added.forEach((element) => {
+						element.setAttribute("data-selecto-selected", "");
+					});
+					ev.removed.forEach((element) => {
+						element.removeAttribute("data-selecto-selected");
+					});
+				})
+				.on("selectEnd", (ev) => {
+					ev.afterAdded.forEach((element) => {
+						element.setAttribute("data-selecto-selected", "");
+					});
+					ev.afterRemoved.forEach((element) => {
+						element.removeAttribute("data-selecto-selected");
+					});
+					ev.selected.forEach((element) => {
+						const maybeChapterId = element.getAttribute("data-chapter-id");
+						if (maybeChapterId != null) {
+							if (validate(maybeChapterId)) {
+								selectedChapters.add(maybeChapterId);
+							}
+						}
+						const maybeMangaId = element.getAttribute("data-manga-id");
+						if (maybeMangaId != null) {
+							if (validate(maybeMangaId)) {
+								selectedChapters.add(maybeMangaId);
+							}
+						}
+					});
+					openDialog();
+				});
+
+			return () => {
+				selecto.destroy();
+			};
+		}
+	});
 </script>
+
+<ChapterFeedSelectoDialog
+	bind:dialog
+	selectedChapters={selected_chapters}
+	selectedMangas={selected_mangas}
+/>
 
 <section>
 	<div class="tab-title">
@@ -29,7 +101,7 @@
 		<ChapterFeedListSelector {style} />
 	</div>
 	<MidToneLine />
-	<section class="content" class:coverfull class:coverless class:isEmpty>
+	<section class="content" bind:this={container} class:coverfull class:coverless class:isEmpty>
 		{#each list as item}
 			{#if coverfull}
 				<ChapterFeedElement2
---
+++ apps/mangadex/lib/componnents/selecto/ChapterFeedSelectoDialog.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import ButtonAccent from "@mangadex/componnents/theme/buttons/ButtonAccent.svelte";
+	import { XIcon as CloseIcon } from "svelte-feather-icons";
+
+	interface Props {
+		dialog: HTMLDialogElement | undefined;
+		selectedMangas: string[];
+		selectedChapters: string[];
+	}
+	let { dialog = $bindable(undefined), selectedChapters, selectedMangas }: Props = $props();
+	function closeDialog() {
+		if (dialog) {
+			dialog.close();
+		}
+	}
+	let chaptersLen = $derived(selectedChapters.length);
+	let chaptersEmpty = $derived(chaptersLen == 0);
+
+	let mangasLen = $derived(selectedMangas.length);
+	let mangasEmpty = $derived(mangasLen == 0);
+</script>
+
+<dialog bind:this={dialog}>
+	<div class="title">
+		<p>Add to Library</p>
+		<ButtonAccent on:click={closeDialog}>
+			<CloseIcon />
+		</ButtonAccent>
+	</div>
+	<div class="body">
+		<p>
+			You have selected {mangasLen} title{#if mangasLen > 1}s{/if} and {chaptersLen} chapter{#if chaptersLen > 1}s{/if}.
+		</p>
+	</div>
+</dialog>
+
+<style lang="scss">
+	dialog {
+		background-color: var(--main-background);
+		color: var(--text-color);
+		width: 75vw;
+		border: 2px solid var(--primary);
+		border-radius: 3px;
+	}
+	dialog::backdrop {
+		backdrop-filter: blur(10px);
+		-webkit-backdrop-filter: blur(10px);
+	}
+</style>
