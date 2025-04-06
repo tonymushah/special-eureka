90cc18f11b22108aa508773326c69198a8dabee0
tonymushah <tonyramanantsalama@gmail.com>
2025-04-06 17:10:55 +0300

    ins rewrite

--- src-tauri/mangadex/src/intelligent_notification_system.rs
+++ src-tauri/mangadex/src/intelligent_notification_system.rs
@@ -1,112 +1,169 @@
-use std::io::Result;
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    hash::Hash,
+    io::Result,
+};
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Copy)]
+pub enum DownloadEntryState {
+    #[default]
+    Queue,
+    Success,
+    Failed,
+}
 
 /// Refactor this into an hash map with an enum
 #[derive(Clone)]
-pub struct DownloadEntry<T: Clone + PartialEq> {
-    queue: Vec<T>,
-    success: Vec<T>,
-    failed: Vec<T>,
-}
-impl<T: Clone + PartialEq> Default for DownloadEntry<T> {
+pub struct DownloadEntries<T>(HashMap<T, DownloadEntryState>);
+
+impl<T> Default for DownloadEntries<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T: Clone + PartialEq> DownloadEntry<T> {
+impl<T> DownloadEntries<T> {
     pub fn new() -> Self {
-        Self {
-            queue: Vec::new(),
-            success: Vec::new(),
-            failed: Vec::new(),
-        }
+        Self(HashMap::new())
     }
-    pub fn get_queue(self) -> Vec<T> {
-        self.queue
+    pub fn is_all_finished(&self) -> bool {
+        self.0
+            .iter()
+            .all(|(_, state)| *state != DownloadEntryState::Queue)
     }
-    pub fn get_queue_ref(&self) -> &Vec<T> {
-        &self.queue
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
     }
-    pub fn get_success(self) -> Vec<T> {
-        self.success
+    pub fn clear(&mut self) {
+        self.0.clear();
     }
-    pub fn get_success_ref(&self) -> &Vec<T> {
-        &self.success
+}
+impl<T> DownloadEntries<T>
+where
+    T: Clone,
+{
+    pub fn get_queue(&self) -> Vec<T> {
+        self.0
+            .iter()
+            .filter(|(_, state)| **state == DownloadEntryState::Queue)
+            .map(|(t, _)| t.clone())
+            .collect()
     }
-    pub fn get_failed(self) -> Vec<T> {
-        self.failed
+    pub fn get_success(&self) -> Vec<T> {
+        self.0
+            .iter()
+            .filter(|(_, state)| **state == DownloadEntryState::Success)
+            .map(|(t, _)| t.clone())
+            .collect()
     }
-    pub fn get_failed_ref(&self) -> &Vec<T> {
-        &self.failed
+    pub fn get_failed(&self) -> Vec<T> {
+        self.0
+            .iter()
+            .filter(|(_, state)| **state == DownloadEntryState::Failed)
+            .map(|(t, _)| t.clone())
+            .collect()
     }
     pub fn get_failed_len(&self) -> usize {
-        self.failed.len()
+        self.0
+            .iter()
+            .filter(|(_, state)| **state == DownloadEntryState::Failed)
+            .count()
     }
     pub fn get_success_len(&self) -> usize {
-        self.success.len()
+        self.0
+            .iter()
+            .filter(|(_, state)| **state == DownloadEntryState::Success)
+            .count()
     }
     pub fn get_queue_len(&self) -> usize {
-        self.queue.len()
+        self.0
+            .iter()
+            .filter(|(_, state)| **state == DownloadEntryState::Queue)
+            .count()
     }
     pub fn get_finished_len(&self) -> usize {
-        self.get_failed_len() + self.get_success_len()
+        self.0
+            .iter()
+            .filter(|(_, state)| **state != DownloadEntryState::Queue)
+            .count()
     }
+    pub fn entries(&self) -> Vec<T> {
+        self.0.keys().cloned().collect()
+    }
+}
+
+impl<T> DownloadEntries<T>
+where
+    T: Eq + Hash,
+{
     pub fn add_in_queue(&mut self, to_insert: T) -> Result<()> {
-        if self.queue.contains(&to_insert) {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::AlreadyExists,
-                "The value to insert is alredy in queue",
-            ));
-        } else {
-            self.queue.push(to_insert);
+        match self.0.entry(to_insert) {
+            Entry::Occupied(_) => {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::AlreadyExists,
+                    "The value to insert is alredy in queue",
+                ))
+            }
+            Entry::Vacant(d) => {
+                d.insert(DownloadEntryState::Queue);
+            }
         }
         Ok(())
     }
     pub fn add_in_failed(&mut self, to_insert: T) -> Result<()> {
-        if !self.queue.contains(&to_insert) {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::NotFound,
-                "The value to insert is'nt in queue",
-            ));
-        } else if self.failed.contains(&to_insert) {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::AlreadyExists,
-                "The value to insert is alredy in failed",
-            ));
-        } else {
-            self.failed.push(to_insert);
+        match self.0.entry(to_insert) {
+            Entry::Occupied(mut e) => match e.get() {
+                DownloadEntryState::Queue => {
+                    *e.get_mut() = DownloadEntryState::Failed;
+                }
+                DownloadEntryState::Success => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::AlreadyExists,
+                        "The value to insert is alredy in success",
+                    ))
+                }
+                DownloadEntryState::Failed => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::AlreadyExists,
+                        "The value to insert is alredy in failed",
+                    ))
+                }
+            },
+            Entry::Vacant(_) => {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::AlreadyExists,
+                    "The value to insert is alredy in failed",
+                ));
+            }
         }
         Ok(())
     }
     pub fn add_in_success(&mut self, to_insert: T) -> Result<()> {
-        if !self.queue.contains(&to_insert) {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::NotFound,
-                "The value to insert is'nt in queue",
-            ));
-        } else if self.success.contains(&to_insert) {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::AlreadyExists,
-                "The value to insert is alredy in success",
-            ));
-        } else {
-            self.success.push(to_insert);
+        match self.0.entry(to_insert) {
+            Entry::Occupied(mut e) => match e.get() {
+                DownloadEntryState::Queue => {
+                    *e.get_mut() = DownloadEntryState::Success;
+                }
+                DownloadEntryState::Success => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::AlreadyExists,
+                        "The value to insert is alredy in success",
+                    ))
+                }
+                DownloadEntryState::Failed => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::AlreadyExists,
+                        "The value to insert is alredy in failed",
+                    ))
+                }
+            },
+            Entry::Vacant(_) => {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::AlreadyExists,
+                    "The value to insert is alredy in failed",
+                ));
+            }
         }
         Ok(())
-    }
-    pub fn is_all_finished(&self) -> bool {
-        if self.get_finished_len() == 0 && self.get_queue_len() == 0 {
-            false
-        } else {
-            self.get_finished_len() >= self.get_queue_len()
-        }
-    }
-    pub fn is_empty(&self) -> bool {
-        self.queue.is_empty()
-    }
-    pub fn clear(&mut self) {
-        self.success.clear();
-        self.failed.clear();
-        self.success.clear();
     }
 }
