3939cec6bb66caa56d8151b385de151f57e1dc34
tonymushah <tonyramanantsalama@gmail.com>
2025-04-12 13:53:03 +0300

    fixes

--- src-tauri/mangadex/src/query/manga/list.rs
+++ src-tauri/mangadex/src/query/manga/list.rs
@@ -125,7 +125,6 @@ impl MangaListQueries {
             .deref()
             .clone();
         let client = get_mangadex_client_from_graphql_context::<tauri::Wry>(ctx)?;
-        let params = self.deref().clone();
         let params = if self.manga_ids.is_empty() {
             self.manga_ids
                 .chunks(100)
