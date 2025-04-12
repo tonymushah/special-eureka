358b656da553f4f4fe3d1dc6bd74d8e129baacc8
tonymushah <tonyramanantsalama@gmail.com>
2025-04-12 15:02:14 +0300

    infinite pagination for ReadMarkerQueries

--- src-tauri/mangadex/src/query/read_marker.rs
+++ src-tauri/mangadex/src/query/read_marker.rs
@@ -33,26 +33,26 @@ impl ReadMarkerQueries {
     pub async fn manga_read_markers(
         &self,
         ctx: &Context<'_>,
-        #[graphql(validator(min_items = 1, max_items = 100))] manga_ids: Vec<Uuid>,
+        #[graphql(validator(min_items = 1))] manga_ids: Vec<Uuid>,
     ) -> Result<Vec<Uuid>> {
         let watches = get_watches_from_graphql_context::<tauri::Wry>(ctx)?;
         let client =
             get_mangadex_client_from_graphql_context_with_auth_refresh::<tauri::Wry>(ctx).await?;
-        if let MangaReadMarkers::Ungrouped(res) = client
-            .manga()
-            .read()
-            .get()
-            .manga_ids(manga_ids)
-            .send()
-            .await?
-        {
-            res.data.iter().for_each(|id| {
-                let _ = watches.read_marker.send_data((*id, true));
-            });
-            Ok(res.data)
-        } else {
-            Err(Error::GotReadMarkersGrouped)
+        let mut to_ret = Vec::<Uuid>::default();
+        for ids in manga_ids.chunks(100) {
+            if let MangaReadMarkers::Ungrouped(res) =
+                client.manga().read().get().manga_ids(ids).send().await?
+            {
+                res.data.into_iter().for_each(|id| {
+                    to_ret.push(id);
+                    let _ = watches.read_marker.send_data((id, true));
+                });
+            } else {
+                return Err(Error::GotReadMarkersGrouped);
+            }
         }
+
+        Ok(to_ret)
     }
     pub async fn manga_read_markers_grouped(
         &self,
@@ -62,28 +62,32 @@ impl ReadMarkerQueries {
         let watches = get_watches_from_graphql_context::<tauri::Wry>(ctx)?;
         let client =
             get_mangadex_client_from_graphql_context_with_auth_refresh::<tauri::Wry>(ctx).await?;
-        if let MangaReadMarkers::Grouped(res) = client
-            .manga()
-            .read()
-            .get()
-            .manga_ids(manga_ids)
-            .grouped(true)
-            .send()
-            .await?
-        {
-            Ok(res
-                .data
-                .into_iter()
-                .map(|i| -> MangaReadMarkerGroupedItems {
-                    i.1.iter().for_each(|id| {
+        let mut to_ret = Vec::<MangaReadMarkerGroupedItems>::default();
+        for ids in manga_ids.chunks(100) {
+            if let MangaReadMarkers::Grouped(res) = client
+                .manga()
+                .read()
+                .get()
+                .manga_ids(ids)
+                .grouped(true)
+                .send()
+                .await?
+            {
+                res.data.into_iter().for_each(|(id, chapters)| {
+                    chapters.iter().for_each(|id| {
                         let _ = watches.read_marker.send_data((*id, true));
                     });
-                    i.into()
-                })
-                .collect())
-        } else {
-            Err(Error::GotReadMarkersUnGrouped)
+                    to_ret.push(MangaReadMarkerGroupedItems {
+                        manga_id: id,
+                        chapters,
+                    });
+                });
+            } else {
+                return Err(Error::GotReadMarkersUnGrouped);
+            }
         }
+
+        Ok(to_ret)
     }
     pub async fn user_history(&self, ctx: &Context<'_>) -> Result<Vec<UserHistoryEntry>> {
         let watches = get_watches_from_graphql_context::<tauri::Wry>(ctx)?;
