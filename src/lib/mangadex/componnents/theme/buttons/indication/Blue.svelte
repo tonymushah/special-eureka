772a2cde07aa38646a17046e41abe953f4681323
tonymushah <tonyramanantsalama@gmail.com>
2025-04-29 18:23:26 +0300

    refactored `indication` buttons

--- src/lib/mangadex/componnents/theme/buttons/indication/Blue.svelte
+++ src/lib/mangadex/componnents/theme/buttons/indication/Blue.svelte
@@ -1,29 +1,12 @@
 <script lang="ts">
+	import type { HTMLButtonAttributes } from "svelte/elements";
 	import ButtonBase from "../base/ButtonBase.svelte";
-	import { createEventDispatcher } from "svelte";
 
-	createEventDispatcher<{
-		click: MouseEvent & {
-			currentTarget: EventTarget & HTMLButtonElement;
-		};
-	}>();
-	interface Props {
-		type?: "reset" | "submit" | "button";
-		style?: string | undefined;
+	interface Props extends HTMLButtonAttributes {
 		isBase?: boolean;
-		children?: import("svelte").Snippet;
-		disabled?: boolean;
 	}
 
-	let {
-		type = "button",
-		style = undefined,
-		isBase = false,
-		children,
-		disabled
-	}: Props = $props();
+	let { isBase = false, ...restProps }: Props = $props();
 </script>
 
-<ButtonBase {isBase} --button-color={"var(--indication-blue)"} {style} on:click {type} {disabled}>
-	{@render children?.()}
-</ButtonBase>
+<ButtonBase {isBase} --button-color={"var(--indication-blue)"} {...restProps} />
