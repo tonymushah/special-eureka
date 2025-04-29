1cb99817c733939bb4b476a0c65f04050dd15ea8
tonymushah <tonyramanantsalama@gmail.com>
2025-04-29 16:39:26 +0300

    Refactored danger buttons

--- src/lib/mangadex/componnents/theme/buttons/danger/L2.svelte
+++ src/lib/mangadex/componnents/theme/buttons/danger/L2.svelte
@@ -1,28 +1,12 @@
 <script lang="ts">
-	import { createEventDispatcher } from "svelte";
+	import type { HTMLButtonAttributes } from "svelte/elements";
 	import ButtonBase from "../base/ButtonBase.svelte";
-	createEventDispatcher<{
-		click: MouseEvent & {
-			currentTarget: EventTarget & HTMLButtonElement;
-		};
-	}>();
-	interface Props {
-		type?: "reset" | "submit" | "button";
-		style?: string | undefined;
+
+	interface Props extends HTMLButtonAttributes {
 		isBase?: boolean;
-		children?: import("svelte").Snippet;
-		disabled?: boolean;
 	}
 
-	let {
-		type = "button",
-		style = undefined,
-		isBase = false,
-		children,
-		disabled
-	}: Props = $props();
+	let { isBase = false, ...restProps }: Props = $props();
 </script>
 
-<ButtonBase --button-color={"var(--danger-l2)"} {disabled} {style} on:click {type} {isBase}>
-	{@render children?.()}
-</ButtonBase>
+<ButtonBase --button-color={"var(--danger-l2)"} {isBase} {...restProps} />
