d2a019e298cfd875dca9efc468c2173f540fd73b
tonymushah <tonyramanantsalama@gmail.com>
2025-04-30 23:03:19 +0300

    config-update

--- eslint.config.ts
+++ eslint.config.ts
@@ -11,45 +11,33 @@ export default defineConfig([
 	{ files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
 	tseslint.configs.recommended,
 	{ files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
-	eslintPluginSvelte.configs.prettier,
+	svelte.configs.prettier,
 	{
 		files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
-		// See more details at: https://typescript-eslint.io/packages/parser/
 		languageOptions: {
 			parserOptions: {
 				projectService: true,
-				extraFileExtensions: [".svelte"], // Add support for additional file extensions, such as .svelte
+				extraFileExtensions: [".svelte"], 
 				parser: tseslint.parser,
-				// Specify a parser for each language, if needed:
-				// parser: {
-				//   ts: ts.parser,
-				//   js: espree,    // Use espree for .js files (add: import espree from 'espree')
-				//   typescript: ts.parser
-				// },
-
-				// We recommend importing and specifying svelte.config.js.
-				// By doing so, some rules in eslint-plugin-svelte will automatically read the configuration and adjust their behavior accordingly.
-				// While certain Svelte settings may be statically loaded from svelte.config.js even if you don’t specify it,
-				// explicitly specifying it ensures better compatibility and functionality.
 				svelteConfig
 			}
 		}
 	},
 	{
-		ignorePatterns: [
+		ignores: [
 			".DS_Store",
 			"node_modules",
-			"/build",
-			"/.svelte-kit",
-			"/package",
+			"build",
+			".svelte-kit",
+			"package",
 			".env",
 			".env.*",
 			"!.env.example",
 			"pnpm-lock.yaml",
 			"package-lock.json",
 			"yarn.lock",
-			"/src-tauri",
-			"/target"
+			"src-tauri",
+			"target"
 		]
 	}
 ]);
