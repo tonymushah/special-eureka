284e39a64f90cd21c3d12a50a08835812f82ca1c
tonymushah <tonyramanantsalama@gmail.com>
2025-04-30 17:51:10 +0300

    refactored aggregate event handling

--- src/lib/mangadex/componnents/manga/page/chapters/aggreate/ChaptersAccordion.svelte
+++ src/lib/mangadex/componnents/manga/page/chapters/aggreate/ChaptersAccordion.svelte
@@ -1,83 +1,30 @@
 <script lang="ts">
 	import ChapterElement1, {
-		createChapterEl1EventDispatcher
+		type ChapterEl1Events
 	} from "@mangadex/componnents/chapter/base/element1/ChapterElement1.svelte";
 	import Accordion from "@mangadex/componnents/theme/accordion/Accordion.svelte";
-	import { createEventDispatcher, type ComponentProps } from "svelte";
+	import { type ComponentProps } from "svelte";
 
-	interface Props {
+	interface Props extends ChapterEl1Events {
 		title: string;
 		chapters: ComponentProps<typeof ChapterElement1>[];
 		isOpen?: boolean;
 	}
 
-	let { title, chapters, isOpen = false }: Props = $props();
+	let { title, chapters, isOpen = false, ...events }: Props = $props();
 
-	const dispatch = createChapterEl1EventDispatcher();
 	let isSingle = $derived(chapters.length == 1);
 	let isEmpty = $derived(chapters.length == 0);
 </script>
 
 {#if isSingle}
-	<ChapterElement1
-		{...chapters[0]}
-		on:download={({ detail }) => {
-			dispatch("download", detail);
-		}}
-		on:downloadKeyPress={({ detail }) => {
-			dispatch("downloadKeyPress", detail);
-		}}
-		on:read={({ detail }) => {
-			dispatch("read", detail);
-		}}
-		on:readKeyPress={({ detail }) => {
-			dispatch("readKeyPress", detail);
-		}}
-		on:remove={({ detail }) => {
-			dispatch("remove", detail);
-		}}
-		on:removeKeyPress={({ detail }) => {
-			dispatch("removeKeyPress", detail);
-		}}
-		on:comments={({ detail }) => {
-			dispatch("comments", detail);
-		}}
-		on:commentsKeyPress={({ detail }) => {
-			dispatch("commentsKeyPress", detail);
-		}}
-	/>
+	<ChapterElement1 {...chapters[0]} {...events} />
 {:else if !isEmpty}
 	<div class="some-margin">
 		<Accordion {title} titleBorder {isOpen}>
 			<div class="chapters">
 				{#each chapters as chapter (chapter.id)}
-					<ChapterElement1
-						{...chapter}
-						on:download={({ detail }) => {
-							dispatch("download", detail);
-						}}
-						on:downloadKeyPress={({ detail }) => {
-							dispatch("downloadKeyPress", detail);
-						}}
-						on:read={({ detail }) => {
-							dispatch("read", detail);
-						}}
-						on:readKeyPress={({ detail }) => {
-							dispatch("readKeyPress", detail);
-						}}
-						on:remove={({ detail }) => {
-							dispatch("remove", detail);
-						}}
-						on:removeKeyPress={({ detail }) => {
-							dispatch("removeKeyPress", detail);
-						}}
-						on:comments={({ detail }) => {
-							dispatch("comments", detail);
-						}}
-						on:commentsKeyPress={({ detail }) => {
-							dispatch("commentsKeyPress", detail);
-						}}
-					/>
+					<ChapterElement1 {...chapter} {...events} />
 				{/each}
 			</div>
 		</Accordion>
