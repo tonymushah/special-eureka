12601a4870d3f622a668bdb4765c6caade128230
tonymushah <tonyramanantsalama@gmail.com>
2025-05-01 09:21:11 +0300

    Added `indexes` context

--- src/lib/mangadex/componnents/chapter/page/readinMode/doublePage/utils/contexts/index.ts
+++ src/lib/mangadex/componnents/chapter/page/readinMode/doublePage/utils/contexts/index.ts
@@ -1,12 +1,15 @@
 import type { Readable } from "svelte/store";
 import { initChapterImagesRatioContext } from "./images-ratios";
 import { initDoublePageChapterImagesContext } from "./images-double";
+import { initDoublePageChapterIndexesContext } from "./indexes";
 
 export default function initDoublePageContexts(param?: { images?: Readable<string[]> }) {
 	const ratios = initChapterImagesRatioContext(param?.images);
 	const doubleImages = initDoublePageChapterImagesContext(ratios);
+	const indexes = initDoublePageChapterIndexesContext(doubleImages);
 	return {
 		ratios,
-		doubleImages
+		doubleImages,
+		indexes
 	};
 }
