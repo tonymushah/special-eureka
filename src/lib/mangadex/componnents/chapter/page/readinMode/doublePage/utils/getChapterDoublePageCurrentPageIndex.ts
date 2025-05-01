946401ddab732464ec00b3ea0454627c118d9cf7
tonymushah <tonyramanantsalama@gmail.com>
2025-05-01 10:33:27 +0300

    import refactor

--- src/lib/mangadex/componnents/chapter/page/readinMode/doublePage/utils/getChapterDoublePageCurrentPageIndex.ts
+++ src/lib/mangadex/componnents/chapter/page/readinMode/doublePage/utils/getChapterDoublePageCurrentPageIndex.ts
@@ -1,9 +1,8 @@
 import { type Readable } from "svelte/store";
 import { getDoublePageChapterCurrentPageIndexContext } from "./contexts/current-page-index";
-import getChapterDoublePageIndexes from "./getChapterDoublePageIndexes";
 
 export default function getChapterDoublePageCurrentPageIndex(
 ): Readable<number> {
-	const images = getChapterDoublePageIndexes();
 	return getDoublePageChapterCurrentPageIndexContext()
 }
+
