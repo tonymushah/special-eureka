56a8d6d2cd41e95a7dd7c7e65913eb768c8ee99f
tonymushah <tonyramanantsalama@gmail.com>
2025-05-02 12:11:37 +0300

    added page limit option

---
+++ src/routes/mangadex/settings/general-settings/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	interface Props {
+		children?: Snippet;
+	}
+	let { children }: Props = $props();
+</script>
+
+<h1>General Settings</h1>
+
+{@render children?.()}
