525cdc87d16d525498960df3d6900993ce42b592
tonymushah <tonyramanantsalama@gmail.com>
2025-05-06 00:20:56 +0300

    `cache-image`

---
+++ src/lib/mangadex/utils/cache-image.ts
@@ -0,0 +1,60 @@
+import { mangadexQueryClient } from "@mangadex/index";
+import { createQueries, createQuery, type QueryFunctionContext } from "@tanstack/svelte-query";
+import { derived, type Readable } from "svelte/store";
+
+export function cacheImage(url: string) {
+	return createQuery({
+		queryKey: ["image", url],
+		staleTime: Infinity,
+		async queryFn(ctx) {
+			const blob = await fetch(url, {
+				signal: ctx.signal
+			}).then((e) => e.blob());
+			return URL.createObjectURL(blob)
+		}
+	}, mangadexQueryClient)
+}
+
+export function cacheImageFromReadable(url_read: Readable<string>) {
+	return createQuery(derived(url_read, (url) => ({
+		queryKey: ["image", url],
+		staleTime: Infinity,
+		async queryFn(ctx: QueryFunctionContext) {
+			const blob = await fetch(url, {
+				signal: ctx.signal
+			}).then((e) => e.blob());
+			return URL.createObjectURL(blob)
+		}
+	})), mangadexQueryClient)
+}
+
+
+export function cacheImages(urls: string[]) {
+	return createQueries({
+		queries: urls.map((url) => ({
+			queryKey: ["image", url],
+			staleTime: Infinity,
+			async queryFn({ signal }: QueryFunctionContext) {
+				const blob = await fetch(url, {
+					signal
+				}).then((e) => e.blob());
+				return URL.createObjectURL(blob)
+			}
+		}))
+	}, mangadexQueryClient)
+}
+
+export function cacheImagesFromReadable(urls_readable: Readable<string[]>) {
+	return createQueries({
+		queries: derived(urls_readable, (urls) => urls.map((url) => ({
+			queryKey: ["image", url],
+			staleTime: Infinity,
+			async queryFn({ signal }: QueryFunctionContext) {
+				const blob = await fetch(url, {
+					signal
+				}).then((e) => e.blob());
+				return URL.createObjectURL(blob)
+			}
+		})))
+	}, mangadexQueryClient)
+}
