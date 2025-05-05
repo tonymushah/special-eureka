15d6ac2e5a07b7f651339ffc6af37275abc2a443
tonymushah <tonyramanantsalama@gmail.com>
2025-05-05 20:51:43 +0300

    contextMenuContext

---
+++ src/lib/utils/contextMenuContext.ts
@@ -0,0 +1,17 @@
+import type { ContextMenuItem } from "$lib/commands/contextMenu";
+import { getContext, setContext } from "svelte";
+
+const KEY = "context-menu-context";
+
+export function setContextMenuContext(items: ContextMenuItem[]) {
+	return setContext(KEY, items)
+}
+
+export function getContextMenuContext(): ContextMenuItem[] {
+	const maybeItems = getContext<ContextMenuItem[] | undefined>(KEY);
+	if (maybeItems) {
+		return maybeItems;
+	} else {
+		return [];
+	}
+}
