90a26a09a9ac4f00ac063a11dd0267eba739d5ea
tonymushah <tonyramanantsalama@gmail.com>
2025-05-09 11:44:52 +0300

    added `RecentlyAdded` page

---
+++ src/routes/mangadex/titles/recently-added/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { derived as sderived } from "svelte/store";
+	import SearchContent from "./SearchContent.svelte";
+	import pageLimit from "@mangadex/stores/page-limit";
+
+	let refetch: (() => void) | undefined = $state(undefined);
+</script>
+
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<h1
+	onclick={() => {
+		refetch?.();
+	}}
+>
+	Recently Added Titles
+</h1>
+
+<SearchContent
+	bind:refetch
+	params={sderived(pageLimit, (e) => ({
+		limit: e
+	}))}
+/>
---
+++ src/routes/mangadex/titles/recently-added/SearchContent.svelte
@@ -0,0 +1,142 @@
+<script lang="ts">
+	import MangaList from "@mangadex/componnents/manga/list/MangaList.svelte";
+	import type { MangaListContentItemProps } from "@mangadex/componnents/manga/list/MangaListContent.svelte";
+	import Fetching from "@mangadex/componnents/search/content/Fetching.svelte";
+	import HasNext from "@mangadex/componnents/search/content/HasNext.svelte";
+	import NothingToShow from "@mangadex/componnents/search/content/NothingToShow.svelte";
+	import type { MangaListParams } from "@mangadex/gql/graphql";
+	import { createInfiniteQuery, type CreateInfiniteQueryOptions } from "@tanstack/svelte-query";
+	import { getContextClient } from "@urql/svelte";
+	import { debounce } from "lodash";
+	import { onDestroy } from "svelte";
+	import { derived, get, type Readable } from "svelte/store";
+	import executeSearchQuery from "./execute";
+	import defaultContentProfile from "@mangadex/content-profile/graphql/defaultProfile";
+
+	const client = getContextClient();
+	const debounce_wait = 450;
+	interface Props {
+		params: Readable<MangaListParams>;
+		refetch?: () => void;
+	}
+
+	let { params, refetch = $bindable() }: Props = $props();
+	const p_p_offline = derived([params, defaultContentProfile], ([merged]) => [merged]);
+	interface InfiniteQueryData {
+		data: MangaListContentItemProps[];
+		offset: number;
+		limit: number;
+		total: number;
+	}
+	const infiniteQuery = createInfiniteQuery(
+		derived(p_p_offline, ([$params]) => {
+			return {
+				queryKey: ["rencently-added-page", $params],
+				initialPageParam: [$params],
+				getNextPageParam(lastPage, allPages, [lastPageParam], allPageParams) {
+					const next_offset = lastPage.limit + lastPage.offset;
+					if (next_offset > lastPage.total) {
+						return null;
+					} else {
+						return [
+							{
+								...lastPageParam,
+								limit: lastPage.limit,
+								offset: next_offset
+							}
+						];
+					}
+				},
+				async queryFn({ pageParam: [p] }) {
+					const res = await executeSearchQuery(client, p);
+					return {
+						data: res.data,
+						...res.paginationData
+					};
+				},
+				getPreviousPageParam(firstPage, allPages, [firstPageParam], allPageParams) {
+					const next_offset = firstPage.limit - firstPage.offset;
+					if (next_offset < 0) {
+						return null;
+					} else {
+						return [
+							{
+								...firstPageParam,
+								limit: firstPage.limit,
+								offset: next_offset
+							}
+						];
+					}
+				}
+			} satisfies CreateInfiniteQueryOptions<
+				InfiniteQueryData,
+				Error,
+				InfiniteQueryData,
+				InfiniteQueryData,
+				[string, MangaListParams],
+				[MangaListParams]
+			>;
+		})
+	);
+	refetch = debounce(() => {
+		get(infiniteQuery).refetch();
+	}, debounce_wait);
+	const titles = derived(infiniteQuery, (result) => {
+		if (result.isLoading) {
+			return [];
+		}
+		return result.data?.pages.map((d) => d.data) ?? [];
+	});
+	const isFetching = derived(infiniteQuery, (result) => result.isFetching);
+	const hasNext = derived(infiniteQuery, (result) => result.hasNextPage);
+	const fetchNext = debounce(async function () {
+		const inf = get(infiniteQuery);
+		return await inf.fetchNextPage();
+	});
+	const observer = new IntersectionObserver(
+		(entries) => {
+			if (!$isFetching && $hasNext) {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						fetchNext();
+					}
+				});
+			}
+		},
+		{
+			threshold: 1.0
+		}
+	);
+	let to_obserce_bind: HTMLElement | undefined = $state(undefined);
+	$effect(() => {
+		if (to_obserce_bind) {
+			observer.observe(to_obserce_bind);
+			return () => {
+				if (to_obserce_bind) observer.unobserve(to_obserce_bind);
+			};
+		}
+	});
+	onDestroy(() => {
+		observer.disconnect();
+	});
+</script>
+
+<MangaList list={$titles}></MangaList>
+
+<div class="observer-trigger" bind:this={to_obserce_bind}>
+	{#if $isFetching}
+		<Fetching />
+	{:else if $hasNext}
+		<HasNext />
+	{:else}
+		<NothingToShow />
+	{/if}
+</div>
+
+<style lang="scss">
+	.observer-trigger {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
