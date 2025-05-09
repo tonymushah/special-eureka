33d68b6a163d08325e32841107ca758179e302fd
tonymushah <tonyramanantsalama@gmail.com>
2025-05-09 15:07:26 +0300

    margin

--- src/routes/mangadex/titles/recently-added/+layout.svelte
+++ src/routes/mangadex/titles/recently-added/+layout.svelte
@@ -7,4 +7,17 @@
 	let { children }: Props = $props();
 </script>
 
-{@render children?.()}
+<div>
+	{@render children?.()}
+</div>
+
+<style lang="scss">
+	div {
+		--layout-margin-x: 1.25em;
+		--layout-margin-y: 0.5em;
+		margin-left: var(--layout-margin-x);
+		margin-right: var(--layout-margin-x);
+		margin-top: var(--layout-margin-y);
+		margin-bottom: var(--layout-margin-y);
+	}
+</style>
