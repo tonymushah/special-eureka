a4f744c419540ac0c53d8b7fb227ad1986a84ad1
tonymushah <tonyramanantsalama@gmail.com>
2025-05-15 15:23:06 +0300

    Added content for custom list feed

--- src/routes/mangadex/list/[id]/feed/+page.svelte
+++ src/routes/mangadex/list/[id]/feed/+page.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
 	import { CustomListVisibility } from "@mangadex/gql/graphql";
 	import type { PageData } from "./$types";
+	import { readonly, writable } from "svelte/store";
+	import SearchContent from "./SearchContent.svelte";
 
 	interface Props {
 		data: PageData;
 	}
 	let { data }: Props = $props();
-	let isPrivate = $derived(data.attributes.visibility == CustomListVisibility.Private);
+	const isPrivate = writable(data.attributes.visibility == CustomListVisibility.Private);
+	$effect(() => {
+		isPrivate.set(data.attributes.visibility == CustomListVisibility.Private);
+	});
+	const customListId = writable<string>(data.id);
+	$effect(() => {
+		customListId.set(data.id);
+	});
 </script>
+
+<SearchContent customListId={readonly(customListId)} isPrivate={readonly(isPrivate)} />
