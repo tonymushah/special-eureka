03091ca62398a21f0b3a22d058249e5becc0ff87
tonymushah <tonyramanantsalama@gmail.com>
2025-05-15 06:13:13 +0300

    refactored +layout.ts

--- src/routes/mangadex/list/[id]/+layout.ts
+++ src/routes/mangadex/list/[id]/+layout.ts
@@ -5,7 +5,9 @@ import customListPageQuery from "@mangadex/componnents/custom-list/page/query";
 export const load: LayoutLoad = async (load_params) => {
 	const id = load_params.params.id;
 	const client = await getClient();
-	const res = await client.query(customListPageQuery, { id: id.replace("private:", ""), private: id.startsWith("private:") }).toPromise();
+	const real_id = id.replace("private:", "")
+	const _private = id.startsWith("private:");
+	const res = await client.query(customListPageQuery, { id: real_id, private: _private }).toPromise();
 	if (res.error) {
 		throw res.error
 	} else if (res.data) {
