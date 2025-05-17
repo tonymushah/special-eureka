0475d79793c28c9df3638b516dd8b1307c1aa8b9
tonymushah <tonyramanantsalama@gmail.com>
2025-05-17 07:35:13 +0300

    Added `latestUploadsPageQuery`

---
+++ src/routes/mangadex/titles/latest-uploads/query.ts
@@ -0,0 +1,70 @@
+import { graphql } from "@mangadex/gql";
+
+export const latest_updates_query = graphql(/* GraphQL */ `
+	query latestUploadsPageQuery($offset: Int, $limit: Int) {
+		chapter {
+			listWithGroupByManga(
+				chapterListParams: {
+					includeEmptyPages: EXCLUDE
+					includeExternalUrl: EXCLUDE
+					includeFutureUpdates: EXCLUDE
+					includeFuturePublishAt: EXCLUDE
+					order: { readableAt: DESCENDING }
+					offset: $offset
+					limit: $limit
+				}
+				feedContent: true
+			) {
+				limit
+				offset
+				total
+				data {
+					manga {
+						id
+						attributes {
+							title
+							originalLanguage
+							lastVolume
+							lastChapter
+						}
+						relationships {
+							coverArt {
+								id
+								attributes {
+									fileName
+								}
+							}
+						}
+					}
+					chapters {
+						id
+						attributes {
+							title
+							chapter
+							volume
+							translatedLanguage
+							externalUrl
+							createdAt
+							readableAt
+						}
+						relationships {
+							scanlationGroups {
+								id
+								attributes {
+									name
+								}
+							}
+							user {
+								id
+								attributes {
+									roles
+									username
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+`);
