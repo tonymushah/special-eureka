6ffd8712fd67728db348bf8629999b80c55cc5bc
tonymushah <tonyramanantsalama@gmail.com>
2025-05-21 11:10:49 +0300

    added `read_markers_batch`

--- src-tauri/mangadex/src/mutation/read_marker.rs
+++ src-tauri/mangadex/src/mutation/read_marker.rs
@@ -1,6 +1,20 @@
-use crate::Result;
+use std::collections::HashMap;
+
+use crate::{
+    Result,
+    objects::chapter::Chapter,
+    utils::{
+        source::SendMultiSourceData,
+        splittable_param::SendSplitted,
+        traits_utils::{MangadexAsyncGraphQLContextExt, MangadexTauriManagerExt},
+    },
+};
 use async_graphql::{Context, Object};
-use mangadex_api_input_types::read_marker::batch_read_marker::MarkChapterBatchParam;
+use mangadex_api_input_types::{
+    chapter::list::ChapterListParams, read_marker::batch_read_marker::MarkChapterBatchParam,
+};
+use mangadex_api_types_rust::RelationshipType;
+use uuid::Uuid;
 
 use crate::utils::{
     get_mangadex_client_from_graphql_context_with_auth_refresh, get_watches_from_graphql_context,
@@ -27,6 +41,69 @@ impl ReadMarkerMutations {
         params.chapter_ids_unread.iter().for_each(|id| {
             let _ = watches.read_marker.send_data((*id, false));
         });
+        Ok(true)
+    }
+    pub async fn read_markers_batch(
+        &self,
+        ctx: &Context<'_>,
+        chapter_ids_read: Vec<Uuid>,
+        chapter_ids_unread: Vec<Uuid>,
+        update_history: Option<bool>,
+    ) -> Result<bool> {
+        let manga_chapter_ids: HashMap<Uuid, Vec<Uuid>> = {
+            let app = ctx.get_app_handle::<tauri::Wry>()?;
+            let watches = get_watches_from_graphql_context::<tauri::Wry>(ctx)?;
+            let chapter_ids = {
+                let mut v = Vec::<Uuid>::new();
+                v.extend(chapter_ids_read.iter().copied());
+                v.extend(chapter_ids_unread.iter().copied());
+                v.dedup();
+                v
+            };
+            let params = ChapterListParams {
+                chapter_ids,
+                ..Default::default()
+            };
+            let client = app.get_mangadex_client()?;
+            let chapters = params.send_splitted_default(&client).await?;
+            chapters
+                .data
+                .into_iter()
+                .inspect(|item| {
+                    let chapter: Chapter = item.clone().into();
+                    let _ = watches.chapter.send_online(chapter);
+                })
+                .fold(HashMap::<Uuid, Vec<Uuid>>::new(), |mut state, item| {
+                    if let Some(manga) = item.find_first_relationships(RelationshipType::Manga) {
+                        state.entry(manga.id).or_default().push(item.id);
+                    }
+                    state
+                })
+        };
+        let params = manga_chapter_ids.into_iter().fold(
+            Vec::<MarkChapterBatchParam>::new(),
+            |mut params, (manga_id, chapter_ids)| {
+                let param = MarkChapterBatchParam {
+                    manga_id,
+                    chapter_ids_read: chapter_ids_read
+                        .iter()
+                        .filter(|id| chapter_ids.contains(id))
+                        .copied()
+                        .collect(),
+                    chapter_ids_unread: chapter_ids_unread
+                        .iter()
+                        .filter(|id| chapter_ids.contains(id))
+                        .copied()
+                        .collect(),
+                    update_history: update_history.unwrap_or_default(),
+                };
+                params.push(param);
+                params
+            },
+        );
+        for param in params {
+            self.manga_read_markers_batch(ctx, param).await?;
+        }
         Ok(true)
     }
 }
