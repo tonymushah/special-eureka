689b8cc1e3c21a3443e59f7e14fd68a94f51bc6f
tonymushah <tonyramanantsalama@gmail.com>
2025-05-22 06:28:00 +0300

    more format

--- src/lib/mangadex/stores/manga/manga_following_status.ts
+++ src/lib/mangadex/stores/manga/manga_following_status.ts
@@ -33,31 +33,37 @@ const unfollowMutation = graphql(`
 `);
 
 export async function get_manga_following_status(manga_id: string): Promise<boolean> {
-	const res = await client.query(query, {
-		id: manga_id
-	}).toPromise();
+	const res = await client
+		.query(query, {
+			id: manga_id
+		})
+		.toPromise();
 	if (res.data) {
-		return res.data.follows.isFollowingManga
+		return res.data.follows.isFollowingManga;
 	} else if (res.error) {
-		throw res.error
+		throw res.error;
 	} else {
 		throw new Error("no data??");
 	}
 }
 
 export async function followManga(manga_id: string) {
-	const res = await client.mutation(followMutation, {
-		id: manga_id
-	}).toPromise();
+	const res = await client
+		.mutation(followMutation, {
+			id: manga_id
+		})
+		.toPromise();
 	if (res.error) {
 		throw res.error;
 	}
 }
 
 export async function unfollowManga(manga_id: string) {
-	const res = await client.mutation(unfollowMutation, {
-		id: manga_id
-	}).toPromise();
+	const res = await client
+		.mutation(unfollowMutation, {
+			id: manga_id
+		})
+		.toPromise();
 	if (res.error) {
 		throw res.error;
 	}
@@ -72,28 +78,33 @@ export async function set_manga_following_status(manga_id: string, is_following:
 }
 
 export type MangaFollowingStatusOption = {
-	getOnMount?: boolean,
-	onGetError?: (e: unknown) => void,
-	initValue?: boolean
-}
+	getOnMount?: boolean;
+	onGetError?: (e: unknown) => void;
+	initValue?: boolean;
+};
 
-export default function manga_following_status(manga_id: string, options: MangaFollowingStatusOption = {
-	getOnMount: true
-}): Readable<boolean> {
+export default function manga_following_status(
+	manga_id: string,
+	options: MangaFollowingStatusOption = {
+		getOnMount: true
+	}
+): Readable<boolean> {
 	return readable(options.initValue ?? false, (set) => {
-		const sub = client.subscription(subscription, {
-			id: manga_id
-		}).subscribe((res) => {
-			if (res.data) {
-				set(res.data?.watchIsFollowingManga ?? false);
-			} else if (res.error) {
-				console.error(res.error);
-			}
-		});
+		const sub = client
+			.subscription(subscription, {
+				id: manga_id
+			})
+			.subscribe((res) => {
+				if (res.data) {
+					set(res.data?.watchIsFollowingManga ?? false);
+				} else if (res.error) {
+					console.error(res.error);
+				}
+			});
 		if (options?.getOnMount)
 			get_manga_following_status(manga_id).catch(options.onGetError ?? console.error);
 		return () => {
-			sub.unsubscribe()
-		}
-	})
+			sub.unsubscribe();
+		};
+	});
 }
