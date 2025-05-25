b26202dfd7961a289f0f402d9eef7fbe7d40a1fb
tonymushah <tonyramanantsalama@gmail.com>
2025-05-26 00:44:12 +0300

    some layout refactor

---
+++ src/lib/mangadex/componnents/layout/MangaDexLayout.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import "@fontsource-variable/josefin-sans/index.css";
+	import "@fontsource/poppins/latin.css";
+	import MangaDexContextDataProvider from "@mangadex/componnents/MangaDexContextDataProvider.svelte";
+	import MangaDexDefaultThemeProvider from "@mangadex/componnents/theme/MangaDexDefaultThemeProvider.svelte";
+	import defaultProfile from "@mangadex/content-profile/graphql/defaultProfile";
+	import { client } from "@mangadex/gql/urql";
+	import { mangadexQueryClient } from "@mangadex/index";
+	import { QueryClientProvider } from "@tanstack/svelte-query";
+	import { SvelteQueryDevtools } from "@tanstack/svelte-query-devtools";
+	import { setContextClient } from "@urql/svelte";
+	import { onDestroy, onMount } from "svelte";
+	import MainContent from "./MainContent.svelte";
+	import MonoLoaders from "./MonoLoaders.svelte";
+
+	interface Props {
+		children?: import("svelte").Snippet;
+	}
+	let { children }: Props = $props();
+	onMount(async () => {
+		const { mangadexTitleBar } = await import("@mangadex/titlebar");
+		await mangadexTitleBar();
+	});
+	onMount(() => {
+		return defaultProfile.subscribe(() => {});
+	});
+	onDestroy(async () => {
+		const { defaultBehavior } = await import(
+			"$lib/window-decoration/stores/decorations.svelte"
+		);
+		defaultBehavior();
+	});
+	setContextClient(client);
+</script>
+
+<QueryClientProvider client={mangadexQueryClient}>
+	<div class="d-content">
+		<style>
+			html::-webkit-scrollbar {
+				display: none;
+			}
+		</style>
+		<MangaDexContextDataProvider>
+			<MangaDexDefaultThemeProvider>
+				<MonoLoaders />
+				<MainContent {children} />
+			</MangaDexDefaultThemeProvider>
+		</MangaDexContextDataProvider>
+	</div>
+	<SvelteQueryDevtools />
+</QueryClientProvider>
+
+<style lang="scss">
+	.d-content {
+		display: contents;
+	}
+</style>
---
+++ src/lib/mangadex/componnents/layout/MonoLoaders.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import SetTitle from "@mangadex/componnents/theme/SetTitle.svelte";
+	import ReadDialog from "@mangadex/componnents/manga/read/ReadDialog.svelte";
+	import AddToList from "@mangadex/componnents/manga/add-to-list/AddToList.svelte";
+</script>
+
+<SetTitle />
+<ReadDialog />
+<AddToList />
