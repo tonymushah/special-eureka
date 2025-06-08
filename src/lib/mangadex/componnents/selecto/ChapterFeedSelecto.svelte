32b9bc637ec79f0efc47c16abeadc35852f0ae51
tonymushah <tonyramanantsalama@gmail.com>
2025-06-08 08:30:07 +0300

    removed selecto

--- src/lib/mangadex/componnents/selecto/ChapterFeedSelecto.svelte
+++ src/lib/mangadex/componnents/selecto/ChapterFeedSelecto.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Selecto from "selecto";
+	import DragSelect from "dragselect";
 	import { validate } from "uuid";
 	import { uniq } from "lodash";
 	import { openSelectoDialog } from "./ChapterFeedSelectoDialog.svelte";
@@ -23,63 +23,58 @@
 	let selected_chapters = $derived(uniq(selectedChapters));
 	$effect(() => {
 		if (container && canSelect) {
-			const selecto = new Selecto({
-				container,
-				selectableTargets: [".manga-element", ".chapter-element"],
-				toggleContinueSelect: "alt",
-				preventDefault: true,
-				preventRightClick: true,
-				preventClickEventOnDrag: true,
-				preventClickEventOnDragStart: true,
-				hitRate: 50,
-				innerScrollOptions: {
-					container: "#mangadex-scroll-container"
+			const dragselect = new DragSelect({
+				//selectables: [...document.querySelectorAll<HTMLElement>(".manga-element"), ...document.querySelectorAll<HTMLElement>(".chapter-element")],
+				area: container
+			});
+			const mutationObserver = new MutationObserver((v) => {
+				for (const mutation of v) {
+					if (mutation.type == "childList") {
+						dragselect.addSelectables([
+							...container.querySelectorAll<HTMLElement>(".manga-element"),
+							...container.querySelectorAll<HTMLElement>(".chapter-element")
+						]);
+					}
 				}
 			});
-			selecto
-				.on("dragStart", () => {
-					selecto
-						.findSelectableTargets()
-						.forEach((e) => e.removeAttribute("data-selecto-selected"));
+			mutationObserver.observe(container);
+			dragselect.subscribe("DS:start", (ev) => {
+				dragselect.SelectableSet.forEach((e) => {
+					e.removeAttribute("data-selecto-selected");
 					selectedChapters = [];
 					selectedMangas = [];
-				})
-				.on("select", (ev) => {
-					ev.added.forEach((element) => {
-						element.setAttribute("data-selecto-selected", "");
-					});
-					ev.removed.forEach((element) => {
-						element.removeAttribute("data-selecto-selected");
-					});
-				})
-				.on("selectEnd", (ev) => {
-					ev.afterAdded.forEach((element) => {
-						element.setAttribute("data-selecto-selected", "");
-					});
-					ev.afterRemoved.forEach((element) => {
-						element.removeAttribute("data-selecto-selected");
-					});
-					ev.selected.forEach((element) => {
-						const maybeChapterId = element.getAttribute("data-chapter-id");
-						if (maybeChapterId != null) {
-							if (validate(maybeChapterId)) selectedChapters.push(maybeChapterId);
-						}
-						const maybeMangaId = element.getAttribute("data-manga-id");
-						if (maybeMangaId != null) {
-							if (validate(maybeMangaId)) selectedMangas.push(maybeMangaId);
-						}
-					});
-					openSelectoDialog({
-						titles: selected_mangas,
-						chapters: selected_chapters
-					});
 				});
-
+			});
+			dragselect.subscribe("DS:end", (ev) => {
+				ev.items.forEach((element) => {});
+				ev.items.forEach((element) => {
+					const maybeChapterId = element.getAttribute("data-chapter-id");
+					if (maybeChapterId != null) {
+						if (validate(maybeChapterId)) selectedChapters.push(maybeChapterId);
+					}
+					const maybeMangaId = element.getAttribute("data-manga-id");
+					if (maybeMangaId != null) {
+						if (validate(maybeMangaId)) selectedMangas.push(maybeMangaId);
+					}
+					element.removeAttribute("data-selecto-selected");
+				});
+				openSelectoDialog({
+					titles: selected_mangas,
+					chapters: selected_chapters
+				});
+			});
+			dragselect.subscribe("DS:select", (ev) => {
+				ev.item.setAttribute("data-selecto-selected", "");
+			});
+			dragselect.subscribe("DS:unselect", (ev) => {
+				ev.item.removeAttribute("data-selecto-selected");
+			});
 			return () => {
-				selecto
-					.findSelectableTargets()
-					.forEach((e) => e.removeAttribute("data-selecto-selected"));
-				selecto.destroy();
+				mutationObserver.disconnect();
+				dragselect.SelectedSet.forEach((e) => {
+					e.removeAttribute("data-selecto-selected");
+				});
+				dragselect.stop();
 			};
 		}
 	});
