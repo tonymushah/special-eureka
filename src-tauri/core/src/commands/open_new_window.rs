506bfbdc1c001e026f22835460870e74772b045e
tonymushah <tonyramanantsalama@gmail.com>
2025-08-17 21:22:14 +0300

    use https scheme to true _ptdr_

--- src-tauri/core/src/commands/open_new_window.rs
+++ src-tauri/core/src/commands/open_new_window.rs
@@ -1,4 +1,4 @@
-use tauri::{command, AppHandle, Result, Runtime, Url, WebviewUrl, WebviewWindow};
+use tauri::{AppHandle, Result, Runtime, Url, WebviewUrl, WebviewWindow, command};
 use uuid::Uuid;
 
 // use crate::builder::menu::set_menu_window;
@@ -7,13 +7,18 @@ pub fn open_new_window_sync_from_app<R: Runtime>(
     app: &AppHandle<R>,
     url: Option<WebviewUrl>,
 ) -> Result<()> {
-    let builder = WebviewWindow::builder(
+    #[allow(unused_mut)]
+    let mut builder = WebviewWindow::builder(
         app,
         format!("main-{}", Uuid::new_v4()),
         url.unwrap_or_default(),
     )
     .title(String::from("Special Eureka"))
     .decorations(false);
+    #[cfg(windows)]
+    {
+        builder = builder.use_https_scheme(true);
+    }
     let _ww = builder.build()?;
     //set_menu_window(&ww.as_ref().window())?;
     Ok(())
