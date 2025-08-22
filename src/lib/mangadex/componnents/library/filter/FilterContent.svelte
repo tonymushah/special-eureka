c79f82d45cec30f93cb1bbc28ef29b506b996f5f
tonymushah <tonyramanantsalama@gmail.com>
2025-08-23 01:18:04 +0300

    some comments :)

--- src/lib/mangadex/componnents/library/filter/FilterContent.svelte
+++ src/lib/mangadex/componnents/library/filter/FilterContent.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 	import type { UserLibrarySectionParam } from "@mangadex/gql/graphql";
-	import { derived, get, type Writable } from "svelte/store";
+	import { derived, get, writable, type Writable } from "svelte/store";
 	import SortBySelector from "./SortBySelector.svelte";
 	import { initMangaSearchPublicationStatusContextStore } from "@mangadex/componnents/manga/search/form/filter/contexts/publicationStatus";
 	import PublicationStatus from "@mangadex/componnents/manga/search/form/filter/content/PublicationStatus.svelte";
 	import { initMangaSearchYearContextStore } from "@mangadex/componnents/manga/search/form/filter/contexts/year";
 	import Year from "@mangadex/componnents/manga/search/form/filter/content/Year.svelte";
 	import { v4 } from "uuid";
+	import {
+		initMangaSearchAuthorArtistsOptions,
+		initMangaSearchAuthorSearchFetcher,
+		type AuthorArtistOptions
+	} from "@mangadex/componnents/manga/search/form/filter/contexts/authorArtist";
+	import gqlAuthorFetcher from "@mangadex/componnents/manga/search/form/filter/contexts/authorArtist/gql";
+	import { onMount } from "svelte";
+	import AuthorArtists from "@mangadex/componnents/manga/search/form/filter/content/AuthorArtists.svelte";
+	import { route } from "$lib/ROUTES";
 
 	interface Props {
 		params: Writable<UserLibrarySectionParam>;
@@ -91,6 +100,25 @@
 		</article>
 	</section>
 	<SortBySelector {params} />
+	<p>
+		Some filters (for tags, content rating, etc...) are not showed here.
+		<span class="lazy-af"> because I was lazy to implement that :) </span>
+		<br />
+		Please change
+		<a href={route("/mangadex/settings/content-profiles")} class="basic-link">
+			your content profile
+		</a> instead.
+	</p>
+	<p>
+		<span class="lazy-af">
+			Tony Mushah: To honest, I could do it (really). It just that it doesn't make sense to me
+			to add those complex filter thingy here.
+		</span>
+	</p>
+	<p class="lazy-af">
+		Also, the author and artists are not saved proprely when this dialog is closed, but it still
+		filter though. I am not planning to fix anytime soon. Maybe in future.
+	</p>
 </section>
 
 <style lang="scss">
@@ -122,5 +150,12 @@
 		gap: 8px;
 		flex-direction: row;
 		flex-wrap: wrap;
+	}
+	a.basic-link {
+		color: var(--primary-l2);
+	}
+	.lazy-af {
+		font-style: italic;
+		color: color-mix(in srgb, var(--main-background) 70%, var(--text-color) 30%);
 	}
 </style>
