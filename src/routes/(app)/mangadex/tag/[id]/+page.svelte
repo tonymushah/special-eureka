1b33462d215e78bce0984c89b608cd34be414c78
tonymushah <tonyramanantsalama@gmail.com>
2025-09-02 20:54:18 +0300

    added `RecentlyAddedFromTag`

--- src/routes/(app)/mangadex/tag/[id]/+page.svelte
+++ src/routes/(app)/mangadex/tag/[id]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageData } from "./$types";
+	import RecentlyAddedFromTag from "./RecentlyAddedFromTag.svelte";
 	import TopTenTags from "./TopTenTags.svelte";
 
 	interface Props {
@@ -9,3 +10,5 @@
 </script>
 
 <TopTenTags id={data.id} />
+
+<RecentlyAddedFromTag id={data.id} />
---
+++ src/routes/(app)/mangadex/tag/[id]/RecentlyAddedFromTag.svelte
@@ -0,0 +1,139 @@
+<script lang="ts">
+	import ErrorComponent from "@mangadex/componnents/ErrorComponent.svelte";
+	import MangaElementBase4WithReadableCoverImage from "@mangadex/componnents/manga/base/base4/MangaElementBase4WithReadableCoverImage.svelte";
+	import defaultContentProfile from "@mangadex/content-profile/graphql/defaultProfile";
+	import tagRecentlyAddedQuery from "@mangadex/gql-docs/tag/page/recentlyAdded";
+	import { CoverImageQuality } from "@mangadex/gql/graphql";
+	import get_cover_art from "@mangadex/utils/cover-art/get_cover_art";
+	import get_value_from_title_and_random_if_undefined from "@mangadex/utils/lang/get_value_from_title_and_random_if_undefined";
+	import { createQuery } from "@tanstack/svelte-query";
+	import { getContextClient } from "@urql/svelte";
+	import { debounce } from "lodash";
+	import { onMount } from "svelte";
+	import { get } from "svelte/store";
+	import type { SwiperContainer } from "swiper/element";
+	import type { SwiperOptions } from "swiper/types";
+
+	interface Props {
+		id: string;
+	}
+	let { id }: Props = $props();
+	let swiper_container: SwiperContainer | undefined = $state(undefined);
+	onMount(() => {
+		// swiper parameters
+		const swiperParams: SwiperOptions = {
+			slidesPerView: "auto",
+			breakpoints: {
+				640: {
+					slidesPerView: 3
+				},
+				1024: {
+					slidesPerView: 5
+				},
+				1360: {
+					slidesPerView: 6
+				}
+			},
+			mousewheel: true,
+			freeMode: true,
+			on: {
+				init() {
+					// ...
+				}
+			}
+		};
+
+		if (swiper_container) {
+			// now we need to assign all parameters to Swiper element
+			Object.assign(swiper_container, swiperParams);
+
+			// and now initialize it
+			swiper_container.initialize();
+		}
+	});
+
+	const client = getContextClient();
+	const query = createQuery({
+		queryKey: ["tag", id, "recently-added", "titles"],
+		async queryFn() {
+			const res = await client
+				.query(tagRecentlyAddedQuery, {
+					id
+				})
+				.toPromise();
+			if (res.error) {
+				throw res.error;
+			} else if (res.data) {
+				return res.data.tag.page.recentlyAdded;
+			} else {
+				throw new Error("No data??");
+			}
+		},
+		networkMode: "online"
+	});
+	$effect(() =>
+		defaultContentProfile.subscribe(
+			debounce(() => {
+				$query.refetch();
+			})
+		)
+	);
+</script>
+
+<section>
+	<h3>Recently Added</h3>
+	{#if $query.isLoading}
+		<div class="loading">
+			<h4>
+				LoAdInG...
+				{#if $query.isPaused}
+					<i>(paused)</i>
+				{/if}
+			</h4>
+		</div>
+	{:else if $query.isSuccess}
+		<swiper-container bind:this={swiper_container} init="false">
+			{#each $query.data as title (title.id)}
+				<swiper-slide>
+					<MangaElementBase4WithReadableCoverImage
+						title={get_value_from_title_and_random_if_undefined(
+							title.attributes.title,
+							"en"
+						) ?? ""}
+						coverImage={get_cover_art({
+							cover_id: title.relationships.coverArt.id,
+							manga_id: title.id,
+							mode: CoverImageQuality.V256,
+							filename: title.relationships.coverArt.attributes.fileName,
+							client
+						})}
+						coverImageAlt={title.relationships.coverArt.attributes.description}
+						mangaId={title.id}
+					/>
+				</swiper-slide>
+			{/each}
+		</swiper-container>
+	{:else if $query.isError}
+		<ErrorComponent
+			label={`Cannot fetch ${id} recently added`}
+			error={$query.error}
+			retry={() => {
+				$query.refetch();
+			}}
+		/>
+	{/if}
+</section>
+
+<style lang="scss">
+	.loading {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		color: var(--mid-tone);
+		border: 2px dashed var(--mid-tone);
+		border-radius: 1em;
+		width: 50%;
+		margin: 0px 25%;
+		height: 7em;
+	}
+</style>
