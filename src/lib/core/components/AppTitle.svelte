02c41309588b8aadbfc34ed47b972496445866cc
tonymushah <tonyramanantsalama@gmail.com>
2025-09-03 21:24:07 +0300

    added `<AppTitle/>`

---
+++ src/lib/core/components/AppTitle.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { getCurrentWindow } from "@tauri-apps/api/window";
+
+	interface Props {
+		title: string;
+	}
+	let { title }: Props = $props();
+
+	const currentWindow = getCurrentWindow();
+	$effect(() => {
+		currentWindow.setTitle(title).catch(console.error);
+		document.title = title;
+	});
+</script>
