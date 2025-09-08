46b290b31866d1534098192bdd855d5518aa8f48
tonymushah <tonyramanantsalama@gmail.com>
2025-09-08 09:35:11 +0300

    added `registerContextMenuEvent`

--- src/lib/core/utils/contextMenuContext.ts
+++ src/lib/core/utils/contextMenuContext.ts
@@ -1,4 +1,5 @@
 import type { ContextMenuItem } from "@special-eureka/core/commands/contextMenu";
+import contextMenu from "@special-eureka/core/commands/contextMenu";
 import { getContext, setContext } from "svelte";
 
 const KEY = "context-menu-context";
@@ -13,5 +14,30 @@ export function getContextMenuContext(): () => ContextMenuItem[] {
 		return maybeItems;
 	} else {
 		return () => [];
+	}
+}
+
+export type RegisterContextMenuEventOptions = {
+	additionalMenus?: () => ContextMenuItem[],
+	includeContext?: boolean;
+	preventDefault?: boolean;
+	stopPropagation?: boolean;
+};
+
+export default function registerContextMenuEvent(options?: RegisterContextMenuEventOptions): (e: MouseEvent) => any {
+	const includeContext = options?.includeContext ?? true;
+	const contextMenuFunc = includeContext ? getContextMenuContext() : () => [];
+	return async (e) => {
+		if (options?.preventDefault) {
+			e.preventDefault();
+		}
+		if (options?.stopPropagation) {
+			e.stopPropagation();
+		}
+		let menu = contextMenuFunc();
+		if (options?.additionalMenus) {
+			menu.push(...options.additionalMenus());
+		}
+		await contextMenu(menu, e)
 	}
 }
