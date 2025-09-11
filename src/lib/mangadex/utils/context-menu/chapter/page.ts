39e4ca5ebbf42c4ad2ba62ff74f0d9b0be27ab49
tonymushah <tonyramanantsalama@gmail.com>
2025-09-11 11:12:39 +0300

    todo

---
+++ src/lib/mangadex/utils/context-menu/chapter/page.ts
@@ -0,0 +1,7 @@
+import type { ContextMenuItem } from "@special-eureka/core/commands/contextMenu";
+
+// TODO I am lazy right now
+// Maybe in 0.3 :3
+export default function chapterPageImageContextMenu(): ContextMenuItem[] {
+	return []
+}
