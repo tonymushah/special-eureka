aec859d50078ff3136a946659ef878a7baa98ead
tonymushah <tonyramanantsalama@gmail.com>
2025-09-20 12:47:21 +0300

    migrated to infinite query for manga covers

--- src/lib/mangadex/componnents/manga/page/covers/Covers.svelte
+++ src/lib/mangadex/componnents/manga/page/covers/Covers.svelte
@@ -2,109 +2,83 @@
 	import { getTitleLayoutData } from "@mangadex/routes/title/[id]/layout.context";
 	import get_cover_art from "@mangadex/utils/cover-art/get_cover_art";
 	import { getContextClient } from "@urql/svelte";
-	import { onDestroy, onMount } from "svelte";
-	import { fade, slide } from "svelte/transition";
+	import { onDestroy } from "svelte";
 	import type { CoverInput } from "./CoverContents.svelte";
 	import CoverContents from "./CoverContents__.svelte";
 	import { getCoversImageStoreContext } from "./utils/coverImageStoreContext";
 	import getMangaCoversQuery from "./utils/query";
-	import { debounce, type DebouncedFunc } from "lodash";
-	import { get, writable, derived as der } from "svelte/store";
+	import { get, derived as der } from "svelte/store";
+	import { createInfiniteQuery } from "@tanstack/svelte-query";
+	import pageLimit from "@mangadex/stores/page-limit";
+	import ErrorComponent from "@mangadex/componnents/ErrorComponent.svelte";
 
 	const d = getTitleLayoutData();
 	const data = d.layoutData;
 	const id = data!.id;
 	const client = getContextClient();
 	const imagesStore = getCoversImageStoreContext();
-	const isLoading = writable(false);
-	const currentOffset = writable(0);
-	const isAtEnd = writable(false);
-	const coversData = writable<CoverInput[]>([]);
-	const fetchCovers = debounce(async function () {
-		if (get(isAtEnd)) {
-			throw new Error("No next data can be fetched");
-		}
-		console.debug("run");
-		const res = await client
-			.query(getMangaCoversQuery, {
-				id,
-				offset: get(currentOffset)
-			})
-			.toPromise();
-		if (res.error) {
-			throw res.error;
-		}
-		const covers: CoverInput[] = [];
-		const list = res.data?.cover.list;
-		if (list) {
-			covers.push(
-				...list.data.map<CoverInput>((v) => {
-					const locale = v.attributes.locale == null ? undefined : v.attributes.locale;
-					const title =
-						v.attributes.volume == null || v.attributes.volume == undefined
-							? v.id
-							: `Volume ${v.attributes.volume}`;
-					return {
-						id: v.id,
-						title,
-						alt: v.attributes.fileName,
-						locale: locale
-					};
+
+	const query = createInfiniteQuery({
+		queryKey: ["title", id, "covers"],
+		async queryFn({ pageParam }) {
+			const res = await client
+				.query(getMangaCoversQuery, {
+					id,
+					offset: pageParam.offset
 				})
-			);
-			imagesStore.setByBatch(
-				list.data.map((v) => {
-					return {
-						id: v.id,
-						image: get_cover_art({
-							cover_id: v.id,
-							filename: v.attributes.fileName,
-							manga_id: id,
-							client
-						})
-					};
-				})
-			);
-			if (covers.length > 0) {
-				if (get(currentOffset) == 0) {
-					coversData.set(covers);
-				} else {
-					coversData.update((cs) => {
-						cs.push(...covers);
-						return cs;
-					});
-				}
-				const nextOffset = get(currentOffset) + list.limit;
-				if (nextOffset >= list.total) {
-					isAtEnd.set(true);
-				} else {
-					currentOffset.set(nextOffset);
-				}
+				.toPromise();
+			if (res.error) {
+				throw res.error;
+			} else if (res.data == undefined) {
+				throw new Error("Require data");
+			} else {
+				return {
+					list: res.data.cover.list.data.map<
+						CoverInput & {
+							filename: string;
+						}
+					>((v) => {
+						const locale = v.attributes.locale == null ? undefined : v.attributes.locale;
+						const title =
+							v.attributes.volume == null || v.attributes.volume == undefined
+								? v.id
+								: `Volume ${v.attributes.volume}`;
+						return {
+							id: v.id,
+							title,
+							alt: v.attributes.fileName,
+							locale: locale,
+							filename: v.attributes.fileName
+						};
+					}),
+					offset: res.data.cover.list.offset,
+					limit: res.data.cover.list.limit,
+					total: res.data.cover.list.total
+				};
+			}
+		},
+		initialPageParam: {
+			offset: 0,
+			limit: get(pageLimit)
+		},
+
+		networkMode: "always",
+		getNextPageParam(lastPage) {
+			const nextPageOffset = lastPage.offset + lastPage.limit;
+			if (nextPageOffset >= lastPage.total) {
+				return {
+					limit: lastPage.limit,
+					offset: nextPageOffset
+				};
+			} else {
+				return null;
 			}
 		}
-	}, 300);
-	async function fetch() {
-		if (!get(isLoading) && !get(isAtEnd)) {
-			isLoading.set(true);
-			if (fetchCovers) {
-				const res = fetchCovers();
-				try {
-					if (res != undefined) {
-						await res;
-					}
-				} finally {
-					isLoading.set(false);
-				}
-			}
-		}
-	}
-	onMount(async () => {
-		await fetch();
 	});
-	const interObs = new IntersectionObserver(async (o, s) => {
+	const interObs = new IntersectionObserver(async (o) => {
 		o.forEach((_o) => {
 			if (_o.isIntersecting) {
-				fetch();
+				$query.fetchNextPage();
 			}
 		});
 	});
@@ -114,16 +88,44 @@
 			interObs.observe(interObsEl);
 		}
 	});
+	const coversData = der(query, ($query) =>
+		new Set($query.data?.pages.flatMap((c) => c.list)).values().toArray()
+	);
+	const querySub = coversData.subscribe(($covers) => {
+		const set = $covers.map((c) => {
+			return {
+				id: c.id,
+				image: get_cover_art({
+					client,
+					cover_id: c.id,
+					manga_id: id,
+					filename: c.filename
+				})
+			};
+		});
+		imagesStore.setByBatch(set);
+	});
 
+	const isDataEmpty = der(coversData, ($covers) => $covers.length == 0);
 	onDestroy(() => {
+		querySub();
 		interObs.disconnect();
 	});
-	const isDataEmpty = der(coversData, ($coversData) => $coversData.length == 0);
-	const isInitialLoading = der(
-		[isLoading, isDataEmpty],
-		([isLoading, isDataEmpty]) => isLoading && isDataEmpty
-	);
 </script>
 
-<CoverContents {isDataEmpty} {isLoading} {isInitialLoading} {coversData} />
+<CoverContents
+	{isDataEmpty}
+	isLoading={der(query, ($query) => $query.isFetching)}
+	isInitialLoading={der(query, ($query) => $query.isLoading)}
+	{coversData}
+/>
+{#if $query.isError}
+	<ErrorComponent
+		label={"Error on fetching title covers"}
+		retry={() => {
+			$query.refetch();
+		}}
+		error={$query.error}
+	/>
+{/if}
 <div bind:this={interObsEl}></div>
