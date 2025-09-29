9b234b019edd0bc1cd46e8d1989cf8ae9f7ab630
tonymushah <tonyramanantsalama@gmail.com>
2025-09-29 11:13:58 +0300

    added add-to-list mutation

---
+++ src/lib/mangadex/componnents/manga/add-to-list/mutation.ts
@@ -0,0 +1,41 @@
+import { mangadexQueryClient } from "@mangadex/index";
+import { createMutation } from "@tanstack/svelte-query";
+import type { Client } from "@urql/svelte";
+import { mutation } from "./query";
+import { ActionMode } from ".";
+
+export const mutationQueryMutation = createMutation({
+	mutationKey: ["title", "add-to-list"],
+	async mutationFn({
+		title,
+		selectedListMap,
+		client
+	}: {
+		selectedListMap: Map<string, ActionMode>,
+		title: string,
+		client: Client
+	}) {
+		const addTo = Array.from(
+			selectedListMap
+				.entries()
+				.filter(([_, mode]) => mode == ActionMode.Add)
+				.map(([id, _]) => id)
+		);
+		const removeFrom = Array.from(
+			selectedListMap
+				.entries()
+				.filter(([_, mode]) => mode == ActionMode.Remove)
+				.map(([id, _]) => id)
+		);
+		const res = await client
+			.mutation(mutation, {
+				addTo,
+				removeFrom,
+				manga_id: title
+			})
+			.toPromise();
+		if (res.error) {
+			throw res.error;
+		}
+	}
+}, mangadexQueryClient);
