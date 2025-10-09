2ca6f8909ea7b056fbfccfe50f2fb681acd2544c
tonymushah <tonyramanantsalama@gmail.com>
2025-10-09 11:42:22 +0300

    typo fix

--- src-tauri/core/build.rs
+++ src-tauri/core/build.rs
@@ -4,8 +4,8 @@ fn main() {
     let mut att = Attributes::default();
 	att = att.codegen(CodegenContext::default());
 	if let Err(err) = try_build(att) {
-		let error = format!("{error:#}");
-		println("{error}");
+		let error = format!("{err:#}");
+		println!("{error}");
 		if error.starts_with("unknown field") {
 			print!("found an unknown configuration field. This usually means that you are using a CLI version that is newer than `tauri-build` and is incompatible. ");
 			println!(
