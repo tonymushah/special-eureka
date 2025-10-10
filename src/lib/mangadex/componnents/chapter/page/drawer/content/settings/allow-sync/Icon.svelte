d518599a0958dce14059efa72ed988e7f64b9cf1
tonymushah <tonyramanantsalama@gmail.com>
2025-10-10 15:44:03 +0300

    Added `AllowSync` button

---
+++ src/lib/mangadex/componnents/chapter/page/drawer/content/settings/allow-sync/Icon.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { allowSync } from "@mangadex/stores/chapter/page/allowSync.svelte";
+	import { RefreshCwIcon, RefreshCcwIcon } from "svelte-feather-icons";
+
+	let allow = $derived(allowSync.allow);
+	const size = "16";
+</script>
+
+{#if allow}
+	<RefreshCwIcon {size} />
+{:else}
+	<RefreshCcwIcon {size} />
+{/if}
