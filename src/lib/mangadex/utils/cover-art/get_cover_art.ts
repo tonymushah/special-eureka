9cec605f238b5d24f93e46a5f2eda7af03ceff19
tonymushah <tonyramanantsalama@gmail.com>
2025-10-11 21:37:54 +0300

    always for `get_cover_art`

--- src/lib/mangadex/utils/cover-art/get_cover_art.ts
+++ src/lib/mangadex/utils/cover-art/get_cover_art.ts
@@ -3,8 +3,9 @@ import { graphql } from "@mangadex/gql/exports";
 import type { CoverImageQuality } from "@mangadex/gql/graphql";
 import { mangadexQueryClient } from "@mangadex/index";
 import { createQuery } from "@tanstack/svelte-query";
-import { Client, queryStore } from "@urql/svelte";
-import { derived, toStore, type Readable } from "svelte/store";
+import { Client } from "@urql/svelte";
+import { derived, type Readable } from "svelte/store";
+import coverNotFound from "@mangadex/assets/artworks/cover-not-found.jpg";
 
 const query = graphql(`
 	query coverImage(
@@ -32,44 +33,49 @@ export default function get_cover_art({
 	client: Client;
 	mode?: CoverImageQuality;
 }): Readable<string | undefined> {
-	const store = () => createQuery(() => (
-		{
-			queryKey: ["cover", "image", cover_id, manga_id, filename, mode],
-			async queryFn() {
-				const res = await client
-					.query(query, {
-						cover_id,
-						manga_id,
-						mode,
-						filename
-					})
-					.toPromise();
-				if (res.error) {
-					throw res.error;
+	const store = () => createQuery(() => ({
+		queryKey: ["cover", "image", cover_id, manga_id, filename, mode],
+		async queryFn() {
+			const res = await client
+				.query(query, {
+					cover_id,
+					manga_id,
+					mode,
+					filename
+				})
+				.toPromise();
+			if (res.error) {
+				throw res.error;
+			}
+			if (res.data) {
+				const url = res.data.cover.getImage;
+				//return URL.createObjectURL(await (await fetch(url)).blob())
+				return url;
+			}
+			throw new Error("No results??");
+		},
+		/*structuralSharing(oldData, newData) {
+			try {
+				if (typeof oldData == "string") {
+					URL.revokeObjectURL(oldData);
 				}
-				if (res.data) {
-					const url = res.data.cover.getImage;
-					//return URL.createObjectURL(await (await fetch(url)).blob())
-					return url;
-				}
-				throw new Error("No results??");
-			},
-			/*structuralSharing(oldData, newData) {
-				try {
-					if (typeof oldData == "string") {
-						URL.revokeObjectURL(oldData);
-					}
-				} catch (e) {
-					console.error(e);
-				}
-				return newData;
-			},*/
-			staleTime: 1000 * 60 * 15
-		}),
+			} catch (e) {
+				console.error(e);
+			}
+			return newData;
+		},*/
+		staleTime: 1000 * 60 * 15,
+		networkMode: "always"
+	}),
 		() => mangadexQueryClient
 	);
 	return derived(internalToStore(store), ($s, set) => {
-		const url: string | undefined = $s.data;
-		set(url);
+		if ($s.isSuccess) {
+			const url: string = $s.data;
+			set(url);
+		} else if ($s.isError) {
+			console.error($s.error);
+			set(coverNotFound)
+		}
 	});
 }
