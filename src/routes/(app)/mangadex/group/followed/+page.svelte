1c2324b0c53ae20cd9b50b5173c7cf0546702e89
tonymushah <tonyramanantsalama@gmail.com>
2025-10-16 15:19:58 +0300

    adding missing content

---
+++ src/routes/(app)/mangadex/group/followed/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import PageTitle from "@mangadex/componnents/pages/PageTitle.svelte";
+	import AppTitle from "@special-eureka/core/components/AppTitle.svelte";
+	import SearchContent from "./SearchContent.svelte";
+</script>
+
+<AppTitle title="Following scanlation groups | Mangadex" />
+
+<PageTitle withReturn title="Following scanlation groups" />
+
+<SearchContent />
