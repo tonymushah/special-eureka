d3ee11869d62832c06bba928c7017920c198b57c
tonymushah <tonyramanantsalama@gmail.com>
2025-10-16 15:58:36 +0300

    new follow links on side bar

--- src/lib/mangadex/componnents/sidebar/body/Follows.svelte
+++ src/lib/mangadex/componnents/sidebar/body/Follows.svelte
@@ -12,5 +12,8 @@
 	<SubMenuItem label="Updates" href={route("/mangadex/titles/feed")} />
 	<SubMenuItem label="Online Library" href={route("/mangadex/titles/library")} />
 	<SubMenuItem label="MDList" href={route("/mangadex/list")} />
-	<SubMenuItem label="Followed Groups" />
+	<SubMenuItem label="Following Groups" href={route("/mangadex/group/followed")} />
+	<SubMenuItem label="Following Users" href={route("/mangadex/user/followed")} />
+	<SubMenuItem label="Following Titles" href={route("/mangadex/titles/followed")} />
+	<SubMenuItem label="Following MDLists" href={route("/mangadex/list/followed")} />
 </SubMenu>
