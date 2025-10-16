277777a3fd361f2e98bf593858f1e46e805d54a3
tonymushah <tonyramanantsalama@gmail.com>
2025-10-16 14:14:34 +0300

    titles followed

---
+++ src/routes/(app)/mangadex/titles/followed/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import PageTitle from "@mangadex/componnents/pages/PageTitle.svelte";
+	import AppTitle from "@special-eureka/core/components/AppTitle.svelte";
+</script>
+
+<AppTitle title="Following titles" />
+
+<PageTitle title="Following titles" withReturn />
