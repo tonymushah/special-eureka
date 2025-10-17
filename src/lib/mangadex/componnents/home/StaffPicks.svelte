04d79e26b31acddf003bf229be12d7afca9c7541
tonymushah <tonyramanantsalama@gmail.com>
2025-10-17 14:18:28 +0300

    refactored `Seasonal`

--- src/lib/mangadex/componnents/home/StaffPicks.svelte
+++ src/lib/mangadex/componnents/home/StaffPicks.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import defaultContentProfile from "@mangadex/content-profile/graphql/defaultProfile";
-	import { CoverImageQuality } from "@mangadex/gql/graphql";
+	import { CoverImageQuality, type StaffPicksQuery } from "@mangadex/gql/graphql";
 	import get_cover_art from "@mangadex/utils/cover-art/get_cover_art";
 	import get_value_from_title_and_random_if_undefined from "@mangadex/utils/lang/get_value_from_title_and_random_if_undefined";
 	import { createQuery } from "@tanstack/svelte-query";
@@ -11,6 +11,10 @@
 	import type { StaffPicksTitle } from "./staff-picks";
 	import Inner from "./staff-picks/Inner.svelte";
 	import query from "./staff-picks/query";
+	import TopTitle from "./utils/TopTitle.svelte";
+	import Content from "./staff-picks/Content.svelte";
+	import HomeErrorComponnent from "./utils/HomeErrorComponnent.svelte";
+	import PopularTitleSpinner from "./utils/PopularTitleSpinner.svelte";
 
 	const client = getContextClient();
 	let staff_picks = createQuery(() => ({
@@ -26,61 +30,55 @@
 			}
 		}
 	}));
-	const data_store = der(
-		toStore(() => staff_picks),
-		($store) => {
-			if ($store?.data != undefined) {
-				const id: string = $store.data.home.staffPicks.id;
-				const mangas =
-					$store.data.home.staffPicks.relationships.titles.map<StaffPicksTitle>((t) => {
-						const manga_id: string = t.id;
-						const cover_id: string = t.relationships.coverArt.id;
-						const filename: string = t.relationships.coverArt.attributes.fileName;
-						const coverImage = get_cover_art({
-							client,
-							cover_id,
-							filename,
-							manga_id,
-							mode: CoverImageQuality.V256
-						});
-						const title =
-							get_value_from_title_and_random_if_undefined(
-								t.attributes.title,
-								"en"
-							) ?? "";
-						const description =
-							get_value_from_title_and_random_if_undefined(
-								t.attributes.description,
-								"en"
-							) ?? "";
-						return {
-							id: manga_id,
-							coverImage,
-							coverImageAlt: cover_id,
-							title,
-							description
-						};
-					});
-				return {
-					id,
-					mangas
-				};
-			} else {
-				return undefined;
-			}
-		}
-	);
-	const execute = debounce(async () => {
-		await staff_picks.refetch();
-	}, 300);
-	const isFetching = toStore(() => staff_picks.isFetching);
+	function treatData(data: StaffPicksQuery) {
+		return data.home.staffPicks.relationships.titles.map<StaffPicksTitle>((t) => {
+			const manga_id: string = t.id;
+			const cover_id: string = t.relationships.coverArt.id;
+			const filename: string = t.relationships.coverArt.attributes.fileName;
+			const coverImage = get_cover_art({
+				client,
+				cover_id,
+				filename,
+				manga_id,
+				mode: CoverImageQuality.V256
+			});
+			const title =
+				get_value_from_title_and_random_if_undefined(t.attributes.title, "en") ?? "";
+			const description =
+				get_value_from_title_and_random_if_undefined(t.attributes.description, "en") ?? "";
+			return {
+				id: manga_id,
+				coverImage,
+				coverImageAlt: cover_id,
+				title,
+				description
+			};
+		});
+	}
 	onMount(() => {
-		return defaultContentProfile.subscribe(() => execute());
+		return defaultContentProfile.subscribe(() => {
+			staff_picks.refetch();
+		});
 	});
-	const error = der(
-		toStore(() => staff_picks),
-		($d) => $d?.error ?? undefined
-	);
 </script>
 
-<Inner {isFetching} {data_store} {error} {execute} />
+<TopTitle
+	label="Staff Picks"
+	fetching={staff_picks.isFetching}
+	onrefresh={async () => {
+		if (!staff_picks.isFetching) {
+			await staff_picks.refetch();
+		}
+	}}
+/>
+
+{#if staff_picks.isSuccess}
+	<Content mangas={treatData(staff_picks.data)} />
+{:else if staff_picks.isError}
+	<HomeErrorComponnent
+		error={staff_picks.error}
+		label="Oops! Something happens when loading the staff picks"
+	/>
+{:else}
+	<PopularTitleSpinner --height="20em" />
+{/if}
