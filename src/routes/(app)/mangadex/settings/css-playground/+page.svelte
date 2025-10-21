4934811704de9cb1a075e32a1731931604ed4061
tonymushah <tonyramanantsalama@gmail.com>
2025-10-21 14:08:06 +0300

    added test links

--- src/routes/(app)/mangadex/settings/css-playground/+page.svelte
+++ src/routes/(app)/mangadex/settings/css-playground/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import AppTitle from "@special-eureka/core/components/AppTitle.svelte";
+	import UsefulLinks from "./UsefulLinks.svelte";
 </script>
 
 <AppTitle title="CSS Playground - MangaDex" />
+
+<UsefulLinks />
 
 <h2>Nothing for now</h2>
