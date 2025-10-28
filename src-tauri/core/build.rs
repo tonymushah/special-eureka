dc2091637999243249cb96a03b5aeb6c7c312699
tonymushah <tonyramanantsalama@gmail.com>
2025-10-28 18:02:53 +0300

    devtools

--- src-tauri/core/build.rs
+++ src-tauri/core/build.rs
@@ -1,17 +1,19 @@
-use tauri_build::{Attributes, try_build, CodegenContext};
+use tauri_build::{Attributes, CodegenContext, try_build};
 
 fn main() {
     let mut att = Attributes::default();
-	att = att.codegen(CodegenContext::default());
-	if let Err(err) = try_build(att) {
-		let error = format!("{err:#}");
-		println!("{error}");
-		if error.starts_with("unknown field") {
-			print!("found an unknown configuration field. This usually means that you are using a CLI version that is newer than `tauri-build` and is incompatible. ");
-			println!(
-				"Please try updating the Rust crates by running `cargo update` in the Tauri app folder."
-			);
-		}
-		std::process::exit(1);
-	}
+    att = att.codegen(CodegenContext::default());
+    if let Err(err) = try_build(att) {
+        let error = format!("{err:#}");
+        println!("{error}");
+        if error.starts_with("unknown field") {
+            print!(
+                "found an unknown configuration field. This usually means that you are using a CLI version that is newer than `tauri-build` and is incompatible. "
+            );
+            println!(
+                "Please try updating the Rust crates by running `cargo update` in the Tauri app folder."
+            );
+        }
+        std::process::exit(1);
+    }
 }
