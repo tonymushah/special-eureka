25b8cc04e11ceac3e27d6fad81ad37638c8e75cc
tonymushah <tonyramanantsalama@gmail.com>
2025-11-05 11:43:23 +0300

    Update auth.rs

--- src-tauri/mangadex/src/query/auth.rs
+++ src-tauri/mangadex/src/query/auth.rs
@@ -12,7 +12,7 @@ pub struct AuthQuery;
 #[Object]
 #[cfg_attr(feature = "hotpath", hotpath::measure_all)]
 impl AuthQuery {
-    pub async fn check(&self, ctx: &Context<'_>) -> Result<AuthCheck> {
+    pub async fn check(&self, ctx: &Context<'_>) -> crate::error::wrapped::Result<AuthCheck> {
         let client =
             get_mangadex_client_from_graphql_context_with_auth_refresh::<tauri::Wry>(ctx).await?;
         Ok(client.auth().check().get().send().await?.into())
