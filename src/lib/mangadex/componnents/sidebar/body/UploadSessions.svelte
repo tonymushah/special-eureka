6553361ca12a2f8b04e8af699227128a05dd392a
tonymushah <tonyramanantsalama@gmail.com>
2025-11-25 18:56:09 +0300

    upload sessions shit

---
+++ src/lib/mangadex/componnents/sidebar/body/UploadSessions.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { route } from "$lib/ROUTES";
+	import Menu from "../base/Menu.svelte";
+	import { RiUploadCloud2Line as UploadIcon } from "svelte-remixicon";
+</script>
+
+<Menu label="Upload sessions" href={route("/mangadex/upload")}>
+	{#snippet icon()}
+		<UploadIcon size="26px" />
+	{/snippet}
+</Menu>
