af00637a5ae8f19a66e91eb1e6ce4471e2ec815e
tonymushah <tonyramanantsalama@gmail.com>
2025-11-27 20:29:08 +0300

    Sessions

---
+++ src/routes/(app)/mangadex/upload/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { route } from "$lib/ROUTES";
+	import PageTitle from "@mangadex/componnents/pages/PageTitle.svelte";
+	import NothingToShow from "@mangadex/componnents/search/content/NothingToShow.svelte";
+	import SimpleUploadSession from "@mangadex/componnents/upload/SimpleUploadSession.svelte";
+	import { sessionsIDs } from "@mangadex/stores/upload/sessions";
+	import AppTitle from "@special-eureka/core/components/AppTitle.svelte";
+</script>
+
+<AppTitle title="Upload Sessions | MangaDex" />
+
+<div class="layout">
+	<PageTitle withReturn title="Upload Sessions" />
+	<div class="content">
+		<div class="sessions">
+			<h2>Sessions</h2>
+			<div class="sessions-list">
+				{#each $sessionsIDs as id (id)}
+					<SimpleUploadSession sessionId={id} />
+				{:else}
+					<NothingToShow />
+				{/each}
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.layout {
+		margin: 0px 1em;
+	}
+	.sessions-list {
+		display: flex;
+		flex-direction: column;
+		gap: 12px;
+	}
+</style>
