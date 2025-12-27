089c7cc34d4bbcd05842d8d36eb78f71e87dc897
tonymushah <tonyramanantsalama@gmail.com>
2025-12-27 09:43:05 +0300

    Update UploadSessions.svelte

--- src/lib/mangadex/componnents/sidebar/body/UploadSessions.svelte
+++ src/lib/mangadex/componnents/sidebar/body/UploadSessions.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import { route } from "$lib/ROUTES";
+	import { CloudUpload } from "@lucide/svelte";
 	import Menu from "../base/Menu.svelte";
-	import { RiUploadCloud2Line as UploadIcon } from "svelte-remixicon";
 </script>
 
 <Menu label="Upload sessions" href={route("/mangadex/upload")}>
 	{#snippet icon()}
-		<UploadIcon size="28px" />
+		<CloudUpload size="24" />
 	{/snippet}
 </Menu>
