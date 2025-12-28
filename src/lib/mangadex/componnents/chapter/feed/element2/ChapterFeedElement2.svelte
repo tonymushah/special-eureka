500a0c3ce5ff1c559037e7a8b618c519408a411e
tonymushah <tonyramanantsalama@gmail.com>
2025-12-28 11:51:48 +0300

    imports fix

--- src/lib/mangadex/componnents/chapter/feed/element2/ChapterFeedElement2.svelte
+++ src/lib/mangadex/componnents/chapter/feed/element2/ChapterFeedElement2.svelte
@@ -3,7 +3,6 @@
 	import ButtonAccent from "@mangadex/componnents/theme/buttons/ButtonAccent.svelte";
 	import Skeleton from "@mangadex/componnents/theme/loader/Skeleton.svelte";
 	import type { Language } from "@mangadex/gql/graphql";
-	import { onMount } from "svelte";
 	import type { Readable } from "svelte/store";
 	import type { Chapter } from "..";
 	import ChapterElement1 from "../../base/element1/ChapterElement1.svelte";
@@ -11,7 +10,6 @@
 	import registerContextMenuEvent, {
 		setContextMenuContext
 	} from "@special-eureka/core/utils/contextMenuContext";
-	import { fade } from "svelte/transition";
 
 	type MouseEnvDiv = MouseEvent & {
 		currentTarget: HTMLDivElement & EventTarget;
