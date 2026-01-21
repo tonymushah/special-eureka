7d2f6a90dbf4721ff7742d7eaf8ddeeaa32af533
tonymushah <tonyramanantsalama@gmail.com>
2026-01-21 08:54:38 +0300

    svelte stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,19 @@
 import type { StorybookConfig } from "@storybook/sveltekit";
 
 const config: StorybookConfig = {
-    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-a11y",
-        "@chromatic-com/storybook",
-        "@storybook/addon-docs"
-    ],
-    framework: {
-        name: "@storybook/sveltekit",
-        options: {}
-    },
-    docs: {},
-    staticDirs: ["../public", "../static"]
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx|svelte)"],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-a11y",
+		"@chromatic-com/storybook",
+		"@storybook/addon-docs",
+		"@storybook/addon-svelte-csf"
+	],
+	framework: {
+		name: "@storybook/sveltekit",
+		options: {}
+	},
+	docs: {},
+	staticDirs: ["../public", "../static"]
 };
 export default config;
