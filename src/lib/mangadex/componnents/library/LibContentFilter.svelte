d5978c76b7c0c79d95fc3e0aab009e924d15e0a1
tonymushah <tonyramanantsalama@gmail.com>
2026-01-24 13:22:35 +0300

    moved dialog

--- src/lib/mangadex/componnents/library/LibContentFilter.svelte
+++ src/lib/mangadex/componnents/library/LibContentFilter.svelte
@@ -1,106 +1,64 @@
 <script lang="ts">
-	import { createDialog, melt } from "@melt-ui/svelte";
 	import ButtonAccent from "../theme/buttons/ButtonAccent.svelte";
-	import { makeScroll, preventScroll, scrollElementId } from "../layout/scrollElement";
-	import { FilterIcon } from "@lucide/svelte";
+	import { makeScroll, preventScroll } from "../layout/scrollElement";
+	import { Filter } from "@lucide/svelte";
 	import type { UserLibrarySectionParam } from "@mangadex/gql/graphql";
 	import type { Writable } from "svelte/store";
-	import { fade } from "svelte/transition";
 	import { XIcon as CloseIcon } from "@lucide/svelte";
 	import FilterContent from "./filter/FilterContent.svelte";
-	const {
-		elements: { trigger, portalled, overlay, content, title, description, close },
-		states: { open }
-	} = createDialog({
-		portal: `#${scrollElementId}`
-	});
+	import { Dialog } from "@ark-ui/svelte/dialog";
+	import { Portal } from "@ark-ui/svelte";
+	import cssDialogMod from "@mangadex/componnents/theme/dialog/dialog.module.scss";
+	import cssMod from "./lib-content-filter.module.scss";
+
 	interface Props {
 		params: Writable<UserLibrarySectionParam>;
 	}
+	let { params }: Props = $props();
+	let open = $state(false);
 	$effect(() => {
-		if ($open) {
+		if (open) {
 			preventScroll();
 		} else {
 			makeScroll();
 		}
 	});
-	let { params }: Props = $props();
 </script>
 
-<ButtonAccent meltElement={trigger}>
+<ButtonAccent
+	onclick={() => {
+		open = !open;
+	}}
+>
 	<div class="icon">
-		<FilterIcon size="20" />
+		<Filter size="20" />
 	</div>
 </ButtonAccent>
 
-{#if $open}
-	<div class="portalled" use:melt={$portalled}>
-		<div
-			class="overlay"
-			use:melt={$overlay}
-			transition:fade={{
-				duration: 150
-			}}
-		></div>
-		<div
-			class="dialog"
-			use:melt={$content}
-			transition:fade={{
-				duration: 150
-			}}
-		>
-			<div class="content">
-				<div class="top">
-					<div class="title-desc">
-						<h2 use:melt={$title}>Filter Library</h2>
+<Dialog.Root bind:open>
+	<Portal>
+		<Dialog.Backdrop class={cssMod.overlay} />
+		<Dialog.Positioner>
+			<Dialog.Content class={cssMod.dialog}>
+				<div class="content">
+					<div class="top">
+						<div class="title-desc">
+							<Dialog.Title class={cssDialogMod.title}>Filter Library</Dialog.Title>
+						</div>
+						<div class="close">
+							<Dialog.CloseTrigger class={cssDialogMod.closeButton}>
+								<CloseIcon class={cssMod.icon} />
+							</Dialog.CloseTrigger>
+						</div>
 					</div>
-					<div class="close">
-						<ButtonAccent meltElement={close}>
-							<CloseIcon />
-						</ButtonAccent>
-					</div>
+					<FilterContent {params} />
 				</div>
-				<FilterContent {params} />
-			</div>
-		</div>
-	</div>
-{/if}
+			</Dialog.Content>
+		</Dialog.Positioner>
+	</Portal>
+</Dialog.Root>
 
 <style lang="scss">
-	.icon {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		padding: 6px;
-	}
-	.portalled {
-		position: absolute;
-		top: 0px;
-		width: 100%;
-		height: 100%;
-	}
-	.dialog {
-		background-color: var(--main-background);
-		color: var(--text-color);
-		width: 75vw;
-		border: 3px solid var(--primary);
-		border-radius: 0.5rem;
-		position: fixed;
-		z-index: 5;
-		transform: translateY(-50%);
-		transform: translateX(-50%);
-		top: 10vh;
-		left: 50%;
-		padding: 0.5rem; /* 24px */
-		height: 75vh;
-	}
-	.overlay {
-		backdrop-filter: blur(10px);
-		-webkit-backdrop-filter: blur(10px);
-		z-index: 5;
-		inset: 0px;
-		position: fixed;
-	}
 	.content {
 		height: 100%;
 		display: flex;
@@ -118,9 +76,6 @@
 		padding: 8px;
 		.title-desc {
 			display: grid;
-			h2 {
-				margin: 0px;
-			}
 		}
 	}
 </style>
