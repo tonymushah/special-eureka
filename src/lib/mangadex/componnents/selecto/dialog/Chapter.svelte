55327e5b806ebc62fcf02f1f99f31d35061fee13
tonymushah <tonyramanantsalama@gmail.com>
2026-01-27 10:00:12 +0300

    Update Chapter.svelte

--- src/lib/mangadex/componnents/selecto/dialog/Chapter.svelte
+++ src/lib/mangadex/componnents/selecto/dialog/Chapter.svelte
@@ -69,10 +69,10 @@
 						uuids: chapters
 					},
 					{
-						onError(error, variables, context) {
+						onError(error) {
 							addErrorToast("Cannot export chapters ids as txt", error);
 						},
-						onSuccess(data, variables, context) {
+						onSuccess(data) {
 							revealItemInDir(data);
 						}
 					}
