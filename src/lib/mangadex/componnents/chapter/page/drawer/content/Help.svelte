d0594ca876f0bcac8c38c0050ca30b4901d9f8a2
tonymushah <tonyramanantsalama@gmail.com>
2026-01-27 11:25:29 +0300

    fix tooltip

--- src/lib/mangadex/componnents/chapter/page/drawer/content/Help.svelte
+++ src/lib/mangadex/componnents/chapter/page/drawer/content/Help.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
 	import ButtonAccentOnlyLabel from "@mangadex/componnents/theme/buttons/ButtonAccentOnlyLabel.svelte";
-	import MangaDexVarThemeProvider from "@mangadex/componnents/theme/MangaDexVarThemeProvider.svelte";
-	import { fade } from "svelte/transition";
 	import Icon from "./help/Icon.svelte";
 	import { fireChapterHelpEvent } from "../../contexts/previousNextEventTarget";
 	import Tooltip from "@mangadex/componnents/Tooltip.svelte";
 </script>
 
 <section>
-	<Tooltip>
+	<Tooltip rootDisplay="grid">
 		{#snippet triggerContent()}
 			<ButtonAccentOnlyLabel icon={Icon} onclick={fireChapterHelpEvent} label="Help" />
 		{/snippet}
 		{#snippet tooltipContent()}
-			<p class="description">It's the manual or whatever I will show there</p>
+			<p class="description">It's the manual or <br /> whatever I will show there</p>
 		{/snippet}
 	</Tooltip>
 </section>
@@ -23,12 +21,9 @@
 		display: flex;
 		flex-direction: column;
 		gap: 5px;
+		position: relative;
 	}
 	.description {
-		padding-left: 1rem; /* 16px */
-		padding-right: 1rem; /* 16px */
-		padding-top: 0.25rem; /* 4px */
-		padding-bottom: 0.25rem; /* 4px */
 		margin: 0;
 		color: var(--text-color);
 	}
