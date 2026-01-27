f7a5f8db423633c2d4b113a11800c0cee79bb49c
tonymushah <tonyramanantsalama@gmail.com>
2026-01-27 17:31:35 +0300

    fix css

--- src/lib/mangadex/componnents/custom-list/fork/CustomListForkDialog.svelte
+++ src/lib/mangadex/componnents/custom-list/fork/CustomListForkDialog.svelte
@@ -12,6 +12,7 @@
 	import { Portal } from "@ark-ui/svelte";
 	import cssMod from "./custom-list-fork-dialog.module.scss";
 	import cssDialogMod from "@mangadex/componnents/theme/dialog/dialog.module.scss";
+	import MangaDexVarThemeProvider from "@mangadex/componnents/theme/MangaDexVarThemeProvider.svelte";
 
 	interface Props {
 		open?: boolean;
@@ -27,117 +28,120 @@
 
 <Dialog.Root bind:open>
 	<Portal>
-		<Dialog.Backdrop class={cssMod.overlay} />
-		<Dialog.Positioner>
-			<Dialog.Content class={cssMod.dialog}>
-				<div class="content">
-					<div class="top">
-						<div class="title-desc">
-							<Dialog.Title class={cssMod.title}>
-								Fork {#if listToForkName}
-									{listToForkName}
-								{:else}
-									{listToFork} MD List
-								{/if}
-							</Dialog.Title>
-						</div>
-						<div class="close">
-							<Dialog.CloseTrigger class={cssDialogMod.closeButton}>
-								<CloseIcon class={cssMod.icon} />
-							</Dialog.CloseTrigger>
-						</div>
-					</div>
-					<div class="body">
-						<section>
-							MD List id: {listToFork}
-						</section>
-						<section class="fork-name">
-							<label for="fork-name">Name:</label>
-							<FormInput
-								bind:value={name}
-								inputProps={{
-									id: "fork-name",
-									required: true
-								}}
-							/>
-						</section>
-						<section class="visb">
-							<h4>
-								Visibility: {#if visiblity == undefined}
-									<span class="default">(private if not set)</span>
-								{/if}
-							</h4>
-							<div class="vis-input">
-								<input
-									type="radio"
-									value={CustomListVisibility.Public}
-									bind:group={visiblity}
-									id="vis-public"
-								/>
-								<label for="vis-public">Public</label>
+		<MangaDexVarThemeProvider>
+			<Dialog.Backdrop class={cssMod.overlay} />
+
+			<Dialog.Positioner>
+				<Dialog.Content class={cssMod.dialog}>
+					<div class="content">
+						<div class="top">
+							<div class="title-desc">
+								<Dialog.Title class={cssDialogMod.title}>
+									Fork {#if listToForkName}
+										{listToForkName}
+									{:else}
+										{listToFork} MD List
+									{/if}
+								</Dialog.Title>
 							</div>
-							<div class="vis-input">
-								<input
-									type="radio"
-									value={CustomListVisibility.Private}
-									bind:group={visiblity}
-									id="vis-private"
-								/>
-								<label for="vis-private">Private</label>
+							<div class="close">
+								<Dialog.CloseTrigger class={cssDialogMod.closeButton}>
+									<CloseIcon class={cssMod.icon} />
+								</Dialog.CloseTrigger>
 							</div>
-						</section>
-						<section class="vis-input">
-							<input type="checkbox" bind:checked={filterTitles} />
-							<label for="filter-titles">Filter titles </label>
-						</section>
-						<section class="buttons">
-							<PrimaryButton
-								onclick={() => {
-									if (name.trim().length == 0) {
-										addErrorToast("Invalid name input", "Please insert a valid name");
-										return;
-									}
-									forkCustomListMutation.mutate(
-										{
-											name,
-											visibility: visiblity,
-											toFork: listToFork,
-											filter: filterTitles
-										},
-										{
-											onSuccess(data) {
-												addToast({
-													title: `Forked ${listToForkName ? listToForkName : `${listToFork} MD List`}`,
-													description: `New MD List ID: ${data.id}`,
-													type: "success"
-												});
-												if (open) {
-													goto(
-														route("/mangadex/list/[id]", {
-															id:
-																data.visibility == CustomListVisibility.Private
-																	? `private:${data.id}`
-																	: data.id
-														})
-													);
-												}
-											},
-											onError(error) {
-												addErrorToast("Cannot fork MD List", error);
-											}
+						</div>
+						<div class="body">
+							<section>
+								MD List id: {listToFork}
+							</section>
+							<section class="fork-name">
+								<label for="fork-name">Name:</label>
+								<FormInput
+									bind:value={name}
+									inputProps={{
+										id: "fork-name",
+										required: true
+									}}
+								/>
+							</section>
+							<section class="visb">
+								<h4>
+									Visibility: {#if visiblity == undefined}
+										<span class="default">(private if not set)</span>
+									{/if}
+								</h4>
+								<div class="vis-input">
+									<input
+										type="radio"
+										value={CustomListVisibility.Public}
+										bind:group={visiblity}
+										id="vis-public"
+									/>
+									<label for="vis-public">Public</label>
+								</div>
+								<div class="vis-input">
+									<input
+										type="radio"
+										value={CustomListVisibility.Private}
+										bind:group={visiblity}
+										id="vis-private"
+									/>
+									<label for="vis-private">Private</label>
+								</div>
+							</section>
+							<section class="vis-input">
+								<input type="checkbox" bind:checked={filterTitles} />
+								<label for="filter-titles">Filter titles </label>
+							</section>
+							<section class="buttons">
+								<PrimaryButton
+									onclick={() => {
+										if (name.trim().length == 0) {
+											addErrorToast("Invalid name input", "Please insert a valid name");
+											return;
 										}
-									);
-								}}
-								isBase
-								disabled={!$isLogged || forkCustomListMutation.isPending}
-							>
-								<p class="send">Fork</p>
-							</PrimaryButton>
-						</section>
+										forkCustomListMutation.mutate(
+											{
+												name,
+												visibility: visiblity,
+												toFork: listToFork,
+												filter: filterTitles
+											},
+											{
+												onSuccess(data) {
+													addToast({
+														title: `Forked ${listToForkName ? listToForkName : `${listToFork} MD List`}`,
+														description: `New MD List ID: ${data.id}`,
+														type: "success"
+													});
+													if (open) {
+														goto(
+															route("/mangadex/list/[id]", {
+																id:
+																	data.visibility == CustomListVisibility.Private
+																		? `private:${data.id}`
+																		: data.id
+															})
+														);
+													}
+												},
+												onError(error) {
+													addErrorToast("Cannot fork MD List", error);
+												}
+											}
+										);
+									}}
+									isBase
+									disabled={!$isLogged || forkCustomListMutation.isPending}
+								>
+									<p class="send">Fork</p>
+								</PrimaryButton>
+							</section>
+						</div>
 					</div>
-				</div>
-			</Dialog.Content>
-		</Dialog.Positioner>
+				</Dialog.Content>
+			</Dialog.Positioner>
+		</MangaDexVarThemeProvider>
 	</Portal>
 </Dialog.Root>
 
@@ -157,6 +161,7 @@
 		justify-content: space-between;
 		display: flex;
 		padding: 8px;
+		align-items: center;
 		.title-desc {
 			display: grid;
 		}
