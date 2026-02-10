1eb213dbfcbbf8739ecdd72fe96410d4d5f3b7a7
tonymushah <tonyramanantsalama@gmail.com>
2026-02-11 01:31:10 +0300

    Update index.ts

--- src/lib/mangadex/index.ts
+++ src/lib/mangadex/index.ts
@@ -11,7 +11,7 @@ export const mangadexQueryClient = new QueryClient({
 	}
 });
 
-interface GetCoverParam {
+export interface GetCoverParam {
 	id: string;
 	asManga?: boolean;
 	quality?: "256" | "512";
