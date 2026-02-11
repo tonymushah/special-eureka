ec3798d79e9ddcf72a05f974af778c9cf70bfb17
tonymushah <tonyramanantsalama@gmail.com>
2026-02-11 10:18:18 +0300

    Update index.ts

--- src/lib/mangadex/componnents/chapter/feed/list/index.ts
+++ src/lib/mangadex/componnents/chapter/feed/list/index.ts
@@ -1,9 +1,7 @@
 import type { Language } from "@mangadex/gql/graphql";
 import type { Chapter } from "..";
-import type { Readable } from "svelte/store";
 
 export type ChapterFeedListItem = {
-	coverImage: Readable<string | undefined>;
 	coverImageAlt: string;
 	title: string;
 	mangaId: string;
