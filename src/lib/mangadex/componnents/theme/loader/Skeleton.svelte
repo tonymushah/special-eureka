1bf3b54f2880d70a49d8552bd6d31735d3a053a2
tonymushah <tonyramanantsalama@gmail.com>
2026-02-11 08:23:25 +0300

    Update Skeleton.svelte

--- src/lib/mangadex/componnents/theme/loader/Skeleton.svelte
+++ src/lib/mangadex/componnents/theme/loader/Skeleton.svelte
@@ -4,25 +4,35 @@
 		width?: string;
 		duration?: string;
 		border_radius?: string;
+		color?: string;
 	}
 
 	let {
 		height = "100px",
 		width = "360px",
 		duration = "1s",
-		border_radius = "0px"
+		border_radius = "0px",
+		color = "var(--mid-tone)"
 	}: Props = $props();
 </script>
 
-<span class="loader" style="animation-duration: {duration}; --width: {width}; --height: {height};">
-	<div style="border-radius: {border_radius};"></div>
-</span>
+<div
+	class="loader"
+	style="animation-duration: {duration}; --width: {width}; --height: {height}; --skeleton-color: {color}; --border-radius: {border_radius}"
+>
+	<div></div>
+</div>
 
 <style lang="scss">
 	.loader {
 		display: flex;
 		width: min-content;
-		background-image: linear-gradient(90deg, transparent, var(--mid-tone) 50%, transparent 80%);
+		background-image: linear-gradient(
+			90deg,
+			transparent,
+			var(--skeleton-color) 50%,
+			transparent 80%
+		);
 		background-repeat: no-repeat;
 		background-size: var(--height);
 		background-position: -50% 0;
@@ -31,10 +41,12 @@
 		animation-timing-function: linear;
 		animation-fill-mode: backwards;
 		animation-iteration-count: infinite;
+		border-radius: var(--border-radius);
 		div {
-			background-color: color-mix(in srgb, var(--main-background) 70%, var(--mid-tone) 5%);
+			background-color: color-mix(in srgb, var(--main-background) 70%, var(--skeleton-color) 5%);
 			width: var(--width);
 			height: var(--height);
+			border-radius: var(--border-radius);
 		}
 	}
 
