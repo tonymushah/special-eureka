b8fcde98fe022789ba711686ce42134807d13694
tonymushah <tonyramanantsalama@gmail.com>
2026-02-12 23:36:50 +0300

    Update Covers.svelte

--- src/lib/mangadex/componnents/selecto/dialog/Covers.svelte
+++ src/lib/mangadex/componnents/selecto/dialog/Covers.svelte
@@ -2,12 +2,18 @@
 	import ButtonAccentOnlyLabel from "@mangadex/componnents/theme/buttons/ButtonAccentOnlyLabel.svelte";
 	import SectionBase from "./SectionBase.svelte";
 	import Selections from "./covers/Selections.svelte";
+	import exportIdsToTxtLoader from "@mangadex/gql-docs/export/ids";
+	import { addErrorToast } from "@mangadex/componnents/theme/toast/Toaster.svelte";
+	import { revealItemInDir } from "@tauri-apps/plugin-opener";
+	import ButtonAccent from "@mangadex/componnents/theme/buttons/ButtonAccent.svelte";
 
 	interface Props {
 		covers?: string[];
 	}
 	let { covers = $bindable([]) }: Props = $props();
 	let currentAction = $state<"selection">("selection");
+	let exportIdsToTxt = exportIdsToTxtLoader();
+	let coversEmpty = $derived(covers.length == 0);
 </script>
 
 <SectionBase>
@@ -25,5 +31,28 @@
 			}}
 			disabled={currentAction == "selection"}
 		/>
+
+		<ButtonAccentOnlyLabel
+			variant="3"
+			disabled={exportIdsToTxt.isPending || coversEmpty}
+			label="Export ids as txt"
+			onclick={() => {
+				exportIdsToTxt.mutateAsync(
+					{
+						uuids: covers
+					},
+					{
+						onError(error) {
+							addErrorToast("Cannot export cover ids as txt", error);
+						},
+						onSuccess(data) {
+							revealItemInDir(data);
+						}
+					}
+				);
+			}}
+		/>
+		<ButtonAccentOnlyLabel variant="3" disabled label="Save covers in a directory" />
+		<ButtonAccentOnlyLabel variant="3" disabled label="Save covers as zip" />
 	{/snippet}
 </SectionBase>
