85a7bb2f25043095f753987a92b16e1471146002
tonymushah <tonyramanantsalama@gmail.com>
2026-03-04 17:39:56 +0300

    added `scanlation_groups`

--- src-tauri/mangadex-blacklist/src/schema.rs
+++ src-tauri/mangadex-blacklist/src/schema.rs
@@ -7,3 +7,13 @@ diesel::table! {
         insert_time -> Nullable<Timestamp>,
     }
 }
+
+diesel::table! {
+    scanlation_groups (group_id) {
+        group_id -> Binary,
+        group_name -> Text,
+        insert_time -> Nullable<Timestamp>,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(authors_artists, scanlation_groups,);
