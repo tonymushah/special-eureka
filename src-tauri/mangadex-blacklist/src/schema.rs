c4e9e373a534f6ac27f5e83e12207bd41c2b8174
tonymushah <tonyramanantsalama@gmail.com>
2026-03-04 17:39:17 +0300

    changes

--- src-tauri/mangadex-blacklist/src/schema.rs
+++ src-tauri/mangadex-blacklist/src/schema.rs
@@ -4,6 +4,6 @@ diesel::table! {
     authors_artists (author_id) {
         author_id -> Binary,
         author_name -> Text,
-        insert_date -> Nullable<Timestamp>,
+        insert_time -> Nullable<Timestamp>,
     }
 }
