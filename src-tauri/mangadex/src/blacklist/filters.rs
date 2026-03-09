287218dcbb12dfbdabd62f0651ea3b12eb67c353
tonymushah <tonyramanantsalama@gmail.com>
2026-03-09 11:15:06 +0300

    added `filter_scanlation_groups_chapters`

--- src-tauri/mangadex/src/blacklist/filters.rs
+++ src-tauri/mangadex/src/blacklist/filters.rs
@@ -24,6 +24,7 @@ pub async fn filter_author_artists_titles<R: Runtime>(
                         })
                         .map(|d| d.id.as_bytes().to_vec()),
                 ),
+                // TODO make this available
                 crate::objects::manga::MangaObject::WithoutRel(_) => None,
             })
             .flatten()
@@ -52,8 +53,57 @@ pub async fn filter_author_artists_titles<R: Runtime>(
                     && black_listed.contains(&t.id)
             })
         }
+        // TODO make this works
         crate::objects::manga::MangaObject::WithoutRel(_) => false,
     });
     titles.shrink_to_fit();
     Ok(titles)
+}
+
+pub async fn filter_scanlation_groups_chapters<R: Runtime>(
+    app: AppHandle<R>,
+    mut chapters: Vec<crate::objects::chapter::Chapter>,
+) -> crate::Result<Vec<crate::objects::chapter::Chapter>> {
+    let black_listed: HashSet<Uuid> = {
+        let scanlation_group_ids = chapters
+            .iter()
+            .filter_map(|c| match c {
+                crate::objects::chapter::Chapter::WithRelationship(api_object) => Some(
+                    api_object
+                        .relationships
+                        .iter()
+                        .filter(|c| matches!(c.type_, RelationshipType::ScanlationGroup))
+                        .map(|c| c.id.as_bytes().to_vec()),
+                ),
+                crate::objects::chapter::Chapter::WithoutRelationship(_) => None,
+            })
+            .flatten()
+            .collect::<Vec<_>>();
+        spawn_blocking(move || -> crate::Result<_> {
+            use diesel::prelude::*;
+            use mangadex_blacklist_raw::schema::scanlation_groups::dsl::*;
+
+            let mut connection = app.blacklist_database_pool()?.get_connection()?;
+            scanlation_groups
+                .select(group_id)
+                .filter(group_id.eq_any(scanlation_group_ids))
+                .load_iter::<Vec<u8>, diesel::connection::DefaultLoadingMode>(&mut connection)?
+                .map(|bin| -> crate::Result<_> {
+                    let bin = bin?;
+                    Ok(Uuid::from_slice(&bin)?)
+                })
+                .collect::<Result<_, crate::Error>>()
+        })
+        .await??
+    };
+    chapters.retain(|c| match c {
+        crate::objects::chapter::Chapter::WithRelationship(api_object) => {
+            !api_object.relationships.iter().any(|o| {
+                matches!(o.type_, RelationshipType::ScanlationGroup) && black_listed.contains(&o.id)
+            })
+        }
+        crate::objects::chapter::Chapter::WithoutRelationship(_) => false,
+    });
+    chapters.shrink_to_fit();
+    Ok(chapters)
 }
