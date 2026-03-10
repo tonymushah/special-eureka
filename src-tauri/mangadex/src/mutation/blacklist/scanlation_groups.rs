4d3380d0be115ef66df54a3d00ad19ff98dc660f
tonymushah <tonyramanantsalama@gmail.com>
2026-03-10 18:54:13 +0300

    refactored scanlation groups

--- src-tauri/mangadex/src/mutation/blacklist/scanlation_groups.rs
+++ src-tauri/mangadex/src/mutation/blacklist/scanlation_groups.rs
@@ -18,26 +18,8 @@ struct InsertScanlationGroup<'a> {
 }
 pub struct BlacklistScanlationGroupsMutations;
 
-#[Object]
 impl BlacklistScanlationGroupsMutations {
-	
-	pub async fn block_one(&self, ctx: &Context<'_>, scanlation_group_id: Uuid) -> crate::Result<BlacklistedScanlationGroupObject> {
-		let app_handle = ctx.get_app_handle::<tauri::Wry>()?.clone();
-
-		spawn_blocking(move|| -> crate::Result<_> {
-			use mangadex_blacklist_raw::schema::scanlation_groups::dsl::*;
-			
-			let mut connection = app_handle.blacklist_database_pool()?.get_connection()?;
-			Ok(diesel::insert_into(scanlation_groups)
-				.values(InsertScanlationGroup {
-							group_id: scanlation_group_id.as_bytes(),
-							group_name: todo!()
-						})
-				.returning(BlacklistedScanlationGroupObject::as_returning())
-				.get_result(&mut connection)?)
-		}).await?
-	}
-	pub async fn block_many(&self, ctx: &Context<'_>, #[graphql(validator(min_items = 1))] scanlation_group_ids: Vec<Uuid>) -> crate::Result<Vec<BlacklistedScanlationGroupObject>> {
+	pub async fn inner_block_many(&self, ctx: &Context<'_>, scanlation_group_ids: Vec<Uuid>) -> crate::Result<Vec<BlacklistedScanlationGroupObject>> {
 		let app_handle = ctx.get_app_handle::<tauri::Wry>()?.clone();
 
 		spawn_blocking(move|| -> crate::Result<_> {
@@ -58,19 +40,7 @@ impl BlacklistScanlationGroupsMutations {
 			
 		}).await?
 	}
-	pub async fn unblock_many(&self, ctx: &Context<'_>, scanlation_group_id:Uuid) -> crate::Result<BlacklistedScanlationGroupObject> {
-		let app_handle = ctx.get_app_handle::<tauri::Wry>()?.clone();
-
-		spawn_blocking(move|| -> crate::Result<_> {
-			use mangadex_blacklist_raw::schema::scanlation_groups::dsl::*;
-			
-			let mut connection = app_handle.blacklist_database_pool()?.get_connection()?;
-			Ok(diesel::delete(scanlation_groups.filter(group_id.eq(scanlation_group_id.as_bytes())))
-				.returning(BlacklistedScanlationGroupObject::as_returning())
-				.get_result(&mut connection)?)
-		}).await?
-	}
-	pub async fn unblock_batch(&self, ctx: &Context<'_>, #[graphql(validator(min_items = 1))] scanlation_group_ids:Vec<Uuid>) -> crate::Result<BlacklistedScanlationGroupObject> {
+	pub async fn inner_unblock_many(&self, ctx: &Context<'_>, scanlation_group_ids:Vec<Uuid>) -> crate::Result<Vec<BlacklistedScanlationGroupObject>> {
 		let app_handle = ctx.get_app_handle::<tauri::Wry>()?.clone();
 
 		spawn_blocking(move|| -> crate::Result<_> {
@@ -87,8 +57,24 @@ impl BlacklistScanlationGroupsMutations {
 					)
 				)
 				.returning(BlacklistedScanlationGroupObject::as_returning())
-				.get_result(&mut connection)?)
+				.get_results(&mut connection)?)
 		}).await?
+	}
+}
+
+#[Object]
+impl BlacklistScanlationGroupsMutations {
+	pub async fn block_one(&self, ctx: &Context<'_>, scanlation_group_id: Uuid) -> crate::error::wrapped::Result<BlacklistedScanlationGroupObject> {
+		Ok(self.inner_block_many(ctx, vec![scanlation_group_id]).await?.into_iter().next().ok_or(crate::Error::FirstValueExpected)?)
+	}
+	pub async fn block_many(&self, ctx: &Context<'_>, #[graphql(validator(min_items = 1))] scanlation_group_ids: Vec<Uuid>) -> crate::error::wrapped::Result<Vec<BlacklistedScanlationGroupObject>> {
+		Ok(self.inner_block_many(ctx, scanlation_group_ids).await?)
+	}
+	pub async fn unblock_one(&self, ctx: &Context<'_>, scanlation_group_id:Uuid) -> crate::error::wrapped::Result<BlacklistedScanlationGroupObject> {
+		Ok(self.inner_unblock_many(ctx, vec![scanlation_group_id]).await?.into_iter().next().ok_or(crate::Error::FirstValueExpected)?)
+	}
+	pub async fn unblock_many(&self, ctx: &Context<'_>, #[graphql(validator(min_items = 1))] scanlation_group_ids:Vec<Uuid>) -> crate::error::wrapped::Result<Vec<BlacklistedScanlationGroupObject>> {
+		Ok(self.inner_unblock_many(ctx, scanlation_group_ids).await?)
 	}
 
 }
