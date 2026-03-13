168bea129d81257c72d14da761f6fac367fbde40
tonymushah <tonyramanantsalama@gmail.com>
2026-03-13 10:30:26 +0300

    removed storybook and millify

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,17 @@
-export const waitAsync = (time: number) => new Promise((resolve) => {
-	setTimeout(resolve, time);
-});
+export const waitAsync = (time: number) =>
+	new Promise((resolve) => {
+		setTimeout(resolve, time);
+	});
 
 export const flexRender = <TProps extends object>(comp: any, props: TProps) => {
-	if (typeof comp === 'function') {
-		return comp(props)
+	if (typeof comp === "function") {
+		return comp(props);
 	}
-	return comp
+	return comp;
+};
+
+export function millify(num: number): string {
+	return new Intl.NumberFormat("en-US", {
+		notation: "compact"
+	}).format(num);
 }
