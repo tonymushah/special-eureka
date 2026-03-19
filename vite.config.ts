de03ef567a90ac125f576342b1c150f1bcac6d22
tonymushah <tonyramanantsalama@gmail.com>
2026-03-19 06:25:32 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
 	clearScreen: false,
 	oxc: {
 		// We need this or else the ts code that uses `using` will not work on linux ;(
-		target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13"
+		target: process.env.TAURI_ENV_PLATFORM == "windows" ? "es2022" : "es2018"
 	},
 	// Tauri expects a fixed port, fail if that port is not available
 	server: {
@@ -29,23 +29,16 @@ export default defineConfig({
 			]
 		}
 	},
+	dev: {},
 	// to access the Tauri environment variables set by the CLI with information about the current target
-	envPrefix: [
-		"VITE_",
-		"TAURI_PLATFORM",
-		"TAURI_ARCH",
-		"TAURI_FAMILY",
-		"TAURI_PLATFORM_VERSION",
-		"TAURI_PLATFORM_TYPE",
-		"TAURI_DEBUG"
-	],
+	envPrefix: ["VITE_", "TAURI_ENV_*"],
 	build: {
 		// Tauri uses Chromium on Windows and WebKit on macOS and Linux
-		target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+		target: process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
 		// don't minify for debug builds
-		minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+		minify: !process.env.TAURI_ENV_DEBUG ? "oxc" : false,
 		// produce sourcemaps for debug builds
-		sourcemap: !!process.env.TAURI_DEBUG,
+		sourcemap: !!process.env.TAURI_ENV_DEBUG,
 		cssMinify: "lightningcss"
 	}
 });
