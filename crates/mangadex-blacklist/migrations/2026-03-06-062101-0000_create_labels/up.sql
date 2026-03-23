3a003b0633fd808b23a3dffe4fd40d1cecee712a
tonymushah <tonyramanantsalama@gmail.com>
2026-03-24 01:49:12 +0300

    Update up.sql

--- crates/mangadex-blacklist/migrations/2026-03-06-062101-0000_create_labels/up.sql
+++ crates/mangadex-blacklist/migrations/2026-03-06-062101-0000_create_labels/up.sql
@@ -2,6 +2,6 @@
 create table labels(
 	label_id BLOB primary key not null,
 	name TEXT not null,
-	create_date DATETIME default 'now',
+	create_date DATETIME default (datetime('locatime')),
 	description TEXT
 );
