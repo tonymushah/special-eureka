ec07d7ba013d13af6a5b74be4570daca9e89f42d
tonymushah <tonyramanantsalama@gmail.com>
2026-03-24 00:37:05 +0300

    Update up.sql

--- crates/mangadex-blacklist/migrations/2026-03-06-065048-0000_create_scanlation_groups_labels/up.sql
+++ crates/mangadex-blacklist/migrations/2026-03-06-065048-0000_create_scanlation_groups_labels/up.sql
@@ -2,7 +2,7 @@
 create table scanlation_groups_labels(
 	scanlation_group BLOB NOT NULL references scanlation_groups(group_id) on delete cascade,
 	label BLOB NOT NULL references labels(label_id) on delete cascade,
-	create_date DATETIME default datetime,
+	create_date DATETIME default 'now',
 	notes TEXT,
 	PRIMARY KEY (scanlation_group, label)
 );
