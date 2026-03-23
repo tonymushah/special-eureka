e58c2bb986671b6b01af743269cbabe4789fca47
tonymushah <tonyramanantsalama@gmail.com>
2026-03-24 01:48:49 +0300

    Update up.sql

--- crates/mangadex-blacklist/migrations/2026-03-04-110510-0000_create_scanlation_group/up.sql
+++ crates/mangadex-blacklist/migrations/2026-03-04-110510-0000_create_scanlation_group/up.sql
@@ -2,5 +2,5 @@
 create table scanlation_groups(
 	group_id BLOB PRIMARY KEY NOT NULL,
 	group_name TEXT NOT NULL,
-	insert_time DATETIME DEFAULT 'now'
+	insert_time DATETIME DEFAULT (datetime('locatime'))
 );
