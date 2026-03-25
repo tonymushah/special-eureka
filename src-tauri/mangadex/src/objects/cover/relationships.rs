025eee8d6f2077dd1587c7ab32a27d62fcf01306
tonymushah <tonyramanantsalama@gmail.com>
2026-03-26 02:16:38 +0300

    Update relationships.rs

--- src-tauri/mangadex/src/objects/cover/relationships.rs
+++ src-tauri/mangadex/src/objects/cover/relationships.rs
@@ -1,11 +1,13 @@
 use std::ops::Deref;
 
 use async_graphql::Object;
+use mangadex_api_schema_rust::error::RelationshipConversionError;
 use mangadex_api_schema_rust::{
     ApiObjectNoRelationships,
     v5::{MangaAttributes, RelatedAttributes, Relationship, UserAttributes},
 };
-use mangadex_api_types_rust::{RelationshipType, error::RelationshipConversionError};
+use mangadex_api_types_rust::RelationshipType;
+use non_exhaustive::non_exhaustive;
 
 use crate::objects::{manga::MangaObject, user::User};
 
@@ -50,11 +52,11 @@ impl CoverRelationships {
                     RelationshipConversionError,
                 > {
                     if let Some(RelatedAttributes::User(ref attributes)) = value.attributes {
-                        Ok(ApiObjectNoRelationships {
+                        Ok(non_exhaustive!(ApiObjectNoRelationships<UserAttributes> {
                             id: value.id,
                             type_: RelationshipType::User,
                             attributes: attributes.clone(),
-                        })
+                        }))
                     } else {
                         Err(RelationshipConversionError::AttributesNotFound(
                             RelationshipType::User,
