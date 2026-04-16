428ccfbbaa02cf67eea3297e908d20c6346ecd75
tonymushah <tonyramanantsalama@gmail.com>
2026-04-16 10:23:53 +0300

    add-to-list backdrop bg change

--- src/lib/mangadex/componnents/manga/add-to-list/AddToList.svelte
+++ src/lib/mangadex/componnents/manga/add-to-list/AddToList.svelte
@@ -128,5 +128,6 @@
 	dialog::backdrop {
 		backdrop-filter: blur(10px);
 		-webkit-backdrop-filter: blur(10px);
+		background-color: color-mix(in srgb, var(--main-background) 50%, transparent 50%);
 	}
 </style>
