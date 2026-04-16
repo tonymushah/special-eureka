f1108f23f4073017218c3821b4ab9566e0c0c1b4
tonymushah <tonyramanantsalama@gmail.com>
2026-04-16 11:11:56 +0300

    lint fix

--- src/routes/(app)/mangadex/title/[id]/NoConflictLayout.svelte
+++ src/routes/(app)/mangadex/title/[id]/NoConflictLayout.svelte
@@ -287,9 +287,12 @@
 
 	const webview = getCurrentWebview();
 	$effect(() => {
-		const sub = webview.listen(`mangadex-title-read-markers-change-${data.layoutData.id}`, () => {
-			chapterReadMarkers.refetch();
-		});
+		const sub = webview.listen(
+			`mangadex-title-read-markers-change-${data.layoutData.id}`,
+			() => {
+				chapterReadMarkers.refetch();
+			}
+		);
 		return () => {
 			sub.then((v) => v());
 		};
@@ -304,7 +307,10 @@
 			[readMarkerStores],
 			([query], set, update) => {
 				if (query.isSuccess) {
-					const tosend = new Map(query.data.map((d) => [d, true])) as Map<string, boolean>;
+					const tosend = new Map(query.data.map((d) => [d, true])) as Map<
+						string,
+						boolean
+					>;
 					set(tosend);
 					const sub = listenToAnyChapterReadMarkers.subscribe((a) => {
 						if (a != undefined) {
