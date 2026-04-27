ecab9f59a6779a3b4b753eae213e3e101ada4cf7
tonymushah <tonyramanantsalama@gmail.com>
2026-04-27 20:42:08 +0300

    Update MangaPageInfo.svelte

--- src/lib/mangadex/componnents/manga/page/chapters/MangaPageInfo.svelte
+++ src/lib/mangadex/componnents/manga/page/chapters/MangaPageInfo.svelte
@@ -12,7 +12,7 @@
 	import { addToast } from "@mangadex/componnents/theme/toast/Toaster.svelte";
 
 	const __res = getTitleLayoutData();
-	let data = $derived.by(() => __res.queryResult);
+	let data = $derived.by(() => __res.value.queryResult);
 	function buildAtlTitles(altTitle: Record<string, string>[]): AltTitleItem[] {
 		let map = manga_altTitle_to_lang_map(altTitle);
 		let output: AltTitleItem[] = [];
@@ -30,8 +30,7 @@
 			if (v.attributes.group == TagGroup.Genre) {
 				returns.push({
 					id: v.id,
-					name:
-						get_value_from_title_and_random_if_undefined(v.attributes.name, "en") ?? ""
+					name: get_value_from_title_and_random_if_undefined(v.attributes.name, "en") ?? ""
 				});
 			}
 		});
@@ -43,8 +42,7 @@
 			if (v.attributes.group == TagGroup.Theme) {
 				returns.push({
 					id: v.id,
-					name:
-						get_value_from_title_and_random_if_undefined(v.attributes.name, "en") ?? ""
+					name: get_value_from_title_and_random_if_undefined(v.attributes.name, "en") ?? ""
 				});
 			}
 		});
@@ -70,8 +68,7 @@
 			if (v.attributes.group == TagGroup.Format) {
 				returns.push({
 					id: v.id,
-					name:
-						get_value_from_title_and_random_if_undefined(v.attributes.name, "en") ?? ""
+					name: get_value_from_title_and_random_if_undefined(v.attributes.name, "en") ?? ""
 				});
 			}
 		});
@@ -83,8 +80,7 @@
 			if (v.attributes.group == TagGroup.Content) {
 				returns.push({
 					id: v.id,
-					name:
-						get_value_from_title_and_random_if_undefined(v.attributes.name, "en") ?? ""
+					name: get_value_from_title_and_random_if_undefined(v.attributes.name, "en") ?? ""
 				});
 			}
 		});
