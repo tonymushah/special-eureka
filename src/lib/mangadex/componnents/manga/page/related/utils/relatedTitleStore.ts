dfa35ddbd26f3a4bc6f501504105ddfe3ede733e
tonymushah <tonyramanantsalama@gmail.com>
2026-04-27 21:02:14 +0300

    Update relatedTitleStore.ts

--- src/lib/mangadex/componnents/manga/page/related/utils/relatedTitleStore.ts
+++ src/lib/mangadex/componnents/manga/page/related/utils/relatedTitleStore.ts
@@ -1,7 +1,7 @@
-// TODO refactor to svelte map
+// [x] refactor to svelte map
 import type { MangaStatus } from "@mangadex/gql/graphql";
-import { getContext, setContext } from "svelte";
-import { type Readable, writable } from "svelte/store";
+import { Context } from "runed";
+import { SvelteMap } from "svelte/reactivity";
 
 export type RelatedTitle = {
 	id: string;
@@ -11,68 +11,28 @@ export type RelatedTitle = {
 	status: MangaStatus;
 };
 
-export type RelatedTitles = Map<string, RelatedTitle>;
-
-export type RelatedTitlesStore = Readable<RelatedTitles> & {
-	addTitle: (title: RelatedTitle) => void;
-	addTitles: (titles: RelatedTitle[]) => void;
-	delete: (id: string) => void;
-	deleteAll: (ids: string[]) => void;
-	get: () => RelatedTitles;
-	clear: () => void;
-};
+export class RelatedTitlesStore extends SvelteMap<string, RelatedTitle> {
+	addTitle(title: RelatedTitle) {
+		this.set(title.id, title);
+	}
+	addTitles(titles: RelatedTitle[]) {
+		titles.forEach((title) => this.addTitle(title));
+	}
+	deleteAll(ids: string[]) {
+		ids.forEach((id) => this.delete(id));
+	}
+}
 
 export default function relatedTitlesStore(): RelatedTitlesStore {
-	const init = new Map<string, RelatedTitle>();
-	const store = writable(init);
-	return {
-		subscribe(run, invalidate) {
-			return store.subscribe(run, invalidate);
-		},
-		addTitle(title) {
-			store.update((v) => {
-				v.set(title.id, title);
-				return v;
-			});
-		},
-		addTitles(titles) {
-			store.update((v) => {
-				titles.forEach((title) => {
-					v.set(title.id, title);
-				});
-				return v;
-			});
-		},
-		delete(id) {
-			store.update((v) => {
-				v.delete(id);
-				return v;
-			});
-		},
-		deleteAll(ids) {
-			store.update((v) => {
-				ids.forEach((id) => {
-					v.delete(id);
-				});
-				return v;
-			});
-		},
-		get() {
-			return init;
-		},
-		clear() {
-			store.update((v) => {
-				v.clear();
-				return v;
-			});
-		}
-	};
+	return new RelatedTitlesStore();
 }
 
 const KEY = "mangadex-title-page-related-titles";
 
+const ctx = new Context<RelatedTitlesStore>(KEY);
+
 export function setRelatedTitlesStoreContext(store: RelatedTitlesStore): RelatedTitlesStore {
-	return setContext(KEY, store);
+	return ctx.set(store);
 }
 
 export function initRelatedTitlesStoreContext(): RelatedTitlesStore {
@@ -80,10 +40,5 @@ export function initRelatedTitlesStoreContext(): RelatedTitlesStore {
 }
 
 export function getRelatedTitlesStoreContext(): RelatedTitlesStore {
-	const context = getContext<RelatedTitlesStore>(KEY);
-	if (context) {
-		return context;
-	} else {
-		throw new Error(`${KEY} context is not defined`);
-	}
+	return ctx.getOr(relatedTitlesStore());
 }
