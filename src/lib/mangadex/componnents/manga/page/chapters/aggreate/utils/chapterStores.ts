d1a57ebbf05690ade5d371f42e7d791cc570cf08
tonymushah <tonyramanantsalama@gmail.com>
2026-04-27 20:56:46 +0300

    Update chapterStores.ts

--- src/lib/mangadex/componnents/manga/page/chapters/aggreate/utils/chapterStores.ts
+++ src/lib/mangadex/componnents/manga/page/chapters/aggreate/utils/chapterStores.ts
@@ -1,100 +1,52 @@
-// TODO refactor to svelte map and remove store
+// [x] refactor to svelte map and remove store
 import type { Props as ChapterElement1Props } from "@mangadex/componnents/chapter/base/element1/ChapterElement1.svelte";
-import { getContext, setContext } from "svelte";
-import { get, writable, type Readable } from "svelte/store";
+import { Context } from "runed";
+import { SvelteMap } from "svelte/reactivity";
 
 type Chapter = ChapterElement1Props;
-
-export type ChapterMap = Map<string, Chapter>;
 
 type SetCommentsEntry = {
 	id: string;
 	comments: number;
 };
 
-export type ChapterStores = Readable<ChapterMap> & {
-	add: (value: Chapter) => void;
-	remove: (id: string) => void;
-	clear: () => void;
-	get: () => ChapterMap;
-	addByBatch: (value: Chapter[]) => void;
-	setComment: (id: string, comments: number) => void;
-	setComments: (input: SetCommentsEntry[]) => void;
-	isPresents: (ids: string[]) => boolean;
-	isPresent: (id: string) => boolean;
-};
+export class ChapterStores extends SvelteMap<string, Chapter> {
+	add(value: Chapter) {
+		this.set(value.id, value);
+	}
+	remove(id: string) {
+		this.delete(id);
+	}
+	addByBatch(value: Chapter[]) {
+		value.forEach((v) => this.add(v));
+	}
+	setComment(id: string, comments: number) {
+		let chapter_data = this.get(id);
+		if (chapter_data) {
+			chapter_data.comments = comments;
+		}
+	}
+	setComments(input: SetCommentsEntry[]) {
+		input.forEach((i) => this.setComment(i.id, i.comments));
+	}
+	isPresents(ids: string[]): boolean {
+		return ids.every((id) => this.has(id));
+	}
+	isPresent(id: string): boolean {
+		return this.has(id);
+	}
+}
 
 export default function chapterStores(): ChapterStores {
-	const init = new Map<string, Chapter>();
-	const store = writable(init);
-	return {
-		subscribe(run, invalidate) {
-			return store.subscribe(run, invalidate);
-		},
-		add(value: Chapter) {
-			store.update((u) => {
-				u.set(value.id, value);
-				return u;
-			});
-		},
-		get() {
-			return init;
-		},
-		addByBatch(value: Chapter[]) {
-			store.update((u) => {
-				value.forEach((v) => {
-					u.set(v.id, v);
-				});
-				return u;
-			});
-		},
-		remove(id: string) {
-			store.update((u) => {
-				u.delete(id);
-				return u;
-			});
-		},
-		clear() {
-			store.update((u) => {
-				u.clear();
-				return u;
-			});
-		},
-		setComment(id, comments) {
-			store.update((u) => {
-				const chapter = u.get(id);
-				if (chapter) {
-					chapter.comments = comments;
-				}
-				return u;
-			});
-		},
-		setComments(input) {
-			store.update((u) => {
-				input.forEach(({ id, comments }) => {
-					const chapter = u.get(id);
-					if (chapter) {
-						chapter.comments = comments;
-					}
-				});
-				return u;
-			});
-		},
-		isPresents(ids) {
-			const inner = get(store);
-			return ids.every((id) => inner.has(id));
-		},
-		isPresent(id) {
-			const inner = get(store);
-			return inner.has(id);
-		}
-	};
+	return new ChapterStores();
 }
 
 const KEY = "mangadex-title-page-chapters";
 
+const ctx = new Context<ChapterStores>(KEY);
+
 export function setChapterStoreContext(store: ChapterStores): ChapterStores {
-	return setContext(KEY, store);
+	return ctx.set(store);
 }
 
 export function initChapterStoreContext(): ChapterStores {
@@ -102,10 +54,5 @@ export function initChapterStoreContext(): ChapterStores {
 }
 
 export function getChapterStoreContext(): ChapterStores {
-	const context = getContext<ChapterStores>(KEY);
-	if (context) {
-		return context;
-	} else {
-		throw new Error(`${KEY} context is missing`);
-	}
+	return ctx.getOr(chapterStores());
 }
