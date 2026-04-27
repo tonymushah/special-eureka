fff452c9a2017c646ed22f46d58bd05b5ae88102
tonymushah <tonyramanantsalama@gmail.com>
2026-04-27 19:42:53 +0300

    added `createReadonlyValue`

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,18 @@
 // place files you want to import through the `$lib` alias in this folder.
 
+import type { Getter } from "runed";
 import type { Readable } from "svelte/store";
 
 export type StoreOrVal<T> = T | Readable<T>;
 
 export interface ReadonlyValue<T> {
 	readonly value: T;
+}
+
+export function createReadonlyValue<T>(val_getter: Getter<T>): ReadonlyValue<T> {
+	return {
+		get value() {
+			return val_getter();
+		}
+	};
 }
