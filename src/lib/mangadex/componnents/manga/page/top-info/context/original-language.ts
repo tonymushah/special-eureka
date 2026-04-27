ee02719e056f910ba01060ef2869a749227f03f2
tonymushah <tonyramanantsalama@gmail.com>
2026-04-27 19:34:01 +0300

    Update original-language.ts

--- src/lib/mangadex/componnents/manga/page/top-info/context/original-language.ts
+++ src/lib/mangadex/componnents/manga/page/top-info/context/original-language.ts
@@ -1,17 +1,19 @@
+import type { ReadonlyValue } from "$lib";
 import type { Language } from "@mangadex/gql/graphql";
-import { getContext, setContext } from "svelte";
+import { Context, type Getter } from "runed";
 
 const key = "top-info-original-language";
 
-export function setTopMangaOriginalLanguageContextStore(originalLanguage: Language) {
-	return setContext<Language>(key, originalLanguage);
+const ctx = new Context<ReadonlyValue<Language>>(key);
+
+export function setTopMangaOriginalLanguageContextStore(originalLanguage: Getter<Language>) {
+	return ctx.set({
+		get value() {
+			return originalLanguage();
+		}
+	});
 }
 
 export function getTopMangaOriginalLanguageContextStore() {
-	const originalLanguage = getContext<Language | undefined>(key);
-	if (originalLanguage) {
-		return originalLanguage;
-	} else {
-		throw new Error(`\`${key}\` is not declared`);
-	}
+	return ctx.get();
 }
