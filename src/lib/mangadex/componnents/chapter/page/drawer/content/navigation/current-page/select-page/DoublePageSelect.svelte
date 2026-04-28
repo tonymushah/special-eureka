6ab3e7e7dc2f736a2b95d82c8e1219f9cc0552c2
tonymushah <tonyramanantsalama@gmail.com>
2026-04-28 07:11:36 +0300

    Update DoublePageSelect.svelte

--- src/lib/mangadex/componnents/chapter/page/drawer/content/navigation/current-page/select-page/DoublePageSelect.svelte
+++ src/lib/mangadex/componnents/chapter/page/drawer/content/navigation/current-page/select-page/DoublePageSelect.svelte
@@ -7,75 +7,68 @@
 	import ButtonAccent from "@mangadex/componnents/theme/buttons/ButtonAccent.svelte";
 	import MangaDexVarThemeProvider from "@mangadex/componnents/theme/MangaDexVarThemeProvider.svelte";
 	import { every, isArray } from "lodash";
-	import { derived, get, type Writable } from "svelte/store";
 	import { slide } from "svelte/transition";
 	import { Direction as ReadingDirection } from "@mangadex/gql/graphql";
 	import { getCurrentChapterDirection } from "@mangadex/componnents/chapter/page/contexts/readingDirection";
-	import type { DoublePageIndex } from "@mangadex/stores/chapter/pages";
+	import type { DoublePageIndex } from "@mangadex/stores/chapter/pages.svelte";
 	import getCurrentChapterImages from "@mangadex/componnents/chapter/page/utils/getCurrentChapterImages";
 	import { type SelectOption } from "@mangadex/utils/legacy/melt-ui-select-option";
 	import { floatingUImenu } from "@mangadex/utils/floating-ui/menu.svelte";
+	import type { WritableValue } from "$lib";
 
 	const readingDirection = getCurrentChapterDirection();
 	type Page = DoublePageIndex;
 	const currentPageContext = getChapterCurrentPageContext();
 	const images = getCurrentChapterImages();
-	const doublePages = derived(images, ($images) => $images.pagesAsDoublePageIndexes());
+	let doublePages = $derived.by(() => images.pagesAsDoublePageIndexes);
 	const currentDoublePageIndex = getChapterDoublePageCurrentPageIndex();
-	const currentPageSelectedReadable = derived(
-		[currentDoublePageIndex, doublePages, readingDirection],
-		([$index, $pages, $dir]) => {
-			const value = $pages[$index];
-			const label = isArray(value)
-				? `${value[$dir == ReadingDirection.Ltr ? 0 : 1] + 1} - ${value[$dir == ReadingDirection.Ltr ? 1 : 0] + 1}`
-				: `${value + 1}`;
-			return {
-				value /*: isArray(value) && $dir == ReadingDirection.Rtl ? value.toReversed() : value*/,
-				label
-			} as SelectOption<Page>;
-		}
-	);
-	const options = derived([doublePages, readingDirection], ([$images, $dir]) =>
-		$images.map<SelectOption<[number, number] | number>>((value) => ({
+	let currentPageSelectedReadable = $derived.by(() => {
+		const index = currentDoublePageIndex.value;
+		const pages = doublePages;
+		const dir = $readingDirection;
+		const value = pages[index];
+		const label = isArray(value)
+			? `${value[dir == ReadingDirection.Ltr ? 0 : 1] + 1} - ${value[dir == ReadingDirection.Ltr ? 1 : 0] + 1}`
+			: `${value + 1}`;
+		return {
+			value /*: isArray(value) && $dir == ReadingDirection.Rtl ? value.toReversed() : value*/,
+			label
+		} as SelectOption<Page>;
+	});
+	let options = $derived.by(() => {
+		return doublePages.map<SelectOption<[number, number] | number>>((value) => ({
 			value,
 			label: isArray(value)
-				? `${value[$dir == ReadingDirection.Ltr ? 0 : 1] + 1} - ${value[$dir == ReadingDirection.Ltr ? 1 : 0] + 1}`
+				? `${value[$readingDirection == ReadingDirection.Ltr ? 0 : 1] + 1} - ${value[$readingDirection == ReadingDirection.Ltr ? 1 : 0] + 1}`
 				: `${value + 1}`
-		}))
-	);
+		}));
+	});
 
-	const selected: Writable<SelectOption<Page>> = {
-		subscribe(run, invalidate) {
-			return currentPageSelectedReadable.subscribe(run, invalidate);
+	let selected: WritableValue<SelectOption<Page>> = {
+		get value() {
+			return currentPageSelectedReadable;
 		},
-		set(_value) {
+		set value(_value) {
 			const value = isArray(_value.value)
-				? _value.value[get(readingDirection) == ReadingDirection.Ltr ? 0 : 1]
+				? _value.value[$readingDirection == ReadingDirection.Ltr ? 0 : 1]
 				: _value.value;
-			currentPageContext.set(value);
-		},
-		update(updater) {
-			const _value = updater(get(currentPageSelectedReadable)).value;
-			const value = isArray(_value)
-				? _value[get(readingDirection) == ReadingDirection.Ltr ? 0 : 1]
-				: _value;
 			currentPageContext.set(value);
 		}
 	};
 	const isSelected = (value: Page) => {
-		if ($selected == undefined) {
+		if (selected.value.value == undefined) {
 			return false;
 		}
 		if (isArray(value)) {
 			return every(value, (i) => {
-				if (isArray($selected.value)) {
-					return $selected.value.includes(i);
+				if (isArray(selected.value.value)) {
+					return selected.value.value.includes(i);
 				} else {
 					return false;
 				}
 			});
-		} else if (!isArray($selected.value)) {
-			return value == $selected.value;
+		} else if (!isArray(selected.value.value)) {
+			return value == selected.value.value;
 		} else {
 			return false;
 		}
@@ -103,18 +96,18 @@
 				open = !open;
 			}}
 		>
-			Page: {$selected.label}
+			Page: {selected.value.label}
 		</ButtonAccent>
 	</div>
 	{#if open}
 		<div class="menu-outer" bind:this={menu}>
 			<MangaDexVarThemeProvider>
 				<menu transition:slide={{ duration: 150, axis: "y" }}>
-					{#each $options as { value, label }}
+					{#each options as { value, label }}
 						<button
 							class="li"
 							onclick={() => {
-								$selected = {
+								selected.value = {
 									value,
 									label
 								};
