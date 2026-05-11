7988301835a4e5130fead221fa66caa898871def
Dirk Tony <tonydirk.smsconsulting@gmail.com>
2026-05-11 16:05:18 +0300

    Update Layout.svelte

--- src/lib/mangadex/componnents/manga/base/base1/Layout.svelte
+++ src/lib/mangadex/componnents/manga/base/base1/Layout.svelte
@@ -5,7 +5,7 @@
 		onclick?: (
 			ev: MouseEvent & {
 				currentTarget: EventTarget & HTMLElement;
-			}
+			},
 		) => any;
 	}
 	interface Props extends Events {
@@ -18,21 +18,22 @@
 
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-<article
+<button
 	onclick={(e) => {
 		onclick?.(e);
 	}}
 	class="layout manga-element"
 	data-manga-id={mangaId}
 	oncontextmenu={registerContextMenuEvent({
-		preventDefault: true
+		preventDefault: true,
 	})}
 >
 	{@render children?.()}
-</article>
+</button>
 
 <style lang="scss">
-	article.layout {
+	.layout {
+		--shadow-color: var(--mid-tone);
 		width: var(--layout-width);
 		display: grid;
 		grid-template-areas: "cover content";
@@ -45,29 +46,42 @@
 			filter 200ms ease-in-out;
 		background-color: var(--accent-l3);
 		border: var(--mid-tone) solid 3px;
-		box-shadow: 0px 3px 0px var(--mid-tone);
+		box-shadow: 0px 4px 0px var(--shadow-color);
 		column-gap: 10px;
+		color: inherit;
+		font-family: inherit;
+		padding: 0px;
+		transform: translateY(-4px);
 	}
 	@media (width < 900px) {
-		article.layout {
+		.layout {
 			grid-template-columns: 90px auto;
 		}
 	}
 	@media (width >= 900px) {
-		article.layout {
+		.layout {
 			grid-template-columns: 100px auto;
 		}
 	}
-	article.layout:hover {
+	.layout:focus {
+		outline: none;
+		border-color: var(--contrast-l1);
+		--shadow-color: var(--contrast-l1);
+	}
+	.layout:hover {
 		background-color: var(--accent-l3-hover);
 	}
-	article.layout:active {
+	.layout:active {
 		background-color: var(--accent-l3-active);
 		box-shadow: none;
-		transform: translateY(3px);
+		transform: translateY(0px);
 	}
 	.manga-element:global([data-selecto-selected]) {
-		background-color: color-mix(in srgb, var(--primary) 50%, transparent 50%);
+		background-color: color-mix(
+			in srgb,
+			var(--primary) 50%,
+			transparent 50%
+		);
 		border-radius: 3px;
 		border: 2px solid var(--primary);
 	}
